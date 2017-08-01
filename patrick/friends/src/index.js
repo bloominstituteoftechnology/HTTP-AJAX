7b9ad269d9d08011152d62711a1fbd9060b0e855
mixelpixel <mixelpix@mac.com>
2017-08-01 14:22:34 -0400

    remove serviceworkers

--- patrick/friends/src/index.js
+++ patrick/friends/src/index.js
@@ -8,18 +8,10 @@ import App from './App';
 import friendsReducer from './reducers'; // <--- ./reducers/index.js
 
 
-
-// import registerServiceWorker from './registerServiceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-
 let STORAGE_WAREHOUSE = createStore(friendsReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
 
 ReactDOM.render(
   <Provider store={STORAGE_WAREHOUSE}>
-    {/* <a href="https://youtu.be/v2f8kd1d278?t=7s">ROOT</a> */}
     <App />
   </Provider>,
   document.getElementById('root'));
