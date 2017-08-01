8c1ea312ec5db1233e00292e3005fd56768a2e0d
mixelpixel <mixelpix@mac.com>
2017-08-01 15:25:30 -0400

    frenz!

--- patrick/friends/src/components/App.js
+++ patrick/friends/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../art/logo.svg';
+import frenz from '../art/frenz.png';
 import '../css/App.css';
 
 class App extends Component {
@@ -8,10 +9,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to LS-AJAX Friends:</h2>
+          <img src={frenz} alt='Friends'></img>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Something <code>AMAZING</code> will go here.
         </p>
       </div>
     );
