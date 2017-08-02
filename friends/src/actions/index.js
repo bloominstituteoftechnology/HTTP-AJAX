a9773786b26736117e756a9ba62c002298d9ed32
jeshsy <jeshsy@users.noreply.github.com>
2017-08-02 17:10:42 -0400

    got it workin in best practice

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -1,8 +1,21 @@
-export const getFriends = (promise) => {
+import axios from 'axios';
+
+export const getFriends = () => {
+  const promise = axios.get('http://localhost:5000/friends');
   return {
     type: GET_FRIENDS,
     payload: promise
   };
 };
 
+export const addFriend = (newFriend) => {
+  const promise = axios.post('http://localhost:5000/new-friend', newFriend);
+  return {
+    type: ADD_FRIEND,
+    payload: promise
+  };
+};
+
 export const GET_FRIENDS = 'GET_FRIENDS';
+
+export const ADD_FRIEND = 'ADD_FRIEND';
