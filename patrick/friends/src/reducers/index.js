8b98ffb2e74e284d11cac822737f10c2dd9352ed
mixelpixel <mixelpix@mac.com>
2017-08-02 16:41:37 -0400

    mapDispatchToProps

--- patrick/friends/src/reducers/index.js
+++ patrick/friends/src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers} from 'redux';
 import { GET_FRIENDS } from '../actions';
+import { ADD_FRIEND } from '../actions';
 
 const friendsReducer = (friends = [], action) => {
   switch(action.type) {
     case GET_FRIENDS:
+      return action.payload.data;
+    case ADD_FRIEND:
       return action.payload.data;
     default:
       return friends;
