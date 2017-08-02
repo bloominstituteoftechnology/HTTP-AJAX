f01aade8f1cdb3f8a305b2880fd55a5d526936f2
mixelpixel <mixelpix@mac.com>
2017-08-02 14:06:15 -0400

    just GET_FRIENDS (no POST_FRIEND)

--- patrick/friends/src/reducers/index.js
+++ patrick/friends/src/reducers/index.js
@@ -1,28 +1,30 @@
 import { combineReducers} from 'redux';
 import { GET_FRIENDS } from '../actions';
-import { POST_FRIEND } from '../actions';
+// import { POST_FRIEND } from '../actions';
 
 const friendsReducer = (friends = [], action) => {
     switch(action.type) {
         case GET_FRIENDS:
             return action.payload.data;
+        // case POST_FRIEND:
+        //     return friends.concat(action.payload);
         default:
             return friends;
     }
 };
 
-const postFriendReducer = (postFriend = [], action) => {
-  switch(action.type) {
-    case POST_FRIEND:
-      return action.payload;
-    default:
-      return postFriend;
-  }
-}
+// const postFriendReducer = (postFriend = [], action) => {
+//   switch(action.type) {
+//     case POST_FRIEND:
+//       return action.payload;
+//     default:
+//       return postFriend;
+//   }
+// }
 
 const rootReducer = combineReducers({
     friends: friendsReducer,
-    postFriend: postFriendReducer
+    // postFriend: postFriendReducer
 });
 
 export default rootReducer;
