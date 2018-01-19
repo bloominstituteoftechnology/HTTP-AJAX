f4f5cbc46c5630b921c1f81eb9499ee00369490c
cgroov <30381292+groov1234@users.noreply.github.com>
2018-01-19 11:16:08 -0500

    Submit Form functionality Added

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,51 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
+import NewFriend from './components/FriendForm.js';
 
 class App extends Component {
-  state = {
-    friends: []
-  }
-
-  componentDidMount() {
-    const getList = 'http://localhost:5000/friends';
-
-    axios.get(getList).then((response) => {
-      this.setState(() => ({ friends: response.data }))
-    }).catch((error) =>  {
-      console.log('ERROR', error);
-    })
-    
-  }
-  
   render() {
-    return <div>
-        <div className="friend-title">Lambda Friends</div>
-        <ul className="friend-grid">
-          {this.state.friends.map(friend => {
-            return <li key={friend.id} className="friend">
-                <div className="friend-name">{friend.name}</div>
-                <div className="friend-age">{`Age: ${friend.age}`}</div>
-                <div className="friend-email">{`Email: ${friend.email}`}</div>
-              </li>;
-          })}
-        </ul>
-        <form>
-          <label>
-            Name:
-            <input type="text" name="name" />
-          </label>
-          <label>
-            Age:
-            <input type="text" name="age" />
-          </label>
-          <label>
-            Email:
-            <input type="text" name="email" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>;
+    return (
+    <NewFriend></NewFriend>
+    
+      )
   }
 }
 
--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,25 +1,94 @@
 import React from 'react';
+import axios from 'axios';
+import './FriendForm.css'
 
-export function newFriend() => {
-  
+class NewFriend extends React.Component {
+  state = {
+    friends: [],
+    newName: '',
+    newAge: '',
+    newEmail: '',
+  };
+
+  componentDidMount() {
+    const endpoint = 'http://localhost:5000/friends';
+
+    axios
+      .get(endpoint)
+      .then(response => {
+        this.setState({ friends: response.data });
+      })
+      .catch(error => {
+        console.log('Error: ', error);
+      });
+  }
+
+  handleNameChange = event => {
+    this.setState({ newName: event.target.value });
+  };
+
+  handleAgeChange = event => {
+    this.setState({ newAge: event.target.value });
+  };
+
+  handleEmailChange = event => {
+    this.setState({ newEmail: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const newFriend = {
+      name: this.state.newName,
+      age: this.state.newAge,
+      email: this.state.newEmail,
+    };
+    this.setState({ newName: '', newAge: '', newEmail: '' });
+    axios
+      .post('http://localhost:5000/friends', newFriend)
+      .then(response => {
+        this.setState({ friends: response.data });
+      })
+      .catch(error => {
+        console.log('Error: ', error);
+      });
+  };
+
   render() {
     return (
-      <div>
-      <form>
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Age:
-          <input type="text" name="age" />
-        </label> 
-        <label>
-          Email:
-          <input type="text" name="email" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+    <div>
+    <div className="friend-title">Lambda Friends</div>
+    <div className="form">
+        <form>
+          <label className="name">
+            Name:
+            <input type="text" value={this.state.newName} onChange={this.handleNameChange} placeholder="Name" />
+          </label>
+          <label className="age">
+            Age:
+            <input type="text" value={this.state.newAge} onChange={this.handleAgeChange} placeholder="Age" />
+          </label>
+          <label className="email">
+            Email:
+            <input type="text" value={this.state.newEmail} onChange={this.handleEmailChange} placeholder="Email" />
+          </label>
+          <button className="submit" onClick={this.handleSubmit}>Submit</button>
+        </form>
       </div>
-    )
-}}
+      <div>
+        
+        
+        <ul className="friend-grid">
+          {this.state.friends.map(friend => {
+            return <li key={friend.id} className="friend">
+                <div className="friend-name">{friend.name}</div>
+                <div className="friend-age">{`Age: ${friend.age}`}</div>
+                <div className="friend-email">{`Email: ${friend.email}`}</div>
+              </li>;
+          })}
+        </ul>
+      </div>
+        </div>);
+  }
+}
+
+export default NewFriend;
