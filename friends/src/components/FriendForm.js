7bbb053d6ddfb3352e5e5ee5fee6c65037807083
Guelmis Cortina <cguelmis@yahoo.com>
2018-01-21 22:41:52 -0500

    add the friendform.js file

---
+++ friends/src/components/FriendForm.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class FriendForm extends React.Component {
+	state = {
+			name: '',
+			age: null,
+			email: ''
+	};
+
+	handleInputChange = (event) => {
+		const { name, value } = event.target;
+		this.setSate = ({ [name]: value
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+
+		axios
+			.post('http://localhost:5000/friends', this.state)
+			.then((response => {
+				this.setState({ name: '', age: '', email: '' });
+			}))
+			.catch(() => {
+				console.error('Could not find the friend');
+			});
+	}
+
+	render () {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<label htmlFor="name">Name</label>
+				<input
+					type="text"
+					id="name"
+					name="name"
+					value={this.state.name}
+					onChange={this.handleInputChange}
+				/>
+
+				<label htmlFor="age">Age</label>
+				<input
+					type="number"
+					id="age"
+					name="age"
+					value={this.state.age}
+					onChange={this.handleInputChange}
+				/>
+
+				<label htmlFor="email">Email</label>
+				<input
+					type="text"
+					id="email"
+					name="email"
+					value={this.state.email}
+					onChange={this.handleInputChange}
+				/>
+				<button type="submit">Save</button>
+			</form>
+		);
+	}
+}
