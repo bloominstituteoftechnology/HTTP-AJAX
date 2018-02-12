80196f5f2b22412d125283d3594b4ecc07615c99
Tommy Coleman <tommycoleman87@gmail.com>
2018-02-12 16:32:51 -0500

    attempted functionality of friends component

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import Friends from './Friends';
 
 class App extends Component {
+  state = {
+    friends: [],
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Friends name={this.state.friends.name} age={this.state.friends.name} eMail={this.state.friends.email}/>
       </div>
     );
   }
--- friends/src/Friends.js
+++ friends/src/Friends.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const friends = (props) => {
-    
+    return (
+        <div>
+        <p> {props.name} </p>
+        <p> {props.age} </p>
+        <p> {props.eMail} </p>
+        </div>
+    )
 }
 
-export default Friends;
+export default friends;
