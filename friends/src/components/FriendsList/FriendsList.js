ab8bd3abfb2bf372d59e2bb95a16cebcf772cf8b
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-12 15:34:48 -0600

    add styling
    Added styling to App, Friend and FriendsList

--- friends/src/components/FriendsList/FriendsList.js
+++ friends/src/components/FriendsList/FriendsList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Friend from '../Friend/Friend.js';
+import './FriendsList.css'
 
 class FriendsList extends Component {
   state = {
