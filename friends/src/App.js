2c8f82d091fda650f67051e1fa8e694a35a48718
cliffkang <cliffgkang@gmail.com>
2018-02-12 14:56:06 -0800

    finished assignment
    -added addFriend(), which is where the axios.post lives
    -fixed up the form and used id's instead of name property types
    -added onSubmit to <form>

--- friends/src/App.js
+++ friends/src/App.js
@@ -18,14 +18,29 @@ class App extends Component {
             />
           })}
         </div>
-        <form>
-          <input type='text' placeholder='Name' />
-          <input type='text' placeholder='Age' />
-          <input type='text' placeholder='Email' />
-          <button />
+        <form onSubmit={this.addFriend}>
+          <input type='text' id='name' placeholder='Name' />
+          <input type='text' id='age' placeholder='Age' />
+          <input type='text' id='email' placeholder='Email' />
+          <input type='submit' value='Submit' />
         </form>
       </div>
     );
+  }
+
+  addFriend() {
+    axios
+      .post('http://localhost:5000/friends', {
+        name: document.getElementById('name').value,
+        age: document.getElementById('age').value,
+        email: document.getElementById('email').value,
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log('error', error);
+      })
   }
 
   componentDidMount() {
@@ -33,7 +48,6 @@ class App extends Component {
       .get('http://localhost:5000/friends')
       .then(response => {
         this.setState({ friends: response.data })
-        console.log(response);
       })
       .catch(error => {
         console.log('error', error);
