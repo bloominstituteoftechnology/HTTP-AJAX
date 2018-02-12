e6017d30db300aa90001f8796ddca8c289dbe1ff
Dixie Korley <dixiekorley@gmail.com>
2018-02-12 18:15:51 -0500

    Fix styling issues. Form needs to update information.

--- friends/src/components/friendslist.js
+++ friends/src/components/friendslist.js
@@ -7,30 +7,48 @@ class FriendsList extends Component {
   };
 
   render() {
-    
+
     return (
       <div>
         <div className="friend-title">Lambda Friends</div>
-          <ul>{this.state.friends.map(friend => {
-            return (
-              <li key={friend.id} className="friend">
-                <div className="friend-name">{friend.name}</div>
-                <div className="friend-age">{`Age: ${friend.age}`}</div>
-                <div className="friend-email">{`Email: ${friend.email}`}</div>
-              </li>
-            );
-          })}
-          </ul>
+
+        <div>
+          <form>
+            <label>
+              Name:
+          <input type="text" name="name" />
+            </label><label>
+              Age:
+          <input type="text" age="age" />
+            </label>
+            <label>
+              Email:
+          <input type="text" email="email" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+
+        <ul>{this.state.friends.map(friend => {
+          return (
+            <li key={friend.id} className="friend">
+              <div className="friend-name">{friend.name}</div>
+              <div className="friend-age">{`Age: ${friend.age}`}</div>
+              <div className="friend-email">{`Email: ${friend.email}`}</div>
+            </li>
+          );
+        })}
+        </ul>
       </div>
     );
   }
 
   componentDidMount() {
-     axios
+    axios
       .get('http://localhost:5000/friends')
       .then(response => {
         console.log(response.data);
-      this.setState({friends: response.data});
+        this.setState({ friends: response.data });
       })
       .catch(error => {
         console.log('there was error', error);
