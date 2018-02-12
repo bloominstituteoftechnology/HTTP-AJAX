8dd589175d233155b02d89f493974fb679cc20bf
Giraud Julemis <giraud.julemis@gmail.com>
2018-02-12 15:53:51 -0500

    Installed packages and created FriendsList Component

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class FriendsList extends Component {
+  render() {
+    return <h1>FriendsList</h1>
+  }
+}
+
+export default FriendsList;
