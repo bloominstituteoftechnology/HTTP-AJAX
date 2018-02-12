e9495da3c0634396389af3db870dd9f3bc22b6f1
Dixie Korley <dixiekorley@gmail.com>
2018-02-12 15:53:34 -0500

    Start installs, create component, make GET request

---
+++ friends/src/components/friends-list.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class FriendsList extends Component {
+  state = {
+    friends: [],
+    loading: false,
+    noData: true
+  };
+
+  render() {
+    if (this.state.loading && this.state.noData) {
+      //will this run
+    }
+    
+    return (
+      <div>
+        <div className="friend-title">Lambda Friends</div>
+        { this.state.loading && <div>Loading Friends...</div>}
+
+        { !this.state.loading && <ul className="friend-grid">
+          {this.state.friends.map(friend => {
+            return (
+              <li key={friend.id} className="friend">
+                <div className="friend-name">{friend.name}</div>
+                <div className="friend-age">{`Age: ${friend.age}`}</div>
+                <div className="friend-email">{`Email: ${friend.email}`}</div>
+              </li>
+            );
+          })}
+        </ul>}
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+     axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+      this.setState({friends: response.data, loading: false});
+      })
+      .catch(error => {
+        this.setState({ loading: false});
+        console.log('there was error', error)
+      });
+  }
+}
+
+export default FriendsList;
