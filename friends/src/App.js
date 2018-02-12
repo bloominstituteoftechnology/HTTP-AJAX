5a50f126e24a730177f4c01a192fbc2c2b74033f
Igor Yermak <igor.yermak@gmail.com>
2018-02-12 16:51:15 -0500

    Data fetched
    need to add form and styling

---
+++ friends/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  state = {
+    friends: [],
+  };
+
+
+
+  render() {
+    return (
+      <div>
+        <div className="friends-title"> Friends </div>
+        <ul className="friends">
+          {this.state.friends.map(friend => {
+            return (
+              <li key={friend.id} className="friend">
+                <div className="friend-name">{friend.name}</div>
+                <div className="friend-age">{'Age: ${friend.age'}'}</div>
+                <div className="friend-email">{'Email:${friend.email'}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        this.setState({ friends: response.data });
+      })
+      .catch(error => {
+        console.log('error', error);
+      });
+  }
+}
+
+
+
+
+
+export default App;
