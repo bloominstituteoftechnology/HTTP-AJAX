d3f3e3c75d50ec17af8fa61a05e979af94c3a892
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-02-12 17:48:54 -0500

    Moved logic to external component
    Created new component to Display Friends
    Moved previous code into new component
    Imported new component so it can be rendered in App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,38 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import AddFriend from './components/AddFriend.js';
-import axios from 'axios';
+import AddFriend from './components/AddFriend';
+import DisplayFriends from './components/DisplayFriends';
 
 class App extends Component {
-  state = {
-    friends: [],
-  }
+
   render() {
     return (
       <div className="App">
-        {this.state.friends.map(friend => {
-          return (
-            <li key={friend.id} className="friend">
-              <div className="friend-name">{friend.name}</div>
-              <div className="friend-age">{friend.age}</div>
-              <div className="friend-email">{friend.email}</div>
-            </li>
-          )
-        })}
-        <div>
-          <AddFriend />
-        </div>
+        <DisplayFriends />
+        <AddFriend />
       </div>
     );
-  }
-  componentDidMount(){
-    axios.get('http://localhost:5000/friends')
-    .then((response) => {
-      this.setState({ friends: response.data})
-    })
-    .catch((error) => {
-      console.log('there was an error', error);
-    });
   }
 }
 
