6c410932fcb928f97929d38806ee3c63a08ca50c
Ronnie Miksch <fakepreferences@gmail.com>
2018-02-12 16:25:09 -0800

    Got delete friend working, and no longer needs to refresh entire page for updating

--- friends/src/App.js
+++ friends/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     state = {
         friends: [],
         name: '',
-        age: 0,
+        age: null,
         email: '',
     };
 
@@ -19,6 +19,7 @@ class App extends Component {
     };
 
     onSubmit = event => {
+        event.preventDefault();
         if (this.state.name && this.state.age && this.state.email) {
             const { name, age, email } = this.state;
 
@@ -30,39 +31,78 @@ class App extends Component {
                 })
                 .then(result => {
                     console.log('success', result);
-                }).catch(error => {
+                    this.getFriends();
+                })
+                .catch(error => {
                     console.log('error', error);
                 });
         } else {
             alert('Please complete required fields'); // probably a better way to handle this without using an alert
         }
-
+        this.setState({ name: '', age: null, email: '' });
     };
+
+    deleteFriend = (id) => {
+        console.log(id);
+        axios
+            .delete(`http://localhost:5000/friends/${id}`)
+            .then((result) => {
+                console.log('sucessfully deleted', result);
+                this.getFriends();
+            })
+            .catch(error => {
+                console.log('error', error);
+            });
+    };
+
+    updateFriend = (id, name, age, email) => {
+
+    }
 
     getFriends = () => {
         axios
-        .get('http://localhost:5000/friends')
-        .then(response => {
-            this.setState({ friends: response.data });
-        })
-        .catch(error => {
-            console.log('error:', error);
-        });
-    }
+            .get('http://localhost:5000/friends')
+            .then(response => {
+                this.setState({ friends: response.data });
+            })
+            .catch(error => {
+                console.log('error:', error);
+            });
+    };
 
     render() {
         return (
             <div className="App">
-            <Friend friends={this.state.friends} />
+                <Friend
+                    friends={this.state.friends}
+                    deleteFriend={this.deleteFriend}
+                    updateFriend={this.updateFriend}
+                />
                 <div className="add-friend-form">
                     <form onSubmit={this.onSubmit}>
                         <label htmlFor="name">Name:</label>
-                        <input id="name" type="text" name="name" onChange={this.onChange} />
+                        <input
+                            id="name"
+                            type="text"
+                            name="name"
+                            onChange={this.onChange}
+                            value={this.state.name}
+                        />
                         <label htmlFor="age">Age:</label>
-                        <input type="number" name="age" onChange={this.onChange} />
+                        <input
+                            type="number"
+                            name="age"
+                            onChange={this.onChange}
+                            value={this.state.age}
+                        />
                         <label htmlFor="age">Email:</label>
-                        <input type="email" name="email" onChange={this.onChange} />
-                        <input type="submit" value="submit" />
+                        <input
+                            type="email"
+                            name="email"
+                            onChange={this.onChange}
+                            value={this.state.email}
+                        />
+                        <input type="submit" value="Add Friend" />
                     </form>
                 </div>
             </div>
--- friends/src/Friend.js
+++ friends/src/Friend.js
@@ -1,23 +1,30 @@
 import React from 'react';
 
 class Friend extends React.Component {
-  state = {
-  }
-  render() {
-    return (
-      <div className='friend-container'>
-        {this.props.friends.map(friend => {
-          return (
-              <li key={friend.id} className="friend">
-                  <div className="friend-name">{friend.name}</div>
-                  <div className="friend-age"> {friend.age}</div>
-                  <div className="friend-email"> {friend.email}</div>
-              </li>
-          );
-      })}
-    </div>
-    );
-  }
+    state = {};
+    render() {
+        return (
+            <div className="friends-container">
+                {this.props.friends.map(friend => {
+                    return (
+                      
+                        <li key={friend.id} className="friend">
+                            <div className="friend-name">{friend.name}</div>
+                            <div className="friend-age"> {friend.age}</div>
+                            <div className="friend-email"> {friend.email}</div>
+                            <button
+                                onClick={() => {
+                                    this.props.deleteFriend(friend.id);
+                                }}
+                            >
+                                Unfriend
+                            </button>
+                        </li>
+                    );
+                })}
+            </div>
+        );
+    }
 }
 
 export default Friend;
