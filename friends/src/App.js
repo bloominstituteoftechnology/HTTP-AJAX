f1edb4a0f168497ae078edad5e600e5dbe24da14
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-02-12 20:07:29 -0500

    Functioning Form
    Finally made the form function, added axios.post to submit instead.
    
    Also did a bit of styling before realizing I hadn't committed since adding the non-functioning form.

--- friends/src/App.js
+++ friends/src/App.js
@@ -6,18 +6,15 @@ import './App.css';
 class App extends Component {
   state = {
     friends: [],
-    newFriend: {
-      id: '',
-      name: '',
-      age: '',
-      email: '',
-    },
+    newName: '',
+    newAge: 0,
+    newEmail: '',
   }
   
   render() {
     return (
-      <div>
-        <div>
+      <div className="container">
+        <div className="grid">
           {this.state.friends.map(friend => {
             return <Friend 
             key={friend.id} 
@@ -25,10 +22,10 @@ class App extends Component {
             />;
           })}
         </div>
-        <form>
-          <input type="text" onChange={this.handleNewFriend} name="Name Here" />
-          <input type="text" onChange={this.handleNewFriend} age="Age Here" />
-          <input type="text" onChange={this.handleNewFriend} email="Email Here" />
+        <form className="form" onSubmit={this.addNewFriend}>
+          <input type="text" onChange={this.handleNewName} placeholder="Name Here"  value={this.state.newName}/>
+          <input type="text" onChange={this.handleNewAge} placeholder="Age Here" value={this.state.newAge}/>
+          <input type="text" onChange={this.handleNewEmail} placeholder="Email Here" value={this.state.newEmail}/>
           <input type="submit" value="Add Friend" />
         </form>
       </div>
@@ -44,6 +41,88 @@ class App extends Component {
         console.log('there was error', error);
       });
   }
+
+  handleNewName = (event) => {
+    this.setState({newName: event.target.value});
+  }
+
+  handleNewAge = (event) => {
+    this.setState({newAge: event.target.value});
+  }
+
+  handleNewEmail = (event) => {
+    this.setState({newEmail: event.target.value});
+  }
+
+  addNewFriend = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:5000/friends', {
+        name: this.state.newName,
+        age: this.state.newAge,
+        email: this.state.newEmail,
+      })
+      .then(response => {
+        this.setState({ friends: response.data});
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 }
 
 export default App;
+
+/*
+state = {
+            todo: [
+                {
+                    id: 1,
+                    text: 'This',
+                },
+                { 
+                    id: 2,
+                    text: 'That',
+                },
+                {
+                    id: 3,
+                    text: 'Other',
+                }
+            ],
+            newToDo: '',
+    }
+
+    addNewToDo = (event) => {
+        event.preventDefault();
+        const todo = this.state.todo;
+        const newID = todo.length + 1;
+        const item = this.state.newToDo;
+        todo.push({id: newID, text: item});
+        this.setState({
+            newToDo: '',
+            todo: todo,
+        });
+    }
+
+    handleNewToDo = (event) => {
+        this.setState({newToDo: event.target.value});
+    }
+
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.state.todo.map(item => {
+                        return <Item key={item.id} item={item} />;
+                    })}
+                </ul>
+              
+                <form onSubmit={this.addNewToDo}>
+                    <input type="text" onChange={this.handleNewToDo} value={this.state.newToDo} />
+                    <input type="submit" value="Submit New To Do Item to List" />
+                </form>
+            </div>
+        )
+    }
+}
+*/
--- friends/src/Components/Friends/friends.js
+++ friends/src/Components/Friends/friends.js
@@ -4,7 +4,7 @@ import './friends.css';
 function Friend(props) {
     return (
         <div className="card">
-            <ul>
+            <ul className="list">
                 <li>{props.friend.name}</li>
                 <li>{props.friend.age}</li>
                 <li>{props.friend.email}</li>
