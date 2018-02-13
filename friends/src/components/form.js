b01c65480d9c44c87791e60f462baf90c208b32f
Nikhil Kamineni <nikhil@nk-mbp.local>
2018-02-13 16:42:11 -0500

    Got age to convert to integer before setting state

--- friends/src/components/form.js
+++ friends/src/components/form.js
@@ -8,32 +8,55 @@ class Form extends Component {
     'email': '',
   }
 
-  handleChange = (event) => {
-    const newState = this.state;
-    newState[event.target.name] = event.target.value;
-    this.setState(newState);
-  }
-
-  onSubmit = (event) => {
-    let { name, age, email } = this.state;
-    axios
-    .post('http://localhost:5000/friends', {name, age, email})
-    .then((response) => {console.log(`Response: ${response}`)})
-    .catch((error) => {console.log(`Error: ${error}`)});
-  }
-
   render() {
     const { name, age, email } = this.state;
     return (
       <div style={{border: '3px solid black', padding: '20px', height: '75px', margin: '-3px 0 0 0'}}>
         <form action="" onSubmit={this.onSubmit}>
-          <input type="text" name="name" value={name} placeholder="Name" onChange={this.handleChange}/> <br/>
-          <input type="number" name="age" value={age} placeholder="Age"onChange={this.handleChange}/> <br/>
-          <input type="text" name="email" value={email} placeholder="Email"onChange={this.handleChange}/> <br/>
+          <input type="name" 
+            name="name" 
+            value={name} 
+            placeholder="Name" 
+            onChange={this.handleChange}
+            /> <br/>
+          <input 
+            type="number" 
+            name="age" 
+            value={age} 
+            placeholder="Age"
+            onChange={this.handleChange}
+            /> <br/>
+          <input 
+            type="email" 
+            name="email" 
+            value={email} 
+            placeholder="Email"
+            onChange={this.handleChange}
+          /> <br/>
           <input type="submit" value="Add"/>
         </form>
       </div>
-    )
+    ) // return
+  } // render()
+
+  handleChange = (event) => {
+    let {name, value} = event.target;
+
+    if (event.target.type === 'number') {
+      value = Number(value);
+    }
+    
+    this.setState({[name]: value});
+    console.log(this.state);
+  }
+
+  onSubmit = (event) => {
+    // event.preventDefault();
+    let { name, age, email } = this.state;
+    axios
+    .post('http://localhost:5000/friends', {name, age, email})
+    .then((response) => {console.log(`Response: ${response}`)})
+    .catch((error) => {console.log(`Error: ${error}`)});
   }
 } // Form component
 
