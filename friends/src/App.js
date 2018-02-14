0760a02739a0f9f29a3661b046699ef29d8b509f
Arkoma <aaron@adburk.net>
2018-02-13 21:54:11 -0600

    add delete function and update function delete was working but not anymore

--- friends/src/App.js
+++ friends/src/App.js
@@ -16,7 +16,15 @@ class App extends Component {
     return (
       <div className="App">
         <AddFriend onCreate={this.getFriends}/>
-        <FriendsList friends={this.state.friends}/>
+        <FriendsList 
+          name={this.state.name}
+          age={this.state.age}
+          email={this.state.email}
+          friends={this.state.friends} 
+          onX={this.deleteFriend} 
+          onNewDeets={this.newDeetsSubmitHandler} 
+          onDeetsChange={this.deetsChangeHandler}
+        />
       </div>
     );
   }
@@ -28,6 +36,32 @@ class App extends Component {
   componentDidMount() {
     this.getFriends();
   }
+  deleteFriend = id => {
+    const endpoint = `http://localhost:5000/friend  s/${id}`;
+    axios.delete(endpoint)
+    .then(response => this.setState({ friends: response.data}))
+    .catch(() => {
+      console.error('error deleting')
+    })
+  }
+  newDeetsSubmitHandler = id => {
+    const endpoint = `http://localhost:5000/friends/${id}`;
+    axios.put(endpoint)
+    .then(response => this.setState({ friends: response.data}))
+    .then(response => this.getFriends())
+    .catch(() => {
+      console.error('error deleting')
+    })
+  }
+  deetsChangeHandler = (event) => {
+    let { name, value } = event.target;
+
+    this.setState({ [name]: value });
+    
+    if (event.target.type === 'number') {
+        value = Number('number')
+    }
+}
 }
 
 export default App;
--- friends/src/components/FriendsList/FriendsList.js
+++ friends/src/components/FriendsList/FriendsList.js
@@ -1,19 +1,34 @@
 import React from 'react';
 
 function FriendsList(props) {
-return (
+    return (
         <div>
             { props.friends.map((card) => {
                 return (
                 <ul key={card.id} className="flc">
-                    <li className="flc__name">{card.name}</li>
+                    <li className="flc__name">
+                        <button onClick={() => {props.onX(card.id)}}>X</button>
+                        {card.name}</li>
                     <li className="flc__age">{card.age}</li>
                     <li className="flc__email">{card.email}</li>
+                    <li>
+                    <hr />
+                    <form className="flc__form" onSubmit={()=>{props.newDeetsSubmitHandler(card.id)}}>
+                        <label>Update Name: </label>
+                        <input type="text" name="name" value={props.name} onChange={props.deetsChangeHandler} placeholder="name"/>
+                        <label>Update Age: </label>
+                        <input type="number" name="age" value={props.age} onChange={props.deetsChangeHandler} placeholder="age"/>
+                        <label>Update Email: </label>
+                        <input type="email" name="email" value={props.email} onChange={props.deetsChangeHandler} placeholder="email"/>
+                        <button type="submit">update</button>
+                    </form>
+                    </li>
                 </ul>
                 )
             })}
         </div>
     )
+
 }
 
 export default FriendsList;
