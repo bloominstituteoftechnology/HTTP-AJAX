78e1ea81e3c4d6ae946fc2496be5db6dfc747604
Unknown <peter@petergraycreative.com>
2018-02-15 18:01:56 -0500

    Build AddFriend Component
    Built out AddForm class with <form>, state object, input handler, and submit action to post to the server.

---
+++ friends/src/AddFriend/AddFriend.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import axios from 'axios';
+
+class AddFriend extends React.Component {
+  state = {
+    name: '',
+    age: '',
+    email: '',
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.postFriend}>
+        <label htmlFor="name">Name</label>
+        <input
+          name="name"
+          type="text"
+          value={this.state.name}
+          onChange={this.handleInput}
+        />
+        <label htmlFor="age">Age</label>
+        <input
+          name="age"
+          type="text"
+          value={this.state.age}
+          onChange={this.handleInput}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          name="email"
+          type="text"
+          value={this.state.email}
+          onChange={this.handleInput}
+        />
+        <button type="submit">Save</button> 
+      </form>
+    );
+  }
+  handleInput = e => {
+    let { name, value } = e.target;
+
+    if (e.target.type === 'number') {
+      value = Number(value);
+    }
+    this.setState({ [name]: value });
+  };
+  postFriend = (e) => {
+    e.preventDefault();
+
+    axios
+    .post('http://localhost:5000/friends', this.state)
+    .then(response => {
+      console.log('respone', response);
+    })
+    .catch(error => {
+      console.error('error', error);
+    });
+  }
+}
+export default AddFriend;
