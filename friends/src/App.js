a7e7ee50e2863bf8206604689cf68de73b170f2f
Jonathan <jonathanbry@comcast.net>
2018-02-20 13:44:15 -0800

    Jonathan Bry final commit

---
+++ friends/src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+import FriendForm from './FriendForm';
+import FriendList from './FriendList';
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    friends: [],
+  }
+  render() {
+    return (
+      <div className="App">
+        <FriendForm onCreate={this.loadFriends} />
+        <FriendList friends={this.state.friends} onDelete={this.removeFriend()} />
+      </div>
+    );
+  }
+
+
+  componentDidMount() {
+    this.loadFriends();
+}
+
+  loadFriends = () => {
+    axios
+    .get('http://localhost:5000/friends')
+    .then(response => {
+      this.setState({
+        friends: response.data,
+      });
+    })
+    .catch(() => {
+      console.error('error getting data');
+    });
+  };
+
+  removeFriend = id => {
+    const endpoint = 'http://localhost:5000/friends/${id}';
+    axios
+    .delete(endpoint)
+    .then((response) => {
+      console.log('response from delete', response)
+    })
+    .catch(() => {
+      console.error('error deleting')
+    });
+  };
+}
+
+export default App;
---
+++ friends/src/FriendList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import axios from 'axios';
+
+class FriendList extends React.Component {
+  state={
+    friends:[]
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.friends.map((friend) => {
+          return(
+          <li key={friend.id}>
+              {friend.name}
+          </li>
+        );
+        })}
+      </ul>
+    );
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends').then((response)=> {
+      this.setState({
+        friends: response.data
+      })
+    } ).catch(() => {
+      console.error('error getting data');
+    });
+  }
+}
+
+export default FriendList;
+
+// http://localhost:5000/friends
