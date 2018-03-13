b680900d8e48169a495b7e959e87ec9e81b6acc2
csstone12 <36163530+csstone12@users.noreply.github.com>
2018-03-13 16:16:30 -0400

    Stone Chris | HTTP-AJAX
    Stone Chris | HTTP-AJAX

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FriendsList from './components/FriendsList';
+
+
 
 class App extends Component {
+  FriendsList = () => {
+      return <FriendsList />;
+    }
+
   render() {
     return (
       <div className="App">
