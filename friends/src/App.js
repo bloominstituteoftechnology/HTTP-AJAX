9f1144e442edf98b9fafeb00bb46cf4cc798d494
DragonRydrz <DragonRydrz@me.com>
2018-03-13 15:32:29 -0400

    get request working, displaying information correctly

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
+import { Card, CardText, CardTitle } from 'reactstrap';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      friends: [],
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      {this.state.friends.map(friend => {
+        return(
+          <Card body inverse color="primary">
+            <CardTitle>{friend.name}</CardTitle>
+            <CardText>{`${friend.name} is ${friend.age} years old.`}</CardText>
+            <CardText>{`${friend.name}'s email address is ${friend.email}.`}</CardText>
+            
+          </Card>
+        )
+      })}
+        
       </div>
     );
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/friends")
+      .then(response => {
+        this.setState({ friends: response.data });
+        console.log(response);
+      })
+      .catch(err => console.log(err))
   }
 }
 
