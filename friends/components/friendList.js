5a59f9011e83245a73310df27342cfbe88708757
Omoniyi Alimi <omoniyialimi@Omoniyis-MacBook-Pro.local>
2018-03-13 18:13:05 -0400

    WIP HTTP/AJAX

---
+++ friends/components/friendList.js
@@ -0,0 +1,13 @@
+import React, { Component } 'react';
+import axios from 'axios';
+
+export default class friendList extends Component {
+
+constructor () {
+    super();
+    this.state = {
+        friends: [],
+    }
+}
+
+}
