c9e5a079cd3ceee066e785137a88c45d4b70e536
astro11 <aquila.strother@asu.edu>
2018-03-13 19:01:26 -0400

    Completed -- Stretch WIP

---
+++ friends/src/Components/FriendList.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import '../index.css';
+
+class FriendList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: [],
+      name: '',
+      age: '',
+      email: ''
+    }
+  }
+
+componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+    .then(response => {
+      console.log(response);
+      this.setState({ friends: response.data });
+    })
+    .catch(error => {
+      console.log(`there was an error getting friends: ${error}`);
+    });
+}
+
+handleChange = (event) => {
+    const { name , value } = event.target;
+    //console.log(event.target);
+    this.setState({ [name]: value })
+    console.log(this.state);
+}
+
+addFriend = (event) => {
+    event.preventDefault()
+    const { name, age, email } = this.state;   
+    axios.post('http://localhost:5000/friends', {
+        name,
+        age,
+        email
+      })
+      .then(response => {
+          console.log(`this is response ${response}`);
+        this.setState({ friends: response.data});
+      })
+      .catch(error => {
+        console.log(`there was an error posting friends: ${error}`);        
+      });
+}
+
+render() {
+    return (
+      <div>
+        <div className="friend-title">List of Friends</div>
+        <ul className="friend-grid">
+          {this.state.friends.map((friend, index) => {
+            return (
+              <li key={index} className="friend">
+                <div className="friend-name">{friend.name}</div>
+                <div className="friend-age">{`Age: ${friend.age}`}</div>
+                <div className="friend-email">{`Email: ${friend.email}`}</div>
+              </li>
+            );
+          })}
+            </ul>
+            <form>
+                <div className="friend-name">Name:
+                <input name="name" value={this.state.name} onChange={this.handleChange} type="text" placeholder="Enter Name"/>
+                </div>
+                <div className="friend-age">Age:
+                <input name="age" value={this.state.age} onChange={this.handleChange} type="text" placeholder="Enter Age"/>
+                </div>
+                <div className="friend-email">Email:
+                <input name="email" value={this.state.email} onChange={this.handleChange} type="text" placeholder="Enter Email"/>
+                </div>
+                <button type="submit" onClick={this.addFriend}>Add</button>
+            </form>
+      </div>      
+    );
+  }    
+}
+
+export default FriendList;
