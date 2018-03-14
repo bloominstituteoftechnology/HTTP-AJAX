1413cd80717520435feceeda62b1802687b6340d
Edward Manda <lilkups@yahoo.com>
2018-03-14 07:19:10 -0500

    a fully completed work!

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import FriendsList from './components/FriendsList';
-
+import Friend from './components/Friend';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <FriendsList />
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path = "/" component={FriendsList} />
+          <Route path = "/friends/:id" component={Friend} />
+        </div>
+      </Router>
     );
   }
 }
