c29fa255d46a74e119b580ab44f869863d789bfc
Ajmal Jalal <ajmaljalal58@gmail.com>
2018-03-14 17:03:49 -0700

    70% of stretch goal is completed

--- friends/src/FriendsList.js
+++ friends/src/FriendsList.js
@@ -3,6 +3,7 @@ import React, {
 } from 'react';
 import axios from 'axios';
 import './FriendsList.css'; 
+import SubmitForm from './SubmitForm';
 
 
 class FriendsList extends Component {
@@ -10,12 +11,18 @@ class FriendsList extends Component {
         super();
         this.state = {
             friends: [],
+            id: '',
             name: '',
             age: '',
             email: '',
         }
     }
 
+    updateId = (e) =>{
+        this.setState({
+            id: Number(e.target.value)
+        });
+    };
     updatName = (e) =>{
         this.setState({
             name: e.target.value
@@ -24,7 +31,7 @@ class FriendsList extends Component {
 
     updatAge = (e) =>{
         this.setState({
-            age: e.target.value
+            age: Number (e.target.value)
         });   
     };
 
@@ -37,6 +44,15 @@ class FriendsList extends Component {
     submitFriend = (e) =>{
         e.preventDefault();
 
+        axios.post('http://localhost:5000/friends', {
+            name: this.state.name,
+            age: this.state.age,
+            email: this.state.email
+        })
+          .then(function (response) {
+            console.log(response.data);
+          })
+
         let newFriends = this.state.friends;
         let newFriend = {
             name: this.state.name,
@@ -48,17 +64,30 @@ class FriendsList extends Component {
         this.setState({
             friends: newFriends
         })
-
-        axios.post('http://localhost:5000/friends', {
-            name: this.state.name,
-            age: this.state.age,
-            email: this.state.email
-          })
-          .then(function (response) {
-            console.log(response.data);
-          })
     };
 
+
+    deleteFriend = (e) => {
+        e.preventDefault();
+
+        axios.delete(`http://localhost:5000/friends/${this.state.id}`)
+            .then(response => {
+            this.setState({friends: response.data});
+        })
+    }
+
+
+    updateFriend = (e) =>{
+        e.preventDefault();
+        axios.put(`http://localhost:5000/friends/${this.state.id}`, {
+                name: this.state.name,
+                age: this.state.age,
+                email: this.state.email,
+        })
+        .then(response => {
+        this.setState({friends: response.data});
+        })
+    };
 
     render(){
         return(
@@ -66,23 +95,16 @@ class FriendsList extends Component {
                 <div className="friend-grid">
                     {this.state.friends.map(friend => {
                         return (
-                        <div key={friend.id} className="friend">
-                            <div className="friend-name">{friend.name}</div>
-                            <div className="friend-age">{`Age: ${friend.age}`}</div>
-                            <div className="friend-email">{`Email: ${friend.email}`}</div>
-                            <div>============================</div>
-                        </div>
-                        
+                            <div key={friend.id} className="friend">
+                                <div className="friend-name">{friend.name}</div>
+                                <div className="friend-age">{`Age: ${friend.age}`}</div>
+                                <div className="friend-email">{`Email: ${friend.email}`}</div>
+                                <div>============================</div>
+                            </div>
                         );
                     })}
                 </div>
-
-                    <form className = 'form'>
-                        <input className = 'form-item' onChange = {this.updatName} placeholder = 'friends name' />
-                        <input className = 'form-item' onChange = {this.updatAge} placeholder = 'friends age' />
-                        <input className = 'form-item' onChange = {this.updatEmail} placeholder = 'friends email' />
-                        <button className = 'form-item' onClick = {this.submitFriend}>add friend</button>
-                    </form>
+                <SubmitForm onUpdate = {this.updateFriend} onDelete = {this.deleteFriend} submit = {this.submitFriend}  onNameChange ={this.updatName} onAgeChange ={this.updatAge} onEmailChange ={this.updatEmail} onIdChange = {this.updateId}/>
             </div>
         );
     }
