f4d1db3d5e19d6fb846cb7f98339ea9c8157569b
Megan Williamson <meganawilliamson@gmail.com>
2018-03-14 23:01:45 -0700

    add friend using post method complete

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import FriendsList from './components/FriendsList';
 
 const App = () => {
-  return <FriendsList />;
+  return (
+    <Router>
+        <div>
+          <Route path='/friends' component={FriendsList} />
+        </div>
+    </Router>
+  )
 }
 export default App;
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -2,41 +2,102 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class FriendsList extends Component {
-    constructor() {
-        super();
-        this.state = {
-            friends: []
-        }
-    }
+  constructor() {
+      super();
+      this.state = {
+        friends: [],
+        name: '',
+        age: '',
+        email: ''
+      };
+  }
 
-    render() {
-        return (
-          <div>
-            <div >Lambda Friends</div>
-            <ul>
-              {this.state.friends.map(friend => {
-                return (
-                  <li key={friend.id}>
-                    <div>{friend.name}</div>
-                    <div>{`Age: ${friend.age}`}</div>
-                    <div>{`Email: ${friend.email}`}</div>
-                  </li>
-                );
-              })}
-            </ul>
+  handleInput = (event) => {
+    // console.log(`This is the state: ${this.state}`);
+    // console.log(`This is the target name: ${event.target.name}`);
+    // console.log(`This is the target value: ${event.target.value}`)
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  submitFriend = (event) => {
+    event.preventDefault();
+    const { name, age, email } = this.state;
+    const newFriend = {
+      name,
+      age,
+      email
+    };
+    axios.post('http://localhost:5000/friends', newFriend )
+      .then(response => {
+        console.log(response);
+        this.setState({ 
+          name: '',
+          age: '',
+          email: '',
+          friends: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  };
+
+  render() {
+    return (
+      <div>
+        <div >Lambda Friends</div>
+        <ul>
+          {this.state.friends.map(friend => {
+            return (
+              <li key={friend.id}>
+                <div>{`Name: ${friend.name}`}</div>
+                <div>{`Age: ${friend.age}`}</div>
+                <div>{`Email: ${friend.email}`}</div>
+              </li>
+            );
+          })}
+        </ul>
+
+        <div>
+              <form type='submit' action='http://localhost:5000/friends' method='post'> {/* !! should action be /friends or http://..5000/friends? */}
+                  <label htmlFor='name'>Name</label>
+                  <input
+                      type='text'
+                      name='name'
+                      value={this.state.name}
+                      onChange={this.handleInput}
+                  />
+                  <label htmlFor='age'>Age</label>
+                  <input
+                      type='number'
+                      name='age'
+                      value={this.state.age}
+                      onChange={this.handleInput}
+                  />
+                  <label htmlFor='email'>Email</label>
+                  <input
+                      type='email'
+                      name='email'
+                      value={this.state.email}
+                      onChange={this.handleInput}
+                  />
+                  <div className='button'>
+                      <button type='submit' onClick={this.submitFriend}>Add friend</button>
+                  </div>
+              </form>
           </div>
-        );
-      }
+      </div>
+    );
+  }
 
-      componentDidMount() {
-        axios.get('http://localhost:5000/friends')
-          .then(response => {
-            this.setState({ friends: response.data });
-          })
-          .then(response => response)
-          .catch(error => {
-            console.log(`Error getting friends: ${error}`);
-          })
-      }
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+      .then(response => {
+        this.setState({ friends: response.data });
+      })
+      .catch(error => {
+        console.log(`Error getting friends: ${error}`);
+      })
+  }
 }
 export default FriendsList;
