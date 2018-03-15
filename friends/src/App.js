7cda0e22e86d82913c70b9995ab3049cc6301aee
KJ Sourcing <tmkogwork@gmail.com>
2018-03-14 20:33:44 -0700

    Some stretch goals tackled

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,16 +1,54 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Friends from './components/Friends';
+import FriendForm from './components/FriendForm';
 
 import './App.css';
 
 class App extends Component {
+  state = {
+    friends: [],
+  };
+
+
+
   render() {
     return (
       <div className="App">
-        <Friends />
+        <FriendForm onCreate={this.loadFriend} />
+        <Friends 
+          friends={this.state.friends} 
+          onDelete={this.removeFriend} />
       </div>
     );
   }
+
+  componentDidMount() {
+    this.loadFriend();
+  }
+
+  loadFriend = () => {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        this.setState( {friends: response.data,} );
+      })
+      .catch(() => { console.error('error getting data'); });
+  };
+
+  removeFriend = id => {
+    console.log(id);
+    const endpoint = `http://localhost:5000/friends/${id}`;
+    axios
+      .delete(endpoint)
+      .then(response => {
+        console.log('response from delete', response);
+        this.setState({ friends: response.data });
+      })
+      .catch(() => {
+        console.error('error deleting');
+      });
+  };
 }
 
 export default App;
--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,75 +1,25 @@
-import React, { Component } from  'react';
-import axios from 'axios';
+import React from  'react';
 import PropTypes from 'prop-types';
 
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-class Friends extends Component {
-    constructor() {
-        super();
-        this.state = {
-            Friends: [],
-            name: '', 
-            email: '',
-            age: 18,
-        };
-    } 
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        const { name, email, age } = this.state
-        axios.post(`http://localhost:5000/friends`, {name, email, age})
-        .then(response => {
-            this.setState({Friends: response.data});
-            console.log(response);
-        })
-    }
-
-    handleChange = (event) => {
-        const { name, value } = event.target
-        this.setState({[name]: value})
-    }
-
-    componentDidMount() {
-        axios.get(`http://localhost:5000/friends`)
-        .then(response => {
-            this.setState({Friends: response.data});
-        });
-    }
-
-    render() {
-        return (
-            <div className="Friends"> 
-                <ul className="Friend__List">
-
-                    {this.state.Friends.map(friend => {
-                        return (
-                            <li key={friend.id} className="Friend">
-                                <div className="Friend__Name">{friend.name}</div>
-                                <div className="Friend__Age">{`Age: ${friend.age}`}</div>
-                                <div className="Friend__Email">{`Email: ${friend.email}`}</div>
-                            </li>
+function Friends(props) {
+    console.log(props);
+    return (
+        <div className="Friends"> 
+            <ul className="Friend__List">
+                {props.friends.map(friend => {
+                    return (
+                        <li key={friend.id} className="Friend">
+                            <div className="Friend__Name">{friend.name}</div>
+                            <div className="Friend__Age">{`Age: ${friend.age}`}</div>
+                            <div className="Friend__Email">{`Email: ${friend.email}`}</div>
+                            <button onClick={() => { props.onDelete(friend.id); }}> Delete </button>
+                        </li>
                         );
-                    })}
-                        <Form>
-                            <FormGroup>
-                            <Label for="Name">Name: </Label>
-                                <Input value={this.state.name} onChange={this.handleChange} name="name"  type="text" placeholder="name"/>
-                            </FormGroup>
-                            <FormGroup>
-                            <Label for="Age">Age: </Label>
-                                <Input value={this.state.age} onChange={this.handleChange} name="age"  type="text" placeholder="age"/>
-                            </FormGroup>
-                            <FormGroup>
-                            <Label for="Email">Email: </Label>
-                                <Input value={this.state.email} onChange={this.handleChange} name="email"  type="text" placeholder="email"/>
-                            </FormGroup>
-                            <Button onClick={this.handleSubmit} value="Save" />
-                        </Form>
-                </ul>
-            </div>
-        );
-    }
+                })}
+            </ul>
+        </div>
+    );
 }
 
 Friends.propTypes = {
@@ -77,5 +27,10 @@ Friends.propTypes = {
     age: PropTypes.number,
     email: PropTypes.string,
 };
+
+Friends.propTypes = {
+    friends: PropTypes.array.isRequired,
+};
+
 
 export default Friends;
