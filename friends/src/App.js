c9fcb7439f5d911f72b2ea915256cb6b251e491d
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-04-11 18:40:50 -0400

    Created form to input new friends, save data to server using post request attached to a save button

--- friends/src/App.js
+++ friends/src/App.js
@@ -52,6 +52,29 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <input
+        type= 'text'
+        onChange={this.handleTextInput}
+        placeholder= 'Name'
+        name= 'name'
+        value= {this.state.name}
+        />
+        <input
+        type= 'text'
+        onChange={this.handleTextInput}
+        placeholder= 'Age'
+        name= 'age'
+        value= {this.state.age}
+        />
+        <input
+        type= 'text'
+        onChange= {this.handleTextInput}
+        placeholder= 'Email'
+        name= 'email'
+        value= {this.state.email}
+        />
+        <button onClick={this.saveNoteData}>Make A Friend </button>
+
         <ListOfFriends friend={this.state.friend} />
       </div>
     );
