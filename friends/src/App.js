f8fc365c1ca8a338fcfe0da537b23299c41e385b
unknown <bsa6.23.94@gmail.com>
2018-04-11 17:11:59 -0400

    finished with everything but stretch

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,21 +1,80 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: [],
+      name: "",
+      age: "",
+      email: ""
+    }
+  }
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/friends`)
+      .then(response => {
+        this.setState({ friends: response.data });
+      })  
+      .catch(err => {
+        console.log(err);
+      });
+    }
+    handleTextChange = e => {
+      this.setState({ [e.target.name]: e.target.value });
+    };
+    saveData = () => {
+      const poo = { name: this.state.name, age: this.state.age, email: this.state.email };
+      axios
+      .post(`http://localhost:5000/friends`, poo)
+      .then(savedData => {
+        console.log(savedData);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+      this.setState({ name: '', age: '', email: '' });
+    };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      {this.state.friends.map(friend => {
+        return (
+          <div className="friend-card" >
+          <div key={friend.id} className="frenNotFood">
+          <div className="friend friendName"> Name: {friend.name} </div>
+          <div className="friend friendAge"> Age: {friend.age} </div>
+          <div className="friend friendEmail"> Email: {friend.email} </div>
+        
+          </div>
+          
+          </div>
+          
+          
+        )
+        
+      })}
+        <div>
+        <div>  <form>
+              <label>
+                <input  type="text" name="name" onChange={this.handleTextChange} placeholder="Name Here" />
+                
+              </label>
+                <input type="number" name="age" onChange={this.handleTextChange} placeholder="Age Here" />
+                
+                <input type="text" name="email" onChange={this.handleTextChange} placeholder="Email Here" />
+              <button type="submit" value="Submit" onClick={this.saveData}> Press ME</button>
+            </form>
+            </div>
+      </div>
       </div>
     );
   }
 }
 
 export default App;
+
