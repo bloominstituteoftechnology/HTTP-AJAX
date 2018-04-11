3614c0cbc362697e671d3865822edd5ae6a757c2
Jared Cuffe <jcuffe@gmail.com>
2018-04-11 12:19:51 -0700

    Refactored to multiple components. Implemented Update on server side.

---
+++ friends/src/Friends/FriendInput.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import axios from 'axios'
+
+class FriendInput extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      age: '',
+      email: '',
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(snapshot);
+    if (prevProps !== this.props && this.props.currentFriendId > 0) {
+      axios.get(`http://localhost:5000/friends/${this.props.currentFriendId}`)
+        .then((response) => {
+          this.setState(response.data);
+        })
+        .catch((error) => {
+          console.log(`There was an error retrieving Friend: ${error}`);
+        });
+    }
+    if (prevProps.currentFriendId > 0 && this.props.currentFriendId < 0) {
+      this.setState({ name: '', age: '', email: '' });
+    }
+  }
+
+  addFriend = (event) => {
+    event.preventDefault();
+    const newFriend = {
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email
+    }
+    if (this.props.currentFriendId > 0) {
+      axios.put(`http://localhost:5000/friends/${this.props.currentFriendId}`, newFriend)
+        .then((response) => {
+          console.log('Friend updated. Response:')
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(`There was an error updating Friend: ${error}`)
+        })
+    } else {
+      axios.post('http://localhost:5000/friends/', newFriend)
+        .then(response => {
+          this.setState({ name: '', age: '', email: '' });
+        })
+        .catch(error => {
+          console.log(`There was an error adding Friend: ${error}`);
+        });
+    }
+  };
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  submitLabel() {
+    if (this.props.currentFriendId > 0) {
+      return 'Update Friend';
+    } else {
+      return 'Create Friend';
+    }
+  }
+
+  render() {
+    return (
+      <form className='friend-input' onSubmit={this.addFriend}>
+        <label>Name:</label> <input type='text' name='name' placeholder='Name' onChange={this.handleInputChange} value={this.state.name} />
+        <label>Age:</label> <input type='number' name='age' placeholder='Age' onChange={this.handleInputChange} value={this.state.age} />
+        <label>Email:</label> <input type='text' name='email' placeholder='Email' onChange={this.handleInputChange} value={this.state.email} />
+        <button className='add-friend' type='submit'>{this.submitLabel()}</button>
+      </form>
+    )
+  }
+}
+
+export default FriendInput;
---
+++ friends/src/Friends/FriendsList.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import FriendInput from './FriendInput'
+
+class FriendsList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: [],
+      currentFriendId: -1
+    }
+  };
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+    .then(response => {
+      this.setState({ friends: response.data });
+    })
+    .catch(error => {
+      console.log( `There was an error getting friends: ${error}`);
+    });
+  }
+
+  selectFriend = (event) => {
+    console.log(event.target.id);
+    this.setState({ currentFriendId: event.target.id })
+  }
+
+  clearSelection = () => {
+    this.setState({ currentFriendId: -1 });
+  }
+  
+  render() {
+    return (
+      <div>
+        <div className="title">Friends</div>
+        <FriendInput currentFriendId={this.state.currentFriendId} />
+        <ul className="grid">
+          <button className="clear-selection" onClick={this.clearSelection}>Clear Selection</button>
+          {this.state.friends.map(friend => (
+            <li key={friend.id} className="friend">
+              <div className="name">{friend.name}</div>
+              <div className="age">{`Age:${friend.age}`}</div>
+              <div className="email">{`Email: ${friend.email}`}</div>
+              <button className="select-friend" id={friend.id} onClick={this.selectFriend}>Select</button>
+              <button className="remove-friend" id={friend.id} onClick={this.removeFriend}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default FriendsList;
