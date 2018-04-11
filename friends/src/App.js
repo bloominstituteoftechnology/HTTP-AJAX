65d7a52a8ea219de0867c9a374bb069ad027ae51
jeffrey <jeffreyneilflynn@gmail.com>
2018-04-11 18:49:55 -0400

    got the data to display

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,16 +2,50 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Display from './components/Display';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+      .then(response => {
+        this.setState({ friends: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  names = () => {
+    return (
+      <div className="friends">
+        {this.state.friends.map(friend => (
+          <div key={friend.id} className="friend">
+            <div className="friend--name">{friend.name}</div>
+            <div className="friend--age">{friend.age}</div>
+            <div className="friend--email">{friend.email}</div>
+          </div>
+        ))}
+      </div>
+    )
+  }
+
   render() {
+    const friendsCopy = [...this.state.friends];
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Display />
+        {this.names()}
+        {console.log(this.state.friends)}
       </div>
     );
   }
--- friends/src/components/Display.js
+++ friends/src/components/Display.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 class Display extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      friend: {...props.friend}
+    }
   }
 
   render() {
     return (
-      <div>hey</div>
+      <div>{this.state.friend}</div>
     )
   }
 }
