000508f06f7b69093dba37982dfecc27f692050d
totsuya <supertatortots@gmail.com>
2018-04-11 18:03:51 -0400

    fixed routing error

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import axios from 'axios';
-import FriendList from './components/FriendList/FriendList.js';
+import FriendList from './components/FriendList/FriendsList.js';
 import './App.css';
 
 class App extends Component {
