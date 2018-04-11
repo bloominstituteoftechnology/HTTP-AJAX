544645e3fcc725bbae63a88cb52eaaa862efc019
totsuya <supertatortots@gmail.com>
2018-04-11 15:47:25 -0400

    added class in FriendList

--- friends/src/components/FriendList/FriendList.js
+++ friends/src/components/FriendList/FriendList.js
@@ -1,2 +1,16 @@
 import React, { Component } from "react";
 
+//should display a list of friends
+//might format into a card form later
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      friends: [],
+      name: '',
+      age: '',
+      email: '',
+    };
+  }
+}
