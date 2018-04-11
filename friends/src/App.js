a56b79e4a6dbe36ddda181989c2b16135436f378
Kyle Kauzlarich <kauzlarich.kyle@gmail.com>
2018-04-11 13:43:32 -0700

    project complete on to streach now

---
+++ friends/src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import Display from './display';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: [],
+      name: '',
+      email: '',
+      age: ''
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/friends`)
+      .then(response => {
+        this.setState({ friends: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  handleTextInput = e => { 
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  saveNoteData = () => {
+    const friend = { name: this.state.name, email: this.state.email, age: this.state.age };
+    axios
+      .post(`http://localhost:5000/friends`, friend)
+      .then(newFriend => {
+        console.log(newFriend);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    this.setState({ name: '', email: '', age: '',});
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Friends</h1>
+        </header>
+        <input
+          type="text"
+          onChange={this.handleTextInput}
+          placeholder="name"
+          name="name"
+          value={this.state.name}
+        />
+        <input
+          type="text"
+          onChange={this.handleTextInput}
+          placeholder="email"
+          name="email"
+          value={this.state.email}
+        />
+        <input
+          type="text"
+          onChange={this.handleTextInput}
+          placeholder="age"
+          name="age"
+          value={this.state.age}
+        />
+        <button onClick={this.saveNoteData}>Make Friend</button>
+        <Display friends={this.state.friends} />
+      </div>
+    );
+  }
+}
+
+export default App;
