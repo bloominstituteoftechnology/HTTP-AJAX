b3cc35ab009149fb153f70435c23059c56227640
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-04-11 19:21:05 -0400

    Adjust styles

--- friends/src/component/ListOfFriends.js
+++ friends/src/component/ListOfFriends.js
@@ -6,8 +6,8 @@ const ListOfFriends = props => {
         <div> 
             <div> {props.friend.map(f => <div key ={f.id} className='friends'> 
             <h1>{f.name}</h1>
-            <h1>{f.age}</h1>
-            <h1>{f.email}</h1>
+            <h2>{f.age}</h2>
+            <h2>{f.email}</h2>
         </div>
         )}
         </div>
