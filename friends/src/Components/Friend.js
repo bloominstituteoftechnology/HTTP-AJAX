dab4589185b3f153a378b124839810c7de0c764e
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-11 14:56:46 -0700

    added friend component

---
+++ friends/src/Components/Friend.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class Friend extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: '',
+            name: '',
+            age: '',
+            email: '',
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            id: this.props.friend.id,
+            name: this.props.friend.name,
+            age: this.props.friend.age,
+            email: this.props.friend.email
+        });
+    }
+
+    render() {
+        return (
+            <div className="Friend-Card">
+                <h2>{this.state.name}</h2>
+                <h4>Age: {this.state.age}</h4>
+                <h4>Email: {this.state.email}</h4>
+            </div>
+        );
+    }
+}
+
+export default Friend;
