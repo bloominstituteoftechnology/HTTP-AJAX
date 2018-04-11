7098531cad9d2f3af2507fc30fcaa53958d5be92
markstez05 <36493764+markstez05@users.noreply.github.com>
2018-04-11 14:26:34 -0500

    finished working on stretch

---
+++ my-react-app/src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: [],
+      name: '',
+      age: '',
+      email:'',
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/friends/get/all`)
+      .then(response => {
+        this.setState({ friends: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  handleTextInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  saveFriendData = () => {
+    const friends = { name: this.state.name, age: this.state.age, email: this.state.email };
+    axios
+      .post(`http://localhost:5000/friends`, friends)
+      .then(savedFriend => {
+        console.log(savedFriend);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    this.setState({ name: '', age: '', email: '' });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Friends!</h1>
+        </header>
+        <input
+          type="text"
+          onChange={this.handleTextInput}
+          placeholder="name"
+          name="name"
+          value={this.state.name}
+        />
+        <input
+          type="text"
+          onChange={this.handleTextInput}
+          placeholder="age"
+          name="age"
+          value={this.state.age}
+        />
+         <input
+          type="text"
+          onChange={this.handleTextInput}
+          placeholder="email"
+          name="email"
+          value={this.state.email}
+        />
+        <button onClick={this.saveFriendData}>Create Friend</button>
+      </div>
+    );
+  }
+}
+
+export default App;
