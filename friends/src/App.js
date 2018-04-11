5c0ebecc707b00a8d5c4572f2b51b5e6d609a3ff
Mark Wong <fmarkwong@gmail.com>
2018-04-11 15:47:20 -0700

    add friend

--- friends/src/App.js
+++ friends/src/App.js
@@ -10,25 +10,65 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      friends: []
+      friends: [],
+      newFriend: {
+        name: '',
+        age: 0,
+        email: ''
+      }
     };
   }
 
   componentDidMount() {
     axios.get('http://localhost:5000/friends')
       .then(response => {
-        console.log(response.data);
         this.setState({friends: response.data}); 
       })
       .catch(error => {
         console.log(error);
       })
   }
+
+  handleAddFriendInput = e => {
+    let newFriendData = { [e.target.name]: e.target.value };
+    this.setState({ newFriend: {...this.state.newFriend, ...newFriendData} })
+  }
+
+  handleSubmitAddFriend = e => {
+    axios.post('http://localhost:5000/friends', this.state.newFriend)
+      .then(response => {
+        this.setState({ friends: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   render() {
     return (
       <Router>
         <div className="App">
           <Route exact path="/" render={ () => <FriendsList {...this.state}/>}/>
+          <br />
+          Add a friend!<br />
+          Name: 
+          <input
+            type="text"
+            name="name"
+            onChange={this.handleAddFriendInput}
+          /><br />
+          Age:
+          <input
+            type="number"
+            name="age"
+            onChange={this.handleAddFriendInput}
+          /><br />
+          Email: 
+          <input
+            type="email"
+            name="email"
+            onChange={this.handleAddFriendInput}
+          /><br />
+          <button onClick={this.handleSubmitAddFriend}>Add</button>
         </div>
       </Router>
     );
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -3,7 +3,7 @@ import Friend from './Friend';
 
 const FriendsList = props => {
   return (
-    props.friends.map(friend => <Friend friend={friend} />)
+    props.friends.map((friend, index) => <Friend key={index} friend={friend} />)
   ); 
 }
 
