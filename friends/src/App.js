ed6aa258b521a0fe02cca45a77d4b267e2d9c922
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-11 15:12:27 -0700

    added files and routes in app.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FriendsList from "./Components/FriendsList";
+import { Route } from "react-router-dom";
+import AddFriend from "./Components/AddFriend";
+import Friend from "./Components/Friend";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Route exact path="/" component={FriendsList} />
+        <Route path="/addfriend" component={AddFriend} />
+        <Route path="friends/:id" component={Friend} />
       </div>
     );
   }
