5b7e000f8eb29a696a3a6fa21815f0c387d23e1e
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-04-11 18:31:20 -0400

    Displayed data onto screen

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import logo from './logo.svg';
 import './App.css';
+import ListOfFriends from './component/ListOfFriends';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      friend: [],
+      name: '',
+      age: '',
+      email: '',
     }
   }
+  
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/friends`)
+      .then(response => {
+        this.setState({ friend: response.data});
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  handleTextInput = e => {
+    this.setState({ [e.target.name]: e.target.value});
+  };
+
+  saveNoteData = () => {
+    const friend = { name: this.state.name, age: this.state.age, email: this.state.email };
+    axios
+      .post(`http://localhost:5000/friends`, friend)
+      .then(savedNote => {
+        console.log(savedNote);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    this.setState({ name: '', age: '', email: ''});
+  };
+    
   render() {
     return (
       <div className="App">
@@ -16,9 +52,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ListOfFriends friend={this.state.friend} />
       </div>
     );
   }
