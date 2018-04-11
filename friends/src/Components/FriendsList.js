4056553fcc7dea93b0ac51a55c588f3e0b564579
Paul Fang <paulfang@pauls-mbp.home>
2018-04-11 14:38:23 -0700

    component for FriendsList

--- friends/src/Components/FriendsList.js
+++ friends/src/Components/FriendsList.js
@@ -1,4 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react';
 import axios from 'axios'
 
+export default class FriendsList extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        friends: [],
+        name: '',
+        age: '',
+        email: '',
+      };
+    }
+    componentDidMount() {
+        axios
+          .get(`http://localhost:5000/friends`)
+          .then(response => {
+            this.setState({ friends: response.data });
+          })
+          .catch((error) => {
+            console.log(`There was an error retrieving Friend: ${error}`);
+          });
+      }
 
+    render() {
+
+        return (
+            <div className="mainDiv">
+                {this.state.friends.map(friend => (
+                    <div className='nameCard'>
+                    <div key={friend.id} classname="friend"></div>
+                     <div Classname="name">Name: {friend.name}</div>
+                    <div Classname="age">Age: {friend.age}</div>
+                    <div Classname="email">{friend.email}</div>
+                </div>
+        ))}
+            </div>
+
+)
+}}
