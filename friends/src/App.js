3e743f154b928d2db76bf0a81ad6286bced89f9b
2940cristian <2940cristian@gmail.com>
2018-04-11 17:52:37 -0700

    grabbed data from server and displayed it onto the screen

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      friends: []
+    }
+}
+componentDidMount() {
+  axios.get('http://localhost:5000/friends')
+    .then(response => {
+      this.setState({friends: response.data});
+      console.log(this.state.friends)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+   };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="friend__card">
+          {this.state.friends.map(friend => (
+              <Friends key={friend.id} friend={friend}/>
+          ))}
+        </div>
     );
   }
+}
+
+function Friends({friend}) {
+  const{name, age, email, id} = friend;
+  return(
+      <div className="friend_test">
+        <div className="friend">{name}</div>
+        <div className="friend">{age}</div>
+        <div className="friend">{email}</div>
+        <div className="friend">{id}</div>
+      </div>
+  )
 }
 
 export default App;
