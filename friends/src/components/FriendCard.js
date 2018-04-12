bba4329bbaa29249e9fa240f9965ff3a1eb46bb1
Justin Hong <jhong2009@gmail.com>
2018-04-11 19:48:20 -0700

    set up fetchFriend and CDM

--- friends/src/components/FriendCard.js
+++ friends/src/components/FriendCard.js
@@ -4,11 +4,28 @@ export default class FriendCard extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-
+			friends: []
 		};
 	}
+
+	componentDidMount() {
+		// console.log(this.props.match.params.id);
+		const id = this.props.match.params.id;
+		this.fetchFriend(id);
+	}
 	
+	fetchFriend = (id) => {
+		axios
+			.get("http://localhost:5000/friends")
+			.then(response => {
+				console.log(`response: ${response}`);
+			})
+			.catch(error => {
+				console.error(error);
+			});
+	}
+
 	render() {
-		return ();
+		return (<h1>Test</h1>);
 	}	
 }
