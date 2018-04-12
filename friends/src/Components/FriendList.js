1f90663f9f2be61f13f1c333c279d1e4f48769b0
Justin M <jekm321@gmail.com>
2018-04-12 02:09:26 -0400

    styling looks decent, components seperated and working correctly

--- friends/src/Components/FriendList.js
+++ friends/src/Components/FriendList.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './FriendList.css';
 
 const FriendList = props => {
     return (
         <div className="friendList">
+            {console.log('props',props)}
+            <button className="button button-save" onClick={() => props.saveInput()} >Save New Friend(s)</button>
             {props.friends.map((friend, index) => (
                 <div className="friends" key={friend.id + index}>
                     <div className="friends_name" >{friend.name}</div>
@@ -18,6 +21,9 @@ const FriendList = props => {
                     <div className="friends_email" >{friend.email}</div>
                 </div>
             ))}
+            <Link to="/addFriend" >
+                <button className="button button-add" > Wanna add a friend? </button>
+            </Link>
         </div>
     );
 };
