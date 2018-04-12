e18a935687bf83273883e17e6d0ba212b7ad7733
2940cristian <2940cristian@gmail.com>
2018-04-11 17:57:56 -0700

    Created new friend functionality, however a small bug, will fix if possible

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import axios from "axios";
-
+import axios from 'axios';
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      friends: []
+      friends: [],
+      name: "",
+      age: "",
+      email: "",
+      id: "",
+      counter: 6
     }
-}
-componentDidMount() {
+  }
+
+  componentDidMount() {
   axios.get('http://localhost:5000/friends')
     .then(response => {
       this.setState({friends: response.data});
@@ -21,13 +26,52 @@ componentDidMount() {
     })
    };
 
+   handleInput = e => {
+     this.setState({[e.target.name]: e.target.value});
+   }
+
+   counterUp = () => {
+     this.setState( {
+       counter: this.state.counter+1
+     })
+   }
+
+   sendFriend = () => {
+     this.setState( {
+       counter: this.counterUp
+     })
+    const newFriend = {
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email,
+      id: this.state.counter
+    }
+    axios.post(('http://localhost:5000/friends'), newFriend) 
+      .then(friendSent => {
+        console.log(friendSent);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+      this.setState( {
+        name: '', age: '', email: ''
+      })
+   };
+
+
   render() {
     return (
+      <div>
         <div className="friend__card">
           {this.state.friends.map(friend => (
               <Friends key={friend.id} friend={friend}/>
           ))}
         </div>
+        <input type="text" onChange={this.handleInput} placeholder="Name" name="name" value= {this.state.name}/>
+        <input type="text" onChange={this.handleInput} placeholder="Age" name="age" value= {this.state.age}/>
+        <input type="text" onChange={this.handleInput} placeholder="Email" name="email" value= {this.state.email}/>
+        <button onClick={this.sendFriend}>New Friend</button>
+      </div>
     );
   }
 }
@@ -43,5 +87,7 @@ function Friends({friend}) {
       </div>
   )
 }
+
+
 
 export default App;
