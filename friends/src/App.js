84b408e04102a26e776d0124a36d768f93e1bb3d
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-12 12:50:03 -0700

    added switch to app js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import FriendsList from "./Components/FriendsList";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import AddFriend from "./Components/AddFriend";
 import Friend from "./Components/Friend";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App-Friend">
-        <Route exact path="/" component={FriendsList} />
-        <Route path="/addfriend" component={AddFriend} />
-        <Route path="friends/:id" component={Friend} />
-      </div>
+      <Switch>
+        <div className="App-Friend">
+          <Route exact path="/" component={FriendsList} />
+          <Route path="/addfriend" component={AddFriend} />
+          <Route path="friends/:id" component={Friend} />
+        </div>
+      </Switch>
     );
   }
 }
