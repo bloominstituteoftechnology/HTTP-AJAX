2e74115b7c0e47f329f84676d0b1abafa22eb0b3
Velsu <riftmage50@gmail.com>
2018-04-13 01:30:13 +0200

    Some issues came up, trying to fix them

--- friends/src/components/FriendPage.js
+++ friends/src/components/FriendPage.js
@@ -1,17 +1,20 @@
-import React from "react";
+// import React from "react";
+// import { Link } from "react-router-dom";
 
-const FriendPage = props => {
-  return (
-    <div className="showFriend">
-      {" "}
-      <ul>
-        <li className="friend"> {props.location.state.name}</li>
-        <li className="friend"> {props.location.state.age}</li>
-        <li className="friend"> {props.location.state.email}</li>
-        <button onClik={props.history.goBack}>Go Back </button>
-      </ul>{" "}
-    </div>
-  );
-};
+// const FriendPage = props => {
+//   return (
+//     <div className="showFriend">
+//       {" "}
+//       <ul>
+//         <li className="friend"> {props.location.state.name}</li>
+//         <li className="friend"> {props.location.state.age}</li>
+//         <li className="friend"> {props.location.state.email}</li>
+//         <Link to="/">
+//           <button className="innerButton">Go Back </button>
+//         </Link>
+//       </ul>{" "}
+//     </div>
+//   );
+// };
 
-export default FriendPage;
+// export default FriendPage;
