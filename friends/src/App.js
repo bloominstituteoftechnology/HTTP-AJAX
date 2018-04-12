6bf8fea367581aa65df0c72181b6d07ee085c1ae
Paul Fang <paulfang@pauls-mbp.home>
2018-04-12 14:23:00 -0700

    added form and button for information

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import FriendsList from './Components/FriendsList'
+import Friends from './Components/Friends.js';
+import FriendsList from './Components/FriendsList.js';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Friends</h1>
-        </header>
-        <FriendsList />
+     <div className="App">
+      <header className="App-header">
+        <h1>My friends</h1>
+      </header>
+     <Friends />
+     <FriendsList />
       </div>
     );
   }
