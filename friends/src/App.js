95302a08a31665b24766eadfbd4354cd259173d1
Justin Hong <jhong2009@gmail.com>
2018-04-12 12:11:46 -0700

    added some comments

--- friends/src/App.js
+++ friends/src/App.js
@@ -6,27 +6,29 @@ import FriendList from "./components/FriendList";
 import FriendCard from "./components/FriendCard";
 
 export default class App extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     friends: []
-  //   };
-  // }
+	// constructor() {
+	//   super();
+	//   this.state = {
+	//     friends: []
+	//   };
+	// }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Friends List</h1>
-        </header>
-        {/* Routes Here */}
-        <Switch>
-          <Route exact path="/" component={FriendList} />
-          <Route path="/friend/:id" component={FriendCard} />
-          <Route component={() => <h1>Invalid! URL GO BACK HOME</h1>} />
-        </Switch>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Friends List</h1>
+				</header>
+				{/* Routes Here */}
+				<Switch>
+					<Route exact path="/" component={FriendList} />
+					<Route path="/friend/:id" component={FriendCard} />
+					<Route
+						component={() => <h1>Invalid! URL GO BACK HOME</h1>}
+					/>
+				</Switch>
+			</div>
+		);
+	}
 }
