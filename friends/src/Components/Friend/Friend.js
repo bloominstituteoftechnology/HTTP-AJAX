95b4d6b695ea9e4ad0b810aab8c46adf2534a0fe
Nedu Robert <nedumrobert@gmail.com>
2018-04-12 22:23:43 +0100

    completed the Friend component and styled it

--- friends/src/Components/Friend/Friend.js
+++ friends/src/Components/Friend/Friend.js
@@ -1,24 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Card, CardTitle, CardText, Container, Row, Col } from 'reactstrap';
+import './Friend.css';
 
-const Friend = (props) => {
-    console.log('props', props.match.params.id);
-    console.log('friends', props.friend);
-    return <div>hi</div>
-    // const {id, name, age, email} = props.friend;
-    // return <Container>
-    //     <Row className="friendsRow">
-    //       {props.friends.map(friend => {
-    //         return <Link key={friend.id} to={`/friends/${friend.id}`} className="friendCard">
-    //             <Col sm="12">
-    //               <Card body>
-    //                 <CardTitle>Name: {friend.name}</CardTitle>
-    //                 <CardText>Email: {friend.email}</CardText>
-    //               </Card>
-    //             </Col>
-    //           </Link>;
-    //       })}
-    //     </Row>
-    //   </Container>;
+class Friend extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            friend: null
+        };
+    }
+
+    componentDidMount() {
+        const { id } = this.props.match.params;
+        this.getFriend(id);
+    }
+
+    getFriend = id => {
+        axios.get(`http://localhost:5000/friends`)
+            .then(res => {
+                this.setState({ friend: res.data[id - 1]})
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    render() {
+        if(!this.state.friend) {
+            return <div>
+                <Container className="friendContainer">
+                  <Row className="friendsRow">
+                    <Col sm="12">
+                      <Card body>
+                        <CardTitle>No Friend found!</CardTitle>
+                      </Card>
+                    </Col>
+                  </Row>
+                </Container>
+              </div>;
+        }
+        const { id, name, age, email } = this.state.friend;
+        return <div>
+            <Container className="friendContainer">
+              <Row className="friendsRow">
+                <Col sm="12">
+                <Card body>
+                    <CardTitle>Name: {name}</CardTitle>
+                    <CardText>ID: {id}</CardText>
+                    <CardText>Age: {age}</CardText>
+                    <CardText>Email: {email}</CardText>
+                </Card>
+                </Col>
+              </Row>
+            </Container>
+          </div>;
+    }
 }
 
 export default Friend;
