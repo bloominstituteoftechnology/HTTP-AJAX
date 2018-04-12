76a3fae0dfb61f6eec43865d6d14346949438931
sadrak105 <sadrak10@gmail.com>
2018-04-12 17:41:00 -0500

    changes

--- friends/src/components/Friend/Friend.jsx
+++ friends/src/components/Friend/Friend.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { makeDelete } from '../../utils'
 
-
+const del = makeDelete("http://localhost:5000")
 
 
 const Friend = ({ name, email, age, id }) => (
@@ -12,7 +12,7 @@ const Friend = ({ name, email, age, id }) => (
     <p className="Friend--email">{email}</p>
     <img className="Friend--avi" src={`https://api.adorable.io/avatars/200/${email}.png`}></img>
     <button className="Friend--delete" onClick={() => {
-
+      del("/friends", {id})
     }} >X</button>
   </div>
 )
