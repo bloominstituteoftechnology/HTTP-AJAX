9899a50e1d5a0a380696bd7367e5f9fda0990370
Nedu Robert <nedumrobert@gmail.com>
2018-04-12 14:04:55 +0100

    completed friends component

--- friends/src/Components/Friends/Friends.js
+++ friends/src/Components/Friends/Friends.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, CardTitle, CardText, Container, Row, Col} from 'reactstrap';
+import './Friends.css';
+import FriendForm from '../FriendForm/FriendForm';
 
 const Friends = props => {
-    return (
-        <div>
-            <div>
-                {props.friends.map(friend => {
-                return <div key={friend.id}>
-                    <h1>{friend.name}</h1>    
-                </div>})}
-            </div>
-        </div>
-    )
+    console.log(props);
+    return <div>
+        <h1>Friends List</h1>
+        <FriendForm handleNewFriend={props.handleNewFriend} addFriend={props.addFriend} state={props.state} />
+        <Container className="FriendsContainer">
+          <Row>
+            {props.friends.map(friend => {
+              return <Link key={friend.id} to={`/friends/${friend.id}`} className="FriendCard">
+                  <Col sm="12" md="6">
+                    <Card body>
+                      <CardTitle>Name: {friend.name}</CardTitle>
+                      <CardText>Email: {friend.email}</CardText>
+                    </Card>
+                  </Col>
+                </Link>;
+            })}
+          </Row>
+        </Container>
+      </div>;
 }
 
 export default Friends;
