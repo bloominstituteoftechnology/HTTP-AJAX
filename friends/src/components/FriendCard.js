a9206a7e33c2a483c3b11895ed9ed4828729e907
Justin Hong <jhong2009@gmail.com>
2018-04-11 19:40:14 -0700

    created FriendCard component

---
+++ friends/src/components/FriendCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class FriendCard extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		};
+	}
+	
+	render() {
+		return ();
+	}	
+}
