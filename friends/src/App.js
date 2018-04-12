700ab45e3ca5b92370cdbe34a454760bfb312edf
BUDDHAPLEX <ericandrade@gmail.com>
2018-04-12 16:21:22 -0400

    finished assignment. stretch goals next.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,37 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import FriendsList from './FriendsList';
-import axios from 'axios';
+import React, { Component } from "react";
+import Friend from "./Friend";
+import Home from "./Home";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
-  constructor () {
-    super();
-    this.state = {
-      friends : []
-    }
-  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <FriendsList />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/friends/" component={Friend} />
+        </Switch>
       </div>
     );
   }
-  componentDidMount() {
-    axios
-    .get('http://localhost:5000/friends')
-    .then(response => {
-      this.setState(() => ({ friends: response.data }));
-    })
-    .catch(error => {
-      console.error('Server Error', error);
-    });
-  }
 }
-
 export default App;
--- friends/src/FriendsList.js
+++ friends/src/FriendsList.js
@@ -1,10 +1,17 @@
-import React from 'react';
-
-const FriendsList = () => {
-    return (
-        <p>Hello</p>
-    )
-}
-
+import React, { Component } from "react";
+const FriendsList = props => {
+  return (
+    <div>
+      {props.friendProp.friends.map(friend => {
+        return (
+          <div className="cards">
+            <div>Name:{friend.name}</div>
+            <div>Age: {friend.age}</div>
+            <div>Email:{friend.email}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 export default FriendsList;
-
