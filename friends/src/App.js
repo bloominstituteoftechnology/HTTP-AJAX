08c0a4dd7ded081be9ce7d1211611728454dff3d
Mark Wong <fmarkwong@gmail.com>
2018-04-11 17:39:42 -0700

    AddFriendForm Component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Button, Form, FormGroup, Label, Input, FormFeedback, FormText, Col } from 'reactstrap';
 import axios from 'axios';
 
 import FriendsList from './components/FriendsList';
+import AddFriendForm from './components/AddFriendForm';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      friends: [],
-      newFriend: {
-        name: '',
-        age: 0,
-        email: ''
-      }
+      friends: []
     };
   }
 
@@ -30,61 +24,16 @@ class App extends Component {
       })
   }
 
-  handleAddFriendInput = e => {
-    let newFriendData = { [e.target.name]: e.target.value };
-    this.setState({ newFriend: {...this.state.newFriend, ...newFriendData} })
+  updateFriendsList = updatedFriendsList => {
+    this.setState({ friends: updatedFriendsList});
   }
 
-  handleSubmitAddFriend = e => {
-    e.preventDefault();
-    axios.post('http://localhost:5000/friends', this.state.newFriend)
-      .then(response => {
-        this.setState({ friends: response.data });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
   render() {
     return (
       <Router>
         <div className="App">
           <Route exact path="/" render={ () => <FriendsList {...this.state}/>}/>
-          <br />
-          <h3>Add a friend!</h3>
-          <Form>
-            <FormGroup row>
-              <Label sm={2}>Name</Label>
-              <Col>
-                <Input
-                  type="text"
-                  name="name"
-                  onChange={this.handleAddFriendInput}
-                />
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label sm={2}>Age</Label>
-                <Col>
-                  <Input
-                    type="number"
-                    name="age"
-                    onChange={this.handleAddFriendInput}
-                  />
-                </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label sm={2}>Email</Label>
-                <Col>
-                  <Input
-                    type="email"
-                    name="email"
-                    onChange={this.handleAddFriendInput}
-                  />
-                </Col>
-            </FormGroup>
-        </Form>
-        <Button color="primary" onClick={this.handleSubmitAddFriend}>Add New Friend</Button>
+          <AddFriendForm updateFriendsList={this.updateFriendsList}/>
         </div>
       </Router>
     );
---
+++ friends/src/components/AddFriendForm.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
+
+class AddFriendForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newFriend: {
+        name: '',
+        age: 0,
+        email: ''
+      }
+    };
+  }
+
+  handleAddFriendInput = e => {
+    let newFriendData = { [e.target.name]: e.target.value };
+    this.setState({ newFriend: {...this.state.newFriend, ...newFriendData} })
+  }
+
+  handleSubmitAddFriend = e => {
+    axios.post('http://localhost:5000/friends', this.state.newFriend)
+      .then(response => {
+        this.props.updateFriendsList(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+      <h3>Add a friend!</h3>
+      <Form>
+        <FormGroup row>
+          <Label sm={2}>Name</Label>
+          <Col>
+            <Input
+              type="text"
+              name="name"
+              onChange={this.handleAddFriendInput}
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label sm={2}>Age</Label>
+            <Col>
+              <Input
+                type="number"
+                name="age"
+                onChange={this.handleAddFriendInput}
+              />
+            </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label sm={2}>Email</Label>
+            <Col>
+              <Input
+                type="email"
+                name="email"
+                onChange={this.handleAddFriendInput}
+              />
+            </Col>
+        </FormGroup>
+    </Form>
+    <Button color="primary" onClick={this.handleSubmitAddFriend}>Add New Friend</Button>
+    </div>
+    );
+  }
+}
+export default AddFriendForm;
