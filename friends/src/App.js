0324219471fd6a1e10d23f54d3c7653ddcd467a1
Jonathan Greene <greeneselectronics@gmail.com>
2018-04-13 09:16:12 -0600

    Couldn't figure out the routing and didn't have internet last night

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 import logo from './logo.svg';
 import axios from 'axios';
+import Friends from './Friends/Friends';
+import FriendCard from './Friends/FriendCard';
+
 import './App.css';
 
 class App extends Component {
@@ -10,7 +14,7 @@ class App extends Component {
        name: '',
        age: '',
        email: '',
-      friendsList: [],
+      updatedFriend: [],
      
     };    
     // name: '';
@@ -27,7 +31,7 @@ class App extends Component {
     axios.get('http://localhost:5000/friends')
     .then(response => {
       // console.log(response.data);
-      this.setState({friendsList: response.data})
+      this.setState({updatedFriend: response.data})
       
     })
     .catch(err => {
@@ -37,7 +41,7 @@ class App extends Component {
 
   handleNameInput = e => {
     this.setState({ [e.target.name]: e.target.value});
-    console.log(this.state.name)
+    //console.log(this.state.name)
   };
   // // handleAgeInput = e => {
   // //   this.setState({ [e.target.age]: e.target.value});
@@ -52,26 +56,101 @@ class App extends Component {
     const newFriend = {name: this.state.name, age: this.state.age, email: this.state.email};
     axios.post('http://localhost:5000/friends', newFriend)
     .then(saveFriend => {
-      console.log(saveFriend);
+      this.getFriends();
+      
+      //console.log(saveFriend);
     })
     .catch(err => {
       console.log(err);
     });
   };
 
-  updateFriendData = () => {
-    const FriendtoUpdate = {name: this.state.name, age: this.state.age, email: this.state.email};
-    axios.post('http://localhost:5000/friends', FriendtoUpdate)
-    .then(updateFriend => {
-      console.log(updateFriend);
+  updateFriendData = friendId => {
+    //console.log('This is running' + friendId + this.state.name)
+    // const newFriend = {name: this.state.name, age: this.state.age, email: this.state.email};
+    // newFriend.map((friend) => {
+    //   // {friend}
+    //   const {name, age, email } = friend;
+    //   //console.log(name);
+    //   //return <h3> Name is: {name} Age is: {age} Email is: {email} </h3> ;
+    //   console.log(friend + friendId);
+    // })
+    this.state.updatedFriend.map((friend) => {
+      //console.log(friend.name + this.state.name); // this is the array of friends
+       //if (this.state.name !== '') {
+     //friend.name = this.state.name;
+     console.log(friend.name + 'this is work' + this.state.name);
+     if (this.state.name !== '') {
+      friend.name = this.state.name;
+    }
+    if (this.state.age !== '') {
+      friend.age = this.state.age;
+    }
+    if (this.state.email !== '') {
+      friend.email = this.state.email;
+    }
+     axios.put(`http://localhost:5000/friends/${friendId}`, {
+       //key : value
+       name: friend.name, age: friend.age, email: friend.email
+     })
+      //console.log(friend)
+      .then(Friend => {
+        console.log(Friend);
+         //Friend.name = this.state.name;
+        this.getFriends();
+      })
     })
-    .catch(err => {
-      console.log(err);
-    });
   };
+     // }
+  //   // if (this.state.age !== '') {
+  //   //   friend.age = this.state.age;
+  //   // }
+  //   // if (this.state.email !== '') {
+  //   //   friend.email = this.state.email;
+  //   // }
+  //   console.log(friend + friendId); // calling object object
+     //axios.put(`http://localhost:5000/friends/${friendId}` )
+     
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
+  // })
+    
+ 
+     // calling object object
+    //const updatedFriend = {};
+     
+  //     // {friend}
+  //     //const {name, age, email } = friend;
+  //     //console.log(name);
+  //     //return <h3> Name is: {name} Age is: {age} Email is: {email} </h3> ;
+  //   //const updatedFriend = {};
+    
+  //   // if (this.state.name !== '') {
+  //   //   friend.name = this.state.name;
+  //   // }
+  //   // if (this.state.age !== '') {
+  //   //   friend.age = this.state.age;
+  //   // }
+  //   // if (this.state.email !== '') {
+  //   //   friend.email = this.state.email;
+  //   // }
+  //   console.log(friend + friendId); // calling object object
+  //   axios.put(`http://localhost:5000/friends/${friendId}`)
+  //   .then(Friend => {
+      
+  //     this.getFriends();
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
+  // })
+  
+  
 
   deleteFriendData = friendId => {
-    console.log(friendId);
+    //console.log(friendId);
     axios.delete(`http://localhost:5000/friends/${friendId}`)
     .then(deleteFriend => {
       this.getFriends();
@@ -87,32 +166,31 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Friends are</h1>
+          <h1 className="App-title">Friends</h1>
         </header>
-        <h3> Enter a new friend below using Name, Age, and Email! </h3>
-        {console.log(this.state)}
+        <h3> Ente a new friend below or update an existing one,  </h3>
+        <h3> using Name, Age, and Email! </h3>
+        {/* {console.log(this.state)} */}
         <input type="text" onChange={this.handleNameInput} placeholder= 'Name' name='name' value={this.state.name} />
         <input type="number" onChange={this.handleNameInput} placeholder= 'Age' name='age' value={this.state.age} />
         <input type="text" onChange={this.handleNameInput} placeholder= 'Email' name='email' value={this.state.email} />
         <button name="button" type="submit" onClick={this.saveFriendData}> Submit </button>
-        <button name="button" type="submit" onClick={this.updateFriendData}> Update </button>
-        {this.state.friendsList.map(friend => {
+        <div>
+        {/* <Route exact path="/" component={Friends} />
+        <Route path="/FriendCard" component={FriendCard} /> */}
+        </div>
+        <h2 class='MyFriends'> My Friends Are </h2>
+        {this.state.updatedFriend.map(friend => {
+          const {name, age, email } = friend;
           return (
             <div key={friend.id}>
-            <h3>{friend.id}</h3>
-            <button onClick={() => this.deleteFriendData(friend.id)}> delete friend </button>
+              <h3> Name: {name} Age: {age} Email: {email} </h3> ;
+              <button class= 'delete' onClick={() => this.deleteFriendData(friend.id)}> delete friend </button>
+              <button name="button" type="submit" onClick={() => this.updateFriendData(friend.id)}> Update </button>
             </div>
           )
         })}
-        {/* <button name="button" type="submit" onClick={this.deleteFriendData}> Delete </button> */}
-        <div> My Friends are {this.state.friendsList.map((friend) => {
-            // {friend}
-            const {name, age, email } = friend;
-            //console.log(name);
-            return <h3> Name is: {name} Age is: {age} Email is: {email} </h3> ;
-          })}</div>
-        </div>
+      </div>
     );
   }
 }
