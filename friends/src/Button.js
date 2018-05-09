12c61e411340efb801b91b29c00d6b7528e9ec74
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-09 18:20:27 -0400

    Created stateless function for Button props

---
+++ friends/src/Button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Button = (props) => {
+    return(
+        <div>
+            <button>Submit</button>
+        </div>
+    );
+}
+
+export default Button;
