b50cfe6e40b4e90c408babcccdb82cf118da90c0
Matthias Bakken <matthias.bakken@gmail.com>
2018-05-10 03:56:09 +0700

    added a list of friends with using axios get then catch

--- friends/src/Friends.js
+++ friends/src/Friends.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import axios from 'axios';
 
 class Friends extends Component {
     state = {
         friends: []
     }
+
+    componentDidMount() {
+        axios.get("http://localhost:5000/friends")
+            .then(response => {
+                this.setState({friends: response.data})
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
     render() {
         return (
             <div className="Friends">
-                HTTP/AJAX BITCHES!!!
+                {this.state.friends.map(friend => {
+                    return (
+                        <div key={friend.id}>{friend.name}</div>
+                    )
+                })}
             </div>
         );
     }
