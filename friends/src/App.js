3199b8501c370c87541912129bd42b892074fd65
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-09 17:02:00 -0400

    -created class componet for base

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Friends from './Friends';
 
 class App extends Component {
-  render() {
+  constructor(){
+    super();
+    this.state = {
+      friends: []
+    }
+  };
+
+  componentWillMount(){
+    axios.get('http://localhost:5000/friends')
+    .then(repsone => {this.setState(() => ({friends:Response.data}))})
+    .catch(err => {console.error(err)});
+  }
+
+  render(){ 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Friends friendsData={this.state.friends}/>
       </div>
-    );
+    )
   }
 }
 
---
+++ friends/src/Friends.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friends = () => {
+    return(
+        <div>
+            Hello
+        </div>
+    )
+}
+
+export default Friends;
