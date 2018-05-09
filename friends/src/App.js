65d159a44e5996ba145659cc195f037a8e641ac3
Chris Honda <christopher.honda92@gmail.com>
2018-05-09 14:02:30 -0700

    Created submit form for new friends in InputForm.js and mounted component in App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Friends from './component/Friends';
+import InputForm from './component/InputForm';
 import axios from 'axios';
 
 class App extends Component {
@@ -20,11 +21,21 @@ class App extends Component {
       .catch(err => console.log(err))
   }
 
+  handleSubmit = (newFriend) => {
+    axios.post("http://localhost:5000/friends", newFriend)
+    .then(res => {
+      const friends = res.data;
+      this.setState({ friends });
+    })
+    .catch(err => console.log(err))
+  }
+
   render() {
       const { friends } = this.state
       console.log(friends)
       return (
         <div>
+          <InputForm handleSubmit={this.handleSubmit} />
           <Friends friends={friends} />
         </div>
       );
---
+++ friends/src/component/InputForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class InputForm extends Component {
+    state = {
+        name: '',
+        age: '',
+        email: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = () => {
+        this.props.handleSubmit(this.state)
+    }
+
+    render() {
+        const {
+            name,
+            age,
+            email
+        } = this.state
+    return (
+        <div>
+            <input type='text' name='name' value={name} onChange={e => this.handleChange(e) } />
+            <input type='text' name='age' value={age} onChange={e => this.handleChange(e) } />
+            <input type='text' name='email' value={email} onChange={e => this.handleChange(e) } />
+            <button onClick={this.handleSubmit}>Submit</button>
+        </div>
+    )
+    }
+}
+
+export default InputForm;
