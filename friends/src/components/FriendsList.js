48caa719e5181c0c613da5d6a812a026aa93bc6a
elitecamper <jggarza5@gmail.com>
2018-05-09 14:54:47 -0500

    got friends data to display

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+const FriendsList = props => {
+
+    return ( 
+        <div>
+            {props.friends.map((friend) => {
+                return (
+                    <div>
+                        <div>{friend.name}</div>
+                        <div>{friend.age}</div>
+                        <div>{friend.email}</div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
+
+export default FriendsList;
