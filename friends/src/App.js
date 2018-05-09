b090c600f160d584c7c90c2baf19ec6a01875ebc
kammagik <kammagik@yahoo.com>
2018-05-09 14:47:30 -0700

    added friends component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Friends from './component/Friends.js';
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+//something goes here
+  }
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <Friends friends={this.state.friends} />
       </div>
     );
   }
---
+++ friends/src/component/Friends.js
@@ -0,0 +1,16 @@
+import react {Component} from 'react';
+
+const Friends => { //used example from react-friends project
+  return (
+    <div>
+    {friends.map(friend=> {
+      return (
+        <div key={friend.name + friend.age + friend.email}>
+        {friend.name} {friend.age} {friend.email}
+        </div>
+      );
+    })}
+    </div>
+  );
+};
+export default Friends;
