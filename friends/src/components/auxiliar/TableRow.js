075d1c66ef386dde9680b624e3697bdba2497da9
jesuarva <jean.ariza@jesuarva.com>
2018-05-09 22:26:18 +0200

    DISPLAY the Friends List.

--- friends/src/components/auxiliar/TableRow.js
+++ friends/src/components/auxiliar/TableRow.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const TableRow = ({...props}) => {
-    console.log(props);
-    // console.log(id,name,age,email);
+    const {id,name,age,email} = props;
+    console.log(id,name,age,email);
     return (
         <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <th scope="row">{id}</th>
+            <td>{name}</td>
+            <td>{age}</td>
+            <td>{email}</td>
         </tr>
     );
 };
