a04c9defc734e6684bb9674c56e5d387f37fbd11
Chris Honda <christopher.honda92@gmail.com>
2018-05-09 13:18:34 -0700

    Created component to display friends

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Friends from './component/Friends';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get("http://localhost:5000/friends")
+      .then(res => {
+        const friends = res.data;
+        this.setState({ friends });
+      })
+      .catch(err => console.log(err))
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      const { friends } = this.state
+      console.log(friends)
+      return (
+        <div>
+          <Friends friends={friends} />
+        </div>
+      );
   }
 }
 
