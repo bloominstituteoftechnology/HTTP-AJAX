ee480ed4dce262fa445d376b787425be47cff34e
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-09 15:06:25 -0700

    friend data save button works

--- friends/src/Friends.js
+++ friends/src/Friends.js
@@ -1,48 +1,81 @@
 import React, {Component} from "react";
-import { defaultCipherList } from "constants";
 import axios from 'axios';
 
 class Friends extends Component {
-    state = {
-        friends: [],
-        name: '',
-        age: '',
-        email: ''
-    }
+  state = {
+    friends: [],
+    name: "",
+    age: "",
+    email: ""
+  };
 
-    componentDidMount() {
-        axios.get("http://localhost:5000/friends")
-            .then(response => {
-                this.setState({friends: response.data})
-            })
-            .catch(err => {
-                console.log(err)
-            }) 
-    }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(response => {
+        this.setState({ friends: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-    handleTextInput = event => {
-        this.setState({
-          [event.target.name]: event.target.value,
-          [event.target.age]: event.target.value,
-          [event.target.email]: event.target.value
+  handleTextInput = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+      [event.target.age]: event.target.value,
+      [event.target.email]: event.target.value
+    });
+  };
+
+  saveFriendsData = () => {
+    const friendData = { name: this.state.name, age: this.state.age, email: this.state.email }
+    axios.post("http://localhost:5000/friends", friendData)
+        .then(savedFriend => {
+            console.log(savedFriend)
+        })
+        .catch(err => {
+            console.log(err)
         });
+        this.setState({ name: '', age: '', email: ''})
     }
 
-    render() {
-        console.log(this.state.friends)
-        return <div>
-            <div>
-              {this.state.friends.map(friend => {
-                return <div key={friend.id}>{friend.name}</div>;
-              })}
-            </div>
-            <input type="text" placeholder="Name" name="name" value={this.state.name} onChange={this.handleTextInput}/>
-            <input type="text" placeholder="Age" name="age" value={this.state.age} onChange={this.handleTextInput}/>
-            <input type="text" placeholder="Email" name="email" value={this.state.email} onChange={this.handleTextInput}/>
+
+  render() {
+    return (
+      <div>
+        <div>
+          {this.state.friends.map(friend => {
+            return <div key={friend.id}>{friend.name}</div>;
+          })}
+        </div>
+        <form>
+            <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleTextInput}
+            />
+            <input
+                type="text"
+                placeholder="Age"
+                name="age"
+                value={this.state.age}
+                onChange={this.handleTextInput}
+            />
+            <input
+                type="text"
+                placeholder="Email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleTextInput}
+            />
             <button onClick={this.saveFriendsData}>Save Friend</button>
-          </div>;
-    }
-
+        </form>
+      </div>
+    );
+  }
 }
 
 
