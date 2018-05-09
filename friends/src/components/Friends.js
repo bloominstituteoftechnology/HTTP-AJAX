37cd970213f596c2a0f2f60e54dfebb4a8df6176
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-09 16:28:54 -0500

    added handleFormChange to connect input onChange and values on state. Also added handleSubmitFriend to post new friend. both functions are functioning as intended

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -9,7 +9,11 @@ class Friends extends Component {
     constructor() {
         super();
         this.state = {
-            friends: []
+            friends: [], 
+            newFriend: {},
+            name: "",
+            age: "",
+            email: ""
         }
     }
 
@@ -18,6 +22,25 @@ class Friends extends Component {
             .then((response => this.setState({friends: response.data})))
             .catch((err) => console.log(err))
     }
+
+
+    handleFormChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value})
+    // this.setState({newFriend: newFriend.name = this.state.newFriend.name})
+        const newObj = {
+            name: this.state.name,
+            age: this.state.age,
+            email: this.state.email
+        }
+        let newFriend = Object.assign({}, newObj) 
+        this.setState({newFriend})
+        // console.log(this.state.newFriend)
+    }
+
+    handleSubmitFriend = () => {
+        axios.post('http://localhost:5000/friends', this.state.newFriend)      
+    }
+
     render() {
         const headerStyle = {
             color: "blue",
@@ -28,13 +51,23 @@ class Friends extends Component {
             fontWeight: "bold"
         }
 
+        const formStyle = {
+            margin: "20px"
+        }
+
         return (
             <div>
-                        <div style={headerStyle}>
-                            <div>Name</div>
-                            <div>Age</div>
-                            <div>Email</div>
-                        </div>
+                <form style={formStyle} >
+                    <input onChange={this.handleFormChange} type="text" placeholder="Name" name="name" value={this.state.name} style={formStyle}></input>
+                    <input onChange={this.handleFormChange} type="text" placeholder="Age" name="age" value={this.state.age} style={formStyle}></input>
+                    <input onChange={this.handleFormChange} type="text" placeholder="Email" name="email" value={this.state.email} style={formStyle}></input>
+                    <button onClick={this.handleSubmitFriend}>Submit</button>
+                </form>
+                <div style={headerStyle}>
+                    <div>Name</div>
+                    <div>Age</div>
+                    <div>Email</div>
+                </div>
                 {this.state.friends.map((friend, index) => { return <Friend key={friend.name + index} friend={friend}/>})}
             </div>
         )
