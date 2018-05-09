925845d6a120b13e75fa0e73274fedc7a01cb9ac
Anne Courtney <annecourtney_1@yahoo.com>
2018-05-09 17:55:09 -0500

    the form is now working and updating the data in the object!

--- friends/src/friendscomponent.js
+++ friends/src/friendscomponent.js
@@ -7,9 +7,27 @@ class Friends extends Component {
     constructor() {
     super();
     this.state = {
+        name: '',
+        age: '',
+        email: '',
         friends: []
     }
 }
+handleFormType = event => {
+    this.setState({ [event.target.name]: event.target.value });
+};
+
+handleFormSubmit = () => {
+    let friends = this.state.friends;
+    let friend = { 
+        name: this.state.name, 
+        age: this.state.age,
+        email: this.state.email,
+        id: this.state.friends.length + 1};
+    friends.push(friend);
+    this.setState({ friends: friends, friend: "" });
+};
+
 componentDidMount() {
     axios.get('http://localhost:5000/friends')
     .then(response => this.setState({ friends: response.data }))
@@ -29,17 +47,17 @@ componentDidMount() {
         <Form>
         <FormGroup>
           <Label for="exampleName">Name</Label>
-          <Input type="name" name="name" id="exampleEmail" placeholder="your name here" />
+          <Input name="name" onChange={ this.handleFormType } id="exampleEmail" placeholder="your name here" />
         </FormGroup>
         <FormGroup>
           <Label for="exampleage">Age</Label>
-          <Input type="age" name="age" id="exampleage" placeholder="your age here" />
+          <Input name="age" onChange={ this.handleFormType } id="exampleage" placeholder="your age here" />
         </FormGroup>
         <FormGroup>
           <Label for="exampleemail">Email</Label>
-          <Input type="email" name="email" id="exampleemail" placeholder="your email here" />
+          <Input name="email" onChange={ this.handleFormType } id="exampleemail" placeholder="your email here" />
         </FormGroup>
-        <Button>Submit</Button>
+        <Button onClick={this.handleFormSubmit}>Submit</Button>
       </Form>
         </div>
     )}
