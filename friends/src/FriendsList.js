29efcefa29f32d4a0bee630078c5c78d0ef24e1b
kelly <kelfro@fastmail.com>
2018-05-09 14:26:07 -0700

    finally got my input boxes to display placeholders
    user can enter text into name, but not age or email. I should probably also add an Add button but I'm still working on things.

--- friends/src/FriendsList.js
+++ friends/src/FriendsList.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
 
 class FriendsList extends Component {
-  constructor() {
-    super ();
+  constructor(props) {
+    super (props);
     this.state = {
       friends: [],
       name: "",
@@ -27,10 +28,51 @@ componentDidMount() {
 }
 
 
+  handleInput = event => {
+    this.setState({ [event.target.name]: [event.target.value] });
+  }
+
+  saveFriend = () => {
+    const friend = {
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email
+    };
+    axios
+      .post(`http://localhost:5000/friends`, friend)
+      .then(listUpdate => {
+	console.log(listUpdate);
+      })
+      .catch(err => {
+	console.log(err);
+      });
+    this.setState({ name: "", age: "", email: ""});
+  }
+ 
 render() {
   return(
     <div>
-    </div>
+      <h1>Friends</h1>
+      <input onChange={this.handleInput} placeholder="name" name="name" value={this.state.name} />
+      <input onChange={this.handleInput} placeholder="age" age="age" value={this.state.age} />
+      <input onChange={this.handleInput} placeholder="email" email="email" value={this.state.email} />
+
+      <div>
+	<Container>
+	   {this.state.friends.map(friend => {
+	     const { name,age,email } = friend;
+	     return(
+	<Row>
+	  <Col xs="6" sm="4"><h3>Name: {name} </h3></Col>
+	  <Col xs="6" sm="4"><h4>Age: {age} </h4></Col>
+	  <Col sm="4"><h4>Email: {email} </h4></Col>
+	</Row>
+	     );
+	   })}
+        </Container>
+      </div>
+
+   </div>
     
   );
 }
