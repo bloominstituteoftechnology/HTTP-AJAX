ae914978e6081af359d0c514c4b6179f4c76f05c
maxwashington <mnwashington18@gmail.com>
2018-05-09 15:50:59 -0500

    -Importing Route and Link -Adding Links and Routes to Friends List and to add a new friend

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import Friends from "./components/Friends";
 import NewFriends from "./components/NewFriends";
+import {Link, Route} from "react-router-dom"
 
 class App extends Component {
   constructor(props) {
@@ -22,7 +23,15 @@ class App extends Component {
   };
 
   render() {
-    return <div />;
+    const { friends } = this.state;
+    return (
+      <div>
+        <Link to='/'><button>Home</button></Link>
+        <Link to='/newbff'><button>New Friends</button></Link>
+        <Route path='/' render=''/>
+        <Route path='newbff' render=''/>
+      </div>
+    )
   }
 }
 
