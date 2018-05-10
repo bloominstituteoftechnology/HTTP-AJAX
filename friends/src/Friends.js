6ef8769bbf116710b30773670ec04d00eabe2696
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-10 19:25:20 -0400

    commented out friend component

--- friends/src/Friends.js
+++ friends/src/Friends.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
 
-const Friends = (props) => {
-    return(
-        <div>
-            {props.friendData.map((friendStuff, index) => (
-                <div key={index}>
-                    {friendStuff.name}    
-                </div>
-            )
-            )}
-        </div>
-    )
-}
+// const Friends = (props) => {
+//     return(
+//         <div>
+//             {props.friendData.map((friendStuff, index) => (
+//                 <ul key={index}>
+//                     <li>
+//                         {friendStuff.name}
+//                     </li>
+//                     <li>
+//                         {friendStuff.age}
+//                     </li>
+//                     <li>
+//                         {friendStuff.email}
+//                     </li>       
+//                 </ul>
+//             )
+//             )}
+//         </div>
+//     )
+// }
 
 export default Friends;
