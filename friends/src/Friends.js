ecc0c0f45ff9e1bd30610bb81b6271ef7bc54e2a
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-10 14:19:15 -0700

    delete button added for friends

--- friends/src/Friends.js
+++ friends/src/Friends.js
@@ -6,6 +6,7 @@ import Friend from "./Friend";
 import "./Friends.css";
 
 class Friends extends Component {
+
   state = {
     friends: [],
     name: "",
@@ -49,13 +50,25 @@ class Friends extends Component {
         this.setState({ name: '', age: '', email: ''})
     }
 
+    handleDeleteFriend = (id) => {
+        axios
+          .delete(`http://localhost:5000/friends/${id}`)
+          .then(savedFriend => {
+            this.updateAll();
+
+          })
+          .catch(err => {
+            console.log(err);
+          });
+
+    }
+
 
   render() {
     return <div>
         <div class="container">
         <h5 class="section-title h1 mt-sm-4">FRIENDS</h5>
         <div class="row">
-            {/* <section id="team" class="pb-5"> */}
             {this.state.friends.map(friend => {
             return <div class="col-sm-4 friend-card mt-sm-4">
                 <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
@@ -64,7 +77,7 @@ class Friends extends Component {
                       <div class="card">
                         <div class="card-body text-center">
                           <p>
-                            <img class=" img-fluid" src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_03.png" alt="card image" />
+                            <img class=" img-fluid" src={`https://picsum.photos/200/300?image=${friend.id + 40}`} />
                           </p>
                           <h4 class="card-title">{friend.name}</h4>
                         </div>
@@ -73,7 +86,10 @@ class Friends extends Component {
                     <div class="backside">
                       <div class="card">
                         <div class="card-body text-center">
-                         <p class="card-title">
+                          {/* <p>
+                            <img class="img-fluid" src={`https://picsum.photos/200/300?image=${friend.id + 40}`} />
+                          </p> */}
+                          <p class="card-title">
                             Name: {friend.name}
                           </p>
                           <p class="card-title">
@@ -89,20 +105,20 @@ class Friends extends Component {
                               Visit {friend.name}'s card
                             </p>
                           </Link>
-                          <ul class="list-inline">
+                          <ul class="list-inline mt-sm-2">
                             <li class="list-inline-item">
                               <a class="social-icon text-xs-center" target="_blank" href="#">
-                                <i class="fa fa-facebook"></i>
+                                <i class="fa fa-facebook" />
                               </a>
                             </li>
                             <li class="list-inline-item">
                               <a class="social-icon text-xs-center" target="_blank" href="#">
-                                <i class="fa fa-twitter"></i>
+                                <i class="fa fa-twitter" />
                               </a>
                             </li>
                             <li class="list-inline-item">
                               <a class="social-icon text-xs-center" target="_blank" href="#">
-                                <i class="fa fa-skype"></i>
+                                <i class="fa fa-skype" />
                               </a>
                             </li>
                             <li class="list-inline-item">
@@ -111,6 +127,7 @@ class Friends extends Component {
                               </a>
                             </li>
                           </ul>
+                          <button onClick={() => this.handleDeleteFriend(friend.id)}>x</button>
                         </div>
                       </div>
                     </div>
@@ -118,7 +135,6 @@ class Friends extends Component {
                 </div>
               </div>;
             })}
-            {/* </section> */}
         </div>
     </div>
 
