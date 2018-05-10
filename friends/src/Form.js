45447095832a2dd4ccd0fc3a87391c43e17d7a4e
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-10 19:25:06 -0400

    commented out form

--- friends/src/Form.js
+++ friends/src/Form.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 
-const Form = (props) => {
-        return (
-        <form>
-            <input className="form" type="text" placeholder="Name" />
-            <input className="form" type="text" placeholder="Age" />
-            <input className="form" type="text" placeholder="Email" />   
-        </form>    
-    );
-}
+// const Form = props => {
 
+
+
+//   render(
+//       <form onSubmit={this.handleSubmit}>
+//         <input type="text" name="name" placeholder="Name" value="this.state.name" onChange="this.handleNameChange"/>
+//       </form>
+//   ) 
+// }
 export default Form;
