7c0f10db6c693d262cfb23baeae875e2874b6972
CMVNK <Kopecky12112@gmail.com>
2018-05-09 18:58:17 -0700

    can't seem to figure out how to get data to populate on submit.
    This is what I have for now. Will try again soon.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import FriendDetails from './FriendDetails';
-import Form from './Form';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Welcome my Friends!</h1>
-        <Form />
         <FriendDetails />
       </div>
     );
--- friends/src/FriendDetails.js
+++ friends/src/FriendDetails.js
@@ -22,12 +22,46 @@ export default class FriendsList extends Component {
     getUsers = () => {
       axios.get('http://localhost:5000/friends')
         .then(response => {
-          this.setState({ friends: response.data })
+            this.setState({ friends: response.data, name: '', age: '', email: ''})
         })
         .catch(err => console.log(err))
     } 
+
+    postUsers = (event) => {
+        const {name, age, email} = this.state
+        axios.post('http://localhost:5000/friends', {name, age, email})
+        .then(response =>{
+            return this.setState({friends: response.data});
+        })
+        .catch(error =>{
+            return `There's an error!`;
+        })
+        
+    }
+
+    handleChange = event => {
+       { this.setState({ [event.target.name]: event.target.value })};
+      }; 
+    
+
     render() {
         return(
+            <div>
+            <div>
+                <h3>Join my Slack Channel!</h3>
+                <form onChange={this.HandleChange}>
+                    <label>Name:
+                        <input type="text" onChange={this.handleChange}  />
+                    </label>
+                    <label>Age:
+                    <input type="number" onChange={this.handleChange}   />
+                    </label>
+                    <label>Email:
+                    <input type="email" onChange={this.handleChange}  />
+                    </label>
+                    <button onClick = {this.postUsers}>Submit Info</button>
+                </form>
+            </div>
             <div>
                 
                 <ul className = "friends">
@@ -46,6 +80,7 @@ export default class FriendsList extends Component {
                     })}
                     
                 </ul>
+            </div>
             </div>
 
             
