ba07ae144408cd3e8aecce86dffe96d42138fd76
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-10 19:24:55 -0400

    added button to handle update

--- friends/src/Button.js
+++ friends/src/Button.js
@@ -1,26 +1,10 @@
-import React, { Component } from 'react';
+import React  from 'react';
 
-class Button extends Component {
-    constructor(props){
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        this.setState(prevState => ({
-         
-        }));
-      }
-
-      render() {
-        console.log("button was clicked");
-        return (
-          <button onClick={this.handleClick}>
-            Submit{}
-          </button>
-        );
-      }
-    
+const Button = () => {
+  return (
+    <div>
+      <input type="text" name="update" placeholder="update" />
+    </div>
+  )
 }
-
 export default Button;
