4191610f70dc407bbc89faaf30bf5fe4359b7747
Lavell Burton <taylormade800@gmail.com>
2018-05-09 20:06:38 -0700

    Made components folder
    added class in friends.js file

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Example from './Component/friendform';
+import Axios from 'axios';
+
+
+
+
 
 class App extends Component {
   render() {
@@ -11,7 +15,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Example />
+        
         
       </div>
     );
