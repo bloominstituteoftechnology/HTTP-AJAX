332b2c3ccdd054477ca140c9ad93d39064d7d399
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-05-10 14:57:22 -0700

    refactored most stuff back into app.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Friends from './Friends.js';
+import { Link } from 'react-router-dom';
+// import Friends from './Friends.js';
+// import Form from './Form.js';
+import Button from './Button.js';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      friends: []
+      friends: [],
+      name: '',
+      age: '',
+      email: ''
     }
   }
   componentDidMount() {
@@ -18,14 +24,65 @@ class App extends Component {
       })
       .catch(err => {
         console.error("error:", err);
+      });    
+  }
+  handleChange = (e) => { this.setState({[e.target.name]: e.target.value}) };
+  buttonSubmit = () => {
+    const { name, age, email } = this.state
+    axios
+      .post('http://localhost:5000/friends', { name, age, email })
+      .then( (response) => {
+        this.setState({ friends: response.data, name: '', age: '', email: ''})
+      })
+      .catch(err => {
+        console.error("error:", err);
       });
-      
+  }
+  buttonUpdate = (id) => {
+    axios
+      .put(`http://localhost:5000/friends/${id}`, {name:'', age: '', email: ''})
+      .then( (response) => {
+        this.setState({ friends: response.data })
+      })
+      .catch(err => {
+        console.error("error:", err);
+      });
+  }
+  buttonDelete = (id) => {
+    axios
+      .delete(`http://localhost:5000/friends/${id}`)
+      .then( (response) => {
+        this.setState({ friends: response.data })
+      })
+      .catch(err => {
+        console.error("error:", err);
+      });
   }
   render() {
-    // console.log("friends:", this.state.friends);
     return (
       <div className="App">
-        <Friends friendData={this.state.friends}/>
+        {/* <Friends friendData={this.state.friends}/>
+        <Form /> */}
+          <Link to="/"><Button /></Link>
+          <input type="text" placeholder=" Name " name="name" onChange={this.handleChange} value={this.state.name} />
+          <input type="number" placeholder=" Age " name="age" onChange={this.handleChange} value={this.state.age} />
+          <input type="text" placeholder=" Email " name="email" onChange={this.handleChange} value={this.state.email} />
+          <button onClick={this.buttonSubmit}>Submit</button>
+          {/* <button onClick={this.buttonUpdate}>Update</button>
+          <button onClick={this.buttonDelete}>Delete</button> */}
+          <ul className="friends">
+            {this.state.friends.map(friend => {
+              return (
+                  <li key={friend.id} className="friend">
+                        <p>Name: {friend.name}</p>
+                        <p>Age: {friend.age}</p>
+                        <p>Email: {friend.email}</p>
+                        <button onClick={()=>this.buttonDelete(friend.id)}>Delete</button>
+                        <button onClick={()=>this.buttonSubmit(friend.id)}>Update</button>
+                  </li>
+              )
+            })}
+          </ul>
       </div>
     );
   }
--- friends/src/Form.js
+++ friends/src/Form.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Form = () => {
     return(
         <div>
-            <input className="form" type="text" placeholder="Enter Name Here" />
-            <input className="form" type="text" placeholder="Enter Age Here" />
-            <input className="form" type="text" placeholder="Enter Email Here" />
+            <input type="text" placeholder=" Name " name="name" onChange={this.handleChange} value={this.state.name} />
+            <input type="number" placeholder=" Age " name="age" onChange={this.handleChange} value={this.state.age} />
+            <input type="text" placeholder=" Email " name="email" onChange={this.handleChange} value={this.state.email} />
         </div>
-    )
+    )             
 }
 
 export default Form;
