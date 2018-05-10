5fb68f0ffae52670475e93a99a64bc2c7121525a
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-10 10:21:39 -0400

    -added button functionality

--- friends/src/Button.js
+++ friends/src/Button.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
-const Button = (props) => {
-    return(
-        <div>
-            <button>Submit</button>
-        </div>
-    );
+class Button extends Component {
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+         
+        }));
+      }
+
+      render() {
+        console.log("button was clicked");
+        return (
+          <button onClick={this.handleClick}>
+            Submit{}
+          </button>
+        );
+      }
+    
 }
 
 export default Button;
