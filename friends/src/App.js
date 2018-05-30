f3b5dabfdbe8c71da58d045406c1de3ef30be1a6
Kevin Mei <derrick.mei.168@gmail.com>
2018-05-30 16:19:33 -0500

    installed eslint and prettier!!!

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import FriendsList from './FriendsList.js'
-
+import FriendsList from './FriendsList.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <FriendsList />
-      </div>
-    );
-  }
-}
+    render () {
+        return (
+            <div className="App">
+                <FriendsList />
+            </div>
+        );
+    };
+};
 
 export default App;
