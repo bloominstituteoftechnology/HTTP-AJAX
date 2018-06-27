1ea6933cbf4fba2372a389793c119f2f7e05a072
Ciara Burkett <ciaraburkett@gmail.com>
2018-06-27 18:21:42 -0400

    Created a component to display the list of friends

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FriendsList from './Components/FriendsList';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -10,9 +18,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <main>
+          <FriendsList />
+        </main>
       </div>
     );
   }
---
+++ friends/src/Components/FriendsList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import axios from 'axios';
+
+class FriendsList extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/friends`)
+      .then(response => {
+        const friends = response.data;
+        this.setState({friends});
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.friends.map(friend => <li key={friend.id}>{friend.name}</li>)}
+      </ul>
+    );
+  }
+}
+
+export default FriendsList;
