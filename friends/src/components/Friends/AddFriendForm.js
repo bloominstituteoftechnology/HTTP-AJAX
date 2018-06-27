46007acf9c4e7a88c5fc61e8ea7c3a9a4ac8b5aa
Kevin <brackkevin@gmail.com>
2018-06-27 16:04:59 -0500

    Added an AddFriendForm component and added it to FriendsList

---
+++ friends/src/components/Friends/AddFriendForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddFriendForm = props => {
+  return <div>ADD FRIEND FORM</div>;
+};
+
+export default AddFriendForm;
--- friends/src/components/Friends/FriendsList.js
+++ friends/src/components/Friends/FriendsList.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Friends from "./Friends";
+import AddFriendForm from "./AddFriendForm";
 
 const FriendsList = props => {
   return (
     <div>
       [FRIENDS LIST]
       <Friends data={props.data} />
+      <AddFriendForm />
     </div>
   );
 };
