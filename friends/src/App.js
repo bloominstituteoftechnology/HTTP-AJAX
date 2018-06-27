4ff0823e02e6961a34f8e4669b5e70f9c42af185
Simon <Simon_Freelance@hotmail.com>
2018-06-27 16:32:40 -0400

    Implemented a way to add friend's name and id for the Post request.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,29 +1,53 @@
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
-import FriendsList from './components/FriendsList/FriendsList';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import FriendsList from "./components/FriendsList/FriendsList";
+import FriendsForm from "./components/FriendsList/FriendsForm";
 class App extends Component {
   constructor() {
     super();
     this.state = {
       friendsData: [],
+      friend: ""
     };
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/friends')
+    axios
+      .get("http://localhost:5000/friends")
+      .then(response => {
+        this.setState({ friendsData: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  changeFriendName = e => {
+    this.setState({ friend: e.target.value });
+  };
+
+  addFriend = e => {
+    const friend = { name: this.state.friend }
+    axios
+    .post('http://localhost:5000/friends', friend)
     .then(response => {
-      this.setState({ friendsData: response.data });
+      console.log('Post', response.data);
+      this.setState({ friendsData: response.data, friend: ''});
     })
-    .catch(err => {
-      console.log(err);
-    });
+    .catch(error => console.log(error));
   }
   render() {
     return (
       <div className="App">
-        <h1 className = 'title'>Friend List</h1>
-        <FriendsList friendsData = {this.state.friendsData} />
+
+        <h1 className="title">Friend List</h1>
+
+        <FriendsForm 
+        changeNameHandler={this.changeFriendName} 
+        addFriendHandler = {this.addFriend} />
+
+        <FriendsList friendsData={this.state.friendsData} />
       </div>
     );
   }
