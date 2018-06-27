68badb4b96dd79d670f547720245bb077e1f24ae
Simon <Simon_Freelance@hotmail.com>
2018-06-27 16:45:35 -0400

    Form can receive name,age, and email plus a button for a Post request

--- friends/src/components/FriendsList/FriendsForm.js
+++ friends/src/components/FriendsList/FriendsForm.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const FriendsForm = (props) => {
-    return (
-        <input type = 'text' 
-        onChange = {props.changeNameHandler} 
-        onKeyDown = {e => {
-            if(e.keyCode === 13) {
-                props.addFriendHandler();
-            }
-        }} />
-    );
-}
- 
+const FriendsForm = props => {
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+      }}
+    >
+      <label>name</label>
+
+      <input type="text" name="name" onChange={props.changeInfoHandler} />
+      <label>age</label>
+
+      <input type="text" name="age" onChange={props.changeInfoHandler} />
+      <label>email</label>
+      <input type="text" name="email" onChange={props.changeInfoHandler} />
+
+      <button onClick = {props.addFriendHandler}>Submit</button>
+    </form>
+  );
+};
+
 export default FriendsForm;
