a5947adb96bc037e8f53e262cc0e4dddab9d513a
Mariana Cerda <raymond_grc@yahoo.com>
2018-06-27 15:58:23 -0500

    Add bootstrap cards displaying user info

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { Route } from "react-router-dom";
+import FriendList from "./components/FriendList"
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+    .then((response) => this.setState({data: response.data}))
+    .catch(error => console.log(error))
+  }
+
+  handleDeleteFriend = (id) => {
+    axios.delete(`http://localhost:5000/friends/${id}`)
+        .then(response => this.setState({data: response.data}))
+        .catch(error => console.log(error))
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route exact path="/" render={props => <FriendList {...props} data={this.state.data}/>}/>
       </div>
     );
   }
