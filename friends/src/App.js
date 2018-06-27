0a502f41474f8df87a88a4eb24c1dcc8cf92174e
Simon <Simon_Freelance@hotmail.com>
2018-06-27 16:02:38 -0400

    Made a GET request to get friends data when component mounts.

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,6 +3,22 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friendsData: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+    .then(response => {
+      this.setState({ friendsData: response.data });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
   render() {
     return (
       <div className="App">
