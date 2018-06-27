04b165cbb4ef12f6fea51082b9842227dc92dc7c
Simon <Simon_Freelance@hotmail.com>
2018-06-27 18:52:14 -0400

    Restyled the list and each Friend component

--- friends/src/components/FriendsList/Friend.js
+++ friends/src/components/FriendsList/Friend.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import "bulma/css/bulma.css";
 
-const Friend = (props) => {
-    return <li>{props.name}</li>
-}
- 
+const Card = styled.div.attrs({
+    className: 'card'
+})`
+margin: 1rem;
+min-width: 100px;
+`;
+const Friend = props => {
+  return (
+      <Card>
+          <h2 className = "title is-6">{props.name}</h2>
+          <p className = 'subtitle is-6'>Email: {props.email}</p>
+          <p className = 'subtitle is-6'>Age: {props.age}</p>
+    </Card>
+  );
+};
+
 export default Friend;
--- friends/src/components/FriendsList/FriendsList.js
+++ friends/src/components/FriendsList/FriendsList.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import Friend from './Friend';
-const FriendsList = (props) => {
-    return (
-        <ul>
-        {props.friendsData.map(friend => {
-            return <Friend key = {friend.id} name = {friend.name} />
-        })}
-        </ul>
-    );
-}
- 
+import React from "react";
+import Friend from "./Friend";
+import styled from "styled-components";
+import "bulma/css/bulma.css";
+const FriendListSection = styled.section.attrs({
+  className: "section"
+})``;
+const Container = styled.div.attrs({
+  className: "container"
+})`
+display: flex;
+flex-direction: column-reverse;
+`;
+const FriendsList = props => {
+  return (
+    <FriendListSection>
+      <Container>
+          {props.friendsData.map(friend => {
+            return (
+                <Friend key={friend.id} name={friend.name} age = {friend.age} email = {friend.email} />);
+          })}
+      </Container>
+    </FriendListSection>
+  );
+};
+
 export default FriendsList;
