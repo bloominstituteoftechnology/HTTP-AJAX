e63a995c0876893b76ec8880cfa4e654c8ab5fd2
CLN <cln@Sags-MacBook-Pro.local>
2018-06-27 19:50:53 -0400

    Get friends names posting, .

--- friends/src/App.js
+++ friends/src/App.js
@@ -7,23 +7,52 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      friendsData: []
+      friendsData: [],
+      friend: ''
     }
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/friends').then(response => {
-      console.log(response);
-      this.setState({friendsData: response.data })
+    axios
+    .get('http://localhost:5000/friends')
+    .then(response => {
+      console.log("Get Response",response);
+      this.setState({friendsData: response.data });
+    })
+    .catch(err => {
+      console.log(err);
     });
   }
+  
+  handleSubmitFriend = () => {
+    const friend ={ name: this.state.friend };
+    axios
+    .post('http://localhost:5000/friends',  friend )
+    .then(response => {
+      console.log("POST Response",response);
+    this.setState({ friendsData: response.data, friend: '' });
+    })
+    .catch(error => console.log(error));
+  };
+
+  handleNameChange = e => {
+    this.setState({ friend: e.target.value });
+  };
+
+  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        
+        <input 
+        type= 'text' 
+        placeholder="friend name" 
+        onChange={this.handleNameChange} 
+        name="friend"
+        value={this.state.friend}
+          />
+          <button onClick={this.handleSubmitFriend}>Submit Friend</button>
         <FriendsList friends={this.state.friendsData} />
         
       </div>
