92b2f47df8fc2b92f6a6076db442260e8894ce32
Kevin <brackkevin@gmail.com>
2018-06-27 16:33:15 -0500

    Added router and created links for main pages in FriendsList.js

--- friends/src/components/Friends/FriendsList.js
+++ friends/src/components/Friends/FriendsList.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Friends from "./Friends";
 import AddFriendForm from "./AddFriendForm";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const FriendsList = props => {
   return (
     <div>
       [FRIENDS LIST]
+      <ul>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/friends"}>Friends</Link>
+        </li>
+      </ul>
       <Friends data={props.data} />
       <AddFriendForm />
     </div>
