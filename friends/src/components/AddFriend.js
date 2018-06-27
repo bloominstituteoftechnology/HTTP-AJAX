fdc72365875df017a000af1bba8b711890765473
Mariana Cerda <raymond_grc@yahoo.com>
2018-06-27 17:12:02 -0500

    Refactor AddFriend into class, prepare for event handler, push to server

--- friends/src/components/AddFriend.js
+++ friends/src/components/AddFriend.js
@@ -1,26 +1,37 @@
 import React from "react";
 
-const AddFriend = props => {
-  return(
-    <div>
-      <form className="add-friend">
-        <div className="form-group">
-          <label htmlFor="InputName">Name</label>
-          <input type="text" className="form-control" id="InputName" placeholder="Name"></input>
-        </div>
-        <div className="form-group">
-          <label htmlFor="InputAge">Age</label>
-          <input type="text" className="form-control" id="InputAge" placeholder="Age"></input>
-        </div>
-        <div className="form-group">
-          <label htmlFor="InputEmail">Email</label>
-          <input type="email" className="form-control" id="InputEmail" placeholder="Email"></input>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
+class AddFriend extends React.Component {
+  constructor(props){
+    super(props);
+        super(props);
+        this.state = {
+            name: "",
+            age: "",
+            email: ""
+        }
+      }
+      render(){
+        return(
+          <div>
+            <form className="add-friend">
+              <div className="form-group">
+                <label htmlFor="InputName">Name</label>
+                <input type="text" className="form-control" id="InputName" placeholder="Name"></input>
+              </div>
+              <div className="form-group">
+                <label htmlFor="InputAge">Age</label>
+                <input type="text" className="form-control" id="InputAge" placeholder="Age"></input>
+              </div>
+              <div className="form-group">
+                <label htmlFor="InputEmail">Email</label>
+                <input type="email" className="form-control" id="InputEmail" placeholder="Email"></input>
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
 
-    </div>
-  )
-}
+          </div>
+        )
+      }
+    }
 
 export default AddFriend;
