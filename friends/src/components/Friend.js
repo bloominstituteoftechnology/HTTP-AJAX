9efe270b8a74d46a75e77ee94ebeef09e53644c0
Nicky Chen <nicky.chen0@gmail.com>
2018-06-27 14:52:42 -0700

    added some styling

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './FriendsList.css';
 
-const Friend = () => {
-
+const Friend = props => {
+    return (
+        <div className="friend">
+            <p>Name: {props.friend.name}</p>
+            <p>Age: {props.friend.age}</p>
+            <p>Email: {props.friend.email}</p>
+        </div>
+    )
 }
 
 export default Friend;
---
+++ friends/src/components/NewFriendForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewFriendForm = () => {
+    return (
+        <form>
+          <input placeholder="add new friends" />
+          <button>Add Friends</button>
+        </form>
+    )
+}
+
+export default NewFriendForm;
