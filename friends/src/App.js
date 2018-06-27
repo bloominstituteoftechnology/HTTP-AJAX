a2ff428b5757f531d262bb81238c2a9d7336a823
Kevin Tena <kevten09@gmail.com>
2018-06-27 17:33:15 -0500

    Pulled list of friends from server and displayed it on render

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      friendsData: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+        .get("http://localhost:5000/friends")
+        .then(response => {
+          this.setState({friendsData:response.data})
+        })
+        .catch(err => {
+          console.log(err);
+        });
+
+  }
   render() {
     return (<div>
+      <h1> My Friends </h1>
+      {this.state.friendsData.map(friend => {
+        return <div key={friend.id}> {friend.name} </div>
+      })}
       Hello
-    </div>)
+    </div>);
   }
 }
 
