9ad0780620efeb5aff60ab6d1482a7b31c5f14ea
Simon <Simon_Freelance@hotmail.com>
2018-06-27 15:33:54 -0400

    Imported BrowserRouter

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router } from 'react-router-dom';
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 registerServiceWorker();
