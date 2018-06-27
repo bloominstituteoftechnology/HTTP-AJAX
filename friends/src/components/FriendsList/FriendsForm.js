5b7d15c3b2f1af6035e4ac1931b79cfbe2456ed3
Simon <Simon_Freelance@hotmail.com>
2018-06-27 18:50:48 -0400

    Restyled the Friends Form

--- friends/src/components/FriendsList/FriendsForm.js
+++ friends/src/components/FriendsList/FriendsForm.js
@@ -1,23 +1,71 @@
 import React from "react";
+import styled from "styled-components";
+import "bulma/css/bulma.css";
+const FormSection = styled.section.attrs({
+  className: "section"
+})``;
+const Container = styled.div.attrs({
+  className: "container"
+})``;
+const Field = styled.div.attrs({
+  className: "field"
+})``;
+const Control = styled.div.attrs({
+  className: "control"
+})``;
 
+const SubmitBtn = styled.button.attrs({
+  className: "button is-info"
+})``;
 const FriendsForm = props => {
   return (
-    <form
-      onSubmit={e => {
-        e.preventDefault();
-      }}
-    >
-      <label>name</label>
-
-      <input type="text" name="name" onChange={props.changeInfoHandler} />
-      <label>age</label>
-
-      <input type="text" name="age" onChange={props.changeInfoHandler} />
-      <label>email</label>
-      <input type="text" name="email" onChange={props.changeInfoHandler} />
-
-      <button onClick = {props.addFriendHandler}>Submit</button>
-    </form>
+    <FormSection>
+      <Container>
+        {/* Name */}
+        <Field>
+          <label className="label">Name</label>
+          <Control>
+            <input
+              className="input"
+              type="text"
+              placeholder="Text input"
+              name="name"
+              id="name"
+              onChange={props.changeInfoHandler}
+            />
+          </Control>
+        </Field>
+        {/* Age */}
+        <Field>
+          <label className="label">Age</label>
+          <Control>
+            <input
+              className="input"
+              type="text"
+              placeholder="Text input"
+              name="age"
+              id="age"
+              onChange={props.changeInfoHandler}
+            />
+          </Control>
+        </Field>
+        {/* Email */}
+        <Field>
+          <label className="label">Email</label>
+          <Control>
+            <input
+              className="input"
+              type="text"
+              placeholder="Text input"
+              name="email"
+              id="email"
+              onChange={props.changeInfoHandler}
+            />
+          </Control>
+        </Field>
+        <SubmitBtn onClick={props.addFriendHandler}>Submit</SubmitBtn>
+      </Container>
+    </FormSection>
   );
 };
 
