cf672924b03d22191915b8314d1d3e41e02c6f84
Kevin <brackkevin@gmail.com>
2018-06-27 15:21:50 -0500

    Imported axios response data to App.state

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,33 @@
 import React, { Component, Fragment } from "react";
 import FriendsList from "./components/Friends/FriendsList";
+import axios from "axios";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friendsData: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(response => {
+        console.log("GIT RESPONSE", response);
+        this.setState({ friendsData: response.data });
+      })
+      .catch(err => {
+        console.log("ERROR", err);
+      });
+  }
+
   render() {
-    return <FriendsList />;
+    return (
+      <Fragment>
+        <FriendsList />
+      </Fragment>
+    );
   }
 }
 
