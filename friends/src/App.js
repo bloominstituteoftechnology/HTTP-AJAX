28b7713e723441315734f7d75cfac75d742e5d3e
DirupT <iamcooled@gmail.com>
2018-06-27 13:35:39 -0500

    added friendsList/friend component / displayed data to screen

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import FriendsList from './components/FriendsList';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      friendsData: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => this.setState({ friendsData: response.data }))
+      .catch(err => console.log(err))
+  }
   render() {
     return (
       <div className="App">
-       
+        <FriendsList friends={this.state.friendsData}/>
       </div>
     );
   }
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Friend from './Friend';
+
+const FriendsList = props => {
+    return (
+        <div>
+            {props.friends.map(friend => <Friend friend={friend}/>)}
+        </div>
+    );
+}
+
+export default FriendsList;
