2e4bc56db9f9616100f5b4a49375f57762e6d140
chainchompa <chainchompa@gmail.com>
2018-06-27 17:12:04 -0400

    initial commit

---
+++ friends/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import friends from '.../server'
+
+class App extends Component {
+  render() {
+    component() {
+      super();
+      this.state = {
+
+      }
+    }
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
