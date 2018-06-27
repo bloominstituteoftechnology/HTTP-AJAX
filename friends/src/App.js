c9bd9a8aafec7763411056b116b161ad1dd98a0f
Tyson Gushiken <tysonism@gmail.com>
2018-06-27 13:06:12 -0700

    Create FriendsList component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import FriendsList from './components/Friends/FriendsList';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+    .then(response => {
+      this.setState({friends: response.data});
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
   render() {
     return (
       <div id="app">
+        <FriendsList friends={this.state.friends} />
       </div>
     );
   }
--- friends/src/components/Friends/FriendsList.js
+++ friends/src/components/Friends/FriendsList.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class FriendsList extends Component {
-  render() {
-    return (
-      <div id="friendsList">
-      </div>
-    );
-  }
+const FriendsList = props => {
+  return(
+    <ul id="friendsList">
+      {props.friends.map(friend => {
+        return(
+          <li key={`${friend.id}-${Math.random()}`}>
+            <div className="friend__name">{friend.name}</div>
+            <div className="friend__age">{friend.age}</div>
+            <div className="friend__email">{friend.email}</div>
+          </li>
+        )})
+      }
+    </ul>
+  )
 }
 
 export default FriendsList;
