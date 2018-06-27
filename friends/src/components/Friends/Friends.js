a7b146fba4b895308968bda5ec1953ff3719f74d
Kevin <brackkevin@gmail.com>
2018-06-27 17:45:55 -0500

    Troubleshooting with Ryan and the gang

--- friends/src/components/Friends/Friends.js
+++ friends/src/components/Friends/Friends.js
@@ -2,6 +2,7 @@ import React from "react";
 import Friend from "./Friend";
 
 const Friends = props => {
+  console.log("Friends Props", props);
   return (
     <div>
       {props.data.map(friend => {
--- friends/src/components/Friends/FriendsList.js
+++ friends/src/components/Friends/FriendsList.js
@@ -4,23 +4,43 @@ import AddFriendForm from "./AddFriendForm";
 import { Route, Link } from "react-router-dom";
 import LandingPage from "../LandingPage/LandingPage";
 
-const FriendsList = props => {
-  return (
-    <div>
-      [FRIENDS LIST]
-      <ul>
-        <li>
-          <Link to={"/"}>Home</Link>
-        </li>
-        <li>
-          <Link to={"/friends"}>Friends</Link>
-        </li>
-      </ul>
-      <Route exact path={"/"} component={LandingPage} />
-      <Route path={"/friends"} component={Friends} />
-      <Route path={"/friends"} component={AddFriendForm} />
-    </div>
-  );
-};
+class FriendsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    console.log("CDM Props", this.props);
+    this.setState({ data: this.props.data });
+  }
+
+  render() {
+    return (
+      <div>
+        [FRIENDS LIST]
+        <ul>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/friends"}>Friends</Link>
+          </li>
+        </ul>
+        {/* <Friends data={props.data} /> */}
+        <Route exact path={"/"} component={LandingPage} />
+        <Route
+          path={"/friends"}
+          render={props => {
+            return <Friends data={this.state.data} {...props} />;
+          }}
+        />
+        <Route path={"/friends"} component={AddFriendForm} />
+      </div>
+    );
+  }
+}
 
 export default FriendsList;
