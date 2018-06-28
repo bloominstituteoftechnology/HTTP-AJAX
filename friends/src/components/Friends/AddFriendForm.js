47c0a9d78ec3dbafd256f1dfdc892dbbf586ab5c
Kevin <brackkevin@gmail.com>
2018-06-28 15:22:02 -0500

    Added name property to forms in AddFriendForm

--- friends/src/components/Friends/AddFriendForm.js
+++ friends/src/components/Friends/AddFriendForm.js
@@ -5,11 +5,11 @@ const AddFriendForm = props => {
     <form>
       <h3>Add a new friend</h3>
       <label htmlFor="name">Name</label>
-      <input type="text" id="name" placeholder="Name" />
+      <input type="text" id="name" name="name" placeholder="Name" />
       <label htmlFor="email">Email</label>
-      <input type="text" id="email" placeholder="Emial" />
+      <input type="text" id="email" name="email" placeholder="Emial" />
       <label htmlFor="age">Age</label>
-      <input type="text" id="age" placeholder="Age" />
+      <input type="text" id="age" name="age" placeholder="Age" />
       <button type="submit">SUBMIT</button>
     </form>
   );
