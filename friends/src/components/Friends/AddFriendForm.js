9d178cf0648f8cdc1ff1b11a78d26a94d46fe7ff
Kevin <brackkevin@gmail.com>
2018-06-28 15:00:10 -0500

    Added form placeholders

--- friends/src/components/Friends/AddFriendForm.js
+++ friends/src/components/Friends/AddFriendForm.js
@@ -3,12 +3,13 @@ import React from "react";
 const AddFriendForm = props => {
   return (
     <form>
+      <h3>Add a new friend</h3>
       <label htmlFor="name">Name</label>
-      <input type="text" id="name" />
+      <input type="text" id="name" placeholder="Name" />
       <label htmlFor="email">Email</label>
-      <input type="text" id="email" />
+      <input type="text" id="email" placeholder="Emial" />
       <label htmlFor="age">Age</label>
-      <input type="text" id="age" />
+      <input type="text" id="age" placeholder="Age" />
       <button type="submit">SUBMIT</button>
     </form>
   );
