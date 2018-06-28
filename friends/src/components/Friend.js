c9f7bc30698feb8cc899809fcebbf65e27cd2226
cmok4290 <cmok4290@gmail.com>
2018-06-27 22:27:45 -0400

    added edit and delete buttons

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -4,6 +4,8 @@ const Friend = props => {
     return (
         <Fragment>
             <p>{`${props.friend.id}: ${props.friend.name}, ${props.friend.age}, ${props.friend.email}`}</p>
+            <button>Edit</button>
+            <button>Delete</button>
         </Fragment>
     );
 }
