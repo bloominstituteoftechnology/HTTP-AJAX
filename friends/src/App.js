af229de80ae4c35bf878dfee30508f69260a7b7a
chainchompa <chainchompa@gmail.com>
2018-06-28 16:15:01 -0400

    installed react-router-dom

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
+import './index.css';
 import axios from 'axios';
-import FriendsList from "./FriendsList";
+import FriendsList from "./components/FriendsList";
+import FriendsForm from "./components/FriendsForm";
 
 class App extends Component {
   constructor() {
@@ -28,47 +29,16 @@ class App extends Component {
       })
   }
 
-  handleSubmitFriend = () => {
-    const friend = {
-      name: this.state.name,
-      age: this.state.age,
-      email: this.state.email
-    }
-    axios
-     .post("http://localhost:5000/friends", friend)
-     .then(response => {
-       this.setState({ friendsData: response.data, name: '' , age: '', email: ''});
-     })
-     .catch(error => console.log(error));
-  };
-
-  handleNameChange = event => {
-    this.setState({ name: event.target.value });
-  };
-
-  handleAgeChange = event => {
-    this.setState({ age: event.target.value });
-  };
-
-  handleEmailChange = event => {
-    this.setState({ email: event.target.value });
-  };
-
   render() {
     return (
       <div className="App">
       <div className="friends">
-        <h1>Friends</h1>
+        <h1>My Friends</h1>
       <FriendsList friends={this.state.friendsData} />
     </div>
     <div className="add-friends">
       <h2>Add Friend</h2>
-      <form>
-        <input type='text' value={this.state.name} onChange={this.handleNameChange} placeholder="Friends name" />
-        <input type='text' value={this.state.age} onChange={this.handleAgeChange} placeholder="Friends age" />
-        <input type='text' value={this.state.email} onChange={this.handleEmailChange} placeholder="Friends email" />
-        <button onClick={this.handleSubmitFriend}>Add Friend</button>
-      </form>
+      <FriendsForm className ='form' handleSetData={this.handleSetData}/>
       </div>
     </div>
     );
---
+++ friends/src/components/FriendsForm.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import axios from 'axios';
+
+class FriendsForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            age: ''
+        };
+    }
+
+    handleSubmitFriend = () => {
+      const friend = {
+        name: this.state.name,
+        age: this.state.age,
+        email: this.state.email
+      };
+        axios
+        .post('http://localhost:5000/friends', friend)
+          .then(response => {
+            this.props.handleSetData(response.data);
+            this.setState({ name: '', email: '', age: '' })
+          })
+          .catch(error => console.log(error));
+        };
+
+
+    handleNameChange = event => {
+      this.setState({ name: event.target.value });
+    };
+
+    handleAgeChange = event => {
+      this.setState({ age: event.target.value });
+    };
+
+    handleEmailChange = event => {
+      this.setState({ email: event.target.value });
+    };
+
+    render() {
+        return (
+            <div className="friends-form">
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Friends name"
+                    value={this.state.name}
+                    onChange={this.handleNameChange}
+                />
+                <input
+                    type="text"
+                    name="age"
+                    placeholder="Friends age"
+                    value={this.state.age}
+                    onChange={this.handleAgeChange}
+                />
+                <input
+                    type="text"
+                    name="email"
+                    placeholder="Friends email"
+                    value={this.state.email}
+                    onChange={this.handleEmailChange}
+                />
+                <button onClick={this.handleSubmitFriend}>Save</button>
+            </div>
+        )
+    }
+}
+
+export default FriendsForm;
