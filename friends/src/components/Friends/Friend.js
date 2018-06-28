7269d3bf89b34824f23000854a4155caaaf62c14
Kevin <brackkevin@gmail.com>
2018-06-28 14:30:57 -0500

    Updated Friend component to accept reformatted props

--- friends/src/components/Friends/Friend.js
+++ friends/src/components/Friends/Friend.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 const Friend = props => {
-  console.log(props);
+  // console.log(props);
   return (
     <div>
       <h3>{props.friend.name}</h3>
       <p>Age: {props.friend.age}</p>
       <p>Email: {props.friend.email}</p>
+      {/* <p>Age: {props.friend.age}</p>
+      <p>Email: {props.friend.email}</p> */}
     </div>
   );
 };
