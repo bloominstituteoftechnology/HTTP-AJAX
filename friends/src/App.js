083052c415d1b2dca517d6a129ac96467ad014d7
Mark Hong <m4rkh0ng@gmail.com>
2018-06-28 18:55:40 -0400

    thursday work

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
-import FriendList from './FriendList'; 
+import FriendList from './FriendList';
+import Friend from './Friend'; 
+import PropTypes from 'prop-types';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       friendsData: [],
-      friend: ''
+      friendName: '',
+      friendAge: '',
+      friendEmail: ''
     };
   }
 
@@ -22,40 +27,54 @@ class App extends Component {
    }
 
    
-   handleNameInput = e => {
-     this.setState({ friend: e.target.value });
-    }
+  handleFriendNameInput = e => {
+    this.setState({ friendName: e.target.value });
+  }
+
+  handleFriendAgeInput = e => {
+     this.setState({ friendAge: e.target.value });
+  }
+  
+  handleFriendEmailInput = e => {
+    this.setState({ friendEmail: e.target.value });
+  }
     // you wouldn't put a post in your lifecycle method unless you wanted your data to be created automatically
     
-  handleSubmitFriend = () => {
-    const friend = { name: this.state.friend };
+  handleSubmitFriend = (e) => {
+    e.preventDefault();
+    const friend = { name: this.state.friendName, age: this.state.friendAge, email: this.state.friendEmail };
     axios
       .post('http://localhost:5000/friends', friend)
       .then(response => {
         console.log("POST RESPONSE: ", response);
-        this.setState({ friendsData: response.data, friend: '' });
+        this.setState({ friendsData: response.data, friendName: '', friendAge: '', friendEmail: '' });
       })
       .catch(err => console.log(err));
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          Friends
-        </header>
-        <input 
-          type="text"
-          placeholder="Add friend"
-          onChange={this.handleNameInput}
-          name="friend"
-          value={this.state.friend}
-        />
-        <button onClick={this.handleSubmitFriend}>Add friend</button>
-        <FriendList friends={this.state.friendsData}/>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <Link className="add-friend-link" to="/add_friends">Click here to start adding friends</Link>
+          </header>
+          <Route path="/add_friends" render={props => <Friend 
+            props = {this.state}
+            handleFriendNameInput={this.handleFriendNameInput}
+            handleFriendAgeInput={this.handleFriendAgeInput}
+            handleFriendEmailInput={this.handleFriendEmailInput}
+            handleSubmitFriend={this.handleSubmitFriend}/>} 
+          />
+          <FriendList friends={this.state.friendsData}/>
+        </div>
     );
   }
+}
+
+App.propTypes = {
+  friendName: PropTypes.string,
+  friendAge: PropTypes.number,
+  friendEmail: PropTypes.string
 }
 
 export default App;
