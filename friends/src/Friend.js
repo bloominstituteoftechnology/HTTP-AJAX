9fe0ed019f581277832a583450d7b41d9ccd9ea2
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-06-27 18:14:50 -0700

    adds edit modal

--- friends/src/Friend.js
+++ friends/src/Friend.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, CardTitle, CardText, Button } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import styled from 'styled-components';
 
 const FriendCard = styled(Card)`
@@ -15,6 +16,12 @@ class Friend extends React.Component {
     };
   }
 
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal,
+    });
+  };
+
   handleDelClick = id => {
     console.log('clicked', id);
     this.props.deleteHandler(id);
@@ -22,36 +29,57 @@ class Friend extends React.Component {
 
   handleEditClick = id => {
     console.log('clicked', id);
+    this.setState({ modal: !this.state.modal });
     // this.props.deleteHandler(id);
   };
 
   render() {
     return (
-      <FriendCard>
-        <Card body style={{ backgroundColor: 'aliceblue' }}>
-          <CardTitle className="text-center">
-            {this.props.friend.name}
-          </CardTitle>
-          <CardText>Age: {this.props.friend.age}</CardText>
-          <CardText>email: {this.props.friend.email}</CardText>
-          <span>
-            <Button
-              style={{ width: '70px' }}
-              onClick={() => this.handleEditClick(this.props.friend.id)}
-              className="btn btn-outline-primary float-leftt"
-            >
-              edit
-            </Button>
-            <Button
-              style={{ width: '70px' }}
-              onClick={() => this.handleDelClick(this.props.friend.id)}
-              className="btn btn-outline-danger float-right"
-            >
-              delete
-            </Button>
-          </span>
-        </Card>
-      </FriendCard>
+      <div>
+        <FriendCard>
+          <Card body style={{ backgroundColor: 'aliceblue' }}>
+            <CardTitle className="text-center">
+              {this.props.friend.name}
+            </CardTitle>
+            <CardText>Age: {this.props.friend.age}</CardText>
+            <CardText>email: {this.props.friend.email}</CardText>
+            <span>
+              <Button
+                style={{ width: '70px' }}
+                onClick={() => this.handleEditClick(this.props.friend.id)}
+                className="btn btn-outline-primary float-leftt"
+              >
+                edit
+              </Button>
+              <Button
+                style={{ width: '70px' }}
+                onClick={() => this.handleDelClick(this.props.friend.id)}
+                className="btn btn-outline-danger float-right"
+              >
+                delete
+              </Button>
+            </span>
+          </Card>
+        </FriendCard>
+        <div>
+          <Modal isOpen={this.state.modal} toggle={this.toggle}>
+            <ModalHeader toggle={this.toggle}>Edit Friend</ModalHeader>
+            <ModalBody>
+              <p>Name: {this.props.friend.name}</p>
+              <p>Age: {this.props.friend.age}</p>
+              <p>email: {this.props.friend.email}</p>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={this.toggle}>
+                Do Something
+              </Button>{' '}
+              <Button color="secondary" onClick={this.toggle}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </Modal>
+        </div>
+      </div>
     );
   }
 }
