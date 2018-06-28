8778162c784b7eb26ab74884ee568ce7be3b10af
Mariana Cerda <raymond_grc@yahoo.com>
2018-06-28 15:42:25 -0500

    Add click handler and attemp post to server, not functional, not broken

--- friends/src/components/AddFriend.js
+++ friends/src/components/AddFriend.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 class AddFriend extends React.Component {
   constructor(props){
@@ -10,21 +11,28 @@ class AddFriend extends React.Component {
             email: ""
         }
       }
+      copyinfo = (event) => {this.setState({[event.target.name]: event.target.value})}
+      postinfo = (event) => {
+        event.preventDefault();
+        const {name, age, email} = this.state;
+        axios.post('http://localhost:5000/friends', {name, age, email})
+          .then(response => console.log(response))
+          .catch(error => console.log(`${error}`))}
       render(){
         return(
           <div>
             <form className="add-friend">
               <div className="form-group">
                 <label htmlFor="InputName">Name</label>
-                <input type="text" className="form-control" id="InputName" placeholder="Name"></input>
+                <input type="text" className="form-control" id="InputName" placeholder="Name" name="name" onChange={this.copyinfo}></input>
               </div>
               <div className="form-group">
                 <label htmlFor="InputAge">Age</label>
-                <input type="text" className="form-control" id="InputAge" placeholder="Age"></input>
+                <input type="text" className="form-control" id="InputAge" placeholder="Age" name="age" onChange={this.copyinfo}></input>
               </div>
               <div className="form-group">
                 <label htmlFor="InputEmail">Email</label>
-                <input type="email" className="form-control" id="InputEmail" placeholder="Email"></input>
+                <input type="email" className="form-control" id="InputEmail" placeholder="Email" name="email" onChange={this.copyinfo}></input>
               </div>
               <button type="submit" className="btn btn-primary">Submit</button>
             </form>
