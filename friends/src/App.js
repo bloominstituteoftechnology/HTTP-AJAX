afa6daaaa130731f6fcddf2d0563d6b9e7e147fb
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-06-28 16:10:49 -0400

    seperated components, created a return button in /addFriend

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import axios from 'axios';
 
 import FriendList from './Components/FriendList';
+import FriendForm from './Components/FriendForm';
 import './App.css';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
     super();
     this.state = {
       friends: [],
+      id: '',
       name: '',
       age: '',
       email: '',
@@ -20,51 +22,56 @@ class App extends Component {
   componentDidMount() {
     axios.get('http://localhost:5000/friends')
       .then(response => {
-        this.setState(() => ({ friends: response.data }));
+        this.setState(() => ({ friends: response.data, id: response.data.length }));
       })
       .catch(err => {
         console.error(err);
       });
   }
 
+  makeObject = () => {
+    const { id, name, age, email } = this.state;
+    const newObj = {
+      id: id,
+      name: name,
+      age: age,
+      email: email
+    }
+    return newObj;
+  }
+
   updateInput = e => {
+    console.log(this.state)
     this.setState({ [e.target.name]: e.target.value })
   }
 
-  emptyCheck = () => {
+  addInput = obj => {
+    // if (this.emptyCheck()) {  //ensures no empty inputs or invalid formats
+      const { newFriend } = this.state;
+      newFriend.push(obj);
+      this.setState({ newFriend, id: (this.state.id + 1), name: '', age: '', email: '' });
+    // }
+  };
+
+  emptyCheck = () => {  //this is meant to lock the add friend button when all fields are empty
     console.log(this.state)
     const name = this.state.name;
     const age = this.state.age;
     const email = this.state.email;
     let result = true;
-    
-    if (name.trim() === "") result = false;
-    else if (age.trim() === "" || age > 120) result = false;
-    else if (email.trim() === "") result = false;
-    else if (email.indexOf('@') < 0) result = false;
-    else if (email.indexOf('.com') < 0) result = false;
+
+    if (name.trim() === "") result = false;  //blank name
+    else if (age.trim() === "" || 13 > age > 120) result = false;   //reasonable age
+    else if (email.trim() === "") result = false;  //would have been messy on a single line
+    else if (email.indexOf('@') < 0) result = false;  //this will check email address
+    else if (email.indexOf('.com') < 0) result = false;  //for some minor format
     console.log(result)
     return result;
   }
 
-  addInput = e => {
-    e.preventDefault();
-    let id = this.state.friends.length + this.state.newFriend.length;
-    if (this.emptyCheck()){
-      const { newFriend } = this.state;
-      newFriend.push({
-        id: id,
-        name: this.state.name,
-        age: this.state.age,
-        email: this.state.email
-      });
-      this.setState({ newFriend, name: '', age: '', email: '' });
-    }
-  };
-
   saveInput = () => {
     const { newFriend } = this.state;
-    newFriend.forEach(friend=> {
+    newFriend.forEach(friend => {
       axios
         .post('http://localhost:5000/friends', friend)
         .then(response => {
@@ -79,19 +86,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <form onSubmit={this.addInput} >
-          Name:
-          <input className="input" name='name' value={this.state.name} onChange={this.updateInput} type="text" placeholder="Friend's Name Here" />
-          Age:
-          <input className="input" name='age' value={this.state.age} onChange={this.updateInput} type="number" placeholder="Friend's Age Here" />
-          E-mail:
-          <input className="input" name='email' value={this.state.email} onChange={this.updateInput} type="email" placeholder="Friend's Email Here" />
-        </form>
-        <button className="button button-add" onClick={this.addInput} > Add New Friend </button>
-        <button className="button button-save" onClick={this.saveInput} >Save New Friend(s)</button>
-        <Route path="/" 
-        render={(props) => <FriendList {...props} friends={this.state.friends} newFriend={this.state.newFriend} onClick={this.addInput} /> } 
-        />
+        <Route exact path="/"
+          render={(props) => <FriendList {...props} saveInput={this.saveInput} friends={this.state.friends} newFriend={this.state.newFriend} onClick={this.addInput} />
+        }/>      
+        <Route path="/addFriend"
+          render={(props) => <FriendForm addInput={this.addInput} id={this.state.id} updateInput={this.updateInput} />
+        }/>  
       </div>
     );
   }
---
+++ friends/src/Components/FriendForm.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class FriendForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            age: '',
+            email: '',
+        };
+    }
+
+    makeObject = () => {
+        const { name, age, email } = this.state;
+        const newObj = {
+            id: this.props.id,
+            name: name,
+            age: age,
+            email: email
+        }
+        this.setState({ name: '', age: '', email: ''});
+        return newObj;
+    }
+
+    updateInput = e => {
+        this.setState({ [e.target.name]: e.target.value})
+    }
+
+    render() {
+        return (
+            <div>
+                {console.log(this.props)}
+                <form onSubmit={this.addInput} >
+                    Name:
+                    <input className="input" name='name' value={this.state.name} onChange={this.updateInput} type="text" placeholder="Friend's Name Here" />
+                    Age:
+                    <input className="input" name='age' value={this.state.age} onChange={this.updateInput} type="number" placeholder="Friend's Age Here" />
+                    E-mail:
+                    <input className="input" name='email' value={this.state.email} onChange={this.updateInput} type="email" placeholder="Friend's Email Here" />
+                </form>
+                <button className="button button-add" onClick={() => this.props.addInput(this.makeObject())} > Add New Friend </button>
+                <Link to="/" >
+                    <button className="button button-return"> Return </button>
+                </Link>
+            </div>
+        );
+    }
+};
+
+export default FriendForm;
