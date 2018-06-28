fc4d5051f217156f76a8766887aeb10113cee6e5
Kevin Tena <kevten09@gmail.com>
2018-06-28 17:07:52 -0500

    Added 3 forms for name, age, and email that when submitted add a new friend to the server data

--- friends/src/App.js
+++ friends/src/App.js
@@ -7,7 +7,17 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      friendsData: []
+      friendsData: [],
+      friend: {
+        name: '',
+        age: '',
+        email: ''
+      },
+      blankFriend:{
+        name: '',
+        age: '',
+        email: ''
+      }
     }
   }
 
@@ -20,11 +30,63 @@ class App extends Component {
         .catch(err => {
           console.log(err);
         });
-
   }
+
+  handleFriendSubmit = () => {
+    const friend = this.state.friend;
+    const blankFriend = this.state.blankFriend; 
+    axios
+      .post("http://localhost:5000/friends", friend)
+      .then(response => {
+        this.setState({friendsData: response.data, friend: blankFriend});
+      })
+      .catch(error => console.log(error));
+  };
+
+  handleFriendName = e => {
+    let nufriend = {...this.state.friend};
+    nufriend.name = e.target.value;
+    this.setState({friend:nufriend});
+  };
+
+  handleFriendAge = e => {
+    let nufriend = {...this.state.friend};
+    nufriend.age = e.target.value;
+    this.setState({friend:nufriend});
+  };
+
+  handleFriendEmail = e => {
+    let nufriend = {...this.state.friend};
+    nufriend.email = e.target.value;
+    this.setState({friend:nufriend});
+  };
+
+
   render() {
     return (<div>
       <h1> My Friends </h1>
+      <input
+        type="text"
+        placeholder="Name"
+        onChange={this.handleFriendName}
+        name="friend"
+        value={this.state.friend.name}
+        />
+        <input
+        type="text"
+        placeholder='age'
+        onChange={this.handleFriendAge}
+        name="age"
+        value={this.state.friend.age}
+        />
+        <input
+        type="text"
+        placeholder="Email"
+        onChange={this.handleFriendEmail}
+        name="email"
+        value={this.state.friend.email}
+        />
+        <button onClick={this.handleFriendSubmit}> Add Friend </button>
       <FriendsList friends={this.state.friendsData}/>
     </div>);
   }
