3ce27d53c6e0f597082fa52bbb4f7b11218d1f55
Kevin <brackkevin@gmail.com>
2018-06-28 17:40:37 -0500

    Added cards and button styling on friends page

--- friends/src/components/Friends/AddFriendForm.js
+++ friends/src/components/Friends/AddFriendForm.js
@@ -3,34 +3,38 @@ import React from "react";
 const AddFriendForm = props => {
   // console.log("ADD FRIEND FORM PROPS", props);
   return (
-    <form onSubmit={props.handleSubmit}>
-      <h3>Add a new friend</h3>
-      <label htmlFor="name">Name</label>
-      <input
-        type="text"
-        id="name"
-        name="newFriendName"
-        placeholder="Name"
-        onChange={props.newTextHandler}
-      />
-      <label htmlFor="email">Email</label>
-      <input
-        type="text"
-        id="email"
-        name="newFriendEmail"
-        placeholder="Emial"
-        onChange={props.newTextHandler}
-      />
-      <label htmlFor="age">Age</label>
-      <input
-        type="text"
-        id="age"
-        name="newFriendAge"
-        placeholder="Age"
-        onChange={props.newTextHandler}
-      />
-      <button type="submit">SUBMIT</button>
-    </form>
+    <div className="container">
+      <form onSubmit={props.handleSubmit}>
+        <h3>Add a new friend</h3>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="newFriendName"
+          placeholder="Name"
+          onChange={props.newTextHandler}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          name="newFriendEmail"
+          placeholder="Emial"
+          onChange={props.newTextHandler}
+        />
+        <label htmlFor="age">Age</label>
+        <input
+          type="text"
+          id="age"
+          name="newFriendAge"
+          placeholder="Age"
+          onChange={props.newTextHandler}
+        />
+        <button type="submit" className="waves-effect waves-light btn">
+          SUBMIT
+        </button>
+      </form>
+    </div>
   );
 };
 
--- friends/src/components/Friends/Friend.js
+++ friends/src/components/Friends/Friend.js
@@ -3,12 +3,16 @@ import React from "react";
 const Friend = props => {
   // console.log(props);
   return (
-    <div>
-      <h3>{props.friend.name}</h3>
-      <p>Age: {props.friend.age}</p>
-      <p>Email: {props.friend.email}</p>
-      {/* <p>Age: {props.friend.age}</p>
+    <div classname="col s12 m6">
+      <div className="container">
+        <div className="card">
+          <h3>{props.friend.name}</h3>
+          <p>Age: {props.friend.age}</p>
+          <p>Email: {props.friend.email}</p>
+          {/* <p>Age: {props.friend.age}</p>
       <p>Email: {props.friend.email}</p> */}
+        </div>
+      </div>
     </div>
   );
 };
