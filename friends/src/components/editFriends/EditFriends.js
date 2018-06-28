88ba80012955f517b39c3a325f88ae2b0619c966
jeffyjkang <jeffyjkang@gmail.com>
2018-06-28 15:52:24 -0700

    update some of the edit

--- friends/src/components/editFriends/EditFriends.js
+++ friends/src/components/editFriends/EditFriends.js
@@ -3,12 +3,34 @@ import React from 'react';
 const EditFriends = props => {
     return(
         <div>
+
+            <form>
             <input
-                type ="text"
-                placeholder = {props.friend.name}
-                onChange = {props.editHandler}
-            />
-            <button onClick = {props.submitEdits}> Save </button>
+                 type = "text"     
+                 name = "name"    
+                 onChange = {props.editHandler}          
+                 value = {props.friend.name}
+                 placeholder = {props.friend.name}
+            /> <br></br>
+             <input 
+                  type = "number"
+                  name = "age"
+                 onChange = {props.editHandler}
+                  value = {props.friend.age}
+                  placeholder = {props.friend.age}
+             /> <br></br>
+             <input 
+                  type = "text"
+                  name = "email"
+               onChange = {props.editHandler}
+               value = {props.friend.email}
+               placeholder = {props.friend.email}
+             /> <br></br>
+             <button onClick = {props.submitEdits}>
+                 Edit
+            </button>
+            </form>
+
         </div>
     )
 }
--- friends/src/components/friends/Friends.js
+++ friends/src/components/friends/Friends.js
@@ -9,12 +9,23 @@ class Friends extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            showEditForm: false,
+            editedFriend: ""
         }
     }
     handleDelete = id => {
         this.props.handleDelete(id);
     }
+
+    toggleForm =() => {
+        this.setState({showEditForm: !this.state.showEditForm})
+    }
+
+    editHandler = e => {
+        this.setState({editedFriend: e.target.value})
+    }
+
+
 
     render(){
     return (
@@ -22,6 +33,13 @@ class Friends extends Component {
             {this.props.friend.name} 
             {this.props.friend.age} 
             {this.props.friend.email} 
+            <EditFriends
+            friend = {this.props.friend}
+            editHandler = {this.editHandler}
+            />
+            <button onClick ={this.toggleForm}>
+                Edit
+            </button>
             <button onClick ={() => this.handleDelete(this.props.friend.id)}> 
                 Delete
             </button>
