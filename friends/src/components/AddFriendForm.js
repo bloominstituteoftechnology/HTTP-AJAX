cc4abe9c1803317811d3e18756e64d10c83067b5
Alec <afroenygam@gmail.com>
2018-06-28 17:59:14 -0400

    Further styling added

--- friends/src/components/AddFriendForm.js
+++ friends/src/components/AddFriendForm.js
@@ -17,28 +17,29 @@ class AddFriendForm extends React.Component {
   render() {
   return (
     <div>
-      <form>
+      <form className="af-form">
         Name: <input
         type="text"
         placeholder="Name"
         name="friendName"
         value={this.props.name}
-        onChange={this.props.handleChange} />
+        onChange={this.props.handleChange} className="af-input" />
         <br/>
         Age: <input
         type="number"
         placeholder="Age"
         name="friendAge"
         value={this.props.age}
-        onChange={this.props.handleChange} />
+        onChange={this.props.handleChange} className="af-input" />
         <br/>
         Email: <input
         type="text"
         placeholder="Email"
         name="friendEmail"
         value={this.props.email}
-        onChange={this.props.handleChange} />
-        <button onClick={this.props.handleSubmitFriend}>Add Friend</button>
+        onChange={this.props.handleChange} className="af-input" />
+        <button onClick={this.props.handleSubmitFriend}
+        className="addFriend-button">Add Friend</button>
       </form>
     </div>
   )
