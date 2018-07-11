6ea7a35cb3c57eabac6c0f02b1c489acdf7efbf8
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-07-11 03:01:09 -0400

    Added styling to the App

--- friends/src/App.js
+++ friends/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import ListOfFriends from './component/ListOfFriends';
 import FriendForm from './component/FriendForm';
 import Navigation from './component/Navigation';
+import { EditForm } from './component/EditForm';
 
 class App extends Component {
   constructor(props) {
@@ -15,8 +16,11 @@ class App extends Component {
     }
   }
   
+  handleTextInput = e => {
+    this.setState({ [e.target.name]: e.target.value});
+  };  
+
   // GET request
-  
   componentDidMount() {
     axios
       .get("http://localhost:5000/friends")
@@ -31,39 +35,29 @@ class App extends Component {
     )
   }
 
-  handleTextInput = e => {
-    this.setState({ [e.target.name]: e.target.value});
-  };  
-
   deleteFriend = (id) => {
-    let friends = [];
-    friends = friends.concat(this.state.friends);
-    friends.forEach(friend => {
-      if (friend.id === id) {
-      axios
-        .delete(`http://localhost:5000/friends/${id}`)
-        .then(deletedNote => {
-          console.log('Deleted');
-        })
-        .catch(err => {
-          console.log(err);
-        });
-      }
-    })
+    axios
+      .delete(`http://localhost:5000/friends/${id}`)
+      .then(deletedNote => {
+        console.log('Deleted');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    window.location.reload();
   };
 
-  // saveNoteData = () => {
-  //   const friend = { name: this.state.name, age: this.state.age, email: this.state.email };
-  //   axios
-  //     .post(`http://localhost:5000/friends`, friend)
-  //     .then(savedNote => {
-  //       console.log(savedNote);
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  //   this.setState({ name: '', age: '', email: ''});
-  // };
+  editFriend = (id, friend) => {
+    axios
+      .put(`http://localhost:5000/friends/:${id}`, friend)
+      .then(editedFriend => {
+        console.log("Edited: ", editedFriend);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    this.setState({ name: '', age: '', email: ''});
+  };
     
   render() {
     return (
@@ -73,10 +67,16 @@ class App extends Component {
         <Route 
           exact path='/' 
           render={(props) => (
-          <ListOfFriends {...props} friends={ this.state.friends } deleteFriend={ this.deleteFriend }/>
-        )}/>
+            <ListOfFriends {...props} 
+            friends={ this.state.friends } 
+            deleteFriend={ this.deleteFriend }
+            />
+          )}
+        />
 
-        <Route path ='/form' component = { FriendForm } />
+        <Route path ='/form' component={ FriendForm }/>
+
+        <Route path='/edit' component={ EditForm }/>
       </div>
     );
   }
