b0c590c248026eab31177a2c43f70f83140ba651
JacobLayton <jlayjlay3@gmail.com>
2018-07-11 13:45:07 -0700

    Displaying friends list

---
+++ friends/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import FriendsList from './components/FriendsList'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friendsData: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends').then(response => {
+      console.log(response);
+      this.setState({ friendsData: response.data });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <FriendsList friends={this.state.friendsData} />
+      </div>
+    );
+  }
+}
+
+export default App;
