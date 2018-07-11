d1e82f4f90876d51d1bef046c2196dfd116467a0
Katie Gorbell <katiegorbell@gmail.com>
2018-07-11 15:05:16 -0500

    added basics for App

---
+++ friends/src/FriendsList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FriendsList = () => {
+    return (
+        <div className='friends-list'>
+        </div>
+    );
+}
+ 
+export default FriendsList;
---
+++ friends/src/NewFriend.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NewFriend = () => {
+    return (
+        <div className='new-friend'>
+        </div>
+    );
+}
+ 
+export default NewFriend;
