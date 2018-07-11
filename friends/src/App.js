cd348fa7e0f80a8e1646cb5acd72a07d1b32df36
sean chavez <seanchavez@seanchavezsMBP.home>
2018-07-11 13:44:52 -0700

    GET friends data from server to state of App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        console.log('GET RESPONSE: ', response);
+        this.setState({data: response.data})
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    
+  }
   render() {
     return (
       <div className="App">
