53091effdd9c045a15c5892a083d7769f936331e
JacobLayton <jlayjlay3@gmail.com>
2018-07-11 14:40:46 -0700

    MVP complete

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import FriendsList from './components/FriendsList'
+import FriendsList from './components/FriendsList';
+import Form from './components/Form';
 
 class App extends Component {
   constructor() {
@@ -16,6 +17,24 @@ class App extends Component {
     axios.get('http://localhost:5000/friends').then(response => {
       console.log(response);
       this.setState({ friendsData: response.data });
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+  
+  handleSubmit = e => {
+    const friend = {
+      name: this.state.name,
+      age: Number(this.state.age),
+      email: this.state.email
+    };
+    axios.post('http://localhost:5000/friends', friend).then(create => {
+      console.log(create);
+      console.log(create.data);
+    }).catch(error => {
+      console.log(error);
     });
   }
 
@@ -30,6 +49,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <FriendsList friends={this.state.friendsData} />
+        <Form handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
       </div>
     );
   }
