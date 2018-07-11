af5577e44a28614f311dd171feb0aa3bc708bdd4
Kairi <kairijb@gmail.com>
2018-07-11 16:13:24 -0500

    created handles for adding name, age, email

--- friends/src/App.js
+++ friends/src/App.js
@@ -7,15 +7,35 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      friends: []
+      friends: [],
+      name: '',
+      age: 0,
+      email: '',
     }
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/friends').then(response => {
+    axios
+    .get('http://localhost:5000/friends')
+    .then(response => {
     console.log(response);
     this.setState({friends: response.data})
     })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  friendName = e => {
+    this.setState({name : e.target.value})
+  }
+
+  friendAge = e => {
+    this.setState({age : e.target.value})
+  }
+
+  friendEmail = e => {
+    this.setState({email : e.target.value})
   }
 
   render() {
@@ -25,9 +45,36 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
+        <form>
+        <input
+        type='text'
+        name='friend'
+        placeholder='name'
+        value={this.state.name}
+        onChange={this.friendName}
+        />
+
+        <input
+        type='number'
+        name='age'
+        placeholder='age'
+        value={this.state.age}
+        onChange={this.friendAge}
+        />
+
+        <input
+        type='email'
+        name='email'
+        placeholder='email'
+        value={this.state.email}
+        onChange={this.friendEmail}
+        />
+        <button onClick={this.newFriend}>Submit</button>
+        </form>
         
           {this.state.friends.map(item => {
-            return <p key={item.id}>{item.name} <br /> {item.age} <br /> {item.email} <br /></p>
+            return <p key={item.id}>Name: {item.name} <br /> Age: {item.age} <br /> Email: {item.email} <br /></p>
           })}
   
       </div>
