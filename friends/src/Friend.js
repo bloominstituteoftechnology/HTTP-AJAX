ad24028db00e25c386c62b6b5f3f9096cceca6b4
Katie Gorbell <katiegorbell@gmail.com>
2018-07-12 18:52:15 -0500

    working on delete friend functionality

--- friends/src/Friend.js
+++ friends/src/Friend.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Friend extends Component {
     constructor() {
@@ -14,7 +15,7 @@ class Friend extends Component {
 
     handleNameChange = e => {
         this.setState({ editedFriend: Object.assign({}, this.state.editedFriend, { name: e.target.value }) })
-        }
+    }
 
     handleAgeChange = e => {
         this.setState({ editedFriend: Object.assign({}, this.state.editedFriend, { age: e.target.value }) })
@@ -22,6 +23,13 @@ class Friend extends Component {
 
     handleEmailChange = e => {
         this.setState({ editedFriend: Object.assign({}, this.state.editedFriend, { email: e.target.value }) })
+    }
+
+    delete = id => {
+        axios
+            .delete(`'http://localhost:5000/friends/${id}'`)
+            .then(response => this.setData(response.data))
+            .catch(error => console.log('error: ', error))
     }
 
 
@@ -36,11 +44,14 @@ class Friend extends Component {
                 </div>
                 <div>
                     <form>
-                        <input type='text' placeholder='Enter name here' onChange={this.handleNameChange} value={this.state.editedFriend.name} />
-                        <input type='number' placeholder='Enter age here' onChange={this.handleAgeChange} value={this.state.editedFriend.age} />
-                        <input type='text' placeholder='Enter email here' onChange={this.handleEmailChange} value={this.state.editedFriend.email} />
+                        <span>name:</span>
+                        <input type='text' placeholder={this.props.friend.name} onChange={this.handleNameChange} value={this.state.editedFriend.name} />
+                        <span>age:</span>
+                        <input type='number' placeholder={this.props.friend.age} onChange={this.handleAgeChange} value={this.state.editedFriend.age} />
+                        <span>email:</span>
+                        <input type='text' placeholder={this.props.friend.email} onChange={this.handleEmailChange} value={this.state.editedFriend.email} />
                         <button>Save Changes</button>
-                        <button>Delete Friend</button>
+                        <button onClick={() => this.delete(this.props.friend.id)}>Delete Friend</button>
                     </form>
                 </div>
             </div>
