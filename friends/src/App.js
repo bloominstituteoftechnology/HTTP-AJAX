771c2ed102eddc9dd5f9315e03bbe919dc9ecffd
unknown <may.alex43@gmail.com>
2018-07-12 00:10:31 -0400

    Got friends list to render

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import FriendsList from 'FriendsList';
+import FriendsList from './components/FriendsList';
 
 class App extends Component {
   constructor() {
     super();
     this.state={
       friendsData:[],
-      friend:""
+      friends:""
     };
   }
 
   handleData = data => this.setState ({friendsData:data});
+  
+  componentDidMount(){
+    axios
+      .get('http://localhost:5000/friends')
+        .then(response => {
+          this.setState({ friendsData: response.data})
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
   render() {
     return (
---
+++ friends/src/components/Friend.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friend= props => {
+    return (
+        <div>
+            {props.friend.name}
+        </div>
+    );
+};
+
+export default Friend;
