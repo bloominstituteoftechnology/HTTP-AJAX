067ba2a466e9e8ead1281633910aec782a2ce849
Adrian Adames <adrianadames@gmail.com>
2018-07-12 23:13:56 -0400

    Created component to display list of friends from server.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from "axios"
+import FriendsList from './Components/FriendsList';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friendsData: [],
+      friend: "",
+    };
+  }
+
+  // handleSetData = data => this.setState({avengersData:data})
+
+  componentDidMount() {
+    axios
+    .get("http://localhost:5000/friends")
+    .then(response => {
+      console.log("GET RESPONSE: ". response);
+      this.setState({friendsData:response.data})
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        
+      <div>
+        <h1>List of Friends</h1>
+        <FriendsList 
+          friends = {this.state.friendsData}
+        />
       </div>
     );
   }
