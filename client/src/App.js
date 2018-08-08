3b00c2c722d7aacc0c09aaccf7d07846f647363a
Ashwin <aswinjeganath@gmail.com>
2018-08-09 02:25:59 +0530

    Implement componentDidMount to load data on to the state

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import FriendList from './FriendsList';
 
+const API_URL = 'http://localhost:5000';
 class App extends Component {
   state = {
     friends: []
   };
+
+  componentDidMount() {
+    axios
+      .get(`${API_URL}/friends`)
+      .then(response => this.setState({ friends: response.data }));
+  }
 
   render() {
     return (
--- client/src/FriendsList.js
+++ client/src/FriendsList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FriendList = ({ friends }) => (
   <div>
     {friends.map(friend => (
-      <div>
+      <div key={friend.id}>
         <h2>{friend.name}</h2>
         <div>{friend.age}</div>
         <div>{friend.email}</div>
