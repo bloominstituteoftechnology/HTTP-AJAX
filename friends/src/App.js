a9d9f1205e8f8b5e076916b90d3cee35ec3fb6fa
LizBaker <lizbaker.pdx@gmail.com>
2018-08-08 14:39:37 -0700

    Finished MVP -added styling and POST functionality to add friend form

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,27 +2,28 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const Friendos = (props) => {
+const Friendos = (props) => { 
   return (
     <div className="friendz">
       {props.friends.map(friend => (
       <div className="friendCard">
-        <div>{friend.name}</div>
-        <div>age: {friend.age}</div>
-        <div>email: {friend.email}</div>
+        <div className="friendoname">{friend.name}</div>
+        <div className="ageo">age: {friend.age}</div>
+        <div className="emailo">email: {friend.email}</div>
       </div>
       ))}
     </div>
   )
 }
 
-const FriendForm = () => {
+const FriendForm = (props) => {
   return (
     <form>
       <p>Add a new frand!</p>
-      <input placeholder="Name"/>
-      <input placeholder="Age"/>
-      <input placeholder="Email"/>
+      <input name="Name" onChange={props.enterAFrand} value={props.val.Name} placeholder="Name"/>
+      <input name="Age" onChange={props.enterAFrand} value={props.val.Age} placeholder="Age"/>
+      <input name="Email" onChange={props.enterAFrand} value={props.val.Email} placeholder="Email"/>
+      <button type="button" onClick={props.addAFrand}>Add</button>
     </form>
   )
 }
@@ -32,7 +33,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      friends: []
+      friends: [],
+      Name: "",
+      Age: null,
+      Email: ""
     };
   }
 
@@ -45,8 +49,25 @@ class App extends Component {
       .catch(error => {
         console.error('Server Error', error);
       });
+  }
 
-     
+  addAFrand = () => {
+    // const currentfrands = this.state.friends.slice();
+    const newfrand = {
+      name: this.state.Name,
+      age: this.state.Age,
+      email: this.state.Email
+    }
+    axios
+    .post('http://localhost:5000/friends', newfrand)
+    .then(response => {
+      this.setState(() => ({ friends: response.data }));
+    })
+    this.setState({Name: "", Age: "", Email: ""})
+  }
+
+  enterAFrand = (event) => {
+    this.setState ({ [event.target.name] : event.target.value});
   }
 
 
@@ -54,7 +75,7 @@ class App extends Component {
     return (
       <div className="App">
       <Friendos friends={this.state.friends}/>
-      <FriendForm />
+      <FriendForm enterAFrand={this.enterAFrand} addAFrand={this.addAFrand} val={this.state.friends}/>
       </div>
     );
   }
