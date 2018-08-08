13173f7a027e63d8702888aaf9b6cb77f38fc767
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-08-08 16:19:12 -0600

    MVP acheived, but I changed my approach half-way through and went without components (even though many were already built)

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import FriendList from './components/FriendList';
-import Friend from './components/Friend';
+import axios from 'axios';
+
+const FriendInfo = props => {
+  return (
+    <div className="main-display">
+      {props.param.map(friend => (
+        <div key={friend.id} className="friend-display">
+        <h3 className="friend-name">{friend.name}</h3>
+        <h5>age: {friend.age}</h5>
+        <p>email: {friend.email}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
 
 class App extends Component {
   constructor() {
@@ -11,10 +24,21 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        this.setState(() => ({ friends: response.data }));
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <FriendList />
+      <div>
+        <FriendInfo param={this.state.friends}/>
       </div>
     );
   }
