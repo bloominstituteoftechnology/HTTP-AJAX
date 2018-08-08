c3afd9ddc173b70e89f1f6645a9a7bad6b852c9a
szincone <sawyerzincone@gmail.com>
2018-08-08 14:18:01 -0600

    imported data correctly on intial app.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,32 @@
 import React, { Component, Fragment } from "react";
+import axios from "axios";
 
 import "./App.css";
 import Friend from "./components/friends/Friend";
 
+let dataUrl = "http://localhost:5000/friends/";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friendsData: [],
+      loading: true
+    };
+  }
+  componentDidMount() {
+    axios.get(dataUrl).then(response => {
+      console.log("RESPONSE", response);
+      this.setState({
+        friendsData: response.data,
+        loading: false
+      });
+    });
+  }
   render() {
     return (
-      <Fragment className="App">
-        <Friend />
+      <Fragment>
+        <Friend friendsData={this.state.friendsData} />
       </Fragment>
     );
   }
--- friends/src/components/friends/Friend.js
+++ friends/src/components/friends/Friend.js
@@ -2,7 +2,14 @@ import React from "react";
 import FriendCard from "./FriendCard";
 
 export default class Friend extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: props.friendsData
+    };
+  }
   render() {
+    console.log("Friend Data", this.state.friends);
     return <FriendCard />;
   }
 }
