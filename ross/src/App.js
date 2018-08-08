de099389c6da7677e6f0273ec7f4477defebc8e9
Erik Kimsey <erik@erikkimsey.com>
2018-08-08 15:37:59 -0400

    Created axios GET of friends data and have temporarily mapped friends names to divs within App.js

--- ross/src/App.js
+++ ross/src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
-
+// https://github.com/LambdaSchool/HTTP-AJAX/pull/275
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      friends: [],
+    }
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000/friends')
+      .then((response)=>{
+        console.log(response.data);
+        this.setFriendsToState(response.data)
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+  }
+
+  setFriendsToState = (f) => {
+    console.log(f);
+    let curr = this.state.friends;
+    this.setState({
+      friends: f
+    })
+  }
+
   render() {
+    console.log(this.state.friends);
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       {
+          this.state.friends.map((ross)=>{
+            console.log(ross);
+            return <div>{ross.name}</div>
+          })
+       }
       </div>
     );
   }
