ea43e84917fcfec7a895378ba2b18d1042f369bb
Josue Peralta <jperal77@gmail.com>
2018-08-08 14:41:27 -0700

    All friends are displayed

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -9,10 +9,28 @@ class FriendList extends Component {
     }
   }
 
+  componentDidMount(){
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        console.log(response.data);
+        this.setState(() => ({ friends: response.data }));
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+  }
+
   render(){
     return (
       <div>
-        
+        <ul>
+          {this.state.friends.map(friend => <li key={friend.id}>
+            <p>Name: {friend.name}</p>
+            <p>id: {friend.id}</p>
+            <p>Age: {friend.age}</p>
+            <p>Email: {friend.email}</p></li>)}
+        </ul>
       </div>
     );
   }
