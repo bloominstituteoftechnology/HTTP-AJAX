129c8d03e9ace2623e82c7c9a726882656f7e176
sca4cs <shannon.atkinson.ca@gmail.com>
2018-08-08 16:08:46 -0400

    data displaying

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import FriendList from './components/FriendList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <FriendList/>
       </div>
     );
   }
---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Friend from './Friend';
+
+export default class FriendList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        this.setState(() => ({ friends: response.data }));
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="friend-list">
+        {this.state.friends.map(friend => (
+          <Friend friend={friend} key={friend.id}/>
+        ))}
+      </div>
+    );
+  }
+}
