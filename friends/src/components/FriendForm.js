78e19fd8ff3562287b73ad145751d4d5ff45b53a
tulf <timzulf@gmail.com>
2018-08-08 18:58:39 -0400

    added dependencies and imports for reactsrap, bootstrap and imported into friendform and friendlist

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,14 +1,16 @@
 import React from 'react'
-
+import {
+    Card,   CardBody, Form,
+  }from 'reactstrap';
 const FriendForm = props => {
   return(
 
-    <form className = "friendform" onSubmit = {props.onSubmit}>
+    <Form className = "friendform" onSubmit = {props.onSubmit}>
         <input onChange = {props.onChange} name = "name" placeholder = "Add name" />
         <input onChange = {props.onChange} name =  "age" placeholder = "Add age" />
         <input onChange = {props.onChange} name = "email" placeholder = "Add email" />
          <button >Submit!</button>
-    </form>
+    </Form>
   )
 }
 
--- friends/src/components/friendlist.js
+++ friends/src/components/friendlist.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import {
+    Card,   CardBody,
+  }from 'reactstrap';
+  import styled from 'styled-components'
 
 const Friendlist = props => {
   return(
-  <div className= "friendlist">
+  <Card className= "friendlist">
       {props.friends.map(friend =>(
         <div className = "individual" key= {friend.id}>
-            <p>{friend.name}  is {friend.age} years old and can be reached at {friend.email}</p>
+            <CardBody>{friend.name}  is {friend.age} years old and can be reached at {friend.email}</CardBody>
         </div>
       ))}
-  </div>
+  </Card>
 );
 }
 
