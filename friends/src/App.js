302a00e86e8634e9650ab6361b4e570f1c98595e
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-08-08 17:21:48 -0400

    Server form filled out and working

--- friends/src/App.js
+++ friends/src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
     this.state = {
       friends: [],
       isLoading: true,
+      name: '',
+      age: '',
+      email: '',
     }
   }
 
@@ -24,6 +27,23 @@ class App extends Component {
           });
   }
 
+  handleInputChange = event => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const newFriend = {
+      name: this.state.name,
+      age: parseInt(this.state.age, 10),
+      email: this.state.email,
+    };
+    axios.post('http://localhost:5000/friends', newFriend)
+          .then( res =>
+            this.setState({name: '', age: '', email: '', friends: res.data})
+          );
+  }
+
   render() {
     return (
       <div className="App">
@@ -31,7 +51,13 @@ class App extends Component {
           <h1>Loading Friends, one moment please...</h1> :
           <FriendList friends={this.state.friends} />
         }
-        <FriendForm />
+        <FriendForm
+          input={this.handleInputChange}
+          submit={this.handleSubmit}
+          name={this.state.name}
+          age={this.state.age}
+          email={this.state.email}
+        />
       </div>
     );
   }
--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 const FriendForm = props => {
   return(
-    <div>Friend Form Placeholder</div>
+    <form onSubmit={props.submit}>
+      <label htmlFor="name">Name: </label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={props.name}
+        placeholder="Enter Name"
+        onChange={props.input}
+      />
+      <label htmlFor="age">Age: </label>
+      <input
+        type="text"
+        name="age"
+        id="age"
+        value={props.age}
+        placeholder="Enter Age"
+        onChange={props.input} />
+      <label htmlFor="email">eMail: </label>
+      <input
+        type="text"
+        id="email"
+        name="email"
+        value={props.email}
+        placeholder="Enter E-Mail"
+        onChange={props.input} />
+      <input type="submit" />
+    </form>
   );
 }
 
