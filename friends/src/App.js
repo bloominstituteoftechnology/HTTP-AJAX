8ff37e5130aa9bb71f4ba32367dc354f8b319916
francistse23 <wtse.ucsd@gmail.com>
2018-08-08 13:18:19 -0700

    MVP Completed. Data is now displaying on the page.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import FriendList from './component/Friend/FriendList';
 import './App.css';
+import axios from 'axios';
+
+const url = 'http://localhost:5000/friends';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(props){
+    super(props);
+    this.state = {
+        friends: [],
+        loading: true
+    }
   }
+  componentDidMount(){
+      axios
+          .get(url)
+          .then(person => {
+              this.setState({friends: person.data, loading: false})
+          })
+          .catch(err => console.log('Server Error', err));
+  }
+  render(){
+      return(
+          <div>
+              <FriendList friends={this.state.friends} />
+          </div>
+      )
+    }
 }
 
 export default App;
