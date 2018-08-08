c22a2110a3e64b6af00cec394ca9849b2e178eb5
szincone <sawyerzincone@gmail.com>
2018-08-08 13:50:55 -0600

    app working with filler data

---
+++ friends/src/components/friends/Friend.js
@@ -0,0 +1,8 @@
+import React from "react";
+import FriendCard from "./FriendCard";
+
+export default class Friend extends React.Component {
+  render() {
+    return <FriendCard />;
+  }
+}
---
+++ friends/src/components/friends/FriendCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FriendCard = props => {
+  return (
+    <div>
+      <h2>Filler</h2>
+    </div>
+  );
+};
+
+export default FriendCard;
