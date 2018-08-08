5aa371c826ab7b85ddcd46f14da3cd4d7ec638fd
codejoncode <jonathanjholloway@gmail.com>
2018-08-08 14:51:25 -0500

    the friends name age and email is displayed to the screen

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios'; 
 import './App.css';
 
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {friends: []};
+  }
+
+componentDidMount () {
+  axios.get('http://localhost:5000/friends').then((response) => {
+    console.log(response.data); 
+    this.setState({
+      friends: response.data
+    })
+  }); 
+  
+}
+
   render() {
+    const friends = this.state.friends.slice(); 
     return (
       <div className="App">
-        <h1>HTTP -AJAX</h1>
+        <h1>HTTP -AJAX Friends</h1>
+        <ul>
+          {friends.map(friend => <li key ={friend.id}>{friend.name}  {friend.age}  {friend.email}</li>)}
+        </ul>
+      
       </div>
     );
   }
