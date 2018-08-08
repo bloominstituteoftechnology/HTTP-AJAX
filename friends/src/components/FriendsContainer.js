46f53d22e8fc57696eb1a2fa7e797b73e81b2023
kamalnrf <kamalnrf@hotmail.com>
2018-08-09 02:38:36 +0530

    Added Delete

--- friends/src/components/FriendsContainer.js
+++ friends/src/components/FriendsContainer.js
@@ -1,5 +1,6 @@
 import React from 'react' 
 import './friendsContainer.css'
+import axios from 'axios'
 
 export default function FriendsContainer (props){
     const friends = props.friends
@@ -7,16 +8,28 @@ export default function FriendsContainer (props){
     return (
         <div className="friendsList">
         {friends.map(friend => 
-            <div className="friendsCard">
+            <div key={friend.id} className="friendsCard">
                 <div className="data">
                     <div><strong>Name:</strong> {friend.name}</div>
                     <div><strong>Age:</strong> {friend.age}</div>
                     <div><strong>Email:</strong> {friend.email}</div>   
                 </div>
-                <button>Update</button>
-                <button>Delete</button>
+                <button id={friend.id} onClick={update}>Update</button>
+                <button id={friend.id} onClick={remove}>Delete</button>
             </div>
         )}
         </div>
     )
+}
+
+function update (event){
+    console.log(event.target.id)
+
+}
+
+function remove (event){
+    const id = event.target.id
+    const URL = `http://localhost:5000/friends/${id}`
+    console.log(URL)
+    axios.delete(URL).then(res => console.log(res))
 }
