4bb3ff7c4222910e50ea304332b98432f3ec78ea
Charles Cortinas <crcortinas@gmail.com>
2018-08-08 17:56:57 -0500

    MVP
    Displays list of friends from the server

---
+++ friends/src/components/Friends.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import axios from 'axios';
+
+class Friends extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            friends: [],
+            url: props.url
+        }
+    }
+
+    componentDidMount() {
+        axios.get(this.state.url).then(response => {
+          this.setState({ friends: response.data });
+        });
+      }
+
+    render () {
+        console.log(this.state.url);
+        return (
+            <div>
+                {this.state.friends.map(friend => {
+                    return(
+                        <div key={friend.id} className='friend'>
+                            <p>Name: {friend.name}</p>
+                            <p>Age: {friend.age}</p>
+                            <p>Email: {friend.email}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+}
+
+export default Friends;
