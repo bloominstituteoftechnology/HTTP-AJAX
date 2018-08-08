8ae4b332f73acf7faced60e069dd4e4c1be905db
Josue Peralta <jperal77@gmail.com>
2018-08-08 14:28:44 -0700

    Boilerplate

---
+++ friends/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import FriendList from './components/FriendList';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <FriendList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class FriendList extends Component {
+  constructor(){
+    super();
+    this.state = {
+      friends: []
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default FriendList;
