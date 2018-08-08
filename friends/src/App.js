f5778e9c0dcf0d4fffdf77ecec0eda4f77a70655
Lauren <llwor94@gmail.com>
2018-08-08 13:09:16 -0700

    MVP complete

--- friends/src/App.js
+++ friends/src/App.js
@@ -18,10 +18,27 @@ class App extends Component {
     });
   }
 
+  handleAddFriend = (name, age, email) => {
+    let friend = {
+      id: this.state.friends.length + 2,
+      name: name,
+      age: age,
+      email: email
+    }
+    axios.post("http://localhost:5000/friends", friend)
+      .then((response) => {
+        console.log(response);
+        this.setState({friends: response})
+      })
+      .catch((response) => {
+        console.log(`error ${response}`)
+      })
+  }
+
   render() {
     return ( 
     <div className="App">
-      <FriendsList friends={this.state.friends} loading={this.state.loading} />
+      <FriendsList friends={this.state.friends} loading={this.state.loading} addFriend={this.handleAddFriend}/>
     </div>
     )
   }
--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,19 +1,46 @@
 import React from 'react';
 
-export default function FriendsList(props) {
-  if (props.loading) {
+export default class FriendsList extends React.Component {
+  state= {
+    name: '',
+    age: '',
+    email: ''
+  }
+
+  handleOnChange = (e) => {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+
+  handleSubmit = () => {
+    console.log(this.state.name, this.state.age, this.state.email)
+    if (this.state.name && this.state.age && this.state.email) {
+      this.props.addFriend(this.state.name, this.state.age, this.state.email)
+    }
+  }
+
+  render() {
+  if (this.props.loading) {
     return <h2>Loading data..</h2>;
-  } else if (!props.loading && props.friends.length === 0) {
+  } else if (!this.props.loading && this.props.friends.length === 0) {
     return <h2>No data here, try again</h2>
   }
 
   return (
     <div>
-      {props.friends.map((friend, i) => (
+      {this.props.friends.map((friend, i) => (
         <div key={i}>
           <p>{friend.name}</p>
         </div>
       ))}
+      <form onSubmit={this.handleSubmit}>
+            <input onChange={this.handleOnChange} name='name' type='text' value={this.state.name} placeholder='Friends Name' />
+            <input onChange={this.handleOnChange} type='number' name='age' value={this.state.age} placeholder='Friends Age' />
+            <input onChange={this.handleOnChange} type='text' name='email' value={this.state.email} placeholder='Friends email' />
+            <input type='submit' value='Submit' />
+          </form>
     </div>
   )
+  }
 }
