8b7c358f81a69f2f35d5044c381637c2075315ce
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-08-08 12:50:29 -0700

    add basic styling

--- friends/src/Components/AddFriendForm.js
+++ friends/src/Components/AddFriendForm.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 const AddFriendForm = (props) => {
     return ( 
-        <form onSubmit={props.handleSubmit} className="friend-form">
-            <input type="text" placeholder="Name" name="name"></input>
-            <input type="number" placeholder="Age" name="age"></input>
-            <input type="email" placeholder="Email" name="email"></input>
-            <button>Submit!</button>
-        </form>
+        <div className="friend-form-container">
+            <h2> Add friend! </h2>
+            <div className="friend-form">
+                <form onSubmit={props.handleSubmit}>  
+                    <input type="text" placeholder="Name" name="name"></input>
+                    <input type="number" placeholder="Age" name="age"></input>
+                    <input type="email" placeholder="Email" name="email"></input>
+                    <button>Submit!</button>
+                </form>
+            </div>
+        </div>
      );
 }
  
