80d878e1d05ba7e7a3e55e0b83af9f0708debb88
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-08-09 14:22:31 -0700

    Major changes - POST functionality working.

--- friends/src/components/friends.js
+++ friends/src/components/friends.js
@@ -7,7 +7,10 @@ export default class FriendList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      friends: [] 
+      friends: [],
+      name:'',
+      age: null,
+      email: ''
     };
   }
 
@@ -24,15 +27,43 @@ export default class FriendList extends Component {
       });
   }
 
+  handleChange = (event) => {
+     this.setState({
+        [event.target.name]: event.target.value,
+     });
+  }
+
+  addNewFriend= () => {
+   
+
+      const newFriendObj = {
+         name: this.state.name,
+         age: this.state.age,
+         email: this.state.email
+      }
+
+      axios
+      .post('http://localhost:5000/friends', newFriendObj)
+      .then(response => {
+         this.setState({ friends: response.data });
+      })
+      .catch(error => {
+         console.log('Error: ', error);
+      });
+  }
+
   render() {
    return (
       <div className="container">
          <h1 className="custom-title">Submit your information!</h1>
+
          <form className="custom-form">
-            <input placeholder="name" type="text"/>
-            <input placeholder="age" type="number"/>
-            <input placeholder="email" type="text"/>
+            <input onChange={this.handleChange} name="name" value={this.state.name} placeholder="name" type="text"/>
+            <input onChange={this.handleChange} name="age" value={this.state.age} placeholder="age" type="number"/>
+            <input onChange={this.handleChange} name="email" value={this.state.email} placeholder="email" type="email"/>
+            <button onClick={this.addNewFriend}>Submit</button>
          </form>
+
          <ul className="friend-grid">
             {this.state.friends.map(friend => {
                return <li key={friend.id} className="friend">
