65e69195b2674775ebeb2c3c250ad0f58546f43c
rodacoram <rodacoram8@gmail.com>
2018-08-08 20:57:50 -0600

    can now delete

--- friends/src/App.js
+++ friends/src/App.js
@@ -27,13 +27,25 @@ class App extends Component {
       console.log(error);
     });
   }
+  remove(id) {
+    axios.delete(`${url}/${id}`)
+      .then((response) => {
+        console.log(response)})
+        .catch((error) => {
+        console.log(error)});
+    window.location.reload();
+  }
   render() {
     return (
       <div className="App">
         <h2>Lambda Friends ... <br/>
             Assemble!!</h2>
         <div className="friendHug">
-          {this.state.friends.map(friend=><FriendsList key={friend.id}><p>{friend.name}</p><p>age:{friend.age}</p></FriendsList>)}
+          {this.state.friends.map(friend=><FriendsList key={friend.id}>
+                                            <p>{friend.name}</p>
+                                            <p>age:{friend.age}</p>
+                                            <button onClick={()=> this.remove(friend.id)}>remove</button>
+                                            </FriendsList>)}
         </div>
         <Form />
       </div>
