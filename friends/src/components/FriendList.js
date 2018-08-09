143f5a313555dd4efcf9d302fb7a892f0a8198bc
jaymzsocool <james.mcraejr@gmail.com>
2018-08-09 17:02:55 -0400

    turned friend cards to links in friend list

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import FriendCard from './FriendCard'
+import {Link} from 'react-router-dom'
 
 
  const FriendList = (props) =>{
     return(
         <div>                
-            <h1>Muh Frans!</h1>
-            {props.friends.map(data =>
-            <FriendCard friends = {data} />   
-            )}               
+            <h1>Muh Frans!</h1>            
+            {props.friends.map(data => (               
+                <Link to = {`/${data.id}`}><FriendCard key = {data.id} friends = {data} /></Link>                
+            ))}                        
         </div>
     )
 } 
