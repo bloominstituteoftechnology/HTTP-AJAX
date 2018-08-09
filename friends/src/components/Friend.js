cf61e6e3c01137d012c02e036f5de55ab9c67263
Carey <careycbaldwin@gmail.com>
2018-08-09 17:58:18 -0400

    latest updates after refactoring, still not getting functionality

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,56 +1,129 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
+
 import './Friend.css';
 
-const Friend = props => {
+class Friend extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      showUpdatedFriend: false,
+      name: '',
+      age: '',
+      email: ''
+    };
+  }
 
+  inputHandler = event => {
+    this.setState({ [event.target.name]:  event.target.value });
+  }
+  
+  showUpdatedFriend = () => {
+    this.setState({showUpdatedFriend: !this.state.showUpdatedFriend })
+  }
+
+  updateHandler = (friendId) => {
+    const friend = {};
+    if (this.state.name !== "" ){
+      friend.name = this.state.name;
+    }
+    if (this.state.name !== "" ){
+      friend.age = this.state.age;
+    }
+    if (this.state.name !== "" ){
+      friend.age = this.state.age;
+    }
+  axios
+    .put(`http://localhost:5000/friends/${friendId})`, friend) 
+    .then (response => {
+      this.setState({ showUpdatedFriend: false, name: '', age: '', email: '' });
+      this.setState({friends: response.data})
+    }) 
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+  deleteHandler = (friendId) => {
+    axios
+      .delete(`http://localhost:5000/friends/${friendId}`)
+      .then (response => { 
+        this.setState({friends: response.data})
+       })
+      .catch(err => console.log(err));
+  }
+  
+  updateHandler = (friendId) => {
+    const friend = {};
+    if (this.state.name !== "" ){
+      friend.name = this.state.name;
+    }
+    if (this.state.age !== "" ){
+      friend.age = this.state.age;
+    }
+    if (this.state.email !== "" ){
+      friend.age = this.state.age;
+    }
+  axios
+    .put(`http://localhost:5000/friends/${friendId})`, friend) 
+    .then (response => {
+      this.setState({ showUpdatedFriend: false, name: '', age: '', email: '' });
+      this.setState({friends: response.data})
+    }) 
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+  render(){
+    const { friend } = this.props;
   return (
-
-    <div className = "friend-info">
-    
-      <h2>{props.friend.name}</h2>
-      <h3>Age: {props.friend.age}</h3>
-      <h3>Email: {props.friend.email}</h3>
+      <div key={friend.id} className = "friend-info">
+        <h2>{friend.name}</h2>
+        <h3>Age: {friend.age}</h3>
+        <h3>Email: {friend.email}</h3>
       <div className = "button-container">
-      <button >Edit</button>
+      <button onClick = {this.showUpdatedFriend}>Edit</button>
 
-    {props.showUpdatedFriend ? (
+    {this.state.showUpdatedFriend ? (
       <div>
-        { ' '}
         {/* important note use name = 'age' since target.name=target.value, 'name' is standard syntax */}
         <input 
-        value = {props.name} 
-        onChange={props.inputHandler} 
+        value = {this.state.name} 
+        onChange={this.state.inputHandler} 
         type = 'text' 
         placeholder='name' 
         name = 'name' 
         />
         
         <input 
-        value = {props.age} o
-        nChange={props.inputHandler} 
+        value = {this.state.age} 
+        onChange={this.state.inputHandler} 
         type = 'text' 
         placeholder='age' 
         name = 'age' 
         />
         <input 
-        value = {props.email} 
-        onChange={props.inputHandler} 
+        value = {this.state.email} 
+        onChange={this.state.inputHandler} 
         type = 'text' 
         placeholder='email' 
         name = 'email' 
         />
         <button 
-        onClick = {props.onClick}>
+        onClick = {() => this.state.updateFriend(friend.id)}>
         Save Changes
         </button>
         </div>
     ) : null}
 
-
-      <button onClick = {() => this.deleteFriend()} > Remove </button>
+      <button onClick = {() => this.state.deleteFriend(friend.id)} > Remove </button>
       </div>
     </div>
+
+
   )
+}
 }
 
 export default Friend
