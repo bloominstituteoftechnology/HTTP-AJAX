d47376bb7c6529a14c436bbfcefa27b9b18c49bb
MichaelBoegner <cmikeb@gmail.com>
2018-08-09 15:54:01 -0400

    New Friend Adding Funcitonal
    Moving on to delete nad put

--- friends/src/App.js
+++ friends/src/App.js
@@ -18,29 +18,7 @@ export default class App extends Component {
     }
   }
 
-  inputHandler = e => {
-    this.setState({[e.target.name]: e.target.value})
-  }
-
-
-
-addNewFriend = () =>{
-  const newFriend = {
-    name: this.state.name,
-    age: this.state.age,
-    email: this.state.email
-  }
-
-  axios
-  .post("http://localhost:5000/friends", newFriend)
-  .then(response => {
-      this.setState({
-      friends: response.data
-    });
-  })
-  .catch((err) => console.log(err))
-}
-
+  
   componentDidMount(){
     axios
     .get("http://localhost:5000/friends")
@@ -50,15 +28,40 @@ addNewFriend = () =>{
     })
   }
 
+  inputHandler = e => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+
+
+  addNewFriend = () =>{
+    const newFriend = {
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email
+    }
+
+  axios
+  .post("http://localhost:5000/friends", newFriend)
+  .then(response => {
+      this.setState({
+      friends: response.data
+    })
+  })
+  .catch((err) => console.log(err))
+}
+
+
   render() {
-    console.log(this.state.form);
+    console.log(this.state.name);
     console.log(this.state.friends);
     return (
       <div className="app">
         {this.state.friends.map(friend => <Friends key={friend.id} friend={friend}/>)} 
           <FriendsForm 
           inputHandler={this.inputHandler}
-            submitHandler={this.submitHandler}
+          addNewFriend={this.addNewFriend}
+          value={this.state}
           />      
       </div>
     );
