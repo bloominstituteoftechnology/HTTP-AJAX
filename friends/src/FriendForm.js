919345982c9275de99eb4f6f4988398d362ec575
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-08-09 15:52:03 -0400

    got name to add

--- friends/src/FriendForm.js
+++ friends/src/FriendForm.js
@@ -2,55 +2,65 @@ import React from 'react';
 import axios from 'axios';
 
 export default class FriendForm extends React.Component {
-    state ={
-        name: '',
-        age: '',
-        email: '',
-    }
-
+        
+        state ={
+            name: '',
+            age: '',
+            email: '',
+        }
+    
+    
     handleChange = event => {
-        this.setState({[event.target.name]: event.target.value, [event.target.age]: event.target.value, [event.target.email]: event.target.value})
+        this.setState({
+            [event.target.name]: event.target.value,
+        })
     };
 
-    
-
-     handleSubmit = event => {
-         event.preventDefault();
-     }
-    
-     addNewFriend = (event) => {
-        const friend = {
+    addNewFriend = (event) => {
+        // event.preventDefault();
+        const newFriendObj = {
             name: this.state.name,
             age: this.state.age,
             email:this.state.email
-           }
+           };
+           axios.post('http://localhost:5000/friends', newFriendObj)
+           .then(response => {
+               this.setState({
+                   friends: response.data,
+                    name : '',
+                    age: '',
+                    email: ''
+               })
+           })
+           .catch((err) => console.log(err))
     }
      
      componentDidMount() {
-        axios.get(`http://localhost:5000/friends`)
-          .then(res => {
-            const persons = res.data;
-            this.setState({ persons });
-          })
-        }
+        axios.get(`http://localhost:5000/friends`).then(response => {
+            this.setState({
+               friends: response.data
+            });
+        });
+
+     }
+          
  
      render() {
         return (
           <div>
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={this.addNewFriend}>
               <label>
                 Friend Name:
                 <input type="text" name="name" onChange={this.handleChange} />
                 <input type="number" name="age" onChange={this.handleChange} />
                 <input type="email" name="email" onChange={this.handleChange} />
               </label>
-              <button type="submit">Add Friend</button>
+              <button type ="submit">Add Friend</button>
             </form>
           </div>
         )
       }
-    
-}
+    }
 
 
 
