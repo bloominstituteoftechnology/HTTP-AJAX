3d430cbfa535f87e4caa8ae1a9e6820ef957b4f5
johnoro <officialjohnorourke@gmail.com>
2018-08-09 10:29:46 -0400

    Added a tooltip.
    It seems to not work well in browsers other than chrome, though.

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,23 +1,25 @@
 import React from "react";
-import { Button, Alert } from "reactstrap";
+import { Button, Alert, UncontrolledTooltip } from "reactstrap";
 
 const Friends = props => {
   return (
     <div className="friends">
-      {props.friends.map(friend => (
+      {props.friends.map((friend, ind) => (
         <div className="friend" key={friend.id}>
           <h4>
-            <a href={`mailto:${friend.email}`}>{friend.name}</a> is {friend.age} years old.
+            <a href={`mailto:${friend.email}`}>{friend.name}</a> is {friend.age}{" "}
+            years old.
           </h4>
-          {
-            friend.hasOwnProperty('isNew') ?
-              <Alert color="success">New!</Alert>
-              : ''
-          }
+          {friend.hasOwnProperty("isNew") ? (
+            <Alert color="success">New!</Alert>
+          ) : ('')}
           <div className="friend-btns">
             <Button id={friend.id} onClick={props.update} color="success">
-              Update
+              <span id={`update${ind}`}>Update</span>
             </Button>
+            <UncontrolledTooltip placement="left" target={`update${ind}`}>
+              Use the form above, then click here to update.
+            </UncontrolledTooltip>
             <Button id={friend.id} onClick={props.delete} color="danger">
               Delete
             </Button>
