a2418a8ad378e01dc377aa276ef5e975966224ef
Carey <careycbaldwin@gmail.com>
2018-08-08 20:02:00 -0400

    built addfriend form and 'submitHandler' function

--- friends/src/App.js
+++ friends/src/App.js
@@ -25,12 +25,20 @@ componentDidMount() {
     .catch(error => console.error(error))
   }
 
+inputHandler = event => {
+  this.setState({ [event.target.name]:  event.target.value })
+}
+
   render() {
     return (
       <div className="App">
         <h1>
-          [ Friends ]
-        <AddFriend />
+          ( Friends )
+        <AddFriend inputHandler = {this.inputHandler} 
+        name = {this.state.name} 
+        age = {this.state.age}
+        email = {this.state.email} 
+        />
         </h1>
         <Friends friends = {this.state.friends} />
       </div>
