3b548a5d6a1bf7af4ff24be87cfa476f8e26b5fd
szincone <sawyerzincone@gmail.com>
2018-08-09 17:09:27 -0600

    styling finished on ind card

--- friends/src/components/friends/FriendCard.js
+++ friends/src/components/friends/FriendCard.js
@@ -2,7 +2,6 @@ import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 
 import "./Friend.css";
-import EditDeleteButtons from "../editDeleteButtons/EditDeleteButtons";
 
 const FriendCard = props => {
   console.log("FriendCard", props);
@@ -16,7 +15,6 @@ const FriendCard = props => {
         <h2>{props.friends.name}</h2>
         <h4>{props.friends.age}</h4>
         <h4>{props.friends.email}</h4>
-        <EditDeleteButtons />
       </Link>
     </Fragment>
   );
--- friends/src/components/friends/IndFriendCard.js
+++ friends/src/components/friends/IndFriendCard.js
@@ -3,33 +3,7 @@ import React, { Fragment } from "react";
 import EditDeleteButtons from "../editDeleteButtons/EditDeleteButtons";
 import "./Friend.css";
 
-// works sort of
-// const IndFriendCard = props => {
-//   let name = props;
-//   for (let i = 0; i < props.data.length; i++) {
-//     name = props.data[i].name;
-//     console.log(props.data[i].name);
-//   }
-//   console.log("IndFriendCardRoute", props.match.params.name);
-//   return (
-//     <Fragment>
-//       <div className="individualCard">
-//         <h2>Hey</h2>
-//         <h2>{name}</h2>
-
-//         <EditDeleteButtons data={props.data} />
-//       </div>
-//     </Fragment>
-//   );
-// };
-// end working
-
 const IndFriendCard = props => {
-  // let name = props;
-  // for (let i = 0; i < props.data.length; i++) {
-  //   name = props.data[i].name;
-  //   console.log(props.data[i].name);
-  // }
   let indName = "";
   let age = 0;
   let email = "";
@@ -47,8 +21,7 @@ const IndFriendCard = props => {
   return (
     <Fragment>
       <div className="individualCard">
-        <h2>Hey</h2>
-        <h2>{indName}</h2>
+        <h1>{indName}</h1>
         <h2>{age}</h2>
         <h2>{email}</h2>
 
@@ -57,123 +30,5 @@ const IndFriendCard = props => {
     </Fragment>
   );
 };
-
-// class IndFriendCard extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     console.log("Ind Props", props.data);
-//     this.state = {
-//       data: props.data,
-//       props: props,
-//       name: ""
-//     };
-//   }
-//   // for (let i = 0; i < props.data.length; i++) {
-//   //   name = props.data[i].name;
-//   //   console.log(props.data[i].name);
-//   // }
-//   // console.log("IndFriendCardRoute", props.match.params.name);
-//   render() {
-//     // for (let i = 0; i < this.state.data; i++) {
-//     //   let clickedName = this.state.props.match.params.name;
-//     //   let friendName = this.state.data;
-//     //   console.log("Clicky", clickedName);
-//     //   console.log("friendName", friendName);
-//     //   if (friendName[i].name === clickedName) {
-//     //     console.log("Clicked name", clickedName);
-//     //     this.setState({ name: this.state.props.match.params.name });
-//     //   }
-//     // }
-//     // console.log("Testing 5", clickedName);
-//     // {
-//     //   for (let i = 0; i < this.state.data; i++) {
-//     //     let clickedName = this.state.props.match.params.name;
-//     //     let friendName = this.state.data;
-//     //     console.log("Clicky", clickedName);
-//     //     console.log("friendName", friendName);
-//     //     if (friendName[i].name === clickedName) {
-//     //       console.log("Clicked name", clickedName);
-//     //       this.setState({ name: this.state.props.match.params.name });
-//     //     }
-//     //   }
-//     // }
-//     // {
-//     //   for (let i = 0; i < this.state.data; i++) {
-//     //     let clickedName = this.state.props.match.params.name;
-//     //     let friendName = this.state.data;
-//     //     console.log("Clicky", clickedName);
-//     //     console.log("friendName", friendName);
-//     //     if (friendName[i].name === clickedName) {
-//     //       console.log("Clicked name", clickedName);
-//     //       this.setState({ name: this.state.props.match.params.name });
-//     //     }
-//     //   }
-//     // }
-//     return (
-//       <Fragment>
-//         {/* {this.state.data.map(friend => (
-//           <div className="individualCard">
-//           <h2>Hey</h2>
-//           <h2>{friend.name}</h2>
-
-//           <EditDeleteButtons data={this.state.data} />
-//           </div>
-//         ))} */}
-//         <div className="individualCard">
-//           <h2>Hey</h2>
-//           <h2>{this.state.name}</h2>
-
-//           <EditDeleteButtons data={this.state.data} />
-//         </div>
-//       </Fragment>
-//     );
-//   }
-// }
-
-// class IndFriendCard extends React.Componet {
-//   constructor(props) {
-//     super(props);
-//     this.state={
-//       name: props.data[i].name
-//     }
-//   }
-//   let name = "";
-//   for (let i = 0; i < props.data.length; i++) {
-//     name = props.data[i].name;
-//     console.log(props.data[i].name);
-//     // props.name = props.data[i].name;
-//     // if (props.data[i].name.includes(props.match.params.name)) {
-//     //   console.log(
-//     //     "match occurded at index of "
-//     //     // props.indexOf(props.match.params.name)
-//     //   );
-//     // } else {
-//     //   // do nothing
-//     // }
-//   }
-//   console.log("IndFriendCardRoute", props.match.params.name);
-//   return (
-//     <Fragment>
-//       {/* <Route
-//         path={`/friends/${props.friends}`}
-//         render={props => <IndFriendCard {...props} data={props.data} />}
-//       /> */}
-//       {/* {props.data.map(friend => {
-//         <div className="individualCard">
-//           <h2>Hey</h2>
-//           <h3>{friend.name}</h3>
-
-//           <EditDeleteButtons data={props.data} />
-//         </div>;
-//       })} */}
-//       <div className="individualCard">
-//         <h2>Hey</h2>
-//         <h2>{name}</h2>
-
-//         <EditDeleteButtons data={props.data} />
-//       </div>
-//     </Fragment>
-//   );
-// };
 
 export default IndFriendCard;
