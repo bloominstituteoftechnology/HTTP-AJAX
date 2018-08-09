9ba553d18b4a8d5ebb89c403c8839994ae5987c8
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-08-09 13:24:21 -0700

    Major changes - Styled App
    Finished one of the stretch goals. Gave it a nice style. Used Facebook colors.

--- friends/src/components/friends.js
+++ friends/src/components/friends.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import './friends.css';
+
 export default class FriendList extends Component {
   constructor(props) {
     super(props);
@@ -23,17 +25,23 @@ export default class FriendList extends Component {
   }
 
   render() {
-    return (
-    <div>
-        <ul className="friend-grid">
-          {this.state.friends.map(friend => {
-            return <li key={friend.id} className="friend">
-                <div className="friend-name">{friend.name}</div>
-                <div className="friend-age">{`Age: ${friend.age}`}</div>
-                <div className="friend-email">{`Email: ${friend.email}`}</div>
-              </li>;
-          })}
-        </ul>
-        </div>
-    )};
+   return (
+      <div className="container">
+         <h1 className="custom-title">Submit your information!</h1>
+         <form className="custom-form">
+            <input placeholder="name" type="text"/>
+            <input placeholder="age" type="number"/>
+            <input placeholder="email" type="text"/>
+         </form>
+         <ul className="friend-grid">
+            {this.state.friends.map(friend => {
+               return <li key={friend.id} className="friend">
+                  <div className="friend-name">{`Name: ${friend.name}`}</div>
+                  <div className="friend-age">{`Age: ${friend.age}`}</div>
+                  <div className="friend-email">{`email: ${friend.email}`}</div>
+               </li>;
+            })}
+         </ul>
+      </div>
+   )};
 }
