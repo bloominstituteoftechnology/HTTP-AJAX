3331ba56aae92f3fe8867d9902c532408b3e3bff
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-08-09 10:04:46 -0400

    MVP completed

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,38 +1,16 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import {  } from "react-router-dom";
 
 import Friends from './components/Friends'
-
 import './App.css';
 
+const url = 'http://localhost:5000/friends';
+
 class App extends Component {
-  constructor() {
-    super();
-    this.state= {
-      friends: []
-    };
-  }
-
-
-  componentDidMount() {
-    axios.get('http://localhost:5000/friends')
-      .then(response => {
-        this.setState({
-          friends: response.data
-        });
-      })
-      .catch(error => {
-        console.error('We have an error', error);
-      });
-  }
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Friends List</h1>
-        </header>
-        <Friends friends={this.state.friends}/>
+        <Friends url={url}/>
       </div>
     );
   }
--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import axios from 'axios';
 
-const Friends = props => {
-    return (
-        <div>
-            {props.friends.map(friend => {
-                <div>
-                    <p>
-                        <a href={`mailto: ${friend.email}`}>{friend.name}</a> 
-                        is {friend.age} years old
-                    </p>
-                </div>
-            })}
-        </div>
-    )
+
+class Friends extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            friends: [],
+            url: props.url
+        }
+    }
+
+    componentDidMount() {
+        axios.get(this.state.url)
+            .then(response => {
+                this.setState({
+                    friends: response.data
+                });
+            });
+    }
+    render() {
+        return (
+            <div>
+                {this.state.friends.map(friend => {
+                   return (
+                   <div key={friend.id}>
+                        <p>
+                            <a href={`mailto: ${friend.email}`}>{friend.name}</a> 
+                            is {friend.age} years old
+                        </p>
+                    </div>
+                   )
+                })}
+            </div>
+        )
+    }
 }
 
 export default Friends;
