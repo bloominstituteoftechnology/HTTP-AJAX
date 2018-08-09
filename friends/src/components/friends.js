d3f714ae9212bdc78862fb6a4b72d5edfc0dc61f
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-08-09 15:57:47 -0700

    Major changes - Added Edit and Delete button
    Edit and Delete buttons are not functional yet.

--- friends/src/components/friends.js
+++ friends/src/components/friends.js
@@ -8,6 +8,7 @@ export default class FriendList extends Component {
     super(props);
     this.state = {
       friends: [],
+      id: null,
       name:'',
       age: null,
       email: ''
@@ -33,9 +34,7 @@ export default class FriendList extends Component {
      });
   }
 
-  addNewFriend= () => {
-   
-
+   addNewFriend= () => {
       const newFriendObj = {
          name: this.state.name,
          age: this.state.age,
@@ -50,7 +49,42 @@ export default class FriendList extends Component {
       .catch(error => {
          console.log('Error: ', error);
       });
-  }
+   }
+
+   // editFriend = (id) => {
+   //    const updatedFriendObj = {
+   //       name: this.state.name,
+   //       age: this.state.age,
+   //       email: this.state.email
+   //    }
+
+   //    axios
+   //    .put(`http://localhost:5000/friends/${id}`, updatedFriendObj)
+   //    .then(response => {
+   //       this.setState({ friends: response.data });
+   //    })
+   //    .catch(error => {
+   //       console.log('Error: ', error);
+   //    });
+   // }
+
+   // deleteFriend= () => {
+   //    const deleteFriendObj = {
+   //       name: this.state.name,
+   //       age: this.state.age,
+   //       email: this.state.email,
+   //       id: this.state.id
+   //    }
+
+   //    axios
+   //    .delete(`http://localhost:5000/friends/${id}`, deleteFriendObj)
+   //    .then(response => {
+   //       this.setState({ friends: response.data });
+   //    })
+   //    .catch(error => {
+   //       console.log('Error: ', error);
+   //    });
+   // }
 
   render() {
    return (
@@ -58,16 +92,20 @@ export default class FriendList extends Component {
          <h1 className="custom-title">Submit your information!</h1>
 
          <form className="custom-form">
-            <input onChange={this.handleChange} name="name" value={this.state.name} placeholder="name" type="text"/>
-            <input onChange={this.handleChange} name="age" value={this.state.age} placeholder="age" type="number"/>
-            <input onChange={this.handleChange} name="email" value={this.state.email} placeholder="email" type="email"/>
+            <input onChange={this.handleChange} name="id" value={this.state.id} placeholder="id" type="number"/>
+            <input onChange={this.handleChange} name="name" value={this.state.name} placeholder="*name" type="text"/>
+            <input onChange={this.handleChange} name="age" value={this.state.age} placeholder="*age" type="number"/>
+            <input onChange={this.handleChange} name="email" value={this.state.email} placeholder="*email" type="email"/>
             <button onClick={this.addNewFriend}>Submit</button>
+            <button onClick={this.editFriend}>Edit</button>
+            <button onClick={this.deleteFriend}>Delete</button>
          </form>
 
          <ul className="friend-grid">
             {this.state.friends.map(friend => {
                return <li key={friend.id} className="friend">
                   <div className="friend-name">{`Name: ${friend.name}`}</div>
+                  <div className="friend-id">{`id: ${friend.id}`}</div>
                   <div className="friend-age">{`Age: ${friend.age}`}</div>
                   <div className="friend-email">{`email: ${friend.email}`}</div>
                </li>;
