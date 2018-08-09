234a30673de3905974001de70534504dff0a1b2b
cubsfan46350 <cubsfan.46350@msn.com>
2018-08-09 13:53:39 -0500

    rendered friends successfully to screen

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import axios from 'axios';
 class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      friends:[]
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        this.setState(() => ({ friends: response.data }));
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+  }
   render() {
+    if (this.state.friends) {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <ul>
+          {this.state.friends.map(friend => (
+            <li key={friend.id}>{friend.name}</li>
+          ))}
+        </ul>
       </div>
     );
+  }
   }
 }
 
