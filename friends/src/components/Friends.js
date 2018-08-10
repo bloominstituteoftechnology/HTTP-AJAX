376aa1563fc2664e594b992b2c11be31bd068cc0
Charles Cortinas <crcortinas@gmail.com>
2018-08-09 19:01:44 -0500

    Add a friend
    Can now add a friend

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -6,13 +6,46 @@ class Friends extends React.Component {
         super(props);
         this.state = {
             friends: [],
-            url: props.url
+            url: props.url,
+            name: '',
+            age: '',
+            email: ''
         }
     }
 
     componentDidMount() {
-        axios.get(this.state.url).then(response => {
-          this.setState({ friends: response.data });
+        this.getFriends();
+    }
+
+    getFriends = () => {
+      axios.get(this.state.url).then(response => {
+        this.setState({ friends: response.data });
+      });
+    }
+
+    inputHandler = e => {
+      this.setState({
+        [e.target.name]: e.target.value
+      })
+    }
+
+    submitHandler = () => {
+      let name = this.state.name;
+      let age = this.state.age;
+      let email = this.state.email;
+      let newFriend = {name: name, age: age, email: email};
+      axios
+        .post(this.state.url, newFriend)
+        .then(response => {
+          this.setState({
+            name:'',
+            age: '',
+            email: ''
+          });
+          this.getFriends();
+        })
+        .catch(err => {
+          console.log(err);
         });
       }
 
@@ -30,6 +63,17 @@ class Friends extends React.Component {
                         </div>
                     )
                 })}
+                <div className='add-friend'>
+                  <h2>Add A Friend</h2>
+                  <form onKeyPress={key => {
+                      if(key.charCode === 13) this.submitHandler();
+                    }}>
+                    <input name='name' placeholder=' Enter Name' onChange={this.inputHandler} value={this.state.name} type='text' />
+                    <input name='age' placeholder=' Enter Age' onChange={this.inputHandler} value={this.state.age} />
+                    <input name='email' placeholder=' Enter Email' onChange={this.inputHandler} value={this.state.email} type='email' />
+                  </form>
+                  <button onClick={this.submitHandler} className='submit'>Submit</button>
+                </div>
             </div>
         )
     }
