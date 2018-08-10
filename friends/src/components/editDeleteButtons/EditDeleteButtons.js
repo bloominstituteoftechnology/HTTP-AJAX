dd1af595dd28b956f743357dbe30ea5fb256c69a
szincone <sawyerzincone@gmail.com>
2018-08-10 14:24:50 -0600

    changed home button to Link on ind cards

--- friends/src/components/editDeleteButtons/EditDeleteButtons.js
+++ friends/src/components/editDeleteButtons/EditDeleteButtons.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import "./EditDeleteButtons.css";
 
@@ -41,6 +42,7 @@ export default class EditDeleteButtons extends Component {
             onFocus={this.removeInputText}
           />
           <button onClick={this.updateChangeHandlerSubmit}>Edit</button>
+          <Link to={"/"}>Home</Link>
           <button onClick={this.updateChangeHandler}>Del</button>
         </form>
       </div>
