866a1bce93c50dab0a162f949370764fb6733278
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-08-09 21:29:02 -0500

    working
    I am attempting the edit user, when I press edit it is getting rid of the users info.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,98 +1,124 @@
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
-import List from './List'; 
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import List from "./List";
 
-const url = "http://localhost:5000/friends"
+const url = "http://localhost:5000/friends";
 
-const InputForm = (props) =>{
-  return(
+const InputForm = props => {
+  return (
     <div className="form-input">
-        <form onSubmit ={this.addNewFriend}>
-          <input type="text" onChange={props.handleChange} value={props.value.name} name="name" placeholder="Name" />
-          <input type="Number" onChange={props.handleChange} value={props.value.age}  name="age" placeholder="Age" />
-          <input type="email" onChange={props.handleChange} value={props.value.email}  name="email" placeholder="E-mail" />
-          <button onClick={props.addNewFriend}>Add New Friend</button>
-          <button onClick={props.editFriend}>Edit Friend</button>
-        </form>
-      </div>
-  )
-
-}
+      <form onSubmit={this.addNewFriend}>
+        <input
+          type="text"
+          onChange={props.handleChange}
+          value={props.value.name}
+          name="name"
+          placeholder="Name"
+        />
+        <input
+          type="Number"
+          onChange={props.handleChange}
+          value={props.value.age}
+          name="age"
+          placeholder="Age"
+        />
+        <input
+          type="email"
+          onChange={props.handleChange}
+          value={props.value.email}
+          name="email"
+          placeholder="E-mail"
+        />
+        <button onClick={props.addNewFriend}>Add New Friend</button>
+      </form>
+    </div>
+  );
+};
 class App extends Component {
-  constructor(){
-    super(); 
-    this.state={
-      friend: [], 
-      name:"", 
-      age: null, 
-      email: "", 
-    }
+  constructor() {
+    super();
+    this.state = {
+      friend: [],
+      name: "",
+      age: null,
+      email: ""
+    };
   }
-  componentDidMount(){
-    axios.get(url).then(response=>{
-      console.log(response);
-      this.setState({
-        friend: response.data
+  componentDidMount() {
+    axios
+      .get(url)
+      .then(response => {
+        console.log(response);
+        this.setState({
+          friend: response.data
+        });
       })
-   })
-   .catch(((err)=> console.log(err)))
+      .catch(err => console.log(err));
   }
-  handleChange = (event) => {
+  handleChange = event => {
     this.setState({
-      [event.target.name]: event.target.value, 
-    })
-  }
+      [event.target.name]: event.target.value
+    });
+  };
   addNewFriend = () => {
-    const newFriend ={
-      name: this.state.name, 
-      age: this.state.age, 
-      email: this.state.email,
-    }
-    axios.post(url, newFriend).then(response => {
-      this.setState({
-        friend: response.date
+    const newFriend = {
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email
+    };
+    axios
+      .post(url, newFriend)
+      .then(response => {
+        this.setState({
+          friend: response.date
+        });
       })
-    })
-    .catch(((err)=> console.log(err)))
-  }
+      .catch(err => console.log(err));
+  };
 
-  editFriend =(id)=>{
-    const updatedFriend ={
-      name: this.state.name, 
-      age: this.state.age, 
-      email: this.state.email,
-    }
-    axios.put(`${url}/${id}`, updatedFriend)
-    .then(response => {
-      this.setState({
-        friends: response.data
+  editFriend = (id) => {
+    const updatedFriend = {
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email
+    };
+
+    axios
+      .put(`${url}/${id}`, updatedFriend)
+      .then(response => {
+        this.setState({
+          friends: response.data
+        });
       })
-    })
-    .catch((err) => console.log(err))
-  }
-  deleteFriend =(id)=>{
-    axios.delete(`${url}/${id}`)
-    .then(response => {
-      this.setState({
-        friends: response.data
+      .catch(err => console.log(err));
+  };
+  deleteFriend = id => {
+    axios
+      .delete(`${url}/${id}`)
+      .then(response => {
+        this.setState({
+          friends: response.data
+        });
       })
-    })
-    .catch((err) => console.log(err))
-  }
+      .catch(err => console.log(err));
+  };
   render() {
     return (
       <div>
-      <div className="friend-list">
-      <List list={this.state.friend}
-      deleteFriend={this.deleteFriend} />
-      </div>
-      <InputForm
-      handleChange={this.handleChange}
-      value={this.state.friend}
-      addNewFriend={this.addNewFriend}
-      editFriend={this.editFriend}
-      />
+        <div className="friend-list">
+          <List 
+          list={this.state.friend} 
+          deleteFriend={this.deleteFriend} 
+          editFriend={this.editFriend}
+          />
+        </div>
+        <InputForm
+          handleChange={this.handleChange}
+          value={this.state.friend}
+          addNewFriend={this.addNewFriend}
+          editFriend={this.editFriend}
+        />
       </div>
     );
   }
--- friends/src/List.js
+++ friends/src/List.js
@@ -1,18 +1,26 @@
-import React from 'react'; 
-import './App.css';
+import React from "react";
+import "./App.css";
 
-const List = props =>{
-    return(
-        <div>
-            {props.list.map(item=>(
-                    <div className="friendList" key={item.id}>
-                    <section>{item.name}</section>
-                    <section>{item.age}</section>
-                    <section>{item.email}</section>
-                    <button onClick={()=>props.deleteFriend(item.id)}>Delete Friend</button>
-                    </div>
-            ))}
+const List = props => {
+  return (
+    <div>
+      {props.list.map(item => (
+        <div className="friendList" key={item.id}>
+          <section>{item.name}</section>
+          <section>{item.age}</section>
+          <section>{item.email}</section>
+          <button onClick={() => props.deleteFriend(item.id)}>
+            Delete Friend
+          </button>
+          <form onSubmit={() => props.editFriend(item.id)}>
+              <input onChange={props.handleChange} type="text" placeholder="name" />
+              <input onChange={props.handleChange} type="age" placeholder="age" />
+              <input onChange={props.handleChange} type="email" placeholder="email" />
+              <button type="submit" onClick={props.editFriend}>Edit Friend</button>
+            </form>
         </div>
-    )
-}
-export default List; 
+      ))}
+    </div>
+  );
+};
+export default List;
