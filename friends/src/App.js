a931569ada12e21c372641b105bf526ed2864335
AI2EJAY <JHammett1314@gmail.com>
2018-08-15 17:49:06 -0500

    Got Router working. Was having issues inside of yarn/node modules

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import Friend from "./components/Friend";
+import { Route, NavLink } from "react-router-dom";
 
 class App extends Component {
   constructor() {
-  super()
+    super();
 
-  this.state = {
-    friends:''
+    this.state = {
+      friends: [],
+      loading: true
+    };
   }
-}
   componentDidMount() {
-    axios.get("http://localhost:5000/friends").then(response => this.setState({freinds: response.data}))
+    axios.get("http://localhost:5000/friends").then(response =>
+      this.setState({
+        freinds: response.data,
+        loading: false
+      })
+    );
   }
+
   render() {
     return (
       <div className="App">
-      
+        <NavLink to={`/${this.state.friends.id}`} />
+
+        <Route path="/:id" component={Friend} />
+        <ul>
+          {this.state.friends.map(friend => (
+            <Friend key={friend.id} friends={friend} />
+          ))}
+        </ul>
+        <h1>hi</h1>
       </div>
     );
   }
