00e3b7bbe0cbcfddac793db9d7ff1eaed49a2c0c
Arthur Bates <aabatesj@gmail.com>
2018-08-15 16:55:37 -0400

    initial setup
    installed dependencies
    
    added friendslist component

---
+++ friends/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import { } from 'react-router-dom';
+import Friends from './components/FriendsList';
+
+const url = 'http://localhost:5000/friends';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Friends url={url} />
+      </div>
+    );
+  }
+}
+
+export default App;
