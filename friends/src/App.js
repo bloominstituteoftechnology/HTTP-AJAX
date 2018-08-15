c1a76a0ac06d34d9c31116c03a0d34322dc276ce
AI2EJAY <JHammett1314@gmail.com>
2018-08-15 15:50:43 -0500

    Friends are now in state

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+  super()
+
+  this.state = {
+    friends:''
+  }
+}
+  componentDidMount() {
+    axios.get("http://localhost:5000/friends").then(response => this.setState({freinds: response.data}))
+  }
   render() {
     return (
       <div className="App">
