28fba28b8fc079a4f69e367afda8611bddf34dca
bri79319 <bri79319@gmail.com>
2018-08-15 22:24:19 -0400

    more changes to code

--- friends/src/Friends.js
+++ friends/src/Friends.js
@@ -3,8 +3,8 @@ import React from "react";
 const Friends = props => {
   return (
     <div>
-      {props.friendsList.map(friend => (
-        <div>
+      {props.friendsList.map((friend, i) => (
+        <div key={i}>
           <h2>{friend.name}</h2>
           <h2>{friend.age}</h2>
           <h2>{friend.email}</h2>
