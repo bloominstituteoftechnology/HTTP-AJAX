3cb78ed8b1d5af776d4e32bd5a2ffedd2c3919b1
Noah-Amar <ne.amar@outlook.com>
2018-08-16 15:44:32 -0500

    friends displaying

--- friends/src/components/FriendsList.jsx
+++ friends/src/components/FriendsList.jsx
@@ -1,18 +1,36 @@
 import React, {Component} from 'react';
-
+import axios from 'axios';
+import Friend from './Friend';
 
 class Friends extends Component {
     constructor(props) {
         super(props);
         this.state = { 
             friends: [],
-            url:props.url
-         }
+        }
     }
+    
+   
+    getFriends = () => {
+        axios
+            .get(`http://localhost:5000/friends`)
+            .then(response => {
+                this.setState({friends: response.data})
+            })
+            .catch(error => {
+                console.log(error);
+             });
+            
+    }
+    componentDidMount() {
+        this.getFriends();
+    }
+
     render() { 
+      console.log(this.state);  
         return ( 
             <div>
-
+                {this.state.friends.map(friend => <Friend friend= {friend} />)}
             </div>
          );
     }
