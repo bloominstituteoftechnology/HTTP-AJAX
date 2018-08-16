f41bce4264fc0e02dd0f862da804101ec4145d02
Jon Simmons <jonsimo@aol.com>
2018-08-16 08:43:50 -0400

    Component work

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+let url = 'htt[://localhost:5000/friends'
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: [],
+      loading: true,
+      friendName: '',
+      friendAge: '',
+      friendEmail: '',
+      number: undefined
+    }
+  }
+
+  componentDidMount() {
+    axios.get(url).then(response => this.setState({friends: response.data, loading: false, number: response.data.length}));
+  }
+
+  
   render() {
     return (
       <div className="App">
