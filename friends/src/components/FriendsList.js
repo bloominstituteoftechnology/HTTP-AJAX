cf50e88e20d90a844846a79b618a859dc36ea5ff
Arthur Bates <aabatesj@gmail.com>
2018-08-16 16:52:39 -0400

    FriendsList refactored
    Friends now holds just a map of the friends array

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,49 +1,15 @@
 import React from 'react';
-import axios from 'axios';
+import Friend from './Friend';
 
-class Friends extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            friends: [],
-            url: props.url
-        }
-    }
 
-    componentDidMount() {
-        axios.get(this.state.url).then(response => {
-            this.setState({ friends: response.data });
-        });
-    }
+const FriendList = props => {
 
-    render() {
-        console.log(this.state.url);
-        return (
-            <table>
-                <thead>
-                    <tr>
-                        <th colSpan="3">Friend Information</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Name</td>
-                        <td>Age</td>
-                        <td>Email</td>
-                    </tr>
-                    {this.state.friends.map(friend => {
-                        return (
-                            <tr key={friend.id} className='friend'>
-                                <td>{friend.name}</td>
-                                <td>{friend.age}</td>
-                                <td>{friend.email}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        )
-    }
+    return (
+        <div>
+            {props.friends.map(item => <Friend friend={item} key={item.id} />)}
+        </div>
+    )
 }
 
-export default Friends; 
+
+export default FriendList;
