18a8599935d3cb1d461d50b007d0d461e4bc817c
decagondev <decagondevelopment@gmail.com>
2018-09-12 22:27:48 +0100

    (DAY 2 MVP) added the submit functionality and done the post request to update the data (MVP DAY 2 COMPLETE)

--- friends/src/App.js
+++ friends/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
   };
 
   componentDidMount() {
+    // use axios to send a get request to the api and pull in the friends data
     axios
       .get("http://localhost:5000/friends")
       .then(response => {
@@ -22,8 +23,26 @@ class App extends Component {
       });
   }
 
+  // handle changes to input fields : trying to do it fairly dry
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
+  }
+
+  // handle the submit of a new friend
+  handleFriendSubmit = () => {
+    // get the id of the data item
+    const id = (this.state.friendsData[this.state.friendsData.length - 1].id + 1);
+
+    // setup the data for the next friend
+    const nextFriend = { id: id, name: this.state.name, age: this.state.age, email: this.state.email };
+
+    // use the axios library to send a post request to the api and update the state
+    axios
+      .post("http://localhost:5000/friends", nextFriend)
+      .then(response => {
+        this.setState({ friendsData: response.data, name: "", age: "", email: "" });
+      })
+      .catch(error => console.log(error));
   }
 
   render() {
@@ -52,6 +71,7 @@ class App extends Component {
             onChange={this.handleChange}
           />
         </form>
+        <button onClick={this.handleFriendSubmit}>Submit</button>
         <FriendsList friends={this.state.friendsData} />
       </div>
     );
