fa8feda7c7e2bee4f58e1daa291bf36914defd2b
JeanetteFernandez <jeanfern555@gmail.com>
2018-09-12 16:36:25 -0500

    Completed MVP #1-5.

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+//import Friend from './Friend';
+//import axios from 'axios';
+
+class FriendsLists extends React{
+    // constructor(){
+    //     super();
+    //     this.state={
+    //         friends:[],
+    //     }
+    // }
+
+    // componentDidMount(){
+    //     axios
+    //         .get('http://localhost:5000/friends')
+    //         .then(response =>{
+    //             console.log(response);
+    //             this.setState({friends:response.data});
+    //         } )
+    //         .catch(err => console.log(err));
+    // }
+
+    // render(){
+    //     return(
+    //         <div className="friends-list">
+    //             <
+    //         </div>
+    //     )
+    // }
+}
+
+export default FriendsLists;
