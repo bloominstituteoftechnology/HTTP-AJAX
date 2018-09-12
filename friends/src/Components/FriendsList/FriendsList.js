1ead2b0094a490caa9110cf302d08cfdab5e0993
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-09-12 17:26:24 -0400

    Create component to display friendslist on server

---
+++ friends/src/Components/FriendsList/FriendsList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+ const FriendsList = props => {
+    if(props.loading){
+        return <h2>Loading Data...</h2>
+    }
+    return (
+        <div>
+            {props.friends.map(friend =>
+                <div key={friend.id}>
+                    <p>Name: {friend.name}</p>
+                    <p>Age: {friend.age}</p>
+                    <p>Email: {friend.email}</p>
+                </div>
+            )}
+        </div>
+    )
+ }
+ export default FriendsList; 
