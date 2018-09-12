5f7166ecc5e8535e5ef22d5a468aba2c90dc98a4
mparedes003 <michelle.g.paredes@gmail.com>
2018-09-12 16:29:37 -0700

    Set a max-width on .App in App.css. Fixed alignment of code in App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -6,22 +6,22 @@ import axios from 'axios';
 
 class App extends Component {
   constructor() {
-      super();
-      this.state={
-          friends: []
-      };
-    }
+    super();
+    this.state = {
+      friends: []
+    };
+  }
 
-    componentDidMount() {
-        axios
-          .get(`http://localhost:5000/friends`)
-          .then (response => {
-            // console.log(response.data);
-            this.setState({ friends: response.data });
-          })
-           .catch(err => {
-             console.log(err);
-          });
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/friends`)
+      .then(response => {
+        // console.log(response.data);
+        this.setState({ friends: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
   render() {
     return (
@@ -30,13 +30,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to my HTTP-AJAX Project</h1>
         </header>
-        
-      {this.state.friends.map(friend => <div className={"friend"}key={friend.id} friend={friend} >
-        {/* Friend's Info Here */}
-        <p>Name: {friend.name}</p>
-        <p>Age: {friend.age}</p>
-        <p>Email: {friend.email}</p>
-      </div>)}
+        <div className="friends-container">
+          {this.state.friends.map(friend => <div className={"friend"} key={friend.id} friend={friend} >
+            {/* Friend's Info Here */}
+            <p>Name: {friend.name}</p>
+            <p>Age: {friend.age}</p>
+            <p>Email: {friend.email}</p>
+          </div>)}
+        </div>
       </div>
     );
   }
