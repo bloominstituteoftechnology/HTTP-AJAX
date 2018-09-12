f767b049bae23ee0bb1ee8e3d5802bd8a615be6f
unknown <cornielleandres@gmail.com>
2018-09-12 18:22:48 -0400

    Style MainPage.

--- friends/src/components/MainPage.js
+++ friends/src/components/MainPage.js
@@ -1,20 +1,38 @@
 // React
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
+// Dependencies
+import PropTypes from 'prop-types';
+
 // Styling
+import './MainPage.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'reactstrap';
 
-const MainPage = () => {
+const MainPage = (props) => {
     return(
-        <Fragment>
+        <div className = 'main-page'>
+            <h1>Welcome to your Friends App!</h1>
+            <h3>You currently have { props.friends.length } friends.</h3>
             <Link to = '/postfriend'>
                 <Button color="primary">Add New Friend</Button>
             </Link>
-            <Link to = '/friendslist'>Go to friends list</Link>
-        </Fragment>
+            <Link to = '/friendslist'>
+                <Button color="info">View Friends List</Button>
+            </Link>
+        </div>
     );
+}
+
+MainPage.propTypes = {
+    friends: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        age: PropTypes.number,
+        email: PropTypes.string,
+        color: PropTypes.string
+    }))
 }
 
 export default MainPage;
