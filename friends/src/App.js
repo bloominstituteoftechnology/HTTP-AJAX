5103fffeadf9ea743e71e7330fef757d661e2d6b
Clint Kunz <clintkunz@gmail.com>
2018-09-12 14:26:15 -0600

    created component that displays the list of friends from the server

---
+++ friends/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Friend from './Friend';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+    .then(response => {
+      console.log(response);
+      this.setState({
+        friends: response.data
+      })
+    })
+    .catch(err => console.log(err));
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {this.state.friends.map(friend => <Friend friend={friend} key={friend.id} />)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ friends/src/Friend.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Friend(props) {
+    return (
+        <li>Name: {props.friend.name}, Age: {props.friend.age}, Email: {props.friend.email}</li>
+    )
+}
+
+export default Friend;
