49ffe1a12c8148f72acf7af695ad35059d454c67
Timothy Hoang <timh1203@gmail.com>
2018-09-12 15:53:04 -0400

    Add render of friends

--- friends/src/App.js
+++ friends/src/App.js
@@ -19,9 +19,21 @@ class App extends Component {
   }
 
   render() {
+    const {friends} = this.state
     return (
       <div className="App">
-        <h1>Axios</h1>
+        <h1>Axios Friends</h1>
+        {
+          friends.map(friend => {
+            return (
+              <div key={friend.id}>
+                <h1>{friend.name}</h1>
+                <h3>{friend.email}</h3>
+                <p>{friend.name} is {friend.age} years old.</p>
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
