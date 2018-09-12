e29cfb1bfe8243ba11b397cbbd8f9388e68ef4c2
Kevin Nguyen <kevinn858@gmail.com>
2018-09-12 15:06:59 -0700

    created a FriendList component and Routed it on the App

---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendList = (props) =>{
+    return(
+        <div>
+            This should be a FriendList.
+        </div>
+    )
+}
+
+export default FriendList;
