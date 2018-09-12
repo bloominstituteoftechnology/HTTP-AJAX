b5dfd22eec0fd70e39fe476e6f3f2848c616d41d
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-12 15:21:07 -0400

    Set state to friends data

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import FriendsList from "./components/FriendsList"
 
 class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+        friends:[]
+      };
+    }
+
+    componentDidMount() {
+      axios
+        .get('http://localhost:5000/friends')
+        .then(response => {
+          console.log(response)
+          this.setState({ friends: response.data.data});
+        })
+        .catch(err => console.log(err));
+    }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div>
+        <h1>List of Friends</h1>
+          <FriendsList/>
+        )}
+      </div>;
   }
 }
 
