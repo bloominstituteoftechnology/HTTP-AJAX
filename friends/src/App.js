f906fdf0969c08d5b119fe06a950f10c1f570fa4
decagondev <decagondevelopment@gmail.com>
2018-09-12 22:09:04 +0100

    (DAY 2 MVP) added a form structure to the app component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import FriendsList from './components/FriendsList';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import FriendsList from "./components/FriendsList";
+import "./App.css";
 
 class App extends Component {
-
   state = {
     friendsData: [],
     name: "",
     age: "",
     email: ""
-  }
+  };
 
   componentDidMount() {
-    axios.get("http://localhost:5000/friends").then(response => {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(response => {
         this.setState({ friendsData: response.data });
-      }).catch(err => {
-        console.log(err);
       })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render() {
     return (
       <div className="App">
+        <form>
+          <input
+            type="text"
+            placeholder="name..."
+            name="name"
+          />
+          <input
+            type="text"
+            placeholder="age..."
+          />
+          <input
+            type="text"
+            placeholder="email..."
+            name="email"
+          />
+        </form>
         <FriendsList friends={this.state.friendsData} />
       </div>
     );
