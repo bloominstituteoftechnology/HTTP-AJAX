72fdd1f673dc47a6830c7f4f475f89a4af3d2356
Andrew <ghobrial.andrew@gmail.com>
2018-09-12 18:35:02 -0400

    working through map fxn of friendlist

--- friends/src/Components/FriendList.js
+++ friends/src/Components/FriendList.js
@@ -12,8 +12,7 @@ class FriendList extends Component {
       .get('http://localhost:5000/friends')
       .then(response => {
         console.log ("this is the response:", response);
-        this.setState(() => ({friendList: response.data }));
-        console.log ("this is friendList object:", this.state.friendList);
+        this.setState({friendList: response.data }, () => console.log(this.state.friendList));
       })
       .catch(error => {
         console.error('Server Error', error);
@@ -23,10 +22,32 @@ class FriendList extends Component {
   render() {
     return (
       <div className="friend-list">
-       
+       {this.state.friendList.map(friend => (
+          <FriendDetails key={friend.id} friend={friend} />
+        ))}
       </div>
     );
   }
+
 }
+
+function FriendDetails({ friend }) {
+  const { name, age, email } = friend;
+  return (
+    <div className="movie-card">
+      <h2>{name}</h2>
+      <div className="name">
+        Name: <em>{name}</em>
+      </div>
+      <div className="age">
+        Age: <strong>{age}</strong>
+      </div>
+      <div className="email">
+        Email: <strong>{email}</strong>
+      </div>
+    </div>
+  );
+}
+
 
 export default FriendList;
