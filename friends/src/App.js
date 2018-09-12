6345f243b7d31487a69292113d2726ffb667ebd9
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-09-12 15:52:51 -0400

    added Friend component

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import Friend from './Friend';
 
 class App extends Component {
   constructor() {
---
+++ friends/src/Friend.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Friend = props => {
+  return (
+    <div>
+      {props.friend.name}
+      {props.friend.age}
+      {props.friend.email}
+    </div>
+  );
+};
+
+export default Friend;
