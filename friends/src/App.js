6a28b5cabc45533edf15c7341b07920d77bd5b26
Joannah Laluces <joannahlaluces@Joannahs-MacBook-Air.local>
2018-09-12 14:39:12 -0700

    Finished basic tasks  and working on a little styling

---
+++ friends/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Friends_Logo from './Friends_Logo.svg';
+import './App.css';
+import axios from 'axios';
+import FriendContainer from './components/FriendContainer';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friendsList: []
+    }
+  }
+
+  componentDidMount() {    
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        console.log(response)
+        this.setState({ friendsList: response.data });
+        console.log(response.data);
+      })
+      .catch(err => console.log(err));
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={Friends_Logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to My Friends List!</h1>
+        </header>
+        <div className='friend-list'>
+          <FriendContainer friends={this.state.friendsList} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// Inside your React application, create a component to display the list of friends coming from the server.
