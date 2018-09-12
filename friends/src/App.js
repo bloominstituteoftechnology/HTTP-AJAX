810eb9de14b4c5482356a367098c7e4ce0cebb57
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-12 18:40:25 -0400

    Created an add new friend function with a post request

--- friends/src/App.js
+++ friends/src/App.js
@@ -27,17 +27,33 @@ class App extends Component {
     handleChange = (event) => {
       this.setState({
       [event.target.name]: event.target.value
-      });
+      })
+    }
+
+    addNewFriend = () => {
+      const newFriend = {
+        name: this.state.name,
+        age: this.state.age,
+        email: this.state.email
+      }
+
+      axios.post('http://localhost:5000/friends', newFriend)
+      .then(response => {
+        this.setState({
+          friends: response.data
+        });
+      })
+      .catch(err => console.log(err));
     };
 
   render() {
     return <div className="app">
         <h1>List of Friends</h1>
-        <form onSubmit="">
+        <form onSubmit={this.addNewFriend}>
           <input onChange= {this.handleChange} name= "name" value={this.state.name} placeholder= "name" type= "text" />
           <input onChange= {this.handleChange} name= "age" value={this.state.age} placeholder= "age" type= "number" />
           <input onChange= {this.handleChange} name= "email" value={this.state.email} placeholder= "email" type= "email" />
-          <button onClick="" >Add Friend</button>
+          <button onClick= {this.addNewFriend} >Add Friend</button>
         </form>
           <FriendsList
             friends={this.state.friends} 
