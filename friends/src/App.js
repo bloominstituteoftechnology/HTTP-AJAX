6f7c671fa613dd32674690f6552d83d01d51735e
Timothy Hoang <timh1203@gmail.com>
2018-09-12 15:42:56 -0400

    Add basic app component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      friends: []
+    }
+  }
+
+  componentDidMount() {
+
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Hello
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
