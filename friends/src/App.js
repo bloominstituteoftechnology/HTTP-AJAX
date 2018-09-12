5237dc01f65f997dfa85156e8a33dfd0a5e16366
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-09-12 17:04:42 -0600

    Make add friend form. Pull and render data.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import Friends from './components/Friends';
+import AddFriendForm from './components/AddFriendForm';
 
 class App extends Component {
+  constructor () {
+    super();
+
+    this.state = {
+      currentname: "",
+      currentage: "",
+      currentemail: ""
+    }
+
+    this.state = {
+      friends: []
+  };
+  }
+
+  formChange = (event) => {
+    this.setState({
+      [`current${event.currentTarget.name}`]: event.currentTarget.value
+    });
+
+    console.log(event.currentTarget.value)
+  }
+
+  formSubmit = () => {
+    
+  }
+
+  addFriend(event) {
+
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends').then(response => {
+        this.setState({friends:response.data})
+    })
+}
+
   render() {
     return (
       <section className="App">
       <h1>Friends</h1>
-
-      <Friends />
+      <AddFriendForm formChange={this.formChange}/>
+      <Friends friends={this.state.friends}/>
 
       </section>
     );
