ba7a45cbcf21f958d5211c87ca20c6a2e2b05ea8
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-12 18:25:26 -0400

    Added a form to add a new friend to the list

--- friends/src/App.js
+++ friends/src/App.js
@@ -7,7 +7,10 @@ class App extends Component {
     constructor() {
       super();
       this.state = {
-        friends:[]
+        friends:[],
+        name: "",
+        age: null,
+        email: ""
       };
     }
 
@@ -21,9 +24,21 @@ class App extends Component {
         .catch(err => console.log(err));
     }
 
+    handleChange = (event) => {
+      this.setState({
+      [event.target.name]: event.target.value
+      });
+    };
+
   render() {
     return <div className="app">
         <h1>List of Friends</h1>
+        <form onSubmit="">
+          <input onChange= {this.handleChange} name= "name" value={this.state.name} placeholder= "name" type= "text" />
+          <input onChange= {this.handleChange} name= "age" value={this.state.age} placeholder= "age" type= "number" />
+          <input onChange= {this.handleChange} name= "email" value={this.state.email} placeholder= "email" type= "email" />
+          <button onClick="" >Add Friend</button>
+        </form>
           <FriendsList
             friends={this.state.friends} 
           />
