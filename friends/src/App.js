b4740052dedb6a623516e14630c0c2e76685cb4d
Vera Simon <ghjkl>
2018-09-12 15:46:48 -0700

    HTTP AJAX Day II
    Realized that server.js doesnt maintain state between client refreshes, so deleted server.js.orig
    Implemented friend adding form with expected data
    Typecast the incoming data like h*ck
    Moving on to the stretch

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,29 +1,72 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
+import FriendBuilder from './components/friendbuilder';
 import FriendsList from './components/friendslist';
 
 class App extends Component {
   constructor() {
     super();
+    this.dataSource = 'http://localhost:5000/friends';
     this.state = {
-      friends: []
+      friends: [],
+      name: '',
+      age: 0,
+      email: ''
     };
   }
 
-  componentDidMount() {
+  componentDidMount = () => {
+    this.getServerData();
+  };
+
+  getServerData = () => {
     Axios
-      .get('http://localhost:5000/friends')
+      .get(this.dataSource)
       .then( (response) => this.setFriends(response.data) )
       .catch( (err) => console.error(err) );
-  }
+  };
 
-  setFriends(friendArr) {
-    this.setState({friends: friendArr});
-  }
+  setFriends = (friendArr) => {
+    this.setState({
+      friends: friendArr,
+      name: '',
+      age: 0,
+      email: ''
+    });
+  };
+
+  postFriend = (event) => {
+    event.preventDefault();
+    if(event.target.name && event.target.age && event.target.email) {
+      Axios
+        .post(this.dataSource, {
+          name: this.state.name,
+          age: this.state.age,
+          email: this.state.email
+        })
+        .then( (response) => this.setFriends(response.data) )
+        .catch( (err) => console.error(err) );
+    }
+  };
+
+  handleInput = (event) => {
+    if(event.target.name === 'age') {
+      this.setState({ [event.target.name]: Number(event.target.value) });
+    } else {
+      this.setState({ [event.target.name]: event.target.value.toString() });
+    }
+  };
 
   render() {
     return (
       <div>
+        <FriendBuilder 
+          handleInput={this.handleInput} 
+          postFriend={this.postFriend} 
+          currentName={this.state.name} 
+          currentAge={this.state.age} 
+          currentEmail={this.state.email}  
+        />
         <FriendsList friends={this.state.friends} />
       </div>
     );
