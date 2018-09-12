2c40fda338f07ac1f82699088de49edf2b1bab00
decagondev <decagondevelopment@gmail.com>
2018-09-12 20:48:21 +0100

    (DAY 1 MVP) : displaying the friends list in the browser after linking up the friends, and friends list component in the app component (DAY 1 MVP COMPLETED)

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import FriendsList from './components/FriendsList';
 import './App.css';
 
 class App extends Component {
@@ -23,6 +23,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <FriendsList friends={this.state.friendsData} />
       </div>
     );
   }
