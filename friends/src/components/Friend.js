aca6819e1ad7396ea1d81fb243f2274685b752cc
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-09-12 15:12:55 -0500

    MVP completed

---
+++ friends/src/components/Friend.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+const Friend = props => {
+  return (
+    <div>
+      <h1>{props.friend.name}</h1>
+      <h3>{props.friend.age}</h3>
+      <h3>{props.friend.email}</h3>
+    </div>
+  );
+};
+
+export default Friend;
