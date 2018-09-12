a7dde3594a3be246bf67d268717ca0201a4ac463
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-12 15:21:25 -0400

    Created Friend and FriendsList components

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Friend from "./Friend"
+
+const FriendsList = props => {
+    return (
+    <div>
+        {/* {props.friends.map((friend,i) => (
+            <Friend
+                friend={friend}
+                key={i}
+            />
+        ))} */}
+    </div>);
+};
+
+export default FriendsList;
