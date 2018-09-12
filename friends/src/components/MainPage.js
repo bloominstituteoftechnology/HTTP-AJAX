9a53ab3ee41f1169d2b2aacaaeca1ee9ccbe670d
unknown <cornielleandres@gmail.com>
2018-09-12 18:11:28 -0400

    Add reactstrap for styling.

--- friends/src/components/MainPage.js
+++ friends/src/components/MainPage.js
@@ -2,10 +2,16 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
+// Styling
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+
 const MainPage = () => {
     return(
         <Fragment>
-            <Link to = '/postfriend'>Add new friend</Link>
+            <Link to = '/postfriend'>
+                <Button color="primary">Add New Friend</Button>
+            </Link>
             <Link to = '/friendslist'>Go to friends list</Link>
         </Fragment>
     );
