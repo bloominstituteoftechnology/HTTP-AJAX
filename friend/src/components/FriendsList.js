c034db1c59421bc36abb551dfb1c14ffa035403c
Csillag61 <csillagok@gmail.com>
2018-09-12 20:25:36 -0400

    FriendsList rendering

--- friend/src/components/FriendsList.js
+++ friend/src/components/FriendsList.js
@@ -1,6 +1,7 @@
 import React from 'react';
-
+import {Link} from "react-router-dom";
 import axios from 'axios';
+import { render } from 'react-dom';
 
 class FriendsList extends React.Component{
     constructor(props){
@@ -13,9 +14,33 @@ class FriendsList extends React.Component{
     componentDidMount(){
         axios.get('http://localhost:5000/friends').then(res=>this.setState({friends:res.data, loading:false})).catch(err=>console.log(err));
     }
-}
+
+render(){
+    if(this.state.friends.length===0 && this.state.loading===false){
+        return(
+            <h1>Sorry, no data</h1>
+        )
+        }
+    return (
+        <div className="friendsList">
+            {this.state.friends.map((e,i)=>
+            <Link to={`/friends/${e.id}`}key={i}>
+                <div className='card'>
+                        
+                        <p>{e.name}</p>
+                        <p>{e.age}</p>
+                        <p>{e.email}</p>
+                        <p>{e.address}</p>
+                    </div>
+   
+                
+            </Link>)}
+        </div>
+    
+            
 
 
+            )}}
 
 
 export default FriendsList;
--- friend/src/index.js
+++ friend/src/index.js
@@ -10,4 +10,5 @@ import { BrowserRouter as Router } from 'react-router-dom';
 ReactDOM.render(
 <Router><App /></Router>, 
 document.getElementById('root'));
+
 registerServiceWorker();
