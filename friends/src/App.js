1fa01f52d2ebd5b6f61639734bd9b431fcf50b1b
Michael Agard <mdagard@gmail.com>
2018-09-13 10:07:19 -0400

    added new feature to add new friends with age

--- friends/src/App.js
+++ friends/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Friends List</h1>
         <FriendList />
       </div>
     );
--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -5,7 +5,8 @@ export default class FriendList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      friends: []
+      friends: [],
+      inputText: ''
     };
   }
 
@@ -19,16 +20,57 @@ export default class FriendList extends Component {
         console.error(err);
       });
   }
+  addNewFriend = event => {
+    event.preventDefault();
+    if (this.state.inputText) {
+      this.setState({
+        friends: [
+          ...this.state.friends,
+          {
+            name: this.state.inputText,
+            age: this.state.inputTextAge,
+            key: this.state.inputText,
+            className: 'friend',
+            id: Date.now()
+          }
+        ],
+        inputText: ''
+      });
+    }
+  };
+  handleInput = event => {
+    this.setState({
+      inputText: event.target.value
+    });
+  };
+  handleInputAge = event => {
+    this.setState({
+      inputTextAge: event.target.value
+    });
+  };
 
   render() {
     return (
       <div className="friend-list">
         {this.state.friends.map(friend => (
-          <div key={friend.name} className={friend.name}>
-            {friend.name}
+          <div key={friend.name} className="friend">
+            {friend.name} | {friend.age}
           </div>
         ))}
         {console.log(`Friends ${this.state.friends}`)}
+        <input
+          autoFocus
+          value={this.inputText}
+          onChange={this.handleInput}
+          placeholder={'Add New Friend'}
+        />
+        <input
+          autoFocus
+          value={this.inputTextAge}
+          onChange={this.handleInputAge}
+          placeholder={'Age'}
+        />
+        <button onClick={this.addNewFriend}>Add New Friend</button>
       </div>
     );
   }
