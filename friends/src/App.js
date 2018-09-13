a954512b5dd3c17d945df1974b569f7275953e37
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-09-13 15:54:35 -0700

    NewFriendForm and FriendsList separated into separate components both rendered in App

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,12 +2,74 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FriendsList from './components/FriendsList.js';
+import NewFriendForm from './components/NewFriendForm.js';
+import axios from 'axios';
+import './components/Friends.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        friends: [],
+        newFriend: {
+            name: '',
+            age: '',
+            email: '',
+        }
+    };
+}
+  
+  componentDidMount() {
+    axios
+        .get('http://localhost:5000/friends')
+        .then(response => {
+            this.setState({ friends: response.data });
+            
+        })
+        .catch(err => console.log(err));
+}
+
+handleNewFriendInput = event => {
+    this.setState({ 
+        newFriend: {
+            ...this.state.newFriend,
+            [event.target.name] : event.target.value 
+        }
+    });
+}
+
+addNewFriend = event => {
+    event.preventDefault();
+    axios
+        .post('http://localhost:5000/friends', this.state.newFriend )
+        .then(response => {
+            console.log(response.data);
+            this.setState({ 
+                friends: response.data, 
+                newFriend: {
+                    ...this.state.newFriend,
+                    newFriendName: '', 
+                    newFriendAge: '', 
+                    newFriendEmail: '',
+                }
+            });
+        })
+        .catch(err => console.log(err));
+       
+    
+}
+
   render() {
     return (
       <div>
-        <FriendsList/>
+        <NewFriendForm 
+          newFriendName={this.state.newFriendName}
+          newFriendAge={this.state.newFriendAge}
+          newFriendEmail={this.state.newFriendEmail}
+          handleNewFriendInput={this.handleNewFriendInput}
+          addNewFriend={this.addNewFriend}
+        />
+        <FriendsList friends={this.state.friends} />
       </div>
     );
   }
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -5,83 +5,20 @@ import './Friends.css';
 import NewFriendForm from './NewFriendForm.js';
 
 
-class FriendsList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            friends: [],
-            newFriend: {
-                newFriendName: '',
-                newFriendAge: '',
-                newFriendEmail: '',
-            }
-        };
-    }
-
-    componentDidMount() {
-        axios
-            .get('http://localhost:5000/friends')
-            .then(response => {
-                this.setState({ friends: response.data });
-                
-            })
-            .catch(err => console.log(err));
-    }
-
-    handleNewFriendInput = event => {
-        this.setState({ 
-            newFriend: {
-                ...this.state.newFriend,
-                [event.target.name] : event.target.value 
-            }
-        });
-    }
-
-    addNewFriend = event => {
-        event.preventDefault();
-        console.log(this.state.newFriend);
-        axios
-            .post('http://localhost:5000/friends', this.state.newFriend )
-            .then(response => {
-                // const friends = [...this.state.friends];
-                // friends.push(this.state.newFriend);
-                console.log(response.data);
-                this.setState({ 
-                    friends: response.data, 
-                    newFriend: {
-                        ...this.state.newFriend,
-                        newFriendName: '', 
-                        newFriendAge: '', 
-                        newFriendEmail: '',
-                    }
-                });
-            })
-            .catch(err => console.log(err));
-           
-        
-    }
-
-    render() {
-        return(
-           <div className='friends-list'>
-               {this.state.friends.map(friend => (
+const FriendsList = props => {
+    return(
+        <div className='friends-list'>
+            {props.friends.map(friend => (
                 <div className='friend' key={friend.id}>
                     <p>Name: {friend.name}</p>
                     <p>Age: {friend.age}</p>
                     <p>Email: {friend.email}</p>
                 </div>
-               ))}
-               <NewFriendForm 
-                newFriendName={this.state.newFriendName}
-                newFriendAge={this.state.newFriendAge}
-                newFriendEmail={this.state.newFriendEmail}
-                handleNewFriendInput={this.handleNewFriendInput}
-                addNewFriend={this.addNewFriend}
-                />
-           </div>
-            
-        );
-    }
+            ))}
+        </div>
+        
+    );
+
 
    
 }
