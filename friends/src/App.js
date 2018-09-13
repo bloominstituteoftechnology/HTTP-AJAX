49c2f56fd9f7a9ba46392546414064d2c3aded19
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-09-13 17:13:07 -0400

    Day II MVP Complete Separated the list of friends and the new friend form Implemented Update and Delete functionality Added slight styling

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,10 +2,48 @@ import React, { Component } from 'react';
 import FriendsList from './Components/FriendsList/FriendsList';
 import './App.css';
 import axios from 'axios';
- 
+import {Route, Link} from 'react-router-dom';
+import NewFriend from './Components/FriendsList/NewFriend';
+import styled from 'styled-components';
+
 const url = 'http://localhost:5000/friends';
 
+const Body = styled.div`
+  margin: 0 auto;
+  max-width: 800px;
+  height: 1000px
+`
+const Header = styled.header`
+  font-size: 1rem;
+  font-weight: 900;
+  color: pink;
+  width: 200px;
+  margin: 0 auto;
+  padding: 10rem 0;
+  text-align: center;
+`
 
+ const Links = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding: 1rem 0;
+  text-decoration: none;
+`;
+
+const Button = styled.button`
+  width: 100px;
+  height: 50px;
+  font-size: 1rem;
+  font-weight: 600;
+  background: black;
+  color: pink;
+`;
+
+const Home = () => {
+  return (
+    <Header> Come check out my pals, yah?</Header>
+  )
+};
 class App extends Component {
   constructor(props){
     super(props);
@@ -14,7 +52,9 @@ class App extends Component {
         loading: true,
         name: '',
         age: '',
-        email: ''
+        email: '',
+        id:''
+
     }
   }
   handleChange = e => {
@@ -22,35 +62,56 @@ class App extends Component {
       [e.target.name]: e.target.value,
     })
   }
-  newFriend = e => {
+  addNewFriend = e => {
     e.preventDefault();
     axios.post(url,{
       name: this.state.name,
       age: this.state.age,
       email: this.state.email
     })
-    
+      
          .then(response => this.setState({friends: response.data}))
+  }
+  updateFriend = e => {
+    axios.put(`${url}/${e.target.id}`, {
+            name: this.state.name,
+            age: this.state.age,
+            email: this.state.email
+          })
+         .then(response => this.setState({friends: response.data}))
+         .catch(err => console.log(err));
+  }
+  deleteFriend = e => {
+    axios.delete(`${url}/${e.target.id}`)
+         .then(response => this.setState({friends: response.data}))
+         .catch(err => console.log(err));
   }
   componentDidMount(){
       axios
           .get(url)
-          .then(person => {
-              this.setState({friends: person.data, loading: false})
+          .then(response => {
+              this.setState({friends: response.data, loading: false})
           })
-          .catch(err => console.log('Server Error', err));
+          .catch(err => console.log(err));
   }
   render(){
+    console.log(this.state.friends)
       return(
-          <div>
-              <FriendsList friends={this.state.friends} />
-              <form onSubmit={this.newFriend}>
-                <input name='name' onChange={this.handleChange} />
-                <input name='age' onChange={this.handleChange} />
-                <input name='email' onChange={this.handleChange} />
-                <button>Save</button>
-              </form>
-          </div>
+        <Body>
+        <Links>
+        <Link to="/"><Button>Home</Button></Link>
+                <Link to="/friends"><Button>Yes!</Button></Link>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/friends"
+                render={(props) => <NewFriend {...props} newFriend={this.addNewFriend}
+                                                          change={this.handleChange} />} />
+        <Route exact path="/friends"
+                render={(props) => <FriendsList {...props} friends={this.state.friends} loading={this.state.loading}
+                                                          delete={this.deleteFriend} update={this.updateFriend} />} />
+            <Links />
+        </Links>
+        
+      </Body>
          
       )
     }
--- friends/src/Components/FriendsList/FriendsList.js
+++ friends/src/Components/FriendsList/FriendsList.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Friend = styled.div`
+    border: 1px solid black;
+    padding: 1rem;
+    margin: 0 auto;
+    width: 275px;
+    color: pink;
+    font-strength: 800;
+`
+const Div = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    padding: 1rem 0;
+`
+
+
 
  const FriendsList = props => {
     if(props.loading){
         return <h2>Loading Data...</h2>
     }
     return (
-        <div>
-            {props.friends.map(friend =>
-                <div key={friend.id}>
+        <Div>
+            {props.friends.map((friend) =>
+                <Friend key={friend.id}>
                     <p>Name: {friend.name}</p>
                     <p>Age: {friend.age}</p>
                     <p>Email: {friend.email}</p>
-                </div>
+                    <button id={friend.id} onClick={props.delete}>Delete Pal</button><br />
+                    <button id={friend.id} onClick={props.update}>Update Pal's Info</button>
+                </Friend>
             )}
-        </div>
+        </Div>
     )
  }
  export default FriendsList; 
