000c98efb6eebb04dc7a4f2935f2c25c9f132d53
JeanetteFernandez <jeanfern555@gmail.com>
2018-09-13 10:06:39 -0500

    Refactored - friend list is still displayed but refactored code to a different way to practice writing it differently and left it as such because I think it looks cleaner.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,33 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import axios from 'axios';
 import FriendsList from './components/FriendsList';
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state={
-        friends:[],
-    }
-}
 
-  componentDidMount(){
-    axios
-        .get('http://localhost:5000/friends')
-        .then(response =>{
-            console.log(response);
-            this.setState({friends:response.data});
-        } )
-        .catch(err => console.log(err));
-}
-
-  render() {
-    return (
-      <div className="App">
-        <FriendsList friends={this.state.friends} />
-      </div>
-    );
-  }
+const App = props => {
+  return (
+    <div className="App">
+      <FriendsList friends={props.friends} />
+    </div>
+  )
 }
 
 export default App;
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import Friend from './Friend';
-//import axios from 'axios';
 
-const FriendsList = props => {
-    return (
-    <div className="friends-list">
-        {props.friends.map(friend => {
-            return <Friend key={friend.id} friend={friend} />
-        })}
-    </div>
-    )
-}
+
+class FriendsList extends Component {
+    constructor(){
+        super();
+        this.state={
+            friends:[]
+        }
+    }
+  
+    componentDidMount(){
+      axios
+        .get('http://localhost:5000/friends')
+        .then(response => {
+          this.setState({friends: response.data});
+        })
+        .catch(err => console.log(err));
+    }
+   
+  
+    render() {
+        return (
+            <div className="friends-list">
+                 {this.state.friends.map(friend => <Friend key={friend.id} friend={friend} />)}
+            </div>
+        )
+    }
+  }
 
 export default FriendsList;
