c649f0037f01af89301eb4cba4882296baaa9fb1
Michael Agard <mdagard@gmail.com>
2018-09-13 22:59:34 -0400

    added AFuckingNavComponent

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import FriendList from './components/FriendList';
 import FriendForm from './components/FriendForm';
 import Friend from './components/Friend';
+import AFuckingNavComponent from './components/AFuckingNavComponent';
 import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Link to="/">Friend List</Link>
-        <Link to="/friendform">Friend Form</Link>
+        <AFuckingNavComponent />
         <Route exact path="/" component={FriendList} />
         <Route path="/friendform" component={FriendForm} />
         <Route path="/friends/:id" component={Friend} />
