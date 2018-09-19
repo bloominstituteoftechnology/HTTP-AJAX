5b9bc1648df1012f34ac7d2271e55af5123d366c
Ray Lomeli <raylom96@gmail.com>
2018-09-19 15:48:05 -0500

    created componenet to display friendlist

--- friends/src/App.js
+++ friends/src/App.js
@@ -14,23 +14,31 @@ class App extends React.Component {
 
   componentDidMount() {
     // fetch data from the api
+    console.log("inside didmount");
     axios
       .get("http://localhost:5000/friends")
       .then(response => {
         console.log(response);
         // set our state with the new data
-        this.setState({ friends: response.data.message });
+        this.setState({ friends: response.data });
       })
       .catch(err => console.log(err));
   }
 
   render() {
+    console.log("inside render");
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">My React APP</h1>
         </header>
+        {this.state.friends.map(friend => (
+          <p key={friend.name}>
+            {" "}
+            {friend.name} {friend.age}{" "}
+          </p>
+        ))}
       </div>
     );
   }
