941035ecabc8f4056f62906741984c59b88f2061
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-09-19 16:45:35 -0500

    friends display

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/friends/`)
+    .then(response => {
+      this.setState({friends: response.data});
+    })
+    .catch(err => {
+      console.log('Error', err);
+    });
+  }
+
+  render() { 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          {this.state.friends.map(friend => <div key={friend.id} className="friend-card" >{friend.name} {friend.age}</div>)}
+        </div>
       </div>
     );
   }
---
+++ friends/src/components/friend.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 
+
+const friend = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default friend;
+
+Friend.propTypes = {
+    friend.PropTypes.arrayOf(),
+}
