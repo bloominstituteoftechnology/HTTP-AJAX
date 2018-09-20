b0412e4b56142ef551736add330c733c8e23e9a0
A2i <abdulfatah.a.ahmad@gmx.com>
2018-09-20 10:10:21 -0400

    Made App.js into statefull component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+      constructor(props){
+        super(props);
+        this.state = {
+          friendsList: [],
+          inputData: ''
+        }
+      }
+
+      componentDidMount() {
+        
+        axios
+          .get('http://localhost:5000/friends')
+            .then(response)
+      }
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-         
-        </p>
-      </div>
+      <React.Fragment>
+          <FriendsList />
+          <FriendForm />
+      </React.Fragment>
     );
   }
 }
