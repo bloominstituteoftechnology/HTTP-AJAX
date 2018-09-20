f9a320f83a4d90031f237d9dbf7a55c6ffe80fbf
Matthew Wright <matthewbwright@gmail.com>
2018-09-20 13:33:41 -0700

    sets up NavLinks

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,11 +3,23 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Friends from './Friends';
 
+const blankFormValues = {
+  name: '',
+  age: '',
+  email: ''
+};
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      friends: []
+      friends: [],
+      friend: {
+        name: '',
+        age: '',
+        email: ''
+      },
+      isUpdating: false
     };
   }
 
@@ -21,8 +33,38 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
+  handleChange = event => {
+    this.setState({
+      friend: {
+        ...this.state.friend,
+        [event.target.name]: event.target.value
+      }
+    });
+  };
+
+  handleAddNewFriend = event => {
+    event.preventDefault();
+    axios
+      .post('http://localhost:5000/friends', this.state.friend)
+      .then(response => this.setState({ friends: response.data }));
+  };
+
   render() {
-    return <Friends friendsProps={this.state.friends} />;
+    return (
+    <div className='App'>
+    <ul className='navbar'>
+    <li>
+      <NavLink exact to="/" activeClassName="activeNavButton">Home</NavLink>
+    </li>
+    <li>
+      <NavLink to="/my-friends" activeClassName="activeNavButton">My Friends</NavLink>
+    </li>
+    <li>
+      <NavLink to="/friend-form" activeClassName="activeNavButton">Be my Friend!</NavLink>
+    </li>
+    </ul>
+      <Friends friendsProps={this.state.friends} />
+    </div>
   }
 }
 
