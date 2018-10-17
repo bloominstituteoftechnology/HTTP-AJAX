91eeb75785acd0bbc3b23f7bde2f5be493d1bcdc
David247365 <ds051128@gmail.com>
2018-10-17 17:04:51 -0500

    added function to add friends to the data of friends

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,13 +2,11 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Friends from "./components/Friends";
-import AddFreind from "./components/AddFriend";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AddFreind />
         <Friends />
       </div>
     );
--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -5,7 +5,12 @@ class Friends extends React.Component {
   constructor() {
     super();
     this.state = {
-      friends: []
+      friends: [],
+      newFriend: {
+        name: "",
+        age: "",
+        email: ""
+      }
     };
   }
 
@@ -15,9 +20,47 @@ class Friends extends React.Component {
       .then(response => this.setState({ friends: response.data }))
       .catch(error => console.log(error));
   }
+
+  changeHandler = e => {
+    this.setState({
+      newFriend: {
+        ...this.state.newFriend,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  addFriend = e => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/friends", this.state.newFriend)
+      .then(response => this.setState({ friends: response.data }));
+  };
+
   render() {
     return (
       <div className="App">
+        <form>
+          <input
+            type="text"
+            onChange={this.changeHandler}
+            name="name"
+            value={this.state.newFriend.name}
+          />
+          <input
+            type="text"
+            onChange={this.changeHandler}
+            name="age"
+            value={this.state.newFriend.age}
+          />
+          <input
+            type="text"
+            onChange={this.changeHandler}
+            name="email"
+            value={this.state.newFriend.email}
+          />
+          <button onClick={this.addFriend}>Add</button>
+        </form>
         {this.state.friends.map(friend => (
           <div className="friend-card" key={friend.id}>
             <h5 className="name">{friend.name}</h5>
