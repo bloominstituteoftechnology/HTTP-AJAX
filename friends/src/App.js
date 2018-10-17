bad3f0634a12bfc77f27f9f81989134721209416
bkwillia052 <bkwillia052@gmail.com>
2018-10-17 15:34:30 -0500

    added a form to gather friend information

--- friends/src/App.js
+++ friends/src/App.js
@@ -8,20 +8,58 @@ import axios from 'axios';
 
 class App extends React.Component {
   state = {
-    friends: []
+    friends: [],
+    newFriend: {
+        name: '',
+        age: '',
+        email: ''
+    }
   }
+
+  handleInput = (e) => {
+    this.setState({
+      newFriend: { ...this.state.newFriend,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  submitFriend = () => {
+    let newFriend = this.state.newFriend;
+    axios
+      .post('http://localhost:5000/friends', newFriend)
+      .then(response => this.setState({friends: response.data}))
+  }
+
   componentDidMount(){
     axios
         .get(' http://localhost:5000/friends')
-        .then(reponse => this.setState({friends: response.data}))
+        .then(response => this.setState({friends: response.data}))
   }
+
+
 
   render() {
     return (
       <div className="App">
-        <div>
+        <div className="friends-list">
+          {this.state.friends.map(friend => (
+            <div>
+            <h1>{friend.name}</h1>
+            <p>{friend.age}</p>
+            <p>{friend.email}</p>
+            </div>
 
+          ) )}
         </div>
+        <form onSubmit={this.submitFriend}>
+          <input type='text' name='name' onChange={this.handleInput} value={this.state.newFriend.name}/>
+          <input type='text' name='age' onChange={this.handleInput} value={this.state.newFriend.age}/>
+          <input type='text' name='email' onChange={this.handleInput} value={this.state.newFriend.email}/>
+
+        </form>
+        <div onClick={this.submitFriend}>Submit</div>
+
       </div>
     );
   }
