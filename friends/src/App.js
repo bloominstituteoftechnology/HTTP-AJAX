197095ba5f5965c6ec9616977e1a5adb7cd4a39f
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-17 17:55:41 -0400

    displaying friends to the screen

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      friends: []
+    }
+  }
+
+  componentDidMount(){
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => this.setState({friends:response.data}))
+      .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.friends.map(friend => {
+          return(
+            <div key={friend.id}>
+              <p><strong>Name:</strong> {friend.name}</p>
+              <p><strong>Age:</strong> {friend.age}</p>
+              <p><strong>Email:</strong> {friend.email}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
