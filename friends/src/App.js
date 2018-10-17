4dbb2655f41bda29d63a1c3f5c315155da2a5a7b
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-17 14:09:21 -0600

    ADded a friendlist component and Routed it on App

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route} from 'react-router-dom';
+import FriendList from './components/FriendList';
 
 class App extends Component {
   render() {
@@ -8,18 +10,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Friends</h1>
         </header>
+        <Route exact path='/friend' component={FriendList} />
       </div>
     );
   }
