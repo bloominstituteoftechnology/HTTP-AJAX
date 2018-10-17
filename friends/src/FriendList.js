a6423a5632171b1b1b7019767fc7aa4fbee76af6
Lee Len Formento <leebatef@gmail.com>
2018-10-17 13:33:16 -0400

    Push before workin on 10. Use fragments instead of divs

--- friends/src/FriendList.js
+++ friends/src/FriendList.js
@@ -3,11 +3,11 @@ import Friend from './Friend';
 
 const FriendList = props => {
     return (
-      <div>
+      <Fragment>
       {props.friend.map((friend, index) => (
         <Friend key={index} friend={friend} />
       ))}
-      </div>
+      </Fragment>
     );
   };
   
