152590cadf2b1e5e8b484297db4fc02f45fa004a
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-17 15:04:33 -0600

    added change handlers and friendform in app.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -10,13 +10,37 @@ class App extends Component {
     super();
     this.state = {
       friend: {
-        id: null,
+        id: 0,
         name: '',
-        age: null,
+        age: undefined,
         email: '',
       },
     }
   }
+handleChange = e => {
+  this.setState ({
+    friend: {
+      ...this.state.friend,
+      [e.target.name]: e.target.value,
+    }
+  });
+}
+
+handleNumberChange = e => {
+  let input = 0;
+  if (e.target.value===null){
+    input = undefined;
+  }else {
+    input = parseInt(e.target.value, 10);
+  }
+  this.setState({
+    friend:{
+      ...this.state.friend,
+      [e.target.name]: input,
+    }
+  })
+}
+  
   render() {
     return (
       <div className="App">
@@ -25,7 +49,16 @@ class App extends Component {
           <h1 className="App-title">Friends</h1>
         </header>
         <Route exact path='/friend' component={FriendList} />
-        <Route path='/' component={FriendForm} />
+        <Route
+          path='/'
+          render={props =>(
+            <FriendForm
+            {...props}
+            friend={this.state.friend}
+            handleChange={this.handleChange}
+            handleNumberChange={this.handleNumberChange} />
+          )}
+        />
       </div>
     );
   }
