f1b3e9909971635ef737f455e1417aee3ec9baed
Axolotl <samuelmachat@gmail.com>
2018-10-17 15:33:38 -0400

    add axios data call

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import Card from './components/Card'
 
 class App extends Component {
+  state = {
+    friends: []
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(res => this.setState({ friends: res.data }))
+      .catch(err => console.log(err))
+  }
+
   render() {
+    const { friends } = this.state
+
     return (
-      <div className="App">
-        <p>hi</p>
+      <div>
+        {friends.length &&
+          friends.map((friend, i) => <Card key={i} {...friend} />)}
       </div>
     )
   }
