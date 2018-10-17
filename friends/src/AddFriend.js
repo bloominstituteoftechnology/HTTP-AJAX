5aa2148a9797a6ebeacbcbfe2e8c4a0d9cf4b277
Ryan Boris <neodynia@icloud.com>
2018-10-17 07:29:13 -0400

    Allows a new name email and age to be added to the friend list and rendered for the user

--- friends/src/AddFriend.js
+++ friends/src/AddFriend.js
@@ -1,38 +1,23 @@
 import React from 'react'
 
-export default class AddFriend extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { name: '', age: '', email: '' }
-    }
-
-    handleChange = (e) => {
-        this.setState({
-          [e.target.name]: e.target.value
-        })
-    }
-
-    handleSubmit(e) {
-      e.preventDefault()
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-                    <input name="name" type="text" value={this.state.nameBox} onChange={this.handleChange} />
-                </label>
-                <label>
-                    Age:
-                    <input name="age" type="text" value={this.state.ageBox} onChange={this.handleChange} />
-                </label>
-                <label>
-                    Email:
-                    <input name="email" type="text" email={this.state.emailBox} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        )
-    }
+const AddFriend = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <label>
+                Name:
+                <input name="name" type="text" value={props.name} onChange={props.handleChange} />
+            </label>
+            <label>
+                Age:
+                <input name="age" type="number" value={props.age} onChange={props.handleChange} />
+            </label>
+            <label>
+                Email:
+                <input name="email" type="text" value={props.email} onChange={props.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+    )
 }
+
+export default AddFriend
--- friends/src/App.js
+++ friends/src/App.js
@@ -7,9 +7,7 @@ import AddFriend from './AddFriend';
 class App extends Component {
     constructor(props) {
         super(props)
-        this.state ={
-            friends: []
-        }
+        this.state = {friends:[], friend: {name: '', age: '', email: ''}}
     }
 
     componentDidMount() {
@@ -19,11 +17,38 @@ class App extends Component {
             .catch(e => console.log(e))
     }
 
+
+    handleChange = (e) => {
+        const target = e.target
+        const value = target.name === 'age' ? parseInt(target.value) : target.value
+        const name = target.name
+        this.setState({
+            [name]: value,
+            friend: {
+                name: this.state.name,
+                age: this.state.age,
+                email: this.state.email
+            }
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        axios
+            .post('http://localhost:5000/friends', this.state.friend)
+            .then((response) => this.setState({ friends: response.data, friend: {name: '', age: '', email: ''}}))
+    }
+
+
+
+
+
+
     render() {
         return (
             <div className="App">
                 <FriendList friends={this.state.friends}/>
-                <AddFriend />
+                <AddFriend handleChange={this.handleChange} handleSubmit={this.handleSubmit} name={this.state.friend.name} age={this.state.friend.age} email={this.state.friend.email} />
             </div>
         )
     }
