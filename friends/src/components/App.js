84cb3bf0f1da88454568e82fcb3a0d51574a985c
Stublink <chrisrey95@gmail.com>
2018-10-17 16:04:23 -0400

    Display Friends

--- friends/src/components/App.js
+++ friends/src/components/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: [],
+      newFriend: {},
+    }
+  }
+
+  componentDidMount() {
+    const url = 'http://localhost:5000';
+    axios.get(`${url}/friends`,)
+      .then(({data}) => this.setState({friends: data}))
+      .catch(err => console.error(err));
+  }
+
   render() {
+    if (!this.state.friends.length) {
+      return <h1>Loading Friends...</h1>
+    }
     return (
       <div className="App">
-        <h1>Hi</h1>
+        {this.state.friends.map(friend => {
+         return (
+          <div key={friend.email}>
+            <span>{friend.name}</span>
+            <span>{friend.age}</span>
+            <span>{friend.email}</span>
+          </div>
+          )
+        })}
       </div>
     );
   }
