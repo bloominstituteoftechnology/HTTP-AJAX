116ee3fe7d4e2e25c4f866bead2627dafd55edbe
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-10-17 16:59:00 -0400

    creates add new friend functionality

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import FriendCard from './components/FriendCard';
+import NewFriendForm from './components/NewFriendForm';
 
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state ={
+    this.state = {
       friends: []
     }
   }
@@ -20,11 +21,20 @@ class App extends Component {
       .catch(error => console.log(error));
   }
 
+  updateState = () => {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        this.setState({ friends: response.data });
+      })
+      .catch(error => console.log(error));
+  }
+
   render() {
-    console.log(this.state.friends);
     return (
       <div className="App">
-      {this.state.friends.map((friend) => <FriendCard key={friend.id} friend={friend} />)}
+        {this.state.friends.map((friend) => <FriendCard key={friend.id} friend={friend} />)}
+        <NewFriendForm updateState={this.updateState} />
       </div>
     );
   }
---
+++ friends/src/components/NewFriendForm.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import axios from 'axios';
+
+class NewFriendForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            age: "",
+            email: ""
+        }
+    }
+
+    // stores input values in state
+    changeHandler = event => {
+        if (event.target.name === "age") {
+            this.setState({
+                [event.target.name]: Number(event.target.value)
+            })
+        } else {
+            this.setState({
+                [event.target.name]: event.target.value
+            })
+        }
+    }
+
+    //posts new friend to server on form "submit"
+    newFriendFormSubmit = (event) => {
+        event.preventDefault();
+        if (this.state.name && this.state.age && this.state.email) {
+            const newFriend = this.state;
+            axios.post('http://localhost:5000/friends', newFriend)
+                .then(response => {
+                    console.log(response);
+                    console.log(response.data);
+                    this.props.updateState();
+                });
+            this.setState({
+                name: "",
+                age: "",
+                email: ""
+            });
+        } else {
+            alert("One or more inputs is empty");
+        }
+    }
+
+    render() {
+        return (
+            <form className="new-friend-form">
+                <input type="text" name="name" placeholder="Name" onChange={this.changeHandler} value={this.state.name} />
+                <input type="number" name="age" placeholder="Age" onChange={this.changeHandler} value={this.state.age} />
+                <input type="email" name="email" placeholder="Email" onChange={this.changeHandler} value={this.state.email} />
+                <button type="submit" onClick={(event) => this.newFriendFormSubmit(event)}>Submit</button>
+            </form>
+        );
+    }
+}
+
+export default NewFriendForm;
