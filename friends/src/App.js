e60f4690fe40886a402112be27fb496a4e6d76c8
Blake <koenneckerb@gmail.com>
2018-10-17 13:13:48 -0700

    Added functionality for adding friends to the list via the form.

--- friends/src/App.js
+++ friends/src/App.js
@@ -6,7 +6,12 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      friends: []
+      friends: [],
+      newFriend: {
+        name: "",
+        age: "",
+        email: ""
+      }
     };
   }
   componentDidMount() {
@@ -16,24 +21,62 @@ class App extends Component {
       .catch(error => console.log(error));
   }
 
+  handleChange = event => {
+    event.preventDefault();
+    this.setState({
+      newFriend: {
+        ...this.state.newFriend,
+        [event.target.name]: event.target.value
+      }
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:5000/friends", this.state.newFriend)
+      .then(response => this.setState({ friends: response.data }))
+      .catch(error => console.log(error));
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Friends</h1>
-        {this.state.friends.map(friend => (
-          <div key={friend.id}>
-            <h2>{friend.name}</h2>
-            <p>Age: {friend.age}</p>
-            <p>
-              Email: <a href={`mailto:${friend.email}`}>{friend.email}</a>
-            </p>
-          </div>
-        ))}
+        <div className="friends-list">
+          {this.state.friends.map(friend => (
+            <div className="friend-card" key={friend.id}>
+              <h2>{friend.name}</h2>
+              <p>Age: {friend.age}</p>
+              <p>
+                Email: <a href={`mailto:${friend.email}`}>{friend.email}</a>
+              </p>
+            </div>
+          ))}
+        </div>
         <form action="submit" method="post">
           <h2>Add a new friend</h2>
-          <input type="text" name="name" placeholder="Name" />
-          <input type="text" name="age" placeholder="Age" />
-          <input type="text" name="email" placeholder="Email" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="age"
+            placeholder="Age"
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            onChange={this.handleChange}
+          />
+          <button type="submit" onClick={this.handleSubmit}>
+            Submit
+          </button>
         </form>
       </div>
     );
