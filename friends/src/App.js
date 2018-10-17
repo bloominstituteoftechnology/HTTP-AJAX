c393141907c427bf2b34343ff06e13770839b394
samsisle <samko9522@gmail.com>
2018-10-17 15:58:16 -0700

    Add post functionality

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import Friend from './components/friend';
+import FriendsForm from './components/FriendsForm';
 
 class App extends Component {
   constructor() {
@@ -20,17 +21,19 @@ class App extends Component {
       .catch(err => console.log(err));
   };
 
+  updateFriends = friends => {
+    this.setState({ friends });
+  };
+
   renderFriends = () => {
-    return this.state.friends.map(friend =>
-      <Friend key={friend.id} friend={friend} />
-    );
-  }
+    return this.state.friends.map(friend => <Friend key={friend.id} friend={friend} />);
+  };
 
   render() {
     return (
       <div className="App">
+        <FriendsForm id={this.state.friends.length} updateFriends={this.updateFriends} />
         <ul className="friend-list">{this.renderFriends()}</ul>
-        
       </div>
     );
   }
---
+++ friends/src/components/FriendsForm.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class FriendsForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', age: '', email: '' };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/friends', {
+        id: this.props.id + 1,
+        name: this.state.name,
+        age: this.state.age,
+        email: this.state.email
+      })
+      .then(res => {
+        this.props.updateFriends(res.data);
+        this.setState({ name: '', age: '', email: '' });
+      })
+      .catch(err => console.log(err));
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.name}
+          placeholder="Name"
+          onChange={this.handleChange}
+          name="name"
+          required
+        />
+        <input
+          type="number"
+          value={this.state.age}
+          placeholder="Age"
+          onChange={this.handleChange}
+          name="age"
+          required
+        />
+        <input
+          type="email"
+          value={this.state.email}
+          placeholder="Email"
+          onChange={this.handleChange}
+          name="email"
+          required
+        />
+        <button type="submit">Post</button>
+      </form>
+    );
+  }
+}
+
+export default FriendsForm;
