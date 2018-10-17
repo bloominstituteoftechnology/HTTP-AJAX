84e51840a54c72f6ba53578d7597611ec5e1ea15
Mindysueasaurus <mymindyyeager@gmail.com>
2018-10-17 16:05:06 -0500

    server information displayed

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import Friend from './Friend'
 
 class FriendsList extends Component {
   constructor(){
     super();
     this.state = {
-      friends: [];
+      friends: []
     }
   }
 
+  componentDidMount(){
+    axios
+    .get('http://localhost:5000/friends')
+    .then(response => {
+      this.setState(() => ({friends: response.data}));
+    })
+    .catch(error => {
+      console.error('Server Error', error);
+    })
+  }
   render(){
-    return <div></div>
+    return (
+      <div className="friend-list">
+      {this.state.friends.map( friend => (
+        <Friend key={friend.id}  friend={friend} />   ))}
+      </div>
+      );
   }
   
 }
