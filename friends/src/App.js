c7e6947d11c383ad049038ceafe944e081ce566e
Brad Mortensen <b.mort86@gmail.com>
2018-10-17 13:14:48 -0600

    built app com to display friends data

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      items: [],        
+      newFriend: {
+        name:'',
+        age:'',
+        email:''
+      }
+    };
+  }
+  componentDidMount(){
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => this.setState({ items: response.data }))
+      .catch(error => console.log(error))
+  }
+  changeHandler() {
+    
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='friends-container'>
+        <h1>Contact Information</h1>
+        <input></input>
+        <input></input>
+        <button></button>        
+        {this.state.items.map(item => 
+          <div key={item.id} className='friend-card'>
+            <h2>{item.name}</h2>
+            <p>{item.age}</p>
+            <p>{item.email}</p>
+          </div>          
+        )}
       </div>
     );
   }
