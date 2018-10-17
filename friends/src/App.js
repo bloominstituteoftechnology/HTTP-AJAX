445d3982b38955cc1c30b64aebb23dd430966035
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-10-17 16:51:16 -0400

    added friends form and state

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import FriendsListForm from "./components/friendsListForm"
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      friend: [],
+    }
+  }
+
+  componentDidMount() {
+    console.log("Mount")
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <FriendsListForm />
+
       </div>
     );
   }
---
+++ friends/src/components/friendsListForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const FriendsListForm = () => {
+    return (
+    <div>
+    <form>
+        <h1>My Friends List</h1>
+        <input placeholder= "Name" />
+        <button>Add</button>
+    </form>
+    </div>
+    )
+}
+
+export default FriendsListForm
