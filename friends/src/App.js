c9cbc11b2d39bd7b7903a221b5dba9c3b5ea3b3a
Adam <ajmboosh@gmail.com>
2018-10-17 16:30:11 -0500

    got basic styling and page formatting done. set up changeHandler for form, form functionality done except for POST functionality in addFriend function

--- friends/src/App.js
+++ friends/src/App.js
@@ -7,7 +7,12 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      friends: []
+      friends: [],
+      newFriend: {
+        name: "",
+        age: "",
+        email: ""
+      }
     };
   }
 
@@ -18,10 +23,81 @@ class App extends Component {
       .catch(err => console.log(err, "Ya done goofed"));
   }
 
+  addFriend = event => {
+    event.preventDefault();
+    let friend = this.state.newFriend;
+    if (friend.name && friend.age && friend.email) {
+      //add friend to list if all fields have had something entered
+    } else {
+      alert(
+        "You clearly don't know enough about them. Fill out all fields if they're really your friend."
+      );
+      this.setState({
+        newFriend: {
+          name: "",
+          age: "",
+          email: ""
+        }
+      });
+    }
+  };
+
+  changeHandler = (key, value) => {
+    this.setState({
+      newFriend: {
+        [key]: value
+      }
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>Muh Friends</h1>
+        <h1>Happy Lambda Friends</h1>
+        <div className="app-container">
+          <div className="friend-container-wrapper">
+            {this.state.friends.map(friend => (
+              <div className="friend-container" key={friend.id}>
+                <h3 className="friend-name">{friend.name}</h3>
+                <p className="friend-age">{friend.age}</p>
+                <p className="friend-email">{friend.email}</p>
+              </div>
+            ))}
+          </div>
+          <div className="new-friend-form">
+            <h2>Made a new friend?</h2>
+            <form onSubmit={this.addFriend}>
+              <input
+                className="add-friend-name"
+                type="text"
+                placeholder="Name"
+                onChange={event =>
+                  this.changeHandler("name", event.target.value)
+                }
+                value={this.state.newFriend.name}
+              />
+              <input
+                className="add-friend-age"
+                type="text"
+                placeholder="Age"
+                onChange={event =>
+                  this.changeHandler("age", event.target.value)
+                }
+                value={this.state.newFriend.age}
+              />
+              <input
+                className="add-friend-email"
+                type="text"
+                placeholder="Email"
+                onChange={event =>
+                  this.changeHandler("email", event.target.value)
+                }
+                value={this.state.newFriend.email}
+              />
+              <button>Add</button>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
