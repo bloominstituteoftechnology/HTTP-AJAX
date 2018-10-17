064f9fef4fedf1eca1c8d4657dbadb6656b1d4ef
Blake <koenneckerb@gmail.com>
2018-10-17 12:32:40 -0700

    Created a component to display the list of friends coming from the server.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(response => this.setState({ friends: response.data }))
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Friends</h1>
+        {this.state.friends.map(friend => (
+          <div key={friend.id}>
+            <h2>{friend.name}</h2>
+            <p>Age: {friend.age}</p>
+            <p>
+              Email: <a href={`mailto:${friend.email}`}>{friend.email}</a>
+            </p>
+          </div>
+        ))}
+        <form action="submit" method="post">
+          <h2>Add a new friend</h2>
+          <input type="text" name="name" placeholder="Name" />
+          <input type="text" name="age" placeholder="Age" />
+          <input type="text" name="email" placeholder="Email" />
+        </form>
       </div>
     );
   }
