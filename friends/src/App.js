be8f65c9c3996259ddca46209c646868b29b0a54
Chad Jemmett <chad.jemmett@gmail.com>
2018-10-17 14:14:05 -0600

    can list friends from server

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import route from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+      this.state ={
+        friends: [],
+        newFriend: {
+          name: '',
+          age: '',
+          email: '',
+        }
+      }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+      .then((response)=> this.setState({friends: response.data}))
+      .catch((error)=> console.log('iz not werking'))
+  }
+
+   changeHandler = (e) => {
+    console.log(e)
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Friends friends={this.state.friends}
+          newFriend={this.state.newFriend}
+          changeHandler={this.changeHandler}
+        />
       </div>
     );
   }
+}
+
+const Friends = props => {
+  return(
+      <div>
+        My friends
+          <form>
+            <input onChange={props.changeHandler} type='text' name="name" value={props.newFriend.name}></input>
+            <input type='text' name='age' value={props.newFriend.age}></input>
+            <input type='text' name='email' value={props.newFriend.email}></input>
+            <button>Submit</button>
+          </form>
+        <div>
+        {props.friends.map(item => (
+            <div className="friends" key={item.id}>
+              <h4>{item.name}</h4>
+              <p>{item.age}</p>
+              <p>{item.email}</p>
+            </div>
+              ))}
+        </div>
+      
+      </div>
+
+      )
 }
 
 export default App;
