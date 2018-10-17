94adc08daa50a6ff55857b8a739ad577677100e5
Lajawanti <lajawanti.dhake@gmail.com>
2018-10-17 15:02:22 -0400

    Friends list display started

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 class App extends Component {
-  render() {
+  constructor() {
+      super();
+      this.state = {
+        friends : []
+      }
+  }
+
+componentDidMount() {
+    console.log("In CDM");
+    axios
+    .get('http://localhost:5000/friends')
+    .then(response => this.setState({ friends : response.data }))
+    .catch(error => console.log(error));
+}
+
+render() {
+    console.log("Rendering", this.state.friends)
     return (
       <div className="App">
         <h1>HTTP....</h1>
+        { this.state.friends.map(friend => {
+              return (
+                      <div>
+                          <h1>Name  :  {friend.name}</h1>
+                          <p> Age  : {friend.age}</p>
+                          <p> Email : {friend.email}</p>
+                      </div>
+                     )
+          })
+        }
       </div>
     );
-  }
-}
+  } //render
+} //App component
 
 export default App;
