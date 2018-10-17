6fea54148981394942b2ae5f2d5abecc6b49804c
KHollobaugh <khollobaugh@gmail.com>
2018-10-17 15:57:59 -0400

    built componentDidMount with API call

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      newFriend: {
+        age: '',
+        name: '',
+        email: ''
+      }
+    }
+  }
+
+componentDidMount() {
+  axios
+    .get('http://localhost:5000/friends')
+    .then(response => this.setState({items: response.data}))
+    .catch(error => console.log(error));
+}
+
   render() {
     return (
       <div className="App">
