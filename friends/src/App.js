4ed0ceffb30ff81a238bd7fee63a68400640cd3c
Brad Mortensen <b.mort86@gmail.com>
2018-10-17 16:42:32 -0600

    finished add functionality and started styling

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,62 +2,76 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
+
 class App extends Component {
   constructor(){
     super();
     this.state={
       items: [],
-      newFriend: {
-        name:'',
-        age:'',
-        email:''
-      }  
+      name: '',
+      age: '',
+      email: ''
     };
   }
+  
   componentDidMount(){
     axios
       .get('http://localhost:5000/friends')
       .then(response => this.setState({ items: response.data }))
       .catch(error => console.log(error))
   }
+
   handleInputChange = event => this.setState({ 
     [event.target.name]: event.target.value 
   });
-  clickHandler = () => {
-    console.log(this.state.newFriend)
-    // axios.post('http://localhost:5000/friends', {addFriend})
-    //   .then(response => this.setState({
-    //     ...this.state.items, response       
-    //   }))
-  }
+
+  clickHandler = event => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:5000/friends", {
+        name: this.state.name,
+        age: this.state.age,
+        email: this.state.email
+      })
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+    this.setState({ name: "", age: "", email: "" });
+    window.location.reload();
+  };
+
   render() {
-    console.log(this.state.newFriend)
     return (
-      <div className='friends-container'>
+      <div className='contact-container'>
         <h1>Contact Information</h1>
-        <input 
-          name='name' 
-          value={this.state.name}
-          placeholder='Name'
-          onChange={this.handleInputChange}></input>
-        <input 
-          name='age' 
-          value={this.state.age}
-          placeholder='Age'
-          onChange={this.handleInputChange}></input>
-        <input 
-          name='email' 
-          value={this.state.email}
-          placeholder='Email'
-          onChange={this.handleInputChange}></input>
-        <button onClick={this.clickHandler}>Add a Contact</button>        
-        {this.state.items.map(item => 
-          <div key={item.id} className='friend-card'>
-            <h2>{item.name}</h2>
-            <p>{item.age}</p>
-            <p>{item.email}</p>
-          </div>          
-        )}
+
+        <div className='cards'>
+          {this.state.items.map(item => 
+            <div key={item.id} className='contact-card'>
+              <h2>{item.name}</h2>
+              <p>Age:{item.age}</p>
+              <p>Email:{item.email}</p>
+            </div>          
+          )}
+          <form className='form'>
+          <h3>Add a contact</h3>
+          <input 
+            name='name' 
+            value={this.state.name}
+            placeholder='Name'
+            onChange={this.handleInputChange}></input>
+          <input 
+            name='age' 
+            value={this.state.age}
+            placeholder='Age'
+            onChange={this.handleInputChange}></input>
+          <input 
+            name='email' 
+            value={this.state.email}
+            placeholder='Email'
+            onChange={this.handleInputChange}></input>
+          <button onClick={this.clickHandler}>Submit</button>
+        </form>  
+        </div>
       </div>
     );
   }
