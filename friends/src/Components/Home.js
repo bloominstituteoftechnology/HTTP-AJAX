21afc88afdfc5a254fb715aff4ee0777bf93f9cc
Matthew Wright <matthewbwright@gmail.com>
2018-10-17 12:30:37 -0700

    sets up a homepage

---
+++ friends/src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <h1>Matt's Friends</h1>
+      <h4>Meet my friends!</h4>
+      <p>Maybe you can be my friend too...</p>
+    </div>
+  );
+}
+
+export default Home;
