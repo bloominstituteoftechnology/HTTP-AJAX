3a286ce96088ab87fc0ac2f61a5b027278b1e59f
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-10-17 15:31:58 -0400

    pulls in data from server, displays data for each friend

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import FriendCard from './components/FriendCard';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state ={
+      friends: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => this.setState({ friends: response.data }))
+      .catch(error => console.log(error));
+  }
+
   render() {
+    console.log(this.state.friends);
     return (
       <div className="App">
-        My Div
+      {this.state.friends.map((friend) => <FriendCard key={friend.id} friend={friend} />)}
       </div>
     );
   }
