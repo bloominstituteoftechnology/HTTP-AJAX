6df08413cc7439508cebb2e2b5d89d80e4070f52
unknown <quan.nguyen199712@gmail.com>
2018-10-17 19:53:32 -0400

    added post

--- friends/src/FriendViewContainer.js
+++ friends/src/FriendViewContainer.js
@@ -1,53 +1,78 @@
 import React, { Component } from "react";
 import axios from "axios";
-import FriendView from './FriendView';
+import FriendView from "./FriendView";
 
 class FriendViewContainer extends Component {
   constructor() {
     super();
     this.state = {
       friendsData: [],
-      newFriend:{
-        id:'',
-        name:'',
-        age:'',
-        email:'',
+      newFriend: {
+        name: "",
+        age: "",
+        email: ""
       }
     };
   }
   componentDidMount() {
     axios
       .get("http://localhost:5000/friends")
-      .then(response => this.setState({friendsData: response.data}))
+      .then(response => this.setState({ friendsData: response.data }))
       .catch(error => console.log(error));
   }
-  changeHandler=(event)=>{
+  changeHandler = event => {
     this.setState({
-        newFriend:{
-            ...this.state.newFriend,
-            [event.target.name]:event.target.value
-        }
-    })
-  }
-  submitHandler=(event)=>{
-      event.preventDefautl();
-      axios.post("http://localhost:5000/friends",this.state.newFriend)
-        .then(response=>console.log(response.data))
-  }
+      newFriend: {
+        ...this.state.newFriend,
+        [event.target.name]: event.target.value
+      }
+    });
+  };
+  submitHandler = event => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:5000/friends", this.state.newFriend)
+      .then(response => this.setState({ friendsData: response.data }));
+    this.setState({newFriend:{
+        name:'',
+        age: '',
+        email:'',
+    }})
+  };
   render() {
     return (
-        <div>
-            <FriendView friendsData={this.state.friendsData}/>
-            <form type='submit' onSubmit={this.submitHandler}>
-                ID: <input type='text' placeholder='ID' name='id' value={this.state.newFriend.id} onChange={this.changeHandler}></input>
-                Name: <input type='text' placeholder='Name' name='name'  value={this.state.newFriend.name} onChange={this.changeHandler}></input>
-                Age: <input type='text' placeholder='Age' name='age'  value={this.state.newFriend.age} onChange={this.changeHandler}></input>
-                Email: <input type='text' placeholder='Email' name='email'  value={this.state.newFriend.email} onChange={this.changeHandler}></input>
-                <button onClick={this.submitHandler}> Add friend</button>
-            </form>
-        </div>
-    )
-    }
+      <div>
+        <FriendView friendsData={this.state.friendsData} />
+        <form type="submit" onSubmit={this.submitHandler}>
+          Name:{" "}
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={this.state.newFriend.name}
+            onChange={this.changeHandler}
+          />
+          Age:{" "}
+          <input
+            type="text"
+            placeholder="Age"
+            name="age"
+            value={this.state.newFriend.age}
+            onChange={this.changeHandler}
+          />
+          Email:{" "}
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={this.state.newFriend.email}
+            onChange={this.changeHandler}
+          />
+          <button onClick={this.submitHandler}> Add friend</button>
+        </form>
+      </div>
+    );
   }
+}
 
 export default FriendViewContainer;
