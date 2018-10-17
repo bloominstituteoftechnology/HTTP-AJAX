0035d4568582f2cb009ac53f40a0fa116e5fe941
Stublink <chrisrey95@gmail.com>
2018-10-17 16:24:33 -0400

    POST new friends

--- friends/src/components/App.js
+++ friends/src/components/App.js
@@ -6,15 +6,35 @@ class App extends Component {
     super(props);
     this.state = {
       friends: [],
-      newFriend: {},
+      newFriend: {
+        name: '',
+        age: '',
+        email: '',
+      },
+      url: 'http://localhost:5000',
     }
   }
 
   componentDidMount() {
-    const url = 'http://localhost:5000';
-    axios.get(`${url}/friends`,)
+    axios.get(`${this.state.url}/friends`,)
       .then(({data}) => this.setState({friends: data}))
       .catch(err => console.error(err));
+  }
+
+  addNewFriend = (ev) => {
+    ev.preventDefault();
+    let empty = false;
+    Object.values(this.state.newFriend).forEach(n => n === ''?empty=true:null);
+    if(empty) return;
+
+    axios.post(`${this.state.url}/friends`, this.state.newFriend)
+      .then(({data}) => this.setState({friends: data, newFriend: {name: '', age: '', email: ''}}))
+      .catch(err => console.error(err));
+  }
+
+  changeHandler = (ev) => {
+    ev.preventDefault();
+    this.setState({ newFriend: { ...this.state.newFriend, [ev.target.name]: ev.target.value}});
   }
 
   render() {
@@ -23,9 +43,15 @@ class App extends Component {
     }
     return (
       <div className="App">
+        <form onSubmit={this.addNewFriend}>
+          <input type="text" name="name" placeholder="Name" value={this.state.newFriend.name} onChange={this.changeHandler}/>
+          <input type="text" name="age" placeholder="Age" value={this.state.newFriend.age} onChange={this.changeHandler}/>
+          <input type="text" name="email" placeholder="Email" value={this.state.newFriend.email} onChange={this.changeHandler}/>
+          <button>Add Friend</button>
+        </form>
         {this.state.friends.map(friend => {
          return (
-          <div key={friend.email}>
+          <div key={friend.id}>
             <span>{friend.name}</span>
             <span>{friend.age}</span>
             <span>{friend.email}</span>
