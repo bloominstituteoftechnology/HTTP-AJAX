972eacbd4457a17baedd677f5ad78aca94cea415
ssmodish <smodish@hotmail.com>
2018-10-18 11:17:03 -0400

    peer code review time

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -8,7 +8,12 @@ class FriendsList extends React.Component {
   constructor() {
     super();
     this.state = {
-      friends: []
+      friends: [],
+      newFriend: {
+        name: '',
+        age: 0,
+        email: ''
+      }
     };
   }
 
@@ -19,12 +24,21 @@ class FriendsList extends React.Component {
       .catch(error => console.log(error));
   }
 
+  changeHandler = e => this.setState(
+    [e.target.name]: e.target.value
+  );
+
+
+  addNewFriend = () => {
+    const newFriend = { name: this.state.newFriend.name, age: this.state.newFriend.age, email: this.state.newFriend.email };
+  }
+
   render() {
     console.log(this.state.friends)
     return (
       <div className='friend-list'>
-        {this.state.friends.map(friend => <Friend name={friend.name} age={friend.age} email={friend.email} key={friend.id}/>)}
-        <NewFriend />
+        {this.state.friends.map(friend => <Friend name={friend.name} age={friend.age} email={friend.email} key={friend.id} />)}
+        <NewFriend newFriend={this.state.newFriend} changeHandler={this.changeHandler} addNewFriend={this.addNewFriend} />
       </div>
     );
   };
--- friends/src/components/NewFriend.js
+++ friends/src/components/NewFriend.js
@@ -4,19 +4,19 @@ import './Friend.css';
 
 function Friend(props) {
   return (
-    <form className='new-friend'>
+    <form className='new-friend' action='submit' onSubmit={(event) => props.addNewFriend(event, props.newFriend)}>
       <div>
         <div className='friend-row'>
           <p>Name</p>
-          <p><input type='text' name='name' /></p>
+          <p><input type='text' name='name' value={props.newFriend.name} onChange={(event) => props.changeHandler(event)} /></p>
         </div>
         <div className="friend-row">
           <p>Age</p>
-          <p><input type="text" name='age' /></p>
+          <p><input type="text" name='age' value={props.newFriend.age} onChange={(event) => props.changeHandler(event)}/></p>
         </div>
         <div className="friend-row">
           <p>Email</p>
-          <p><input type="text" name='email' /></p>
+          <p><input type="text" name='email' value={props.newFriend.email} onChange={(event) => props.changeHandler(event)}/></p>
         </div>
       </div>
       <div className='button-cell'>
