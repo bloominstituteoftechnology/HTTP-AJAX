cc10438692b77df194ec49ec843a17b431751010
Stublink <chrisrey95@gmail.com>
2018-10-18 17:43:38 -0400

    Form Validation

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -7,8 +7,8 @@ const FriendForm = ({friend, changeHandler, addNewFriend, updateFriend}) => {
             <form onSubmit={addNewFriend} className="form">
                 <div className="form-inputs">
                     <StyledInput type="text" name="name" placeholder="Name" value={friend.name} onChange={changeHandler} autoComplete="off"/>
-                    <StyledInput type="text" name="age" placeholder="Age" value={friend.age} onChange={changeHandler} autoComplete="off"/>
-                    <StyledInput type="text" name="email" placeholder="Email" value={friend.email} onChange={changeHandler} autoComplete="off"/>
+                    <StyledInput type="number" name="age" placeholder="Age" value={friend.age} onChange={changeHandler} autoComplete="off"/>
+                    <StyledInput type="email" name="email" placeholder="Email" value={friend.email} onChange={changeHandler} autoComplete="off"/>
                 </div>
                 <div className="form-buttons">
                     <StyledButton enabled={!updateFriend}>Add Friend</StyledButton>
