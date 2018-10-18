f3271ebe14bb738da74ab427081e0cbcec5bdb73
Elvis Ibarra <ielvisd@gmail.com>
2018-10-18 14:03:23 -0700

    Form and POST request adding a new friend is working

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FriendsWrapper = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+const FriendsCardWrapper = styled.div`
+  border: 1px solid white;
+  padding: 30px;
+`;
 
 function FriendsList(props) {
   console.log('friends props are: ', props);
   return (
-    <div className="friends-list-wrapper">
+    <FriendsWrapper>
       {props.friends.map(friend => (
-        <div
-          className="friend-card"
-          key={friend.id}
-        >
-          <p>{friend.name}</p>
+        <FriendsCardWrapper className="friend-card" key={friend.id}>
+          <h2>{friend.name}</h2>
           <p>{friend.age}</p>
           <code>{friend.email}</code>
-        </div>
+        </FriendsCardWrapper>
       ))}
-    </div>
+    </FriendsWrapper>
   );
 }
 
--- friends/src/index.js
+++ friends/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import FriendsList from './components/FriendsList';
+import './App.css';
 
 class App extends React.Component {
   // add constructor and CDM
@@ -10,8 +11,8 @@ class App extends React.Component {
     this.state = {
       friends: [],
       newFriend: {
-        age: '',
         name: '',
+        age: null,
         email: ''
       }
     };
@@ -24,15 +25,53 @@ class App extends React.Component {
       .catch(error => console.log(error));
   }
 
+  // handleChange method - When user types into the box (*changes something*) then that thing they are changing gets updated on state with what they typed in (value). The [] are an ES6 feature that evaluates that property to a string so we can pop it into state as a string. Arrow notation is necessary to have access to the keyword - this
+  handleChange = event => {
+    console.log(event.target.name);
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  addNewFriend = () => {
+    const newFriendObj = {
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email
+    };
+    axios
+      .post('http://localhost:5000/friends', newFriendObj)
+      .then(response => {
+        this.setState({
+          friends: response.data
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
   render() {
     console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Welcome to the Friends App!
-          </p>
-          <FriendsList friends = {this.state.friends}/>
+          <p>Welcome to the Friends App!</p>
+
+          <FriendsList friends={this.state.friends} />
+
+          <div>
+            <form onSubmit={this.addNewFriend}>
+              <input onChange={this.handleChange} name="name" placeholder="name" value={this.state.name} type="text" />
+              <input onChange={this.handleChange} name="age" placeholder="age" value={this.state.age} type="number" />
+              <input
+                onChange={this.handleChange}
+                name="email"
+                placeholder="email"
+                value={this.state.email}
+                type="email"
+              />
+            </form>
+            <button onClick={this.addNewFriend}>Add New Friend</button>
+          </div>
         </header>
       </div>
     );
@@ -41,5 +80,3 @@ class App extends React.Component {
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
-
-
