9233eb0d42f2d8f7b5d060b8dbf16e627e23662c
Daniel Ferrer <danfrrr13@gmail.com>
2018-10-18 14:57:37 -0400

    added functionality. Can now add new friends.

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
+const blankFriend = {
+  name: '',
+  age: '',
+  email: ''
+}
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      friends: []
+      friends: [],
+      newFriend: {
+        name: '',
+        age: '',
+        email: '',
+      }
     }
   }
+
+  
 
   componentDidMount() {
     axios.get('http://localhost:5000/friends')
@@ -16,14 +29,34 @@ class App extends Component {
       .catch(error => console.log(error))
   }
 
+  changeHandler = (event) => {
+    this.setState({ newFriend: {
+      ...this.state.newFriend,
+      [event.target.name]: event.target.value
+    } })
+  }
+
+  addFriend = event => {
+    event.preventDefault();
+    axios
+      .post('http://localhost:5000/friends', this.state.newFriend)
+      .then(response => {
+        this.setState({ 
+          friends: response.data,
+          newFriend: blankFriend, 
+        })
+      });
+    
+  }
+
   render() {
     return (
       <div className="App">
-        <form>
-          <input type="text" placeholder="name" />
-          <input type="number" placeholder="age" />
-          <input type="text" placeholder="email" />
-          <input type="submit" />
+        <form onSubmit={this.addFriend}>
+          <input name="name" onChange={this.changeHandler} value={this.state.newFriend.name} type="text" placeholder="name" />
+          <input name="age" onChange={this.changeHandler} value={this.state.newFriend.age} type="number" placeholder="age" />
+          <input name="email" onChange={this.changeHandler} value={this.state.newFriend.email} type="text" placeholder="email" />
+          <input onClick={this.addFriend} value="Submit" type="submit" />
         </form>
 
         {this.state.friends.map(friend => (
