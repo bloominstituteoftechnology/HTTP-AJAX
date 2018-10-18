9ca55b3edba755da54a071713437dce24c968064
GannonDetroit <gkleblanc@gmail.com>
2018-10-18 16:55:28 -0400

    made an update file, still working on it though

---
+++ friends/src/Components/updateFriends.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const UpdateFriends = props => {
+    return(
+        <div>
+
+            <form>
+            <input
+                 type = "text"     
+                 name = "name"    
+                 onChange = {props.updateHandler}          
+                 placeholder = {props.friend.name}
+            /> <br></br>
+             <input 
+                  type = "number"
+                  name = "age"
+                 onChange = {props.updateHandler}
+                  placeholder = {props.friend.age}
+             /> <br></br>
+             <input 
+                  type = "text"
+                  name = "email"
+               onChange = {props.updateHandler}
+               placeholder = {props.friend.email}
+             /> <br></br>
+             <button onClick = {props.submitFriendUpdate}>
+                 Submit Update
+            </button>
+            </form>
+
+        </div>
+    )
+}
+
+export default UpdateFriends;
