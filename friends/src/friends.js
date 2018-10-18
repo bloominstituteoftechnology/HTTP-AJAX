57b491a063008fa4c4e7c50ad54eebab70ecc2f3
AM_Myrick <amyrick.820@gmail.com>
2018-10-18 15:26:16 -0400

    adds delete functionality

--- friends/src/friends.js
+++ friends/src/friends.js
@@ -1,6 +1,11 @@
 import React from "react";
 import axios from "axios";
 
+const blankFriend = {
+    name: "",
+    email: "",
+    age: "",
+}
 class Friends extends React.Component {
     constructor(props){
         super(props);
@@ -25,7 +30,7 @@ class Friends extends React.Component {
         this.setState({ 
             newFriend: {
                 ...this.state.newFriend,
-                [e.target.name]: e.target.value
+                [e.target.name]: e.target.value,
         }
         });
     }
@@ -36,11 +41,17 @@ class Friends extends React.Component {
             .post("http://localhost:5000/friends", this.state.newFriend)
             .then(res => this.setState(
                 {friends: res.data,
-                newFriend: {
-                    name: "",
-                    email: "",
-                    age: "",}
+                newFriend: blankFriend,
                 }))
+    }
+
+    deleteFriend = (e, id) => {
+        e.preventDefault();
+        axios
+            .delete(`http://localhost:5000/friends/${id}`)
+            .then(res => this.setState({
+                friends: res.data,
+            }))
     }
 
     render() {
@@ -48,12 +59,17 @@ class Friends extends React.Component {
         <div>
             <h1>A List of Friends</h1>
             {this.state.friends.map(friend => {
-            return <div key={friend.id}><p>Name: {friend.name}</p><p>Email: {friend.email}</p><p>Age: {friend.age}</p></div>
+            return <div key={friend.id}>
+                <p>Name: {friend.name}</p>
+                <p>Email: {friend.email}</p>
+                <p>Age: {friend.age}</p>
+                <button onClick={e => this.deleteFriend(e, friend.id)}>Delete Friend</button>
+                </div>
         })}
             <form onSubmit={this.addFriend}>
                 <input type="text" onChange={this.changeHandler} name="name" value={this.state.newFriend.name} placeholder="name" ></input>
-                <input type="text" onChange={this.changeHandler} name="email" value={this.state.newFriend.email} placeholder="email"></input>
-                <input type="text" onChange={this.changeHandler} name="age" value={this.state.newFriend.age} placeholder="age"></input>
+                <input type="email" onChange={this.changeHandler} name="email" value={this.state.newFriend.email} placeholder="email"></input>
+                <input type="number" onChange={this.changeHandler} name="age" value={this.state.newFriend.age} placeholder="age"></input>
             </form>
             <button onClick={this.addFriend}>Add Friend</button>
         </div>
