0ef0c73ace118a21c07f203ed9bd0586ea8abcd0
samsisle <samko9522@gmail.com>
2018-10-22 01:00:43 -0700

    Configure for hosting on heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 let nextId = 7;
@@ -14,38 +15,38 @@ let friends = [
     id: 1,
     name: 'Ben',
     age: 30,
-    email: 'ben@lambdaschool.com',
+    email: 'ben@lambdaschool.com'
   },
   {
     id: 2,
     name: 'Austen',
     age: 32,
-    email: 'austen@lambdaschool.com',
+    email: 'austen@lambdaschool.com'
   },
   {
     id: 3,
     name: 'Ryan',
     age: 35,
-    email: 'ryan@lambdaschool.com',
+    email: 'ryan@lambdaschool.com'
   },
   {
     id: 4,
     name: 'Sean',
     age: 35,
-    email: 'sean@lambdaschool.com',
+    email: 'sean@lambdaschool.com'
   },
   {
     id: 5,
     name: 'Michelle',
     age: 67,
-    email: 'michelle@gmail.com',
+    email: 'michelle@gmail.com'
   },
   {
     id: 6,
     name: 'Luis',
     age: 47,
-    email: 'luis@lambdaschool.com',
-  },
+    email: 'luis@lambdaschool.com'
+  }
 ];
 
 app.use(cors());
@@ -69,17 +70,25 @@ app.put('/friends/:id', (req, res) => {
     friends[friendIndex] = { ...friends[friendIndex], ...req.body };
     res.status(200).json(friends);
   } else {
-    res
-      .status(404)
-      .json({ message: `The friend with id ${id} does not exist.` });
+    res.status(404).json({ message: `The friend with id ${id} does not exist.` });
   }
 });
 
 app.delete('/friends/:id', (req, res) => {
-	friends = friends.filter(friend => friend.id != req.params.id);
-	res.status(200).json(friends);
+  friends = friends.filter(friend => friend.id != req.params.id);
+  res.status(200).json(friends);
 });
 
-app.listen(5000, () => {
+app.use(express.static(path.resolve(__dirname, './friends/', 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './friends/', 'build/index.html'), err => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+app.listen(process.env.PORT || 5000, () => {
   console.log('server listening on port 5000');
 });
