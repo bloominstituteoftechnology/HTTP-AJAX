da6dde9324fbec7d2e2d4c1e4a50a6039d63b4ec
Colin Dismuke <colin.dismuke@gmail.com>
2018-10-22 22:40:33 -0500

    Built Friends component to display list of friends

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Link } from 'react-router-dom';
 
 import Friend from './Friend';
@@ -6,7 +6,20 @@ import Friend from './Friend';
 const Friends = (props) => {
     return (
         <div>
-            {props.friends.map( friend => )}
+            {props.friends.map( friend => {
+                return (
+                    <div key={friend.id}>
+                        <h1>{friend.name}</h1>
+                        <h3>{friend.email}</h3>
+                        <p>{friend.name} is <strong>{friend.age}</strong> years old.</p>
+                        <Link to={`/${props.match.url}/${friend.id}`} style={{textDecoration: 'none'}}>See More</Link>
+
+                        <Route path={`/${props.match.path}/${friend.id}`} render={ props =>
+                            <Friend {...props} friends={props.friends}/>
+                        }/>
+                    </div>
+                )
+            })}
         </div>
     )
 }
