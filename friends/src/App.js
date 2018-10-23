60100cb65e049116757a9104f4523301bd162c70
kurtve <kurt@vanetten.us>
2018-10-22 23:24:14 -0400

    Use axios to get the friends list and then display it

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+      .then(response => this.setState({friends: response.data}));
+  }
+
+
   render() {
     return (
       <div className="App">
         <h1>Friend App</h1>
+        <ul>
+          {this.state.friends.map( (friend) => {
+            return (
+              <li key={friend.id}>
+                Name: {friend.name} Age: {friend.age} Email: {friend.email}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
