2b2c2a976533266978d9cade807e18401ee42710
stevekutz <stkutz@gmail.com>
2018-10-22 23:15:07 -0500

    building Friend & FriendList components, modifying Apps to renfer data

---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Friend from './Friend';
+
+const FriendList  = (props) => {
+  return (
+    <div>
+      {props.f}
+
+
+    </div>
+  );
+
+
+
+};
+
+export default FriendList;
