68a86defcad6649cd5c295f75b140e2b4ce22a87
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2018-10-23 14:16:54 -0500

    Implemented GET & POST

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-class App extends Component {
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friendsList: [],
+      name: "",
+      age: "",
+      email: ""
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends').then(response => this.setState({friendsList: [...response.data]}))
+  }
+
+  addNewFriend = e => {
+    e.preventDefault();
+    if(this.state.name && this.state.age && this.state.email) {
+        const friend = {
+        id: this.state.friendsList.length + 1,
+        name: this.state.name,
+        age: this.state.age,
+        email: this.state.email
+      }
+      axios.post('http://localhost:5000/friends', friend).then(axios.get('http://localhost:5000/friends')
+        .then(response => this.setState({
+        friendsList: [...response.data],
+        name: "",age:"",email:""
+        })
+      ))
+    }        
+  }
+  changeHandler = e => this.setState({[e.target.name]: e.target.value});
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return  <div className="App">
+                {this.state.friendsList.map(friend => <p key={friend.id}>{friend.name}</p>)}
+                <form>
+                    <input placeholder="name" onChange={this.changeHandler} name="name" value={this.state.name}></input>
+                    <input placeholder="age" onChange={this.changeHandler} name="age" value={this.state.age}></input>
+                    <input placeholder="email" type="email" onChange={this.changeHandler} name="email"  value={this.state.email}></input>
+                    <button onClick={this.addNewFriend}>Send Post</button>
+                </form>
+                
+            </div>  
   }
 }
-
-export default App;
