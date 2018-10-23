30c8ed662e777e126d6cd1f202f37c44d2f8ec82
alasalle <angela.lasalle91@gmail.com>
2018-10-23 16:32:50 -0400

    implemented update and delete functionality using PUT and DELETE

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,73 +1,99 @@
-import React from 'react';
-import axios from 'axios';
-import Friend from './Friend';
-import NewFriendForm from './NewFriendForm';
+import React from "react";
+import axios from "axios";
+import Friend from "./Friend";
+import NewFriendForm from "./NewFriendForm";
 
 export default class FriendList extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            friends: [],
-            newFriend: {
-                name: '',
-                age: 0,
-                email: '',
-            }
-        }
-    }
-    componentDidMount() {
-        axios.get('http://localhost:5000/friends')
-            .then( response => {
-                this.setState({
-                    friends: response.data
-                });
-            })
-            .catch( error => {
-                console.log(error);
-            })
-    }
-
-    inputChange = e => {
-        e.preventDefault();
+  constructor() {
+    super();
+    this.state = {
+      friends: [],
+      newFriend: {
+        name: "",
+        age: 0,
+        email: ""
+      }
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(response => {
         this.setState({
-            newFriend: {
-                ...this.state.newFriend,
-                [e.target.name]: e.target.value,
-            }
-        })
-    }
+          friends: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
-    numberInputChange= e => {
-        e.preventDefault();
-        this.setState({
-            newFriend: {
-                ...this.state.newFriend,
-                [e.target.name]: parseInt(e.target.value),
-            }
-        })
-    }
+  inputChange = e => {
+    e.preventDefault();
+    this.setState({
+      newFriend: {
+        ...this.state.newFriend,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
 
-    addNewFriend = () => {
-        axios.post('http://localhost:5000/friends', this.state.newFriend)
-            .then(response => console.log(response))
-            .catch(error => console.log(error));
-        
-        this.setState({
-            newFriend: {
-                name: '',
-                age: 0,
-                email: ''
-            }
-        })
-    }
+  numberInputChange = e => {
+    e.preventDefault();
+    this.setState({
+      newFriend: {
+        ...this.state.newFriend,
+        [e.target.name]: parseInt(e.target.value)
+      }
+    });
+  };
 
-    render() {
-        return (
-            <>
-            {this.state.friends.map(friend => <Friend key={friend.id} friend={friend} />)}
-            <NewFriendForm inputChange={this.inputChange} addNewFriend={this.addNewFriend} numberInputChange={this.numberInputChange} newFriend={this.state.newFriend}/>
-            </>
-            
-        )
-    }
+  addNewFriend = (e) => {
+      e.preventDefault();
+    axios
+      .post("http://localhost:5000/friends", this.state.newFriend)
+      .then(response => this.setState({friends: response.data}))
+      .catch(error => console.log(error));
+
+    this.setState({
+      newFriend: {
+        name: "",
+        age: 0,
+        email: ""
+      }
+    });
+  };
+
+    updateFriend = (friend) => {
+    axios.put(`http://localhost:5000/friends/${friend.id}`, friend)
+        .then(response => this.setState({friends: response.data}))
+        .catch(error => console.log(error))
+  };
+
+  deleteFriend = (friend) => {
+      axios.delete(`http://localhost:5000/friends/${friend.id}`)
+        .then(response => this.setState({ friends: response.data}))
+        .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.friends.map(friend => (
+          <Friend
+            key={friend.id}
+            friend={friend}
+            updateFriend={this.updateFriend}
+            deleteFriend={this.deleteFriend}
+          />
+        ))}
+        <NewFriendForm
+          inputChange={this.inputChange}
+          addNewFriend={this.addNewFriend}
+          numberInputChange={this.numberInputChange}
+          newFriend={this.state.newFriend}
+        />
+      </>
+    );
+  }
 }
