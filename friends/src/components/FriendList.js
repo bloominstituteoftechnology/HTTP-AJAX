a448416b116d212b5a2007e0184f44a2f8431140
TheGameFreak720 <luis.alvarez@commithub.com>
2018-10-23 00:02:34 -0400

    we can now fetch the friend array and add it to state

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class FriendList extends Component {
     constructor() {
@@ -9,10 +10,22 @@ class FriendList extends Component {
     }
 
     componentDidMount() {
-
+        axios.get('http://localhost:5000/friends')
+            .then(function (response) {
+                this.setState({
+                    friends: response.data
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     render() {
-        
+        return (
+            <p>Hello World</p>
+        );
     }
 }
+
+export default FriendList;
