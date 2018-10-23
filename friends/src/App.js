a872d7d541970958da94e56ea9549b5f59e6aa82
cferrell2 <cferrell12182@gmail.com>
2018-10-23 00:39:54 -0400

    load friends data using axios - App.js - componentDidMOunt

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+
+import FriendsList from './components/FriendsList.js'
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor(){
+    super();
+    this.state = {
+      friends: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get("http://localhost:5000/friends")
+      .then(response =>{
+        this.setState({
+          friends: response.data
+        })
+      })
+  }
+
+  render(){
     return (
       <div className="App">
-        HTTP-AJAX APP
+        {console.log(this.state.friends)}
       </div>
-    );
+    )
   }
+
 }
 
 export default App;
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FriendsList = (props) =>{
+
+    return (
+        <div>
+            FriendsList
+        </div>
+    )
+
+}
+
+export default FriendsList;
