8649929f54e9694416514e2c56cf00a274154e9f
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-10-23 17:00:00 -0600

    Working through this thing. Having a few errors but I have made some progress.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,33 +1,21 @@
 import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
+import { Route } from 'react-router-dom';
 
-class App extends Component {
+import FriendList from './components/FriendList';
+
+export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      friends: [],
-    };
-  }
-
-  componentDidMount() {
-    axios
-      .get('http://localhost:5000/friends')
-      .then(response => {
-        this.setState(() => ({ friends: response.data }));
-      })
-      .catch(error => {
-        console.error('Server Error', error);
-      });
+        savedList: [],
+    }
   }
 
   render() {
     return (
       <div className="App">
-
+        <Route exact path="/" component={FriendList} />
       </div>
     );
-  }
-}
-
-export default App;
+  };
+};
---
+++ friends/src/components/FriendCard.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+
+// const FriendCard = props => {
+//     const { name, age, email } = props.friend;
+//     return (
+//         <div className="friend-card">
+//             <div className="friend-name">
+//                 <h2>{name}</h2>
+//             </div>
+//             <div className="friend-age">
+//                 Age: <strong>{age}</strong>
+//             </div>
+//             <div className="friend-email">
+//                 Email: <em>{email}</em>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default FriendCard;
