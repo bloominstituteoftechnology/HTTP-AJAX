570fa3ff96fdf09d9b19cf43a2b67cd34366c1d7
Mitchell Robles <mitchellr92@live.com>
2018-10-22 20:41:50 -0700

    Created friends app and created a component to get started

---
+++ friends/src/Component/FriendList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class FriendList extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <>
+                Friend List
+            </>
+        )
+    }
+}
+
+export default FriendList;
