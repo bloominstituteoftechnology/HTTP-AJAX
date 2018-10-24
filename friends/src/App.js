3659f1d481bedf8718e68c942a7a49be0fbeaa10
kammagik <kammagik@yahoo.com>
2018-10-23 21:00:06 -0700

    trying to post new friend

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,37 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   state = {
-    posts:[]
+    lists:[]
   }
 
   componentDidMount() {
     axios.get(`http://localhost:5000/friends`)
     .then(response => {
-      const posts = response.data.friends
+      const lists = response.data;
+      this.setState({lists});
     })
+    .catch(err=> {
+      console.log(err);
+    });
   }
-  
+
+handleChange = event => {
+  this.setState({ name: event.target.value});
+}
+
+handleSubmit = event => {
+  event.preventDefault();
+
+  const friend = {
+    name: this.state.name
+  };
+
+  axios.post(`http://localhost:5000/friends`, {friend})
+  .then(response => {
+    console.log(response);
+    console.log(response.data);
+  })
+}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Friends List</h1>
+        <ul>{this.state.lists.map(list => {
+          return (
+            <li key={list.id}>
+            <div>Name:{list.name}</div>
+            <div>Age:{list.age}</div>
+            <div>Email:{list.email}</div>
+            </li>
+          )
+        })}</ul>
+<form onSubmit={this.handleSubmit}>
+  <label>
+    Name:
+    <input type="text" name="name" onChange={this.handleChange}/>
+    </label>
+    <label>
+      Age:
+    <input type="text" name="age" onChange={this.handleChange}/>
+    </label>
+    <label>
+      Email:
+    <input type="text" name="email" onChange={this.handleChange}/>
+  </label>
+  <input type="submit" value="Submit" />
+</form>
       </div>
+
     );
   }
 }
