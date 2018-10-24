e6575e846a93c15984420798d57f3ec67545ae30
Griffin <griffinshuth@gmail.com>
2018-10-23 18:25:28 -0600

    running into an error on failed to compile

---
+++ friends/src/components/Friends.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Friends extends Component {
+    constructor(){
+    super();
+    this.state ={
+        friends: []
+    }
+    
+    
+    
+    return({
+
+    })
+}
+}
+   
+
+   
+export default Friends;
