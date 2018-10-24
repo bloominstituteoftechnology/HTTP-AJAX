4e41abb300f1f6a5737fab7e199e7a3788541b27
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-10-23 22:55:04 -0400

    added the code needed to show the list of friends

---
+++ friends/src/ListFriends.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function ListFriends(props) {
+  return (
+    <div>
+      {props.friends.map(friend => {
+        return(
+          <div key={friend.id}>
+            <h1>This friend is named {friend.name}</h1>
+            <h3>Their age is {friend.age}</h3>
+            <h3>Their email is: {friend.email}</h3>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
