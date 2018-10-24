2fbf35a253ac06eb830f7a69bd2e53b60aec14f9
Levi Appenfelder <levi.appenfelder@gmail.com>
2018-10-23 21:02:05 -0600

    Code Bug Fix

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from '../node_modules/axios'
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+
+      friends: []
+    }
+  }
+
+  componentDidMount() {
+        axios
+        .get("http://localhost:5000/friends")
+        .then (response => {this.setState({
+          friends: response.data})
+       })
+        .catch (err => console.log(err));
+    }
   render() {
+    console.log(this.state.friends)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <section className="Friends">
+         {this.state.friends}
+        </section>
       </div>
     );
   }
