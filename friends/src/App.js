d0406663f9d20999ed02e0a70fbff05e9634da22
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-10-23 22:55:50 -0400

    added listfriends component to the App.js file

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import ListFriends from './ListFriends'
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    friends : [],
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        console.log(response.data);
+        this.setState({friends: response.data})
+      })
+  }
+
   render() {
     return (
       <div className="App">
+      <ListFriends friends={this.state.friends} />
       </div>
     );
   }
