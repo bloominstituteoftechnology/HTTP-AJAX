e417dd2a418119e464e0eca30bd9e5cfba196fba
Kyle Baker <k@gnomiq.com>
2018-10-23 19:22:20 -0700

    List friends coming from server

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      friends: []
+    }
+  }
+
+  componentDidMount() {
+      axios
+          .get(`http://localhost:5000/friends/`)
+          .then(response => {
+              this.setState({friends: response.data});
+          })
+          .catch(error => {
+              console.error('Server Error', error);
+          });
+  }
+
   render() {
+    const { friends } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App flex-column">
+          {!!friends.length && friends.map(friend => (
+              <span>{friend.name}</span>
+          ))}
       </div>
     );
   }
