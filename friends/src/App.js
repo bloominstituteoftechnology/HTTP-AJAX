4a86bfae830d292c7522abb34c56a1ca5752539c
jaklundun <govi.escudero@gmail.com>
2018-10-24 15:37:44 -0700

    Created placeholder FriendsList SFC.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
-// import jaklundun from 'govi'; initial commit
+import './App.css';// import jaklundun from 'govi'; initial commit
+
+import FriendsList from './components/FriendsList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <FriendsList />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+function FriendsList (props) {
+    return (
+        <div>
+            <ul>
+                <li>Friends List - Comming Soon!</li>
+            </ul>
+        </div>
+    )
+}
+
+export default FriendsList;
