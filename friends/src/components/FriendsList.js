87d977388d521923bfc6060f9c68f52af272af78
Pascale Pierre <pascaleee@gmail.com>
2018-10-23 20:30:59 -0500

    friends list noe visible

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+
+export default class FriendsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends').then(response => {
+      this.setState(() => ({friends: response.data}));
+      console.error('State', this.state.friends);
+    }).catch(error => {
+      console.error('Server Error', error);
+    });
+  }
+
+  render() {
+      return (
+
+
+        <div className="movie-list">
+
+          {this.state.friends.map(friend => (
+              <FriendDetails key={friend.email} friend={friend} />
+          ))}
+
+        </div>
+
+
+      );
+    }
+}
+
+function FriendDetails({ friend }) {
+  const { name, age, email } = friend;
+  return (
+    <div className="friend-card">
+      <h2>{name}</h2 > <div className="friend-age">
+      Age:
+      <em>{age}</em>
+    </div>
+    <div className="friend-email">
+      email:
+      <em>{email}</em>
+    </div>
+
+  </div>);
+  }
