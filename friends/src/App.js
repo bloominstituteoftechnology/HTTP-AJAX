278c08188efa8e5d9089cc7b217919193f8ec73d
kohler.nai@gmail.com <kohler.nai@gmail.com>
2018-10-23 20:15:09 -0600

    Displaying friends and properties moving to friendslist.js now

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+import Friendslist from './components/Friendslist'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: [],      
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        console.log(response);
+        this.setState({
+          friends: response.data
+        })
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Friends List</h1>
+          {this.state.friends.map(friend => {
+            return (
+              <div>
+                <h2>{friend.name}</h2>
+                <p>{friend.age}</p>
+                <p>{friend.email}</p>
+              </div>            
+            )
+          })}
       </div>
     );
   }
