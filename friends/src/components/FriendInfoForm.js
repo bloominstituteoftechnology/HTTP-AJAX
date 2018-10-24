65aa4fdd2d14d5e73e14b880e99d23dc9fcce469
jaklundun <govi.escudero@gmail.com>
2018-10-24 16:39:15 -0700

    Created a placeholder FriendInfoForm SFC.

---
+++ friends/src/components/FriendInfoForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+function FriendInfoForm (props) {
+    return (
+        <div>
+            <form>
+                <input type="text" placeholder="Placeholder Friend Info Form - FRIEND Name "/>
+                <input type="text" placeholder="Placeholder Friend Info Form - FRIEND Country "/>
+                <input type="text" placeholder="Placeholder Friend Info Form - FRIEND City "/>
+                <input type="email" placeholder="Placeholder Friend Info Form - FRIEND Email"/>
+                <input type="tel" placeholder="Placeholder Friend Info Form - FRIEND Numbers"/>
+            </form>
+        </div>
+        )
+    }
+
+export default FriendInfoForm;
