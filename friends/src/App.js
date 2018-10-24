44bb1552afabcc9a4f67accb2996e30a4147265c
iepoch <staclem@live.com>
2018-10-23 20:19:30 -0700

    Added axios.put, and post. Need to refactor the site.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
-import Axios from "axios";
-import Friends from './components/friends'
-import {Route } from 'react-router-dom'
-
+import axios from "axios";
+import Friends from "./components/friends";
+import { Route } from "react-router-dom";
+import { Input, Button } from "@material-ui/core";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      friends: []
+      friends: [],
+      name: "",
+      age: null,
+      email: ""
     };
-
   }
 
   componentDidMount() {
-    Axios.get("http://localhost:5000/friends")
+    axios
+      .get("http://localhost:5000/friends")
       .then(response => {
         console.log(response);
         this.setState({ friends: response.data });
@@ -29,13 +31,65 @@ class App extends Component {
       });
   }
 
+  addNewFriend = () => {
+    const newFriendObj = {
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email
+    };
+    axios
+      .post("http://localhost:5000/friends", newFriendObj)
+      .then(response => {
+        this.setState({
+          friends: response.data,
+          name: '',
+          age: '',
+          email: ''
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
   render() {
-
-
-    return <div className="App">
+    return (
+      <div className="App">
         <h1>Friends</h1>
-      <Route path="/" render={() => <Friends friends={this.state.friends} />} />
-      </div>;
+        <Route
+          path="/"
+          render={() => <Friends friends={this.state.friends} />}
+        />
+
+        <form onSubmit={this.addNewFriend}>
+          <Input
+            onChange={this.handleChange}
+            name="name"
+            value={this.state.name}
+            type="text"
+            placeholder="Name"
+          />
+          <Input
+            onChange={this.handleChange}
+            name="age"
+            value={this.state.age}
+            type="number"
+            placeholder="age"
+          />
+          <Input
+            onChange={this.handleChange}
+            name="email"
+            value={this.state.email}
+            type="email"
+            placeholder="Email"
+          />
+        </form>
+        <Button onClick={this.addNewFriend}> Add</Button>
+      </div>
+    );
   }
 }
 
