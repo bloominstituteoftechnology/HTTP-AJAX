0e306c332e1a8b0b32634ee7b17bdf72e28ed83a
kammagik <kammagik@yahoo.com>
2018-10-23 18:35:11 -0700

    started  get

---
+++ friends/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    posts:[]
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/friends`)
+    .then(response => {
+      const posts = response.data.friends
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
