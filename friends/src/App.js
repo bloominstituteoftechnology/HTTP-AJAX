58e01afeae1947543f8121699e672b0765175035
Javier Alavrez <javieralvarez82@live.com>
2018-10-23 23:09:25 -0700

    added axios to componentDidMount confirmed data is being accessed working on friends component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: [{}],
+    }
+  }
+    componentDidMount() {
+      Axios
+        .get("//localhost:5000/friends")
+        .then(Response => {
+          console.log(Response);
+          this.setState({ friends: Response.data});
+        })
+    }
+
+
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
         </header>
       </div>
     );
