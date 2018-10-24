12f0e6384c44e9e1fcb97ede7ae2701c80df3836
marcusbass323 <marcus@bassconsultantgroup.com>
2018-10-23 19:49:48 -0700

    First Commit

---
+++ friends/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import friends_display from './components/friends_display';
+
+
+class App extends Component {
+  componentDidMount() {
+    console.log("inside CDM");
+    
+  }
+}
+
+export default App;
---
+++ friends/src/components/friends_display.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+
+class friends_display extends React.Component {
+    
+}
+   
+
+export default friends_display;
