4d9d81e7d594d0da63872376d2a4e338ce337f45
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-10-24 00:00:43 -0400

    changed import order added StyledApp

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import styled from 'styled-components';
 
 import FriendsContent from './components/FriendsList/FriendsContent.js';
+
+const StyledApp = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 80%;
+`;
 
 class App extends Component {
   render() {
