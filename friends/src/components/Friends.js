f90f782abdbbcbb30ce3dfa597132027591bdbde
Denise Escobar <descobar1490@gmail.com>
2018-10-23 22:13:03 -0500

    POST request is functioning
    -Created to handle envents one for the input and on the on submiting the form preforms a post request
    -My list of friends updates based on the form inputs

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -6,7 +6,11 @@ class Friends extends Component{
     constructor(){
         super()
         this.state= {
-            friends: []
+            friends: [],
+            name: '',
+            age: '',
+            email: '',
+            id: 0,
         }
     }
 
@@ -16,10 +20,42 @@ class Friends extends Component{
             .then(response =>{
                 console.log(response.data)
                 this.setState({
-                    friends: response.data
+                    friends: response.data,
+                    id: response.data.length
                 })
             })
             .catch(err => console.log(err));
+    }
+
+    inputHandle = (event) =>{
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+        console.log(this.state.name);
+        console.log(this.state.age);
+        console.log(this.state.email)
+    }
+
+    submitHandle = (event) =>{
+        axios
+            .post('http://localhost:5000/friends',{
+                id: this.state.id +1,
+                name: this.state.name,
+                age: this.state.age,
+                email: this.state.email
+            })
+            .then(response =>{
+                console.log(response);
+                this.setState({
+                    name: "",
+                    age: "",
+                    email: "",
+                    id: this.state.friend.length
+                })
+            })
+            .catch(err =>{
+                console.log(err);
+            })
     }
 
     render(){
@@ -28,6 +64,13 @@ class Friends extends Component{
                 {this.state.friends.map(item => {
                     return (<Friend key={item.id} friend={item}/>)
                 })}
+                <form>
+                    <h1>Add a Friend</h1>
+                    <span>Name: </span><input type='text' placeholder='Name' name='name' value={this.state.name} onChange={this.inputHandle}></input>
+                    <span>Age: </span><input type='text' placeholder='Age' name='age' value={this.state.age} onChange={this.inputHandle}></input>
+                    <span>Email: </span><input type='text' placeholder='Email' name='email' value={this.state.email} onChange={this.inputHandle}></input>
+                    <input type='submit' onClick={this.submitHandle}></input>
+                </form>
             </div>
         )
     }
