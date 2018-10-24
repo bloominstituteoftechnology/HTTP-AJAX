12985ccd7d6aea9f4d6ebf9107bdbdfb25d5a9b3
Griffin <griffinshuth@gmail.com>
2018-10-23 21:33:16 -0600

    got my POST done

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,84 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
-import Friends from 'Friends';
+import Friends from './components/Friends';
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: [],
+      id: '',
+      name: '',
+      age: '',
+      email: '',
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        console.log(response.data);
+        this.setState({ friends: response.data })
+      })
+  }
+
+  name = (event) => {
+    const newName = event.target.value;
+    this.setState({
+      name: newName
+    })
+  }
+  age = (event) => {
+    const newAge = event.target.value;
+    this.setState({
+      age: newAge
+    })
+  }
+  email = (event) => {
+    const newEmail = event.target.value;
+    this.setState({
+      email: newEmail
+    })
+  }
+  addFriend = (event) => {
+    event.preventDefault();
+    let newId = this.state.friends.length + 1;
+    this.setState({
+      id: newId
+    })
+
+    const newFriend = {
+      id: this.state.id,
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email,
+    }
+
+    axios
+      .post('http://localhost:5000/friends', newFriend)
+      .then(response => {
+        this.setState({
+          friends: response.data,
+        })
+          
+      })
+      .catch(error => { console.log('error!') })
+  }
   render() {
     return (
       <div className="App">
-        <Friends/>
+        {this.state.friends.map(friend => (
+          <Friends key={friend.id} friend={friend} />
+        ))}
+        <form>
+          <input placeholder='Name' onChange={this.name}></input>
+          <input placeholder='Age' onChange={this.age}></input>
+          <input placeholder='Email' onChange={this.email}></input>
+          <button onClick={this.addFriend}>Save</button>
+
+        </form>
+
       </div>
     );
   }
--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 
 class Friends extends Component {
-    constructor(){
-    super();
-    this.state ={
-        friends: []
-    }
-    
-    
-    
-    return({
+    constructor(props){
+    super(props);
+   
+} 
+    render(){
+    return(
+        <div>
+            <p>Name:{this.props.friend.name}</p>
+            <p>Age:{this.props.friend.age}</p>
+            <p>Email:{this.props.friend.email}</p>
+        </div>
+    )
+}
+}
 
-    })
-}
-}
+
    
 
    
