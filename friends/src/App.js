accfdb2b8677ea471bbf7ff77a92ff9a9dba504d
Rob Salzberg <robsalzberg@gmail.com>
2018-10-23 22:45:30 -0400

    Set up get request but does not seem to be rendering

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
+
+import FriendsList from './components/FriendsList'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+  componentDidMount() {
+    axios.get("http://localhost:5000/friends").then(response => {
+      this.setState({
+        data: response.data
+      });
+    });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <FriendsList data={this.state.data} />;
   }
 }
 
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FriendsList = props => {
+    return (
+      <div>
+        {props.data.map(friend => {
+          return (
+            <div key={friend.id}>
+              <h1>Name: {friend.name}</h1>
+              <p>Age: {friend.age}</p>
+              <p>Email: {friend.email}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+   export default FriendsList;
