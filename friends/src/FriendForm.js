e94727282e9fd5a430dc63f5ca86cf4f11b69a18
Robin U Familara <robin.familara@cgius.com>
2018-10-23 19:34:12 -0500

    Add a form to gather information about a new friend

---
+++ friends/src/FriendForm.js
@@ -0,0 +1,33 @@
+import React, {Fragment} from 'react';
+ const FriendForm = props => {
+    return (
+      <Fragment>
+      <form>
+        <label>Name: </label>
+        <input 
+          type="text" 
+          placeholder="Name" 
+          required value={props.name} 
+          onChange={props.handleChange} 
+          name="name" />
+        <label>Age: </label>
+        <input 
+          type="text" 
+          placeholder="Age" 
+          required value={props.age}
+          type="number" 
+          onChange={props.handleChange} 
+          name="age" />
+        <label>Email: </label>
+        <input 
+          type="text" 
+          placeholder="Email" 
+          required value={props.email}  
+          onChange={props.handleChange} 
+          name="email" />
+      </form>
+    </Fragment>
+  );
+ }
+  
+  export default FriendForm;
