64a195d84755a172d49eb5829b3dbe4c9b23c58c
Joe Mercado <JoeM@Firsts-MacBook-Pro-13.local>
2018-10-23 23:06:47 -0400

    second commit

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import FriendsList from './components/FriendsList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <Router>
+        <div>
+          <Route path='/friends' component={FriendsList} />
+        </div>
+    </Router>
+  )
 }
-
 export default App;
