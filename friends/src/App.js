3e806b0fa98485cac290730b5a0d99c0c3d6e02a
BrendonKootz <brendonkootz@gmail.com>
2018-10-25 12:50:05 -0500

    Friends Data displays now

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+	constructor(){
+		super();
+		this.state = {friends:[]}
+	}
+
+	componentDidMount(){
+		axios
+			.get('http://localhost:5000/friends')
+			.then(response => this.setState({friends : response.data}))
+			.catch(error => console.log(error));
+	}
+
+
+
+
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1> FRIENDS </h1>
+        {this.state.friends.map(friend => {
+        	return(
+        		<div>
+        			<div>{friend.id}</div>
+        			<div>{friend.name}</div>
+        			<div>{friend.age}</div>
+        			<div>{friend.email}</div>
+        		</div>
+        	)
+        })}
       </div>
     );
   }
