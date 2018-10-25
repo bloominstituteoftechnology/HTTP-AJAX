032acb9aa0996625f17a955ed6561bc20298d382
Dewayne87 <dewaynelindsay87@gmail.com>
2018-10-25 16:58:17 -0700

    got update functionality to work

--- friends/src/App.js
+++ friends/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import FriendList from './Friendlist';
 
 
+
 class App extends Component {
   constructor() {
     super();
@@ -51,8 +52,8 @@ class App extends Component {
       });
 
   }
-  update = (key) => {
-    axios.put(`http://localhost:5000/friends/${key}`)
+  update = (id,friend) => {
+    axios.put(`http://localhost:5000/friends/${id}`,friend)
       .then((response) => {
         console.log(response);
         this.setState(() => ({ friendlist: response.data }));
@@ -60,14 +61,14 @@ class App extends Component {
       .catch(error => {
         console.log(error);
       });
-    console.log('thi', key)
   }
 
   render() {
     return (
       <div className="App">
         <Route exact path="/" render={(props) => <HomePage {...props} newFriend={this.addNewFriend} />} />
-        <Route path="/friendlist/" render={(props) => <FriendList {...props} delete={this.delete} friends={this.state.friendlist} />} />
+        <Route path="/friendlist/" render={(props) => <FriendList {...props} delete={this.delete} update={this.update} friends={this.state.friendlist} />} />
+         
       </div>
     );
   }
--- friends/src/Friend.js
+++ friends/src/Friend.js
@@ -1,17 +1,54 @@
 import React from 'react'
 
 class Friend extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            age: '',
+            email: '',
+        };
+    }
+    changeHandler = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+
+    }
+    handleUpdateSubmit = (event) => {
+        event.preventDefault();
+        const friend = {
+            name: this.state.name,
+            age: this.state.age,
+            email:this.state.email
+        }
+            const id = this.props.friends.id;
+        this.props.update(id,friend)
+        event.target.reset();
+        console.log(friend,id)
+    }
 
     render() {
+        console.log(this.props)
         return (
             <div className="cta">
-                <p className="text">
-                    Name: {this.props.friends.name}<br />
-                    Age: {this.props.friends.age}yrs<br />
-                    Email: {this.props.friends.email}<br />
-                </p>
-                <button onClick={() => this.props.delete(this.props.friends.id)}>Delete Friend</button>
+                <div>
+                    <p className="text">
+                        Name: {this.props.friends.name}<br />
+                        Age: {this.props.friends.age}yrs<br />
+                        Email: {this.props.friends.email}<br />
+                    </p>
+                    <button onClick={() => this.props.delete(this.props.friends.id)}>Delete Friend</button>
+                </div>
+                <div>
+                    <form onSubmit={this.handleUpdateSubmit}>
+                        Name: <input onChange={this.changeHandler} name="name" type="text" /><br />
+                        Age: <input onChange={this.changeHandler} name="age" type="text" /><br />
+                        Email: <input onChange={this.changeHandler} name="email" type="text" /><br />
+                        <input type="submit" value="Update Friend" />
 
+                    </form>
+                </div>
             </div>
         );
     }
