6ebbd8bee23e7896efd8fcb168f8caafa84e2751
Matthew Meitl <darksamus6@gmail.com>
2018-10-25 18:47:36 -0500

    got form and post and delete to work

--- friends/src/App.js
+++ friends/src/App.js
@@ -6,7 +6,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      friends: []
+      friends: [],
+      name: "",
+      age: "",
+      email: ""
     };
   }
   componentDidMount() {
@@ -20,6 +23,59 @@ class App extends Component {
       });
   }
 
+
+  addNewFriend = e => {
+    e.preventDefault();
+    const newFriendObj = {
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email
+    };
+    Axios
+      .post("http://localhost:5000/friends", newFriendObj)
+      .then(response => {
+        this.setState({
+          friends: response.data,
+          name: "",
+          age: "",
+          email: ""
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
+  updateFriend = (id, name, age) => {
+
+  console.log(id, name, age)
+
+  };
+
+  deleteFriend = id => {
+    console.log(id)
+    return () => {
+      Axios
+        .delete(`http://localhost:5000/friends/${id}`)
+        .then(response =>
+          this.setState({
+            friends: response.data,
+            name: "",
+            age: "",
+            email: ""
+          })
+        )
+        .catch(err => console.log(err));
+
+      console.log(id);
+    };
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+    console.log(this.state.name)
+  };  
+
   render() {
     return (
       <div className="App">
@@ -31,9 +87,43 @@ class App extends Component {
               <span>{friend.age} {""}</span>
               <br/>
               <span>{friend.email}</span>
+              <span>{friend.id}</span>
+              <input type='submit' onClick={this.deleteFriend(friend.id)} value='Delete' />
             </div>
           );
         })}
+
+
+        <input
+          onChange={this.handleChange}
+          name="name"
+          value={this.state.name}
+          type="text"
+          placeholder="Name"
+          
+        />{" "}
+        {""}
+        <input
+          onChange={this.handleChange}
+          name="age"
+          value={this.state.age}
+          type="number"
+          placeholder="age"
+          
+        />{" "}
+        <input
+          onChange={this.handleChange}
+          name="email"
+          value={this.state.email}
+          type="email"
+          placeholder="Email"
+          
+        />
+    
+          <input type="submit" onClick={this.addNewFriend} value='add' />
+      <input type="submit" onClick={this.updateHandler} value='update' />
+
+           
       </div>
     );
   }
