9accef7306d9e1d0338957c65428726ccd00d9d3
Chris Juelfs <CRJuelfs24@comcast.net>
2018-10-25 16:55:53 -0500

    just the event handlers left to add

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+      .then( response => this.setState({friends: response.data}))
+      .catch( err => console.log(err))
+    // console.log(this.state.friends);
+  }
+
+  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <form>
+        <h3>Add New friend:</h3>
+        <input placeholder='name'></input>
+        <input placeholder='age'></input>
+        <input placeholder='email'></input>
+        <button>Submit</button>
+      </form>
+        {this.state.friends.map( (eachFriend) => {
+          return (
+            <div key={eachFriend.id} className='friend-box'>
+              <div>Name: {eachFriend.name}</div>
+              <div>Age: {eachFriend.age}</div>
+              <div>Email: {eachFriend.email}</div>
+              <form>
+                <p>Update friend:</p>
+                <input placeholder='name'></input>
+                <input placeholder='age'></input>
+                <input placeholder='email'></input>
+                <button>Submit</button>
+              </form>
+              <button>DELETE FRIEND</button>
+            </div>
+          )
+        })}
       </div>
     );
   }
