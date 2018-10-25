5aa440f1276aafaac2a00d90d25ba44e8a823e2c
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-10-25 17:03:13 -0500

    fixed contact info display error

--- friends/src/Contact.js
+++ friends/src/Contact.js
@@ -5,10 +5,10 @@ function Contact(props) {
   return(
   <div className="contactCard">
     <h3>{props.name}</h3>
-    <table className="info">
+    <div className="info">
       <p className="age">{props.age} years old</p>  
       <p className="email">{props.address}</p>
-    </table>  
+    </div>  
   </div>  
   )
 }
