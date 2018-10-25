2a594115f9befae6e06207dccc228a47583d5f01
mikaelacurrier <mikaela.currier@gmail.com>
2018-10-25 16:45:10 -0400

    data from server showing up, need to fix the form input

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  constructor(props) {
+    super(props);
+    this.state = { 
+      friends: [],
+        name: '',
+        age: '',
+        email: ''
+      
+     }
+  }
+  componentDidMount () {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => this.setState({ friends: response.data }))
+      .catch(error => console.log(error));
+  }
+  changeHandler = event => {
+    this.setState({
+      [event.target.name]: event.target.value} 
+    )
+  }
+  
+  addFriend = event => {
+    event.preventDefault();
+    axios.post('http://localhost:5000/friends', this.state.newFriend)
+    .then(response => console.log(response.data))
+    .catch(err => console.log(err))
+  }
+
+  render() { 
+    return ( 
+    <div>
+        <div>{this.state.friends.map((friend, key) => {
+          return (
+            <div key={key}>
+              Number {friend.id} is {friend.name}. They are {friend.age} years old and thier email is {friend.email}. You should reach out more.
+            </div>
+          )
+        })
+
+        }
       </div>
-    );
+      <form>
+        <input type="text" onChange={this.changeHandler} name='friends' value={this.state.name} placeholder='name'></input>
+        <input type="text" onChange={this.changeHandler} name='age' value={this.state.age} placeholder='age'></input>
+        <input type="text" onChange={this.changeHandler} name='email' value={this.state.email} placeholder='email'></input>
+      </form> 
+      <button onClick={this.addFriend}>Add Friend</button>
+    </div>);
   }
 }
-
+ 
 export default App;
