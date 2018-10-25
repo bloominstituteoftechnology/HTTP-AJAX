288ef4371f6bdda743072715089ff4661c1904c3
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-10-24 20:47:03 -0400

    fixed a bug where numbers would show up as a string

--- friends/src/addFriend.js
+++ friends/src/addFriend.js
@@ -26,7 +26,7 @@ export default class Add extends React.Component {
           })
         }}>
           <input value={this.state.name} onChange={e => this.setState({name: e.target.value, id: this.props.friends.length + 1 })} placeholder='Please add name' />
-          <input value={this.state.age} onChange={e => this.setState({age: e.target.value})} placeholder='Please add age' />
+          <input value={this.state.age} onChange={e => this.setState({age: Number(e.target.value)})} placeholder='Please add age' />
           <input value={this.state.email} onChange={e => this.setState({email: e.target.value})} placeholder='Please add email' />
           <button type='submit' >Submit</button>
         </form>
