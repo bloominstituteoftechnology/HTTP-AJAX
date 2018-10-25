687e575fbf9217c3aab4b2257e90ceace1ad4254
jaklundun <govi.escudero@gmail.com>
2018-10-25 14:36:26 -0700

    Styled FriendInputForm cmpnt by refactoring code to leverage the styled components library (npm package).

--- friends/src/components/FriendInfoForm.js
+++ friends/src/components/FriendInfoForm.js
@@ -1,16 +1,36 @@
 import React from 'react'; //
+import styled from 'styled-components';
+
+const FriendInputForm = styled.div`
+display: flex;
+flex-direction:column;
+align-items: center;
+justify-content:space-around;
+width: 100%;
+`
 
 function FriendInfoForm (props) {
     return (
-        <div>
-            <form className= 'friend-input'>
+        <FriendInputForm>
+            {/* <form className= 'friend-input'> */}
+                <input type="text" placeholder="Friend Name"/>
+                <br/>
+                <input type="text" placeholder="Friend Country"/>
+                <br/>
+                <input type="text" placeholder="Friend City"/>
+                <br/>
+                <input type="email" placeholder="Friend Email"/>
+                <br/>
+                <input type="tel" placeholder="Friend Numbers"/>
+            {/* </form> */}
+            {/* <form className= 'friend-input'>
                 <input type="text" placeholder="Placeholder Friend Info Form - FRIEND Name "/>
                 <input type="text" placeholder="Placeholder Friend Info Form - FRIEND Country "/>
                 <input type="text" placeholder="Placeholder Friend Info Form - FRIEND City "/>
                 <input type="email" placeholder="Placeholder Friend Info Form - FRIEND Email"/>
                 <input type="tel" placeholder="Placeholder Friend Info Form - FRIEND Numbers"/>
-            </form>
-        </div>
+            </form> */}
+        </FriendInputForm>
         )
     }
 
