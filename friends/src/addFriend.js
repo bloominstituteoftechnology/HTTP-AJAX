435304f627d031b94c2475948c736c3ec20c0d82
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-10-24 23:11:35 -0400

    refactored the addFriend component

--- friends/src/addFriend.js
+++ friends/src/addFriend.js
@@ -13,21 +13,27 @@ export default class Add extends React.Component {
     } 
   }
   
+  changeHandler = (e) => {
+    this.setState({[e.target.name]: e.target.value, id: this.props.friends.length + 1})
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault()
+    this.props.submit(this.state)
+    this.setState({
+      name: '',
+      age: '',
+      email: ''
+    })
+  }
+
   render() {
     return (
       <div>
-        <form onSubmit={(e) => {
-          e.preventDefault()
-          this.props.submit(this.state)
-          this.setState({
-            name: '',
-            age: '',
-            email: ''
-          })
-        }}>
-          <input value={this.state.name} onChange={e => this.setState({name: e.target.value, id: this.props.friends.length + 1 })} placeholder='Please add name' />
-          <input value={this.state.age} onChange={e => this.setState({age: Number(e.target.value)})} placeholder='Please add age' />
-          <input value={this.state.email} onChange={e => this.setState({email: e.target.value})} placeholder='Please add email' />
+        <form onSubmit={this.submitHandler}>
+          <input value={this.state.name} name='name' onChange={this.changeHandler} placeholder='Please add name' />
+          <input value={this.state.age} name='age' onChange={this.changeHandler} placeholder='Please add age' />
+          <input value={this.state.email} name='email' onChange={this.changeHandler} placeholder='Please add email' />
           <button type='submit' >Submit</button>
         </form>
       </div>
