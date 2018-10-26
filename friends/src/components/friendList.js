1fcd8462f735121f679fa9357e0b6eda7df6dd75
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2018-10-26 00:08:31 -0400

    fixed ID

--- friends/src/components/friendList.js
+++ friends/src/components/friendList.js
@@ -5,11 +5,7 @@ export default class FriendList extends Component {
     return (
       <div className='friendsList'>
         <h1>Friends List </h1>
-        <ol>
-          {this.props.data.map((friend) => <li key={friend.id}> <span>Name: {friend.name} , Age: {friend.age}, Email: {friend.email} </span> <button onClick={this.props.deleteFriend(friend.id)}>Remove Friend</button></li>)}
-
-        </ol>
-
+        {this.props.data.map((friend) => <p key={friend.id}> <span> Id:{friend.id},  Name: {friend.name} , Age: {friend.age}, Email: {friend.email} </span> <button onClick={this.props.deleteFriend(friend.id)}>Remove Friend</button></p>)}
       </div>
     )
   }
