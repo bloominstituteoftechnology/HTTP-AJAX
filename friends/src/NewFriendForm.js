a4efcb76211e91d497539c75b1fad6e007a65495
Robert Billington <chillingtonbillington@gmail.com>
2018-10-25 19:45:47 -0500

    Completed semantic markup for NewFriendForm

--- friends/src/NewFriendForm.js
+++ friends/src/NewFriendForm.js
@@ -29,12 +29,13 @@ class NewFriendForm extends React.Component{
             <form onSubmit = {this.handleSubmit} className = 'add-form-container'>
                 <div className = 'add-form-header'>
                     <h2>New Friend Form</h2>
+                </div>
+                <div className = 'add-form-input-container'>    
+                    <input onChange = {this.handleChange} name = 'name' type = 'text' placeholder = '...Feed me a new friend name'/>
+                    <input onChange = {this.handleChange} name = 'age' type = 'text' placeholder = '...And their age' />
+                    <input onChange = {this.handleChange} name = 'email' type = 'text' placeholder = '...As well as an email address' />
+                    <button type = 'submit'>Add dat friend</button>
                 </div>    
-
-                <input onChange = {this.handleChange} name = 'name' type = 'text' placeholder = '...Feed me a new friend name'/>
-                <input onChange = {this.handleChange} name = 'age' type = 'text' placeholder = '...And their age' />
-                <input onChange = {this.handleChange} name = 'email' type = 'text' placeholder = '...As well as an email address' />
-                <button type = 'submit'>Add dat friend</button>
             </form>
         )
     }    
