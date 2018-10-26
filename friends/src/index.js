dc61b58bc94a3ffbb777d84d40f49e1d73dc1ab4
FreeKeyBoy <jjhutton81@gmail.com>
2018-10-25 20:52:55 -0700

    Had technical difficulties that prevented me from pushing. I finished the app. Friends can be added, updated, and deleted. No styling. I did accidentally complete the routing portion of the strech goals.

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
 
 
-serviceWorker.unregister();
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
