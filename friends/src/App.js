bba1e8a0b726ed57dbb29fbe4b09446bc96a33d0
Chris Juelfs <CRJuelfs24@comcast.net>
2018-10-25 22:23:18 -0500

    all done!

--- friends/src/App.js
+++ friends/src/App.js
@@ -7,7 +7,6 @@ class App extends Component {
     super(props);
     this.state = {
       friends: [],
-      id: '',
       name: '',
       age: '',
       email: '',
@@ -23,7 +22,7 @@ class App extends Component {
 
   //C R U D 
   // Create = POST
-  handleNewFriendInput = (e) => {
+  handleFriendInput = (e) => {
     console.log([e.target.value]);
     this.setState({[e.target.placeholder]: e.target.value})
   }
@@ -32,17 +31,18 @@ class App extends Component {
     // console.log(this.state);
     axios.post('http://localhost:5000/friends', {
       name: this.state.name,
-      age: this.state.age,
+      age: parseInt(this.state.age, 10),
       email: this.state.email
     })
       .then( response => this.setState({friends: response.data}))
       .catch( err => console.log(err))
-    console.log(e);
+    // console.log(this.state);
+
   }
 
   handleDeleteFriendButton = (e) => {
     // console.log(e.target.parentNode.children[0]);
-    // console.log(e.target.parentNode.id)
+    console.log(e.target)
     // console.log(this.state);
     axios.delete(`http://localhost:5000/friends/${e.target.parentNode.id}`)
       .then( response => {
@@ -52,14 +52,25 @@ class App extends Component {
       .catch( err => console.log(err))
   }
 
+  handleUpdateFriendSubmit = (e) => {
+    e.preventDefault();
+    axios.put(`http://localhost:5000/friends/${e.target.parentNode.id}`, {
+      name: this.state.name,
+      age: parseInt(this.state.age, 10),
+      email: this.state.email
+    })
+      .then( response => this.setState({friends: response.data}))
+      .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
       <form onSubmit={this.handleNewFriendSubmit}>
         <h3>Add New friend:</h3>
-        <input placeholder='name' onChange={this.handleNewFriendInput}></input>
-        <input placeholder='age' onChange={this.handleNewFriendInput}></input>
-        <input placeholder='email' onChange={this.handleNewFriendInput}></input>
+        <input placeholder='name' onChange={this.handleFriendInput}></input>
+        <input placeholder='age' onChange={this.handleFriendInput}></input>
+        <input placeholder='email' onChange={this.handleFriendInput}></input>
         <button>Submit</button>
       </form>
         {this.state.friends.map( (eachFriend) => {
@@ -68,11 +79,11 @@ class App extends Component {
               <div>{eachFriend.name}</div>
               <div>{eachFriend.age}</div>
               <div>{eachFriend.email}</div>
-              <form>
+              <form onSubmit={this.handleUpdateFriendSubmit}>
                 <p>Update friend:</p>
-                <input placeholder='name'></input>
-                <input placeholder='age'></input>
-                <input placeholder='email'></input>
+                <input onChange={this.handleFriendInput} placeholder='name'></input>
+                <input onChange={this.handleFriendInput} placeholder='age'></input>
+                <input onChange={this.handleFriendInput} placeholder='email'></input>
                 <button>Submit</button>
               </form>
               <button onClick={this.handleDeleteFriendButton}>DELETE FRIEND</button>
