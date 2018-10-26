cbca18afb870e9e11bd73e35cc539208d709f5d2
Robert Billington <chillingtonbillington@gmail.com>
2018-10-25 19:41:41 -0500

    Completed functionality of add and delete

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,29 +4,62 @@ import './App.css';
 
 import axios from 'axios';
 
-import FriendTable from './FriendTable.js';
+import FriendTable from './FriendTable';
+import NewFriendForm from './NewFriendForm';
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      friendsData : []
+      friendsArray : [
+      ]
     }
   }
   componentWillMount(){
     axios
       .get('http://localhost:5000/friends')
-        .then(friends =>{
-          this.setState({friendsData : friends.data})
+        .then(response =>{
+          this.setState({friendsArray : response.data})
       })
-      // .catch(error =>{
-      //   console.log(error);
-      // })
+      .catch(error =>{
+        console.log(error);
+      })
   }
+    //submitHandler should be passed to FriendForm and post a new instance of data to the url in post method
+  addFriend = (newFriend) =>{
+    //prevents the page from reloading at the point of the event which in this case would be the form in NewFriendForm
+    
+    //will post incoming data from form to our outside server
+    axios
+      .post('http://localhost:5000/friends', newFriend)
+        .then(response =>{
+          this.setState({friendsArray : response.data})
+      })
+      .catch(error =>{
+        console.log(error);
+      })
+  }
+
+  deleteFriend = (id) =>{
+    
+    axios
+      .delete(`http://localhost:5000/friends/${id}`)
+        .then(response =>{
+          this.setState({friendsArray : response.data})
+        })
+        .catch(error =>{
+          console.log(error);
+        })
+        
+  }
+
+
   render() {
     return (
       <div className="App">
-       <FriendTable friends = {this.state.friendsData}/>
+       <NewFriendForm addFriend = {this.addFriend}/>
+       <FriendTable friends = {this.state.friendsArray} deleteFriend = {this.deleteFriend}/>
+       
       </div>
     );
   }
