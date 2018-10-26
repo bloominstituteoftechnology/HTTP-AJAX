98b6ac6b76e71deef5cb04bae2b5aac76c98fe5f
Javier Alavrez <javieralvarez82@live.com>
2018-10-25 23:56:42 -0700

    added buttons for removed and update debugged issue console error unique id working on update button fuctionality

--- friends/src/Components/Form.js
+++ friends/src/Components/Form.js
@@ -6,23 +6,21 @@ class Form extends React.Component {
     
     render() {
         return(
+<form className="form-inline">
+    <label className="sr-only">First Name</label>
+      <input type="text" onChange={this.props.addNameToList} className="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="First Name"/>
 
-            <form className="form-inline">
-  <label className="sr-only" for="inlineFormInputName2">First Name</label>
-  <input type="text" onChange={this.props.addNameToList} className="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="First Name"/>
-
-  <label className="sr-only" for="inlineFormInputName2">Age</label>
-  <input type="text" onChange={this.props.addAgeToList} className="form-control mb-2 sm-sm-2" id="inlineFormInputName2" placeholder="Age"/>
-  
-  <label className="sr-only" for="inlineFormInputGroupUsername2">Email</label>
-  <div className="input-group mb-2 mr-sm-2">
+    <label className="sr-only">Age</label>
+      <input type="text" onChange={this.props.addAgeToList} className="form-control mb-2 sm-sm-2" id="inlineFormInputName2" placeholder="Age"/>
+    <label className="sr-only">Email</label>
+    <div className="input-group mb-2 mr-sm-2">
     <div className="input-group-prepend">
-      <div className="input-group-text">@</div>
+    <div className="input-group-text">@</div>
     </div>
-    <input type="text" onChange={this.props.addEmailToList} className="form-control" id="inlineFormInputGroupUsername2" placeholder="Email"/>
-  </div>
+      <input type="text" onChange={this.props.addEmailToList} className="form-control" id="inlineFormInputGroupUsername2" placeholder="Email"/>
+    </div>
 
-  <button type="submit" onClick={this.props.submitToList} className="btn btn-primary mb-2">Submit</button>
+    <button type="submit" onClick={this.props.submitToList} className="btn btn-primary mb-2">Submit</button>
 </form>
         )
     }
--- friends/src/Components/Friends.js
+++ friends/src/Components/Friends.js
@@ -6,42 +6,41 @@ class Friends extends React.Component {
 
     render(){
         return(
-
-            
-            <div>
-                <img src="./public/asset/lambda-2.png" alt="Lambda Logo" />
-                <h1>Lambda Student List</h1><br/>
-                <Form 
-                    friends={this.props.friends}
-                    submitToList={this.props.submitToList}
-                    addNameToList={this.props.addNameToList}
-                    addAgeToList={this.props.addAgeToList}
-                    addEmailToList={this.props.addEmailToList}
-                    /><br/>
-            {this.props.friends.map(friend => {
-                return(
-                    <table className="table">
-                    <thead>
-                        <tr>
-                        <th scope="col">id</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Age</th>
-                        <th scope="col">Email</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <th scope="row">{friend.id}</th>
-                        <td>{friend.name}</td>
-                        <td>{friend.age}</td>
-                        <td>{friend.email}</td>
-                        </tr>
-                    </tbody>
-                    </table>
-               
-                )
-            })}
-            </div>
+    <div>
+        <img src="./public/asset/lambda-2.png" alt="Lambda Logo" />
+        <h1>Lambda Student List</h1><br/>
+        <Form 
+            friends={this.props.friends}
+            submitToList={this.props.submitToList}
+            addNameToList={this.props.addNameToList}
+            addAgeToList={this.props.addAgeToList}
+            addEmailToList={this.props.addEmailToList}
+            /><br/>
+    {this.props.friends.map(friend => {
+        return(
+            <div key={friend.toString()}>
+            <table className="table">
+                <thead>
+                    <tr>
+                    <th scope="col">First</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Email</th>
+                    <button type="submit" className="btn btn-info">Update</button>
+                    <button type="submit" className="btn btn-danger">Remove</button>  
+                    </tr>
+                </thead>
+            <tbody>
+                    <tr>
+                    <td>{friend.name}</td>
+                    <td>{friend.age}</td>
+                    <td>{friend.email}</td>
+                    </tr>
+            </tbody>
+            </table>  
+            </div>    
+        )
+    })}
+    </div>
         )
     }
 }
