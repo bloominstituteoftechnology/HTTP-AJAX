919cbc7b437cd02639757a58edfdf8be7206bd47
ShardeDeLane <rhcprocker@yahoo.com>
2018-10-27 00:15:15 -0400

    catch added to response

--- friends/src/App.js
+++ friends/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
         this.setState({
           friends: response.data
         })
-      })
+      }).catch(error => { console.log('error')})
       
     })
   }
