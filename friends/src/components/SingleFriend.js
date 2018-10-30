5f14a52a4802ccbc662e71cdab328920ba4889ba
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-10-29 23:41:20 -0400

    Saving changes so far.

--- friends/src/components/SingleFriend.js
+++ friends/src/components/SingleFriend.js
@@ -13,26 +13,63 @@ class SingleFriend extends React.Component {
         }
     }
 
+    componentDidMount(){
+        axios
+            .get('http://localhost:5000/friends')
+            .then(data => {
+                this.setState({
+                    friends: data
+                })
+            })
+    }
+
+    changeHandler = (event) => {
+        this.setState({[event.target.name]: event.target.value })
+    }
+
+    addFriend = () => {
+        const friendObj = {name: this.state.name, age: this.state.age, email: this.state.email}
+        axios
+        .post('http://localhost:5000/friends', friendObj)
+        .then(friendObj => {
+            console.log(friendObj)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+    }
     render(){
     return(
         <div>
-        <form method="post">
+        <form  method="post">
         <ul>
             <li>
             <h4>Name:</h4>
-                <input />
+                <input 
+                type='text'
+                onChange={this.changeHandler} 
+                name='name' 
+                value={this.state.name} />
             </li>
             <li>
             <h4>Age:</h4>
-                <input />
+                <input 
+                onChange={this.changeHandler}
+                type='text' 
+                name='age' 
+                value={this.state.age} />
             </li>
             <li>
             <h4>Email:</h4>
-                <input />
+                <input onChange={this.changeHandler} 
+                type='text' 
+                name='email' 
+                value={this.state.email} />
             </li>
             <li>
             
-                <button>
+                <button onClick={this.addFriend} >
                     Add Friend
                 </button>
             </li>
