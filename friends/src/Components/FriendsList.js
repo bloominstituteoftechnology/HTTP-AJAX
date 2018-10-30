a68d4bef211f605486b0f2ba12f1728f8d5e018c
FBT328 <Katherine.Rust.Andrews@gmail.com>
2018-10-29 21:26:40 -0700

    FriendsList.js and SoloFriend.js added, no meaningful data yet.

---
+++ friends/src/Components/FriendsList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+const FriendsList = props => {
+    return (
+        <div>
+           'test data FriendsList goes here' 
+        </div>
+    )
+}
+
+// friends is an array of friend info on server.js .
+
+export default FriendsList
---
+++ friends/src/Components/SoloFriend.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+const SoloFriend = props => {
+    return (
+        <div>
+           'test data SoloFriend goes here' 
+        </div>
+    )
+}
+
+export default SoloFriend
