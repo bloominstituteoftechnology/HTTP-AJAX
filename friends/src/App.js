8848e0c28bc519a3d6929af220c7b11df3e07d21
Jesse <theiseja@gmail.com>
2018-10-29 23:07:52 -0500

    Built the form, I seem to be getting an axios error though. I'll work at it tomorrow then reach out if needed

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Friend from './Friend.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+       data: []
+  }
+}
+
+componentDidMount = () => {
+  axios.get('http://localhost:5000/friends')
+    .then( response => {
+      console.log('It works');
+      console.log(response.data)
+      this.setState({ data: response.data})
+    })
+    .catch( err=> console.log(err))
+  }
+
+
+test = (e) => {
+  e.preventDefault();
+  console.log(this.state.data)
+}
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          {this.state.data.map(item => (
+          <Friend key={item.id} friend={item} />
+        ))}
+
+        <form>
+            <h1>Add a new friend!</h1>
+            <input type='text' placeholder='name'></input>
+            <input type='number' placeholder='age'></input>
+            <input type='email' placeholder='email'></input>
+            <button onClick={this.test}>Submit</button>
+        </form>
+        </div>
     );
   }
 }
