65d9ccfb65e48ade25f200ccb20625dcde64b5f2
paulapivat <paul.apivat@gmail.com>
2018-10-30 15:05:34 +0700

    complete styling Friendlist

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 import './FriendList.css';
 
-
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: white;
+`
 
 const FriendList = props => {
 
@@ -12,14 +16,18 @@ const FriendList = props => {
             <div className="friendlist-container">
                 {props.friends.map(friend => (
                     <div className="friend-card" key={friend.id}>
-                        <h2>{friend.name}</h2>
-                        <p>ID: {friend.id}</p>
-                        <p>AGE: {friend.age}</p>
-                        <p>EMAIL: </p>
-                        <p>{friend.email}</p>
+                        <div className="friend-card-header">
+                            <h2>{friend.name}</h2>
+                        </div>
+                        <div >
+                            <p>ID: {friend.id}</p>
+                            <p>AGE: {friend.age}</p>
+                            <p>EMAIL: </p>
+                            <p>{friend.email}</p>
+                        </div>
                         <div className="friendlist-buttons">
                             <div className="friendlist-delete"><h3 style={{cursor: "pointer"}}>DELETE</h3></div>
-                            <div className="friendlist-detail"><Link to={`/friendslist/${friend.id}`}><h3>DETAILS</h3></Link></div>
+                            <div className="friendlist-detail"><StyledLink to={`/friendslist/${friend.id}`}><h3>DETAILS</h3></StyledLink></div>
                         </div>
                     </div>
                 ))}
