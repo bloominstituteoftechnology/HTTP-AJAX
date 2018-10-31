747a07842dc0ac5d44dbb22b472bc0413daae5ad
Alex Ostrander <alexanderostrander23@gmail.com>
2018-10-30 23:28:48 -0400

    created friends component to display friends. Used axios to get the friends data

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Route } from 'react-router-dom';
+import Friends from './Friends';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+ class App extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      friends: [],
+    }
+  }
+  
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then( response => {
+        this.setState({friends: response.data})
+        console.log(this.state.friends)
+      })
+  }
+	 
+   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.friends.map( friends => {
+          return (
+          <Friends id={friends.id} name={friends.name} age={friends.age} email={friends.email} /> 
+          )
+        })
+    
+        }
       </div>
     );
   }
 }
-
-export default App;
+ export default App;
---
+++ friends/src/Friends.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+ class Friends extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+			
+         }
+    }
+     render() {
+        return (
+            <div>
+                <span>{this.props.name}</span> <span>{this.props.age}</span> <span>{this.props.email}</span>
+             </div>
+        )
+    }
+};
+ export default Friends;
