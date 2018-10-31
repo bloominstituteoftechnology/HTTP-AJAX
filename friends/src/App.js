ceb5eb5737942f9750cdc69666f80581df6a8c4b
unknown <jerrardsmith215@gmail.com>
2018-10-30 22:32:16 -0400

    Jerrard Smith component tree made

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Route 
       </div>
     );
   }
