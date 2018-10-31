1453022db6e018b49a904bf94bbf6ca0418ec63d
cocoitali <cmburatto@gmail.com>
2018-10-30 20:10:27 -0700

    MVP (fetched data from server)

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      friends: []
+    }
+  }
+  componentDidMount() {
+ 
+    axios.get('http://localhost:5000/friends')
+      .then(response => {
+        this.setState({friends: response.data});
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
   render() {
     return (
       <div className="App">
