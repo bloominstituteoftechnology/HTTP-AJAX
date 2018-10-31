2eeea6fec972f9ae44407756e34d29ad5ead4a54
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-10-31 10:05:09 -0400

    (INITIAL COMMIT) - imported Browser Router and created initial file structure for FriendsList component

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class FriendsList extends Component {
+constructor(props) {
+    super(props);
+    this.state = {
+        friends: []
+        };
+    }
+
+    componentDidMount() {
+    axios
+        .get('http://localhost:5000/friends')
+        .then(response => {
+        this.setState({ friends: response.data });
+        console.log(this.state.friends)})
+        .catch(error => {
+        console.error('Server Error', error);
+        });
+    }
+
+    render() { 
+        let friends = this.state.friends;
+        // Force this data into array format!
+        friends = [...friends]
+    return (
+        <div className="friends-list">
+        {this.state.friends.map(friend => (
+            <div>
+                <div>{friend.id}</div>
+                <div>{friend.name}</div>
+            </div>
+        ))}
+        Friend List Here
+        </div>
+        );
+    }
+}
