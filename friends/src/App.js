786ff1a9e468d192f04b71d2e48d8444716d06a5
cocoitali <cmburatto@gmail.com>
2018-10-31 19:55:14 -0700

    MVP (added save button, able to save friend, but doesn't work on enter)

--- friends/src/App.js
+++ friends/src/App.js
@@ -8,19 +8,45 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      friends: []
+      friends: [],
+      name: "",
+      age: "",
+      email: ""
     };
   }
   componentDidMount() {
     axios
       .get("http://localhost:5000/friends")
-      .then(response => {
-        this.setState({ friends: response.data });
+      .then(res => {
+        this.setState({ friends: res.data });
       })
       .catch(err => {
         console.log(err);
       });
   }
+
+  submitHandler = () => {
+    const newFriend = {
+      id: this.state.friends[this.state.friends.length - 1].id + 1,
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email
+    };
+
+    axios
+      .post("http://localhost:5000/friends", newFriend)
+      .then(res => {
+        this.setState({friends: res.data, name: "", age: "", email: "" });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  onChangeHandler = e => {
+    this.setState({ [e.target.name]: e.target.value }); //allows for reuse :)
+  };
+
   render() {
     return (
       <div>
@@ -30,10 +56,29 @@ class App extends Component {
           ))}
         </div>
         <form>
-          <input type="text" placeholder="name..." />
-          <input type="text" placeholder="age..." />
-          <input type="text" placeholder="email..." />
+          <input
+            type="text"
+            placeholder="name..."
+            value={this.state.name}
+            name="name"
+            onChange={this.onChangeHandler}
+          />
+          <input
+            type="text"
+            placeholder="age..."
+            value={this.state.age}
+            name="age"
+            onChange={this.onChangeHandler}
+          />
+          <input
+            type="text"
+            placeholder="email..."
+            value={this.state.email}
+            name="email"
+            onChange={this.onChangeHandler}
+          />
         </form>
+        <button onClick={this.submitHandler}>save</button>
       </div>
     );
   }
