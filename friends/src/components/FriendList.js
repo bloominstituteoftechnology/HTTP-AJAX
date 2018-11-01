747930426d9c5a343cc1270663dfcfd448a8db77
cocoitali <cmburatto@gmail.com>
2018-10-31 21:06:02 -0700

    FIX (name, age, email display to page, unstyled)

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -6,6 +6,8 @@ const FriendsList = props => {
   return (
     <div>
       <h1>{props.friends.name}<button>delete</button></h1>
+      <h2>{props.friends.age}</h2>
+      <h3>{props.friends.email}</h3>
 
     </div>
   );
