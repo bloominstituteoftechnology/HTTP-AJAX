83a33174e7e620c4c96047283a95a03be77c9b86
jbierly1 <james.bierly@gmail.com>
2018-11-01 22:01:09 -0400

    initial data request successful

---
+++ friends/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      friends:[]
+    }
+
+  }
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
+  }
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
