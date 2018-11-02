5423fd3d18d324b7e7a3ac6af8bc3c451308831d
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-11-02 14:35:11 -0500

    added comments and created the render route passing the friend prop

--- friends/src/App.js
+++ friends/src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import FriendList from './Friendlist';
 
 
 class App extends Component {
@@ -20,7 +21,9 @@ class App extends Component {
 
   componentDidMount() {
     axios
+    //Aking for info from the local server
     .get('http://localhost:5000/friends')
+    //then gets the response and using the callback function sets the friends object in the state to the response data
     .then(response => {
       console.log(response);
       this.setState({
@@ -31,16 +34,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      {/* nav bar structure */}
       <nav>
       <NavLink exact to="/">Home</NavLink>
       <NavLink exact to="/about">About</NavLink>
       <NavLink exact to="/contact">Contact</NavLink>
       <NavLink exact to="/friends">Friends</NavLink>
       </nav>
+      {/* routes */}
       <Route exact path='/' component={Home}/>
       <Route path='/about' component={About}/>
       <Route path='/contact' component={Contact}/>
-      {/* <Route exact path='/friends' render={} /> */}
+      {/* Render Routes : this routes pass props to the components */}
+      <Route exact path='/friends' render={() => <FriendList friends={this.state.friends} />} />
       </div> 
     );
   }
