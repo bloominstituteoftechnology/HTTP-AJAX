764ef83310e9622661d39c09343151f23995bca7
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-11-02 14:18:50 -0500

    created friend and friendlist components

---
+++ friends/src/Friendlist.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Friend from './Friend';
+
+export default props => {
+    return(
+        <div className="friend-list">
+            {props.friends.map(friend => {
+                return(
+                    <Friend 
+                    id={friend.id}
+                    name={friend.name}
+                    age={friend.age}
+                    email={friend.email}
+                    />
+                )
+            })}
+        </div>
+    )
+}
