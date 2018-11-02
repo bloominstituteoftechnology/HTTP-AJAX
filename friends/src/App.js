00ae860494a832fd20d9d2a867aac300d8537a3b
jbierly1 <james.bierly@gmail.com>
2018-11-01 22:59:32 -0400

    finished getting names to display

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const FriendsListStyled=styled.div`
+display:flex;
+flex-direction:column;
+width:40%;
+height:20rem;
+justify-content:space-between;
+margin:auto;
+`
+
 
 class App extends Component {
   constructor(){
@@ -13,12 +24,20 @@ class App extends Component {
   componentDidMount() {
     axios
       .get('http://localhost:5000/friends')
-      .then(response => console.log(response))
+      .then(response => (this.setState({friends: response.data})) )
+      .then(response =>console.log(this.state.friends))
       .catch(err => console.log(err));
   }
   render() {
     return (
       <div className="App">
+      <FriendsListStyled>
+      {this.state.friends.map(
+        (friend)=>{return <div>name:{friend.name} age: {friend.age} email: {friend.email}
+        </div>}
+      )}
+      </FriendsListStyled>
+      
 
       </div>
     );
