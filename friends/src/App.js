c59fc6d101ce4ccbbc1877c2abe089200debb775
cocoitali <cmburatto@gmail.com>
2018-11-02 19:53:07 -0700

    MVP (delete fully functional)

--- friends/src/App.js
+++ friends/src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
         ...this.state.friend,
         [e.target.name]: e.target.value
       }
-    }); //allows for reuse :)
+    });
   };
 
   submitHandler = e => {
@@ -48,44 +48,21 @@ class App extends Component {
   };
 
   deleteHandler = friendId => {
-    axios.delete(`http://localhost:5000/friends/${friendId}`)
-    .then(res => {
-      this.setState({ friends: res.data })});
-    }
-  // const newFriend = {
-  //   id: this.state.friend[this.state.friend.length - 1].id + 1,
-  //   name: this.state.name,
-  //   age: this.state.age,
-  //   email: this.state.email
-  // };
-
-  //   axios
-  //     .post("http://localhost:5000/friends/", this.state.friend)
-  //     .then(res => {
-  //       this.setState({ friends: res.data, name: "", age: "", email: "" });
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // };
-
-  // enterHandler = e => { //Trying to submit on Enter, will come back to this
-  //   if(e && e.KeyCode === 13) {
-  //     console.log()
-  //   }
-  // }
-
-  // axios.delete(`http://localhost:5000/friends/${id}`)
-  //  .then(res => {
-
-  //})  NOPE
+    axios.delete(`http://localhost:5000/friends/${friendId}`).then(res => {
+      this.setState({ friends: res.data });
+    });
+  };
 
   render() {
     return (
       <div>
         <div>
           {this.state.friends.map(friends => (
-            <FriendsList key={friends.id} friends={friends} deleteHandler={this.deleteHandler} />
+            <FriendsList
+              key={friends.id}
+              friends={friends}
+              deleteHandler={this.deleteHandler}
+            />
           ))}
         </div>
         <FriendForm
@@ -93,37 +70,6 @@ class App extends Component {
           changeHandler={this.changeHandler}
           submitHandler={this.submitHandler}
         />
-        {/* <form>
-          <input
-            className="input"
-            type="text"
-            placeholder="name..."
-            required
-            value={this.state.name}
-            name="name"
-            onChange={this.onChangeHandler}
-          />
-          <input
-            className="input"
-            type="text"
-            placeholder="age..."
-            required
-            value={this.state.age}
-            name="age"
-            onChange={this.onChangeHandler}
-          />
-          <input
-            className="input"
-            type="text"
-            placeholder="email..."
-            required
-            value={this.state.email}
-            name="email"
-            onChange={this.onChangeHandler}
-          />
-        </form>*/}
-
-        {/* <button onClick={this.submitHandler}>save</button>  */}
       </div>
     );
   }
--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,17 +1,26 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 
 const FriendsList = props => {
 
   return (
     <div>
-      <h1>{props.friends.name}</h1>
-      <h2>{props.friends.age}</h2>
-      <h3>{props.friends.email}</h3>
-      <button>delete</button>
+      <div>
+        <h1>{props.friends.name}</h1>
+        <h2>{props.friends.age}</h2>
+        <h3>{props.friends.email}</h3>
 
+      </div>
+        <button onClick={() => props.deleteHandler(props.friends.id)}>delete</button>
     </div>
+
   );
 };
+
+FriendsList.propTypes = {
+  friends: PropTypes.object,
+  deleteHandler: PropTypes.func,
+}
 
 export default FriendsList;
