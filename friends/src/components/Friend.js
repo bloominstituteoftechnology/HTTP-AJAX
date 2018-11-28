317fbdc834699de78c05d7c437efc5fc8eb4fe94
Thomas Claydon <tomclaydon102@gmail.com>
2018-11-28 17:23:18 -0500

    MVP Day One Complete - Up to Instruction 6

---
+++ friends/src/components/Friend.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './friends.css';
+
+const Friend = props => {
+  return (
+    <div className="friendCard">
+      <h2>{props.data.name}</h2>
+      <p>Age: {props.data.age}</p>
+      <p>Email: {props.data.email}</p>
+    </div>
+  );
+};
+
+export default Friend;
