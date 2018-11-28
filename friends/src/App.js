b568f4d97c542a3b91bd43675c8b65e1dc51d54c
steelworx <eileencuevas62@gmail.com>
2018-11-28 17:26:07 -0500

    added some styling to App.css

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
 import axios from 'axios';
 import FriendList from './components/FriendList';
+import './App.css';
 
 class App extends Component {
   constructor() {
