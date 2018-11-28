8a37ff0c6f84feaa2fd2fd60ddce22aba6807de0
Yasirah Krueng <ykrueng@MacBook-Pro.local>
2018-11-28 13:34:56 -0600

    initial commit

---
+++ friends/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = {}
+  render() { 
+    return (
+      <div>
+        Hello From App
+      </div>
+    );
+  }
+}
+
+export default App;
