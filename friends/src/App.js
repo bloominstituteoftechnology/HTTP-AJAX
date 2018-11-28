d935ab5cb92f128c9708d44183d658deee85397a
kmarker1101 <kmarker1101@icloud.com>
2018-11-28 17:23:33 -0600

    wip

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,126 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Link, Switch} from 'react-router-dom';
+import axios from 'axios';
+
+import NewFriendForm from './Components/NewFriendForm';
+import FriendCard from './Components/FriendCard';
+import FriendList from './Components/FriendList';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: [],
+      newFriend: {
+        name: '',
+        age: 0,
+        email: '',
+      },
+      currentId: '',
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        this.setState({
+          friends: response.data,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  inputChange = e => {
+    e.preventDefault();
+    this.setState({
+      newFriend: {
+        ...this.state.newFriend,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  numberInputChange = e => {
+    e.preventDefault();
+    this.setState({
+      newFriend: {
+        ...this.state.newFriend,
+        [e.target.name]: parseInt(e.target.value),
+      },
+    });
+  };
+
+  addNewFriend = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/friends', this.state.newFriend)
+      .then(response =>
+        this.setState({
+          friends: response.data,
+          newFriend: {name: '', age: 0, email: ''},
+        }),
+      )
+      .catch(error => console.log(error));
+  };
+
+  updateFriend = friend => {
+    axios
+      .put(`http://localhost:5000/friends/${friend.id}`, friend)
+      .then(response => this.setState({friends: response.data}))
+      .catch(error => console.log(error));
+  };
+
+  deleteFriend = friend => {
+    axios
+      .delete(`http://localhost:5000/friends/${friend.id}`)
+      .then(response => this.setState({friends: response.data}))
+      .catch(error => console.log(error));
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Route
+          exact
+          path="/friends"
+          render={props => (
+            <FriendList
+              {...props}
+              friends={this.state.friends}
+              updateFriend={this.updateFriend}
+              deleteFriend={this.deleteFriend}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/friends/:id"
+          render={props => (
+            <FriendCard
+              {...props}
+              friends={this.state.friends}
+              updateFriend={this.updateFriend}
+              deleteFriend={this.deleteFriend}
+            />
+          )}
+        />
+
+        <Route
+          exact
+          path="/friends"
+          render={props => (
+            <NewFriendForm
+              {...props}
+              inputChange={this.inputChange}
+              addNewFriend={this.addNewFriend}
+              numberInputChange={this.numberInputChange}
+              newFriend={this.state.newFriend}
+            />
+          )}
+        />
       </div>
     );
   }
