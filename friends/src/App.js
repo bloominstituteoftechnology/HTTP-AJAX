1d2a4a5790013d78100a86db8aa0c7e71af183a1
Wesley Meeks <wesmeeks88@gmail.com>
2018-11-28 13:29:31 -0700

    initial commit

---
+++ friends/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+// import axios from 'axios';
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Friends App</h1>
+        <h2>Bet no one told you life was gonna be this way</h2>
+        <h4>clap, clap, clap, clap</h4>
+      </div>
+    );
+  }
+}
+
+export default App;
