8ea2a9b22c1c78935531313ce94b5ffc25334b6d
ToMMaaY <thuynh629@gmail.com>
2018-11-28 12:07:56 -0800

    Imported axios to get the friends data and displayed them using the App component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 import { Router } from "react-router-dom";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => {
+        console.log(res);
+        this.setState({ data: res.data });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="container">
-        <h1>Hello</h1>
+        {this.state.data.map(friend => (
+          <div className="friend" key={friend.id}>
+            <h3>{friend.name}</h3>
+            <p>{friend.age}</p>
+            <p>{friend.email}</p>
+          </div>
+        ))}
       </div>
     );
   }
