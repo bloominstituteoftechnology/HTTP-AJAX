551a5fb443a7b1d55e85d22425f1fae9af867586
ToMMaaY <thuynh629@gmail.com>
2018-11-28 11:56:21 -0800

    Added some global CSS stylng

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,7 +4,11 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="container">
+        <h1>Hello</h1>
+      </div>
+    );
   }
 }
 
