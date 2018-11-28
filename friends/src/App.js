0b817de93ce89cc97277945643261d697812afee
ToMMaaY <thuynh629@gmail.com>
2018-11-28 11:55:12 -0800

    Created the React App called Friends

---
+++ friends/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Router } from "react-router-dom";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div className="App" />;
+  }
+}
+
+export default App;
