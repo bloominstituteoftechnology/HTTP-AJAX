ac8950d89c46797849e9334bfa34e6c6fb2b2e52
Yasirah Krueng <ykrueng@MacBook-Pro.local>
2018-11-28 13:45:05 -0600

    get data using axios

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
-  state = {}
+  state = {
+    friends: [],
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(res => {
+        this.setState({
+          friends: res.data,
+        });
+      })
+      .catch(err => {
+        console.log('ERR');
+      });
+  }
+
   render() { 
     return (
       <div>
