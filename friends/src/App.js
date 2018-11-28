09dd903d159f992561a8d0a1e9e9849a2686f4c7
Wesley Meeks <wesmeeks88@gmail.com>
2018-11-28 13:53:19 -0700

    added axios get() and created component to display list of friends.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from "react";
-// import axios from 'axios';
+import axios from 'axios';
+import Friendlist from './components/FriendList';
 
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/friends")
+      .then(response => {
+        console.log(response);
+        this.setState({
+          data: response.data
+        })
+      })
+  }
   render() {
     return (
       <div className="App">
-        <h1>Friends App</h1>
-        <h2>Bet no one told you life was gonna be this way</h2>
-        <h4>clap, clap, clap, clap</h4>
+        <header className='friends-header'>
+          <h1>Friends App</h1>
+          <h2>Bet no one told you life was gonna be this way...</h2>
+          <h4>clap! clap! clap! clap!</h4>
+        </header>
+        <Friendlist data={this.state.data} />
       </div>
     );
   }
---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+const FriendList = props => {
+    return (
+        <div>
+            {props.data.map(friend => {
+                return (
+                    <div className='friendslist-container'
+                        key={friend.id}>
+                        <h1>Name: {friend.name}</h1>
+                        <p>Age: {friend.age}</p>
+                        <p>Email: {friend.email}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+export default FriendList;
