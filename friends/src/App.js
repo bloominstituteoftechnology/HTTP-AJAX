19e3b32ed5c86a7705164b33d0f679fc82c2c3c7
chasefulks <chase.fulks@gmail.com>
2018-11-28 17:32:15 -0600

    finished with day 1

---
+++ friends/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+import FriendsList from './components/FriendList';
+ class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      friends: []
+    }
+  }
+   componentDidMount(){
+    axios
+    .get('http://localhost:5000/friends')
+    .then(response => {
+      console.log(response.data);
+      this.setState({ friends: response.data});
+    })
+    .catch(err =>{
+      console.log(err);
+    });
+  }
+   render() {
+    return (
+      <div className="App">
+        <FriendsList friends={this.state.friends}/>
+      </div>
+    );
+  }
+}
+ export default App;
