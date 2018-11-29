e8a8d9138057f1a3f3e8230c5410a5af10cc1af3
David Freitag <freitagdavid@outlook.com>
2018-11-28 19:15:19 -0500

    friends: various files

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,40 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
 import FriendsList from './Components/FriendsList';
-import axios from 'axios'
+import axios from 'axios';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    background: #e2e1e0;
+    height: 100vh;
+`;
 
 class App extends Component {
-
-  constructor() {
-    super()
-    this.state = {
-      friends: []
+    constructor() {
+        super();
+        this.state = {
+            friends: []
+        };
     }
-  }
 
-  componentDidMount() {
-    axios
-      .get('http://localhost:5000/friends')
-      .then(response => {
-        console.log(response)
-        this.setState({ friends: response.data })
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+    componentDidMount() {
+        axios
+            .get('http://localhost:5000/friends')
+            .then(response => {
+                console.log(response);
+                this.setState({ friends: response.data });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 
-  addNewFriend(e) {
+    addNewFriend(e) {}
 
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <FriendsList friends={this.state.friends} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Div className="App">
+                <FriendsList friends={this.state.friends} />
+            </Div>
+        );
+    }
 }
 
 export default App;
--- friends/src/Components/FriendsList.js
+++ friends/src/Components/FriendsList.js
@@ -1,14 +1,59 @@
 import React from 'react';
 import FriendsForm from './FriendsForm';
-import shortid from 'shortid'
+import shortid from 'shortid';
+import styled from 'styled-components';
+
+const Ul = styled.ul`
+    --mdc-elevation-baseline-color: black;
+    --mdc-elevation-umbra-opacity: 0.2;
+    --mdc-elevation-penumbra-opacity: 0.14;
+    --mdc-elevation-ambient-opacity: 0.12;
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    li {
+        list-style: none;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        ul {
+            background: #fff;
+            min-width: 745px;
+            width: 30%;
+            display: flex;
+            justify-content: space-between;
+            border: solid rgba(0, 0, 0, 0.1) 1px;
+            border-radius: 10px;
+            margin: 10px 0;
+            box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19),
+                0 6px 6px rgba(0, 0, 0, 0.23);
+            li {
+                display: flex;
+                width: calc(100% / 3);
+                justify-content: flex-start;
+                padding: 10px;
+                border-radius: 0px;
+            }
+        }
+    }
+`;
 
 const FriendsList = props => {
     return (
         <>
             <FriendsForm />
-            <ul>
-                {props.friends.map(friend => <li key={shortid.generate()}>{friend.name} | {friend.age} | {friend.email}</li>)}
-            </ul>
+            <Ul>
+                {props.friends.map(friend => (
+                    <li key={shortid.generate()}>
+                        <ul>
+                            <li>{friend.name}</li>
+                            <li>{friend.age}</li>
+                            <li>{friend.email}</li>
+                        </ul>
+                    </li>
+                ))}
+            </Ul>
         </>
     );
 };
