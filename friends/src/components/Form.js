29f91cc81264d4a8ac1591a6a5e90bb720d001b2
BrandCam <BrandCam>
2018-11-29 15:08:23 -0600

    added dynamic state to form component

--- friends/src/components/Form.js
+++ friends/src/components/Form.js
@@ -1,27 +1,58 @@
 import React from "react";
 
-function Form(props) {
-  return (
-    <div>
-      <h1>Add New Friend</h1>
-      <form>
-        Name:
-        <br />
-        <input type="text" />
-        <br />
-        Age:
-        <br />
-        <input type="text" />
-        <br />
-        Email:
-        <br />
-        <input type="text" />
-        <br />
-        <br />
-        <input type="submit" value="Add New Friend" />
-      </form>
-    </div>
-  );
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      age: 0,
+      email: ""
+    };
+  }
+
+  changeHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h1>Add New Friend</h1>
+        <form>
+          Name:
+          <br />
+          <input
+            onChange={this.changeHandler}
+            type="text"
+            name="name"
+            value={this.state.name}
+          />
+          <br />
+          Age:
+          <br />
+          <input
+            onChange={this.changeHandler}
+            type="text"
+            name="age"
+            value={this.state.age}
+          />
+          <br />
+          Email:
+          <br />
+          <input
+            onChange={this.changeHandler}
+            type="text"
+            name="email"
+            value={this.state.email}
+          />
+          <br />
+          <br />
+          <input type="submit" value="Add New Friend" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Form;
