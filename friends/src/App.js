4507edf232f8febd633e3c8d7b4101d37822c6ba
Civoremo <omerovic_n@hotmail.com>
2018-11-28 22:25:03 -0500

    clear btn will clear inputFields and post new friend is working

--- friends/src/App.js
+++ friends/src/App.js
@@ -5,6 +5,8 @@ import { Route, NavLink } from 'react-router-dom';
 
 import Friends from './components/friends';
 
+let url = 'http://localhost:5000/friends';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -18,7 +20,7 @@ class App extends Component {
 
   componentDidMount() {
     axios
-      .get('http://localhost:5000/friends')
+      .get(url)
       .then(res => {
         // console.log(res);
         this.setState({
@@ -26,7 +28,7 @@ class App extends Component {
         });
       })
       .catch(err => {
-        // console.log(err);
+        console.log(err);
       })
   }
 
@@ -34,15 +36,35 @@ class App extends Component {
     this.setState({
       [e.target.name]: e.target.value
     });
-    // console.log(e.target.name);
-    console.log(this.state.inputName);
   }
 
   clearFields = e => {
     e.preventDefault();
     this.setState({
       inputName: "",
+      inputAge: "",
+      inputEmail: "",
     });
+  }
+
+  addFriend = (name, age, email) => {
+    // e.preventDefault();
+    if(this.state.inputName && this.state.inputAge && this.state.inputEmail){
+      axios
+        .post(url, {name, age , email})
+        .then(res => {
+          console.log(res)
+          this.setState({
+            data: res.data,
+          });
+        })
+        .catch( err => {
+          console.log('something went wrong trying to post new data')
+        });
+    }
+    else {
+      alert("All input fields required to add data!");
+    }
   }
 
   render() {
@@ -50,14 +72,22 @@ class App extends Component {
     return (
       <div className="App">
         <div>
-          <h2>Hello there Friends</h2>
+          {/* <h2>Hello there Friends</h2> */}
           <nav>
             <NavLink to='/' ><div className='navLink'>Home</div></NavLink>
             <NavLink to='/friends' ><div className='navLink'>Friends List</div></NavLink>
           </nav>
           <Route 
             path={`/friends`} 
-            render={ props => <Friends {...props} info={this.state.data} handleChange={this.handleChange} clearFields={this.clearFields}/>} 
+            render={ 
+              props => 
+              <Friends {...props} 
+              info={this.state} 
+              handleChange={this.handleChange} 
+              clearFields={this.clearFields}
+              addFriend={this.addFriend}
+              />
+            } 
           />
         </div>
       </div>
--- friends/src/components/friends.js
+++ friends/src/components/friends.js
@@ -7,39 +7,39 @@ function friends(props){
     return (
     <div>
         <div className='addFriend-container'>
-        <form onSubmit={props.addFriend} id='inputForm'>
+        <form  id='inputForm'>
                 <div className='friendInfo-input-container'>
                     <input 
                         type="text" 
                         placeholder='Friend Name'
                         name='inputName'
-                        value={props.inputName}
+                        value={props.info.inputName}
                         onChange={props.handleChange}
                     />
                     <input 
                         type="number" 
                         placeholder='Friend Age'
                         name='inputAge'
-                        value={props.inputAge}
+                        value={props.info.inputAge}
                         onChange={props.handleChange}
                     />
                     <input 
                         type="text" 
                         placeholder='Friend Email'
                         name='inputEmail'
-                        value={props.inputEmail}
+                        value={props.info.inputEmail}
                         onChange={props.handleChange}
                     />
                 </div>
                 <div className='button-container'>
-                    <button type='submit'>Save Friend</button>
-                    <button onClick={() => props.clearFields()}>Clear</button>
+                    <button onClick={() => props.addFriend(props.info.inputName, props.info.inputAge, props.info.inputEmail)}>Save Friend</button>
+                    <button onClick={props.clearFields}>Clear</button>
                 </div>                
             </form>
         </div>
         <div>
-            <h2>Current Friend List</h2>
-            {props.info.map(friend => (
+            <h2>Current Friend List of {props.info.data.length}</h2>
+            {props.info.data.map(friend => (
                 <div key={friend.id} className='friend-container'>
                     <div className='friend-label-container'>
                         <div className='infoFriendment'>Name:</div>
@@ -50,6 +50,10 @@ function friends(props){
                         <div className='infoFriendment'><span>{friend.name}</span></div>
                         <div className='infoFriendment'><span>{friend.age}</span></div> 
                         <div className='infoFriendment'><span>{friend.email}</span></div>
+                    </div>
+                    <div className='friend-option-container'>
+                        <button className='friend-option-btn'>delete</button> 
+                        <button className='friend-option-btn'>update</button> 
                     </div>
                 </div>
             ))}
