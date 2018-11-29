cafa99c7f6032cf5a6d7800114b9e92c1680dea3
steelworx <eileencuevas62@gmail.com>
2018-11-29 18:34:13 -0500

    Removed some commented code to make things neater

--- friends/src/components/EditForm.js
+++ friends/src/components/EditForm.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// const EditForm = props => {
-
 class EditForm extends React.Component {
     constructor(props) {
         super(props);
--- friends/src/components/Form.js
+++ friends/src/components/Form.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// const Form = props => {
-
 class Form extends React.Component {
     constructor(props) {
         super(props);
