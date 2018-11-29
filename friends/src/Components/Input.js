6aa4bc9292b4847bdc2f9979e82527f9824a50a2
David Freitag <freitagdavid@outlook.com>
2018-11-29 17:59:42 -0500

    friends/src/Components/Input: styling

--- friends/src/Components/Input.js
+++ friends/src/Components/Input.js
@@ -3,14 +3,17 @@ import styled from 'styled-components';
 
 const Div = styled.div`
     position: relative;
-    margin-bottom: 45px;
+    width: 100%;
+    margin-bottom: 15px;
     input {
-        font-size: 18px;
-        padding: 10px 10px 10px 5px;
+        font-size: 1rem;
+        padding: 10px 0px 10px 0px;
         display: block;
-        width: 300px;
+        width: 100%;
         border: none;
         border-bottom: 1px solid #757575;
+        background-color: rgba(0, 0, 0, 0);
+        margin: 0;
         &:focus {
             outline: none;
         }
@@ -20,9 +23,42 @@ const Div = styled.div`
             font-size: 14px;
             color: #5264ae;
         }
-        &:focus ~ Bar:before,
-        input:focus ~ Bar:after {
+        &:focus ~ .bar:before,
+        &:focus ~ .bar:after {
             width: 50%;
+        }
+        &:focus ~ .highlight {
+            animation: inputHighlighter 0.3s ease;
+        }
+    }
+    .highlight {
+        position: absolute;
+        height: 60%;
+        width: 50%;
+        top: 25%;
+        left: 0;
+        pointer-events: none;
+        opacity: 0.5;
+    }
+    .bar {
+        position: relative;
+        display: block;
+        width: 100%;
+        &:before,
+        &:after {
+            content: '';
+            height: 2px;
+            width: 0;
+            bottom: 1px;
+            position: absolute;
+            background: #5264ae;
+            transition: 0.2s ease all;
+        }
+        &:before {
+            left: 50%;
+        }
+        &:after {
+            right: 50%;
         }
     }
     label {
@@ -35,36 +71,23 @@ const Div = styled.div`
         top: 10px;
         transition: 0.2s ease all;
     }
-`;
-const Highlight = styled.span``;
-const Bar = styled.span`
-    position: relative;
-    display: block;
-    width: 300px;
-    &:before,
-    &:after {
-        content: '';
-        height: 2px;
-        width: 0;
-        bottom: 1px;
-        position: absolute;
-        background: #5264ae;
-        transition: 0.2s ease all;
-    }
-    &:before {
-        left: 50%;
-    }
-    &:after {
-        right: 50%;
+    @keyframes inputHighlighter {
+        from {
+            background: #5264ae;
+        }
+        to {
+            width: 0;
+            background: transparent;
+        }
     }
 `;
 
 const Input = props => {
     return (
         <Div>
-            <input type={props.type} />
-            <Highlight />
-            <Bar />
+            <input type={props.type} name={props.name} value={props.value} onChange={props.handleChange} />
+            <span className="highlight" />
+            <span className="bar" />
             <label>{props.placeholder}</label>
         </Div>
     );
