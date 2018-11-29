19ffac7cc0c5fc4334ae92ae68b73637cdfe14cc
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2018-11-29 17:23:38 -0600

    styled the css and made it look better

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -11,15 +11,23 @@ export default class Friend extends Component {
   render() {
     return (
       <div className="friend" >
-      <div className='info' onClick={() => {
+      <div className='card' onClick={() => {
         
         this.props.startUpdate(this.props.friend)
         this.props.history.push('/friendform')
         
         }}>
-        <p>Name:{this.props.friend.name}</p>
-        <p>Age:{this.props.friend.age}</p>
-        <p>Email:{this.props.friend.email}</p>
+        <h1>{this.props.friend.name}</h1>
+        <div className='info'>
+          <div>
+            <h4>Age</h4>
+            <p>{this.props.friend.age}</p>
+          </div>
+          <div>
+            <h4>Email</h4>
+            <p>{this.props.friend.email}</p>
+          </div>
+        </div>
       </div>
       <button onClick={() => this.props.deleteItem(this.props.friend.id)}>X</button>
       </div>
