eebef88b79e45e4bde15d1e1c6c98f49d7b0f3f3
Alex <alexander.t.carter@gmail.com>
2018-12-04 11:00:25 -0800

    friend post added

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -5,7 +5,10 @@ export default class FriendList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      friends: []
+      friends: [],
+      name: '',
+      email: '',
+      age: ''
     };
   }
 
@@ -20,21 +23,47 @@ export default class FriendList extends Component {
       });
   }
 
-  /*
-  fetchMovie = id => {
-    axios
-      .get(`http://localhost:5000/api/movies/${id}`)
+  addFriend = () => {
+
+    if (this.state.name === '' || this.state.email === '' || this.state.age === '') {
+      this.clearInputText();
+      return
+    }else{
+      let friend = {
+        id: this.state.friends.length + 1,
+        name: this.state.name,
+        age: this.state.age,
+        email: this.state.email,
+      }
+      //make a POST Request
+      axios
+      .post("http://localhost:5000/friends", friend )
       .then(response => {
-        this.setState(() => ({ movie: response.data }));
+        console.log(response);
+        this.clearInputText();
+        this.setState({
+          friends: [... this.state.friends, friend]
+        });
       })
-      .catch(error => {
-        console.error(error);
-      });
-  };
-  */
+      .catch(err => console.log(err));
+    }
+  }
+
+  updateInputText = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  clearInputText = () => {
+    this.setState({
+      name: '',
+      email: '',
+      age: ''
+    });
+  }
 
   render() {
-    console.log(this.state.friends);
     if (this.state.friends === null) {
       return <div>Loading friends...</div>;
     }
@@ -42,9 +71,17 @@ export default class FriendList extends Component {
     //const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="friends-wrapper">
+        <h1>These are my friends</h1>
          {this.state.friends.map(friend => (
           <div className="friend" key={friend.id} friend={friend.name} >{friend.name}</div>
         ))}
+
+        <h1>Add a Friend</h1>
+        <input type="text" name="name" className="formInputText" placeholder="name.." value={this.state.name} onChange={this.updateInputText} ></input>
+        <input type="text" name="email" className="formInputText" placeholder="email.." value={this.state.email} onChange={this.updateInputText} ></input>
+        <input type="text" name="age" className="formInputText" placeholder="age.." value={this.state.age} onChange={this.updateInputText} ></input>
+        <button onClick={this.addFriend} type="button">Add</button>
+        <button onClick={this.clearInputText} type="button">Clear</button>
       </div>
     );
   }
