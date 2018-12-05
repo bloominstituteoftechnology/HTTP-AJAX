0a4be5b9c1a82fc02fa6c58bd2d0e071b37ebd64
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-12-05 17:14:29 -0500

    working on app.js

---
+++ friends/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import reactDOm from 'react-dom';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      friends : []
+    }
+  }
+
+
+
+componentDidMount () {
+  
+}
+
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
