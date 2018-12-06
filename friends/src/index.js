96ac8641f930280cc74e9d278d6f97014159a2d1
J A <jarata@gmail.com>
2018-12-06 14:08:36 -0800

    error

--- friends/src/index.js
+++ friends/src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Router>
+import { BrowserRouter as Router } from 'react-router-dom';
+
+ReactDOM.render(
+  <Router>
   <App/>
-</Router>, document.getElementById('root'));
+  </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
