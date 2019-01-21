f39d91502539338c0cfb99a090a20aed4e1e8751
120356aa <yurlovandrew@gmail.com>
2019-01-20 17:55:26 -0800

    cleanup and added axiox and state

---
+++ friends/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+class App extends Component {
+  state = {
+    friends: []
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
