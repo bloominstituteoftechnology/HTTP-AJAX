295ecf04c7a798a6a0f8ed2d4fae5ddcb51e077b
Noah <noah.stoeckel@gmail.com>
2019-01-21 16:55:51 -0500

    Initialize App
    -Create new react app called friends
    -Add router to dependencies
    -had axios to dependencies
    -Wrap app In Browser router

---
+++ friends/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state= {
+      friends:[]
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+}
+
+export default App;
