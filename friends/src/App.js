ae545a3a22d70784854b4b7eca07c4d3b8e27ee3
120356aa <yurlovandrew@gmail.com>
2019-01-20 18:05:43 -0800

    created friends component and set state with data

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import Friends from './components/Friends';
 import './App.css';
 
 class App extends Component {
@@ -8,9 +9,17 @@ class App extends Component {
     friends: []
   }
 
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(res => this.setState({ friends: res.data }))
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
+        <Friends friends={this.state} />
       </div>
     );
   }
