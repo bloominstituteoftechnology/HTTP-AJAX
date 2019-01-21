5a25ec4bdccb99aefcdd8645566d42edfd0a3654
ltann <leeroyleeroy18@gmail.com>
2019-01-20 22:37:15 -0800

    first attempt at creating a friends form

---
+++ friends/src/components/FriendsForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function FriendsForm(props) {
+    return(
+        <div>
+            <form action="">
+                <input type="text" placeholder="Name" />
+                <input type="text" placeholder="Age" />
+                <input type="text" placeholder="Email" />
+            </form>
+        </div>
+      
+    )
+}
+
+export default FriendsForm
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { Route, NavLink } from 'react-router-dom'
 import axios from 'axios';
+import FriendsForm from './FriendsForm';
 
 class FriendsList extends Component {
     constructor () {
         super();
         this.state ={
-            friends: []
+            friends: [],
+            newFriend: {
+                name: '',
+                age: '',
+                email: '',
+            }
         }
     }
 
@@ -23,10 +30,21 @@ class FriendsList extends Component {
     render() {
         return(
             <div>
-                <p>Hi from FriendsList</p>
-                {this.state.friends.map(friend => {
-                   return <li key={friend.id}>{friend.name} {friend.age} {friend.email}</li>
-                })}        
+                <div>
+                    <p>Hi from FriendsList</p>
+                    {this.state.friends.map(friend => {
+                    return <li key={friend.id}>{friend.name} {friend.age} {friend.email}</li>
+                    })}
+                </div> 
+                <div>
+                    <NavLink exact to="/friendsform" >Add Friend</NavLink>
+                </div>
+                <div>
+                    <Route 
+                        path="/friendsfrom" 
+                        render={props => <FriendsForm {...props} newFriend={this.state.newFriend} />} 
+                    />
+                </div>       
             </div>
         )
     }
