05c6e5ce7c0e1a45ef31ebfff3fd7bc4f8ebb1f0
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-01-21 10:05:26 -0700

    adds style to App

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import axios from 'axios';
+// import styled from 'styled-components';
+
+// const FriendCard = styled.div`
+//   color: green;
+//   background: dodgerblue;
+// `
 
 class FriendsList extends React.Component {
   constructor() {
@@ -19,11 +25,23 @@ class FriendsList extends React.Component {
       .catch(err => {
         console.log(err);
       });
-}
+  }
 
 render(){
   return(
-    <h1>These are my friends:</h1>
+    <div>
+      <h1 className="friends-title">These are my friends:</h1>
+      <div className="friends-list">
+        {this.state.friends.map(friend => (
+          <div className="friend-card">
+            <h3 className="friend-name">Name: {friend.name}</h3>
+            <div>Age: {friend.age}</div>
+            <div>Email: {friend.email}</div>
+          </div>
+          ))}
+      </div>
+        
+    </div>
   )
 }
 
