d19c6a98a30855b19b998f26446a6e4bdea3063b
120356aa <yurlovandrew@gmail.com>
2019-01-20 19:41:57 -0800

    added friendform form

--- friends/src/components/FriendsFrom.js
+++ friends/src/components/FriendsFrom.js
@@ -9,10 +9,39 @@ class FriendsForm extends React.Component {
     }
   }
 
+  addFriend = event => {
+
+  }
+
+  changeHandler = event => {
+    
+  }
+
   render() {
     return (
       <from>
-
+        <input
+          name="name"
+          onChange={this.changeHandler}
+          placeholder="Name"
+          type="text"
+          value={this.state.friend.name}
+        />
+        <input 
+          name="age"
+          onChange={this.changeHandler}
+          placeholder="Age"
+          type="text"
+          value={this.state.friend.age}  
+        />
+        <input 
+          name="email"
+          onChange={this.changeHandler}
+          placeholder="Email"
+          type="text"
+          value={this.state.friend.email}
+        />
+        <button onClick={this.addFriend} type="submit">Add Friend</button>
       </from>
     );
   }
