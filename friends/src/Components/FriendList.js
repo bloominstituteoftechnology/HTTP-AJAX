3c6e97811bd768381ad8db42f906260a9047a334
jrizza88 <jetorres88@gmail.com>
2019-01-21 23:10:10 -0500

    added some small styling to the FriendList component

--- friends/src/Components/FriendList.js
+++ friends/src/Components/FriendList.js
@@ -1,16 +1,30 @@
 import React from "react";
+import styled from 'styled-components';
 // import ReactDOM from "react-dom";
 // import {Route, Link} from 'react-router-dom';
 
+const FriendListDiv = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+background: lavender;
+width: 50opx;
+margin: 15px auto;
+padding: 5px;
+border-radius: 20px;
+`
+
+
 const FriendList = props => {
     const friends = props.friends;
-   const friendArray = friends.map( (friend) => 
-  <ul><b>{friend.name}</b> {friend.age} {friend.email}</ul>
+   const friendArray = friends.map( (friend, key) => 
+  <ul key={friend.id}>  <div className="friendContainer">{friend.name}</div> {friend.age} {friend.email} </ul>
   );
       return(
-            <div>
+            <FriendListDiv>
                 {friendArray}
-            </div>
+            </FriendListDiv>
       );
 }
 
