a4877de40c363368389a475c4296a7fefdb7eb97
Noah <noah.stoeckel@gmail.com>
2019-01-21 22:19:05 -0500

    Moves Axios Calls Into FriendsList
    - moved axios calls and two friends list because every time
      the component mounts you want it to call the server
      in case of an update or deletion.

--- friends/src/App.js
+++ friends/src/App.js
@@ -12,21 +12,10 @@ class App extends Component {
   constructor(){
     super();
     this.state= {
-      friends:[] 
     };
   }
 
-  componentDidMount(){
-    axios.get('http://localhost:5000/friends')
-    .then((result)=>{
-      // console.log(result);
-      this.setState({friends: result.data})
-    })
-  }
-
-
   render() {
-
     return (
       <div className="App">
         <Route path="/" component={NavBar} />
--- friends/src/Components/FriendsList.js
+++ friends/src/Components/FriendsList.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 
 import Friend from './Friend';
 
-const FriendsList = (props) => {
+class FriendsList extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            friends:[]
+        }
+    }
     // console.log(props)
-    const { friendsList } = props;
+    // const { friendsList } = props;
     // console.log(friendsList)
-    return ( 
-        <div className="list-container">
-            {friendsList.map(friend=><Friend friend={friend} key={friend.id} />)}
-        </div> 
-    );
+
+    componentDidMount(){
+        axios.get('http://localhost:5000/friends')
+        .then((result)=>{
+          // console.log(result);
+          this.setState({friends: result.data})
+        })
+      }
+
+    render() {
+        return ( 
+            <div className="list-container">
+                {this.state.friends.map(friend=><Friend friend={friend} key={friend.id} />)}
+            </div> 
+        );
+    }
+    
 }
  
 export default FriendsList;
