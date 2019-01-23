3f4c4c0cdbd6cd9541be47cdeda2b7a3434042e9
CatsAreMLG <joshuadgon@gmail.com>
2019-01-22 21:19:01 -0600

    Added friends component

---
+++ friends/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Friends from "./components/Friends";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Friends />
+      </div>
+    );
+  }
+}
+
+export default App;
