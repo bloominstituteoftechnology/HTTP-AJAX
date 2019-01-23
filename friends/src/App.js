ca979f4c9d75975fd1351e050f96defa46555dce
Kseniya Platonava <ksu2008@gmail.com>
2019-01-22 23:09:19 -0500

    Add FriendsList

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,40 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from "axios"
+import FriendsList from '../src/components/FriendsList'
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      friends: []
-    };
-  }
-  componentDidMount() {
-    axios 
-    .get('http://localhost:5000/friends')
-    .then(response =>{
-      console.log(response);
-     this.setState({friends: response.data})
-    })
-    .catch(err => {
-      console.log(err);
-    });
-
-  }
-  render() {
-    return (
+  render(){
+    return(
       <div className="App">
-      <h1>Friends</h1>
-      {this.state.friends.map(friend => (
-        <div key ={friend.id}>
-          <h2>{friend.name}</h2>
-          <h2>{friend.age}</h2>
-          <h2>{friend.mail}</h2>
-        </div>
-      ))}
+         <FriendsList />
       </div>
-    );
+    )
   }
 }
 
 export default App;
+
+
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,39 @@
+import React, { Component} from 'react';
+import axios from 'axios';
+
+class FriendsList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+  componentDidMount() {
+    axios 
+    .get('http://localhost:5000/friends')
+    .then(response =>{
+      console.log(response);
+     this.setState({friends: response.data})
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  }
+  render() {
+    return (
+      <div className="friends-list">
+      <h1>Friends</h1>
+      {this.state.friends.map(friend => (
+        <div key ={friend.id}>
+          <h2>Name: {friend.name}</h2>
+          <h2>Age: {friend.age}</h2>
+          <h2>Email: {friend.mail}</h2>
+        </div>
+      ))}
+    </div>
+    );
+  }
+}
+
+export default FriendsList;
