b8cccc17c2fa555b3d860afb9108db63b6c5d425
iradsanchez <ira.d.sanchez@gmail.com>
2019-01-22 21:18:30 -0700

    completed steps 1-6. Data not displaying and am not sure why

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
-
-import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+  componentdidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => this.setState({ friends: res.data }))
+      .catch(err => console.log(err));
+  }
+
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        {this.state.friends.map(friend => (
+          <p key={friend.id}>{friend.name}</p>
+        ))}
+      </div>
+    );
   }
 }
 
