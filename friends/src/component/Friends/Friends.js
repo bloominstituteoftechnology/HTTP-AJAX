1bf25d07824203ead537ec0fe8ab9d2aa9d69ce1
KieranVieira <kieranvieira@live.com>
2019-01-23 18:53:32 -0500

    Added styled components styling (Stretch goal)

--- friends/src/component/Friends/Friends.js
+++ friends/src/component/Friends/Friends.js
@@ -1,22 +1,61 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FriendItem = styled.div`
+    width: 150px;
+    text-align: center;
+    border: 1px solid lightgrey;
+    background-color: lightblue;
+    border-radius: 15px;
+    margin: 15px;
+    padding: 15px 10px;
+    i{
+        font-size: 10rem;
+        margin: 10px;
+        background-color: white;
+        border-radius: 500px;
+        border: 5px solid white
+    }
+    h1{
+        margin: unset;
+    }
+    p{
+        font-size:1.2rem;
+        font-weight: 500;
+        margin: 5px 0;
+    }
+    button{
+        background-color: white;
+        border: 1px solid lightgrey;
+        padding: 5px 10px;
+        border-radius: 5px;
+        margin: 0 5px
+    }
+`;
+
+const ItemContainer = styled.div`
+    width: 65%;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+`;
 
 const Friends = props => {
     return(
-        <>
+        <ItemContainer>
             {props.friendsList.map(friend => {
                 return (
-                    <div key={friend.id}>
+                    <FriendItem key={friend.id}>
+                        <i className="fas fa-user-circle" />
                         <h1>{friend.name}</h1>
-                        <ul>
-                            <li>{friend.email}</li>
-                            <li>{friend.age}</li>
-                        </ul>
+                        <p>{friend.email}</p>
+                        <p>{friend.age}</p>
                         <button onClick={e => props.deleteFriend(e, friend.id)}>x</button>
                         <button onClick={e => props.updateFriend(e, friend)}>Update</button>
-                    </div>
+                    </FriendItem>
                 )
             })}
-        </>
+        </ItemContainer>
     )
 }
 
--- friends/src/component/Friends/NewFriend.js
+++ friends/src/component/Friends/NewFriend.js
@@ -1,13 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledForm = styled.form`
+    width: 200px;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    h1{
+        text-align:center;
+    }
+    input{
+        margin-bottom: 15px;
+        padding: 8px;
+        &::placeholder{
+            text-align:center;
+        }
+    }
+    button{
+        background-color: lightgrey;
+        border: 1px solid lightgrey;
+        border-radius: 5px;
+        padding: 5px 10px;
+    }
+`;
 
 const NewFriend = props => {
     return(
-        <form onSubmit={props.updating ? props.handleUpdate : props.submitFriend} onChange={props.handleChange}>
+        <StyledForm onSubmit={props.updating ? props.handleUpdate : props.submitFriend} onChange={props.handleChange}>
+            <h1>New Friend:</h1>
             <input type="text" name="name" value={props.nameValue} placeholder="Friends Name" required/>
             <input type="text" name="email" value={props.emailValue} placeholder="Friends E-Mail" required/>
             <input type="number" name="age" value={props.ageValue} placeholder="Friends Age" required/>
             <button type="submit">{props.updating ? 'Update' : 'Submit'}</button>
-        </form>
+        </StyledForm>
     )
 }
 
