8a10957005872fac254a66ec08673888a9aec05c
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-01-22 21:22:27 -0700

    adds styles to form and inputs

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 
 const FriendForm = props => {
   return (
-    <form>
-      <input type="text" name="name" />
-      <input type="number" name="number" />
-      <input type="text" name="email" />
-    </form>
+    <div className="friend-form-container">
+      <h2 className="friend-form-title">Add a new friend</h2>
+      <form className="friend-form">
+        <input type="text" name="name" placeholder="Name"/>
+        <input type="number" name="age" placeholder="Age"/>
+        <input type="text" name="email" placeholder="Email"/>
+      </form>
+    </div>
   )
 }
 
