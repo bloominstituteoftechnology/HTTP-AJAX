6c27fa08aa06fe5dc00b20f7c776c4a4ed2bcd3e
Taylor Belk <belktaylor12@gmail.com>
2019-01-22 21:41:47 -0600

    App.js outputs the data

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      friends: {}
+      friends: []
     }
   }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+        .then(response => {
+          console.log(response.data)
+          this.setState({friends: response.data})
+        })
+        .catch( err => {
+          console.log(err)
+        })
+      }
+
   render() {
+    console.log(this.state.friends)
     return (
       <div className="App">
-        
+        {this.state.friends.map( d => {
+          return (
+            <div key={d.id}>
+              <ul>
+                <li>{d.name}</li>
+                <li>{d.age}</li>
+                <li>{d.email}</li>
+              </ul>
+            </div>
+          )
+        })}
       </div>
     );
   }
