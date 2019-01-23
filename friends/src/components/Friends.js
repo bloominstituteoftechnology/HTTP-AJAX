d38f681e3a3cc0415f69c761c57cb09c88bd8227
CatsAreMLG <joshuadgon@gmail.com>
2019-01-22 21:57:14 -0600

    displaying all friends

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,5 +1,31 @@
 import React from "react";
+import axios from "axios";
 
-export default function Friends() {
-  return <div>Friends</div>;
+export default class Friends extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => {
+        this.setState({
+          friends: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+  render() {
+    return (
+      <div>
+        {this.state.friends &&
+          this.state.friends.map(e => {
+            return <div>{JSON.stringify(e)}</div>;
+          })}
+      </div>
+    );
+  }
 }
