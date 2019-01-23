0fd96cee678f566261c789c28dd99d246d7fb1b0
KieranVieira <kieranvieira@live.com>
2019-01-23 17:05:28 -0500

    Added add friend function remove friend function using post and delete

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import Friends from './component/Friends/Friends'
 import NewFriend from './component/Friends/NewFriend'
 import './App.css';
+import { format } from 'url';
 
 class App extends React.Component {
   constructor(){
@@ -27,22 +28,44 @@ class App extends React.Component {
         {
           name: e.target.name.value,
           email: e.target.email.value,
-          age: e.target.age.value
+          age: e.target.age.value,
+          id: Date.now()
         }
       ]
     })
     axios.post('http://localhost:5000/friends', {
         name: e.target.name.value,
         email: e.target.email.value,
-        age: e.target.age.value
+        age: e.target.age.value,
+        id: Date.now()
     })
+    e.target.name.value = '';
+    e.target.email.value = '';
+    e.target.age.value = '';
+  }
+
+  updateFriend = (e,friend) => {
+    // form.name.value = friend.name;
+    // form.email.value = friend.email;
+    // form.age.value = friend.age;
+  }
+
+  deleteFriend = (e, id) => {
+    e.preventDefault();
+    axios
+      .delete(`http://localhost:5000/friends/${id}`)
+      .then(res => this.setState({ friends: res.data })
+      )
+      .catch(error => this.setState({ error }));
   }
 
   render() {
     return (
       <div className="App">
-      <NewFriend  submitFriend={this.submitFriend}/>
-      {this.state.friends ? <Friends friendsList={this.state.friends}/> : <h2>loading...</h2>}
+      <NewFriend 
+        submitFriend={this.submitFriend} 
+      />
+      {this.state.friends ? <Friends updateFriend={this.updateFriend} deleteFriend={this.deleteFriend} friendsList={this.state.friends}/> : <h2>loading...</h2>}
       </div>
     );
   }
--- friends/src/component/Friends/Friends.js
+++ friends/src/component/Friends/Friends.js
@@ -5,12 +5,14 @@ const Friends = props => {
         <>
             {props.friendsList.map(friend => {
                 return (
-                    <div>
+                    <div key={friend.id}>
                         <h1>{friend.name}</h1>
                         <ul>
                             <li>{friend.email}</li>
                             <li>{friend.age}</li>
                         </ul>
+                        <button onClick={e => props.deleteFriend(e, friend.id)}>x</button>
+                        <button onClick={e => props.updateFriend(e,friend)}>Update</button>
                     </div>
                 )
             })}
