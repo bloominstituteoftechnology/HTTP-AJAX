040850cea2997870eaff63331a9b17db6ea7701b
Trip1701 <bj2336@email.vccs.edu>
2019-01-23 12:03:41 -0500

    State friends updated via axios get request

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      friends: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+         .then(res => {
+           console.log(res);
+           this.setState({ friends: res.data })
+         })
+         .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
