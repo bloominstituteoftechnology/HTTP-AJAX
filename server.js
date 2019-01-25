2ff034384dcf6509d57c0da360f790e50becc093
dickwinters <coding1965@gmail.com>
2019-01-24 21:39:41 -0600

    prod

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
