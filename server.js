263e401296363d8d3e9b63e68af85d2df1950b4b
dickwinters <coding1965@gmail.com>
2019-01-24 21:26:36 -0600

    prod

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 let nextId = 7;
@@ -51,6 +52,7 @@ let friends = [
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
