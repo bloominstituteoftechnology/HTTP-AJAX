66799852f675105df8ddf61510f882ebf36c7200
Jonathan Dorety <kyullog@gmail.com>
2019-01-24 22:03:10 -0500

    Make FriendsList entries click through to individual Friend component

--- friends/src/Components/FriendsList.js
+++ friends/src/Components/FriendsList.js
@@ -1,19 +1,22 @@
 import React from "react";
 import LinkButton from "./LinkButton";
 import Friend from "./Friend";
+import { Link } from "react-router-dom";
 
 const FriendsList = props => {
   return (
     <div className="friends-list">
       {props.friends.map(friend => {
         return (
-          <Friend
-            key={friend.id}
-            id={friend.id}
-            friend={friend}
-            {...props}
-            deleteFriend={props.deleteFriend}
-          />
+          <Link to={`/${friend.id}`}>
+            <Friend
+              key={friend.id}
+              id={friend.id}
+              friend={friend}
+              {...props}
+              deleteFriend={props.deleteFriend}
+            />
+          </Link>
         );
       })}
       <LinkButton to={"/addfriend"}>Add Friend</LinkButton>
