196755aa33c72c37d6e3ec551bdefde1f6dcb2d7
echopecho <pbsmithtbn@gmail.com>
2019-01-25 01:11:19 -0600

    Add style to form
    I came with a decent design for the form. I may spend a little more time refining it to see what I can come up with.

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,5 +1,56 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+const FormContainer = styled.div `
+  width: 30%;
+  margin: 15px auto;
+  border: 1px solid grey;
+  border-radius: 6px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    background-color: #ede7e7;
+
+    h2 {
+      margin: 0 0 3px;
+      color: grey;
+    }
+
+    label {
+      display: block;
+      margin-bottom: -10px;
+      z-index: 2;
+      background-color: #c5d9fd;
+      overflow: hidden;
+      width: 16%;
+      float: left;
+      margin-left: 6%;
+    }
+
+    input {
+      margin: 0 10px 10px;
+      height: 26px;
+      text-align: center;
+      background-color: #c5d9fd;
+      border: 1px solid black;
+      border-radius: 3px;
+    }
+
+    button {
+      width: 30%;
+      height: 24px;
+      margin: 10px auto;
+      background: none;
+      border: 1px solid black;
+      border-radius: 3px;
+      box-shadow: 1px 1px 1px rgba(0,0,0,0.25);
+      background: #79afe4;
+    }
+  }
+`
+
 
 class FriendForm extends React.Component {
   constructor(props) {
@@ -49,26 +100,30 @@ class FriendForm extends React.Component {
 
   render() {
     return (
-      <div>
+      <FormContainer>
         <form>
+          {this.props.create ? <h2>Add a Friend</h2> : <h2>Update this Friend's Info</h2>}
+          <label for="name">Name</label>
           <input 
             type="text"
             name="name"
-            placeholder="Name"
+            // placeholder="Name"
             onChange={this.handleChange}
             value={this.state.friend.name}
           />
+          <label for="age">Age</label>
           <input 
-            type="number"
+            type="text"
             name="age"
-            placeholder="Age"
+            // placeholder="Age"
             onChange={this.handleChange}
             value={this.state.friend.age}
           />
+          <label for="email">Email</label>
           <input 
             type="text"
             name="email"
-            placeholder="Email"
+            // placeholder="Email"
             onChange={this.handleChange}
             value={this.state.friend.email}
           />
@@ -76,7 +131,7 @@ class FriendForm extends React.Component {
           {this.props.create ? <button onClick={this.postFriend}>Create</button> : <button onClick={this.putFriend}>Update</button>}
            
         </form>
-      </div>
+      </FormContainer>
     )
   }
 }
