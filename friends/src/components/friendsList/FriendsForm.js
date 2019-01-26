a76204c2cdfdb7a3305b9d2b65bc3a6b57336959
Andrew <andrewryanogle@gmail.com>
2019-01-25 21:25:29 -0800

    working on the friends form

--- friends/src/components/friendsList/FriendsForm.js
+++ friends/src/components/friendsList/FriendsForm.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 class FriendsForm extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state ={
-            newFriend:{
-                id:'',
+        this.state = {
+            newFriend: {
+                id: '',
                 name: '',
                 age: '',
                 email: ''
@@ -13,18 +13,56 @@ class FriendsForm extends Component {
         }
     }
 
-    handleChange = event =>{
+    handleChange = event => {
         this.setState({
-            newFriend:{
+            newFriend: {
                 ...this.state.newFriend,
                 [event.target.name]: event.target.value
             }
         })
     }
+    postFriend = event => {
+        event.preventDefualt();
+        this.props.postMessage(this.state.newFriend);
+        this.setState({
+            newFriend: {
+                id: '',
+                name: '',
+                age: '',
+                email: ''
+            }
+        })
+    }
 
-    render() { 
-        return ( <div></div> );
+    render() {
+        return (
+        <div className ='friendsForm'>
+            <h2>Add new Friend</h2>
+            <form onSubmit = {this.postFriend}>
+                <input 
+                    type ='text'
+                    name ='name'
+                    placeholder = 'name'
+                    onChange = {this.handleChange}
+                    value = {this.state.newFriend.name}
+                />
+                <input 
+                    type ='text'
+                    name ='age'
+                    placeholder = 'age'
+                    onChange = {this.handleChange}
+                    value = {this.state.newFriend.age}
+                />
+                <input 
+                    type ='text'
+                    name ='email'
+                    placeholder = 'email'
+                    onChange = {this.handleChange}
+                    value = {this.state.newFriend.email}
+                />
+            </form>
+        </div>);
     }
 }
- 
+
 export default FriendsForm;
