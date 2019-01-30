139d41d824dda9facbae0b213cb9222889292051
J A <jarata@gmail.com>
2019-01-30 13:23:22 -0800

    mounting friendlist to state

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    }
+  }
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+  }
+
   render() {
     return (
       <div className="App">
