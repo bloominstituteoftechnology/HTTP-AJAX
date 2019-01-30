184e7cbd6ebb05849604c1cdb1e025d98c283ce3
J A <jarata@gmail.com>
2019-01-30 13:38:48 -0800

    added Promise and filled out componentDidMount

--- friends/src/App.js
+++ friends/src/App.js
@@ -7,11 +7,25 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      friends: []
+      friends: [],
+        error: 'No friends!'
     }
   }
+  // mounting localhost:5000 to state
+  // made Promise that returns data if true otherwise returns error
   componentDidMount() {
-    axios.get('http://localhost:5000/friends')
+    axios
+        .get('http://localhost:5000/friends')
+        .then(response => {
+          console.log(response.data)
+          this.setState({
+            friends: response.data
+          })
+        })
+        .catch(error => {
+            this.setState({ error: error.response })
+          console.log(error)
+        })
   }
 
   render() {
