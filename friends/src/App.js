ecc36ba4919f8c907c3257e20e046a25a44f7add
JamesB <jblazer1@yahoo.com>
2019-01-30 15:53:28 -0800

    Just started project

---
+++ friends/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    friends: [],
+    error: ""
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>PH</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
