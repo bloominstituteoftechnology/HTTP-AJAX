1a9eeb877011d0b30f85a4d5fd76b6eb382d5053
Paul DeYonghe <p.deyonghe@gmail.com>
2019-02-05 22:09:39 -0600

    stuck with parse error extending Component

---
+++ friends/src/components/Friends.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+const Friends extends Component {
+    constructor(props) {
+        super(props) 
+        this.state = { friends: [], }
+    }
+
+    render() {
+        return <div>Content to go here!</div>
+    }
+}
+
+
+export default Friends;
