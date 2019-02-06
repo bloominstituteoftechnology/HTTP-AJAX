cb42250a342d69811d53672bcc909cbf8ab1c8e3
Paul DeYonghe <p.deyonghe@gmail.com>
2019-02-05 22:14:02 -0600

    fixed parse error

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-const Friends extends Component {
+class Friends extends Component {
     constructor(props) {
         super(props) 
         this.state = { friends: [], }
