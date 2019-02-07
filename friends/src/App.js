dabb83bf92f81c333984825a2526c477770a6bf4
unknown <tomutsey@yahoo.com>
2019-02-06 21:55:20 -0500

    Add FriendsList component
    render a string to confirm its connectivity to the
    App component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import FriendsList from './Components/friendsList'
 
 class App extends Component {
-  render() {return <div>App under development</div>
+  render() {return <div>App under development
+    <FriendsList/>
+  </div>
   }
 }
 
---
+++ friends/src/Components/friendsList.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class FriendsList extends Component {
+
+    render() {return (<div>friendsList under development</div>)
+    }
+  }
+  export default FriendsList;
