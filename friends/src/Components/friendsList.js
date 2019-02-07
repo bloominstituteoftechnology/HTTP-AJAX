9a9058672cc46c766fc81c7f4a850b863d7d4319
unknown <tomutsey@yahoo.com>
2019-02-06 22:22:17 -0500

    Render a list of the friends in file axios is called on
    assign key to div while mapping over each entree.

--- friends/src/Components/friendsList.js
+++ friends/src/Components/friendsList.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 class FriendsList extends Component {
-
-    render() {return (<div>friendsList under development</div>)
+    constructor(){
+        super ();
+    this.state = {
+        friends:[]
     }
-  }
+}
+
+        componentDidMount(){
+            axios.get('http://localhost:5000/friends')
+              .then(response => {
+                console.log(response);
+                this.setState({friends: response.data});
+              })
+              .catch(err => {
+                console.log(err);
+              });
+          };
+        
+        
+    render() {return (<div>{this.state.friends.map(friend => {
+        return <div key={friend.id}>{`${friend.name} ${friend.age} ${friend.email}`}</div>
+    })}</div>)
+    }
+}
   export default FriendsList;
