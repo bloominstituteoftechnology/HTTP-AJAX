9cc161b1dc2f126fd607c9bbdf04c3c056f00435
Derek Jones <derek@derekjones.io>
2019-02-08 16:28:30 -0500

    Upload changes to GitHub to pull back down later

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 import FriendList from './components/FriendList';
 
+// class App extends Component {
+//   render() {
+//     return (
+//       <FriendList />
+//     );
+//   }
+// }
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(res => {
+        console.log(res.data);
+        this.setState({ friends: res.data });
+        console.log(this.state.friends);
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
-      <FriendList />
-    );
+      <FriendList friends={this.state.friends} />
+    )
   }
 }
 
--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,32 +1,41 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
+// import axios from 'axios';
 
-class FriendList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      friends: []
-    }
-  }
+import Friend from './Friend';
 
-  componentDidMount() {
-    axios
-      .get('http://localhost:5000/friends')
-      .then(res => {
-        this.setState({ friends: res.data });
-        console.log(res)
-      })
-      .catch(err => console.log(err));
-  }
+// class FriendList extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       friends: []
+//     }
+//   }
 
-  render() {
-    return (
-      <div>
-        <h1>Friend List</h1>
-        {this.state.friends.map(friend => <p>{friend.name}</p>)};
-      </div>
-    )
-  }
+//   componentDidMount() {
+//     axios
+//       .get('http://localhost:5000/friends')
+//       .then(res => {
+//         this.setState({ friends: res.data });
+//         console.log(res.data);
+//       })
+//       .catch(err => console.log(err));
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Friend List</h1>
+//         {this.state.friends.map(friend => <Friend key={friend.id} name={friend.name} age={friend.age} />)};
+//       </div>
+//     )
+//   }
+// }
+
+const FriendList = props => {
+  return (
+    <h1>Friend List</h1>
+    {this.state.friends.map()} // unreachable code
+  )
 }
 
 export default FriendList;
