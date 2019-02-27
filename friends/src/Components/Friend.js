0c2bd808c464038d19310afa0ed5d8aa7900f031
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-02-27 18:33:09 -0500

    added styled components

--- friends/src/Components/Friend.js
+++ friends/src/Components/Friend.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Mastr = styled.div`
+display: flex;
+flex-direction: column;
+align-content: center;
+`
 
 const Friend = ({friend})=>{return(
-    <div>
+    <Mastr>
         <p>{friend.id}</p>
         <h1>{friend.name} ({friend.age})</h1>
         <p>
             <a href="/">{friend.email}</a>
         </p>
-    </div>
+    </Mastr>
 )};
 export default Friend;
