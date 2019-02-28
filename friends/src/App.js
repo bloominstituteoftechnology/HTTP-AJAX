75cbcdea0d25d9f6c688ea5ef188efe6d66581a4
cori-peck <cori.peck@ymail.com>
2019-02-28 17:12:02 -0600

    Add a Friend functionality works

--- friends/src/App.js
+++ friends/src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
         });
   }
 
+
   render() {
     return (
       <div className="App">
--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,16 +1,45 @@
 import React from 'react';
+import axios from 'axios'
 
-function friendForm() {
-    return (
-        <>
-            <form>
-                <input type="text" placeholder="Name" />
-                <input type="number" placeholder="Age" />
-                <input type="text" placeholder="Email" />
-                <button type="submit">Add My New Friend</button>
-            </form>
-        </>
-    )
+class FriendForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        let newfriend = {
+            name: this.state.name,
+            age: this.state.age,
+            email: this.state.email
+        }
+        axios
+            .post("http://localhost:5000/friends", newfriend)
+            .then(res => {
+                    this.setState({ data: res.data})
+                  })
+            .catch(err => console.log('You messed up!'))
+    }
+
+    render() {
+        return (
+            <>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Name" value={this.name} name='name' onChange={this.handleChange} />
+                    <input type="number" placeholder="Age" value={this.age} name='age'onChange={this.handleChange} />
+                    <input type="text" placeholder="Email" value={this.email} name='email' onChange={this.handleChange} />
+                    <button type="submit">Add My New Friend</button>
+                </form>
+            </>
+        )
+    }
 }
 
-export default friendForm;
+export default FriendForm;
