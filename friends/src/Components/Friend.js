a0dd1c342d5d00fd59119fc17f69cf92eeee2d96
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-02-28 16:37:39 -0500

    added more styles with styled components

--- friends/src/Components/Friend.js
+++ friends/src/Components/Friend.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
+//styles
+
 const Mastr = styled.div`
 display: flex;
 flex-direction: column;
 align-content: center;
+border: solid grey 5px;
+max-width: 400px;
+margin: 5px auto;
 `
 
+const P = styled.p `
+border: solid black 2px;
+text-align: center;
+margin: 2% 2%;
+width: 5%;
+border-radius: 100px;
+`
+const H = styled.h1 `
+margin 0 auto;
+`
+//styles end
+
 const Friend = ({friend})=>{return(
+
     <Mastr>
-        <p>{friend.id}</p>
-        <h1>{friend.name} ({friend.age})</h1>
+        <P>{friend.id}</P>
+        <H>{friend.name} ({friend.age})</H>
         <p>
             <a href="/">{friend.email}</a>
         </p>
     </Mastr>
+    
 )};
 export default Friend;
