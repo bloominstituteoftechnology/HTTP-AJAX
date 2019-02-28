6bdb627721848e82ee5cee7365182a2f90b1b2b9
cori-peck <cori.peck@ymail.com>
2019-02-28 09:43:54 -0600

    saving to save added styles - not complete yet

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Card, CardHeader, CardBody, CardSubtitle, CardText, CardLink } from 'reactstrap';
+import * as Icon from 'react-feather';
+import './FriendsList.css';
 
 const friendsList = (props) => {
     return (
-            <div className="friend-card">
-                <p className="friend-name">{props.friend.name}</p>
-                <h4 className="sub-cat">Age: </h4>
-                <p className="friend-details">{props.friend.age}</p>
-                <h4 className="sub-cat">Email: </h4>
-                <p className="friend-details">{props.friend.email}</p>
+            <div className="card-container">
+                <Card body outline color="secondary">
+                    <CardHeader tag="h3">
+                        <Icon.User className="user-icon"/>
+                        <p className="friend-name">{props.friend.name}</p>
+                    </CardHeader>
+                    <CardBody>
+                        <CardSubtitle>Age: </CardSubtitle>
+                        <CardText>{props.friend.age}</CardText>
+                        <CardSubtitle>Email: </CardSubtitle>
+                        <CardText>{props.friend.email}</CardText>
+                        <CardLink><Icon.Edit className="link-icon"/></CardLink>
+                        <CardLink><Icon.Trash className="link-icon"/></CardLink>
+                    </CardBody>
+                </Card>
             </div>
         )
 }
