db67d5c15af173a7dc6dd2ff36e7640afeb02b84
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-02-28 19:10:03 -0500

    changed bg

--- friends/src/Components/Form.js
+++ friends/src/Components/Form.js
@@ -21,30 +21,29 @@ margin: 5px 5%;
 `
 
 const Btn = styled.button `
-    position: relative;
-    display: inline-block;
-    box-sizing: border-box;
-    margin: 0 8px;
-    border: none;
-    border-radius: 2px;
-    padding: 0 16px;
-    min-width: 64px;
-    height: 36px;
-    vertical-align: middle;
-    text-align: center;
-    text-overflow: ellipsis;
-    text-transform: uppercase;
-    color: #fff;
-    background-color: #1c5d76;
-    font-family: 'Roboto', 'Segoe UI', BlinkMacSystemFont, system-ui,
-      -apple-system;
-    font-size: 14px;
-    font-weight: 500;
-    line-height: 36px;
-    overflow: hidden;
-    outline: none;
-    cursor: pointer;
-    transition: box-shadow 0.2s;
+position: relative;
+display: inline-block;
+box-sizing: border-box;
+margin: 0 8px;
+border: none;
+border-radius: 2px;
+padding: 0 16px;
+min-width: 64px;
+height: 36px;
+vertical-align: middle;
+text-align: center;
+text-overflow: ellipsis;
+text-transform: uppercase;
+color: #fff;
+background-color: #1c5d76;
+font-family: 'Roboto', 'Segoe UI', BlinkMacSystemFont, system-ui,-apple-system;
+font-size: 14px;
+font-weight: 500;
+line-height: 36px;
+overflow: hidden;
+outline: none;
+cursor: pointer;
+transition: box-shadow 0.2s;
 `
 
 //end styles
