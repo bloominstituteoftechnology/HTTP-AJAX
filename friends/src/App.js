add2938b4dbdf973806b2689e4bb533dac7b5c26
Marc Thiongo <marcthuo@gmail.com>
2019-03-14 00:50:00 -0400

    edits in app/comp

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import { Route } from 'react-router-dom';
+import axios from 'axios';
 import FriendList from './component/FriendList';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        friends: []
+    };
+  }  
+
+componentDidMount() {
+  axios
+    .get("https://localhost:5000/friends")
+    .then(response => this.setState({ friends: response.data }))
+    .catch(error => console.log(error));
+}
+
   render() {
     return (
-      <div className="App">
-      <Route path='/friends/:id' component={FriendList} 
-      render={props => <FriendList {...props} friends={this.state.friends} />} />
+      <div className='App'>
+          render={props => <FriendList {...props} friends={this.state.friends} />}
       </div>
     );
-  }
+  }   
 }
 
 export default App;
--- friends/src/component/friendlist.js
+++ friends/src/component/friendlist.js
@@ -1,29 +1,14 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-// import { Link } from 'react-router-dom';
+import React from 'react';
 
-class FriendList extends Component {
-    constructor() {
-        super();
-        this.state = {
-            friends: []
-        };
-    }
-
-componentDidMount() {
-    axios
-        .get("https://localhost:5000/friends")
-        .then(response => this.setState({ friends: response.data }))
-        .catch(error => console.log(error));
-}
-
-  render() {
-    return (
-        <div className='friend-list'>
-           {this.state.friends.map(friend => (friend))}
-        </div>
-    );
-  }
+const FriendList = (props) => {
+ render() {
+  return (
+    <div className="friend-list">
+      {this.state.friends.map(friend => (friend))}
+    </div>
+    render={props => <Friends {...props} friends={this.state.friends} />}
+    )
+ };
 }
 
 export default FriendList;
