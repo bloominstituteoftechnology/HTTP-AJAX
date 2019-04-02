3e47d56ffc90a864495348747154108f0cfc9ff6
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-04-01 23:00:53 -0700

    Added an Add New Friend form, more styling

--- friends/src/App.js
+++ friends/src/App.js
@@ -24,9 +24,28 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="header">Friends List</h1>
-        <div className="friends-list-wrapper">
-          {this.state.friends.map(friend => <FriendsList key={friend.id} name={friend.name} age={friend.age} email={friend.email} />)}
+        <div className="content">
+          <form className="add-friend">
+            <h4>Add a friend</h4>
+            <div className='input-field'>
+              <label for='name'>Name</label>
+              <input id='name' />
+            </div>
+            <div className='input-field'>
+              <label for='age'>Age</label>
+              <input id='age' />
+            </div>
+            <div className='input-field'>
+              <label for='email'>Email</label>
+              <input id='email' />
+            </div>
+            <button type='submit'>Add Friend</button>
+          </form>
+          <div className="friends-list-wrapper">
+            {this.state.friends.map(friend => <FriendsList key={friend.id} name={friend.name} age={friend.age} email={friend.email} />)}
+          </div>
         </div>
+        
       </div>
     );
   }
