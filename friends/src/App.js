8b812a2ed19d959c5c433364e06f0527092b5a45
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-04-01 20:35:46 -0700

    Initial commit. Created a React App

---
+++ friends/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hi
+      </div>
+    );
+  }
+}
+
+export default App;
