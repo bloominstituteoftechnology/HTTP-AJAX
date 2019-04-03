985b1d22c93b8fcc81a83a6fcd4c7b7f5b456d2a
shayaanakbar <shayaanakbar1106@gmail.com>
2019-04-02 21:22:16 -0500

    Added BrowserRouter

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import ReactDOM from "react-dom";
 
 class App extends Component {
 
