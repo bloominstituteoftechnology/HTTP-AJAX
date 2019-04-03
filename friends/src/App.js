46827b88c2156eb149fc41fc56ba57366b9395d8
shayaanakbar <shayaanakbar1106@gmail.com>
2019-04-02 21:17:25 -0500

    Forgot to add axios now imported axios to app.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+
+    constructor() {
+        super();
+        this.state = {
+            friends: []
+        };
+    }
+
+    componentDidMount() {
+        axios
+            .get('http://localhost:5000/friends')
+            .then(response => {
+                console.log(response);
+                this.setState({ friends: response.data })
+            })
+    }
+    render() {
+        return (
+            <div className="App">
+                <h1>Shayaan Works on HTTP-AJAX</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
