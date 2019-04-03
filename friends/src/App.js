ad3b0e86f82f517ac9739320a7d46bb6943df1a9
Josh Akeman <joshakeman@gmail.com>
2019-04-03 15:38:03 -0400

    added form

---
+++ friends/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <form>
+        <input placeholder="name..."></input>
+        <input placeholder="age..."></input>
+        <input placeholder="email..."></input>
+      </form>
+      
+      </div>
+    );
+  }
+}
+
+export default App;
