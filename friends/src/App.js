aff6eec9eaee23f6ab05596d54fdc0ef4c5299be
James Wigdor <jpwigdor@gmail.com>
2019-04-02 21:53:37 -0400

    add components to show Friends List

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { Route } from "react-router-dom";
+
+import Friend from "./Components/Friend";
+import FriendsList from "./Components/FriendsList";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/friends`)
+      .then(result => {
+        this.setState({ friends: result.data });
+      })
+      .catch(err => console.log(`unable to load Data`));
+  }
+
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Route path="/friends" exact render={FriendsList} />
+        <Route path="friends/:id" render={Friend} />
+      </div>
+    );
   }
 }
 
