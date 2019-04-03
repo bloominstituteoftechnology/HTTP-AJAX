5accefc6d889fcf0e9dd300e87eafabd72e4a4b5
shayaanakbar <shayaanakbar1106@gmail.com>
2019-04-02 21:30:44 -0500

    Hit MVP but having DOM errors

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 import ReactDOM from "react-dom";
+import { Route, Link } from "react-router-dom";
 
 class App extends Component {
 
@@ -18,7 +19,7 @@ class App extends Component {
         axios
             .get('http://localhost:5000/friends')
             .then(response => {
-                console.log(response);
+                console.log("response", response);
                 this.setState({ friends: response.data })
             })
             .catch( err => console.log("Whoopsie you got an error:", err));
@@ -27,6 +28,12 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>Shayaan Works on HTTP-AJAX</h1>
+                {this.state.friends.map(friend => (
+                    <ul>{friend.name}</ul>
+                ))
+                }
+                <Route exact path="/" />
+                <Route path ="/friends" />
             </div>
         );
     }
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,3 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Route, Link} from 'react-router-dom';
