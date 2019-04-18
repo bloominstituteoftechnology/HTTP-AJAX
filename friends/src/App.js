8aca3f7fe53ed9575b06e29fb374b6cc0ed8b494
Sean Doyle <sdoylelambda@gmail.com>
2019-04-18 17:32:55 -0400

    Get, then and catch methods.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 import FriendsList from './components/FriendsList';
 import FriendForm from './components/FriendForm';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friendsArray: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(res => {
+        console.log(res);
+        this.setState({ friendsArray: res.data})
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ error: err })
+      } )
+  }
+
+
   render() {
     return (
       <div className="App">
