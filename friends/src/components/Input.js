88d5514b8c35b65e435a9b836e64d0dd24ad6254
Cory Hedeen <hedeencory@gmail.com>
2019-05-29 17:41:38 -0400

    Add button

--- friends/src/components/Input.js
+++ friends/src/components/Input.js
@@ -7,22 +7,21 @@ const Input = props => {
   return(
     <div>
       <Link to="/">Home</Link>
-
-      <form>
-        <h2>Name</h2>
-        <input
-        type="text"
-        />
-        <h2>Age</h2>
-        <input
-        type="text"
-        />
-        <h2>Email</h2>
-        <input
-        type="text"
-        />
-
-      </form>
+        <form>
+          <h2>Name</h2>
+          <input
+          type="text"
+          />
+            <h2>Age</h2>
+            <input
+            type="text"
+            />
+          <h2>Email</h2>
+          <input
+          type="text"
+          />
+        </form>
+      <button>enter</button>
     </div>
   );
 }
