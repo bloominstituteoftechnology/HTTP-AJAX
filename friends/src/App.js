e22a3107c1822503e81261713801bfc0fa54b3e6
oloruntobi <awoderuoloruntobi@gmail.com>
2019-06-12 12:38:53 +0100

    Created a Friend Component and displayed the friends to the browser

---
+++ friends/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Friends from './component/Friend'
+
+function App() {
+  return (
+    <div className="App">
+      <Friends />
+    </div>
+  );
+}
+
+export default App;
---
+++ friends/src/component/Friend.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import axios from'axios';
+
+export default class Friends extends React.Component {
+    state = {
+        friend: null,
+        errorMessage: null
+    }
+FetchDataWithAxios = () => {
+    axios.get('http://localhost:5000/friends')
+    .then(response => {
+        this.setState({ friend: response.data });
+      })
+      .catch(error => {
+        this.setState({ errorMessage: error.message });
+      })
+}
+
+componentDidMount(){
+    this.FetchDataWithAxios();
+}
+render(){
+    return(
+       <div>
+           {
+          this.state.friend &&
+          <div>{this.state.friend.map(each => {
+              return  each.id + each.name + each.age;
+          })}</div>
+        }
+       </div>
+    );
+}
+}
