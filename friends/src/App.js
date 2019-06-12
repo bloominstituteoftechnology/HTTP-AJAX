a8e42401ed87dd9ba56b162b7e6ff63c93dafd24
basilcea <ogbonna.basil3@gmmail.com>
2019-06-12 13:12:18 +0100

    create onchangehander form method - pass props to form component - Add route to Friend Method - Pass Updatefriend Method to Friend component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import ClassList from './Component/friendsList'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
+      <ClassList />
       </header>
     </div>
   );
--- friends/src/Component/friendsList.js
+++ friends/src/Component/friendsList.js
@@ -1,28 +1,31 @@
 import React from 'react';
-import axois from  'axois';
+import axois from  'axios';
+import Form from './form';
+import Friend from './friend'
+import {NavLink, Route} from 'react-router-dom';
 
 export default class FriendList extends React.Component{
 state={
-    friend:null,
+    friends:null,
     errorMessage:'',
-    newFriend:{},
+    newName:'',
+    newAge:'',
+    newEmail:'',
 }
 getFriends = async () =>{
     try{
         const axoisData = await axois.get('http:localhost:5000/api/friends')
-        this.setState({ friend: axoisData.data });
+        this.setState({ friends: axoisData.data });
     }
     catch(err){
         this.setState({
             errorMessage:err.message
         })
-
     }
 }
 postFriend = async() => {
     try{
         await axois.post('http:localhost:5000/api/friends',this.state.newFriend)
-        return this.getFriends;
         }
     catch(err){
         this.setState({
@@ -33,19 +36,16 @@ postFriend = async() => {
 updateFriend = async(id) =>{
     try{
         await axois.patch(`http:localhost;5000/api/friends/${id}`, this.state.newFriend)
-        return this.getFriends
     }
     catch(err){
         this.setState({
             errorMessage:err.message
         })
-    }
     }
 }
 deleteFriend = async(id) => {
     try{
         await axois.delete(`http:localhost:5000/api/friends/${id}`)
-        return this.getFriends;
     }
     catch(err){
         this.setState({
@@ -53,5 +53,33 @@ deleteFriend = async(id) => {
         })
     }
 }
-
+onChangeHandler =(e, text) =>{
+(text === 'name') && this.setState({newName: e});
+(text === 'age') && this.setState({newAge: e});
+(text === 'email') && this.setState({newEmail: e});
+}
+componentDidMount(){
+    this.getFriends()
+}
+componentDidUpdate(){
+    this.getFriends()
+}
+render(){
+    return(
+        <div>
+        <h1>FriendList</h1>
+        {this.state.friends.map(friend => {
+            // eslint-disable-next-line no-unused-expressions
+            <div>
+            <NavLink key={friend.id} to={`/api/friends/${friend.id}`}>{friend.name}</NavLink>
+            <Route path='/api/friends/:id' 
+            render ={props =>
+                <Friend update={this.updateFriend} {...props}/>}
+            />
+            <Form inputValue ={this.onChangeHandler} submit={this.postFriend}/>
+            </div>
+        })}
+        </div>
+    )
+}
 }
