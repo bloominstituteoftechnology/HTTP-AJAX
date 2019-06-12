f9ac9d4109e079ef2ea8a1d62ff49840cb0742d3
Mariana Huh <mhuh0176@gmail.com>
2019-06-11 20:18:17 -0700

    mvp

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import FriendsList from './components/FriendsList';
+import FriendForm from './components/FriendForm';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 
 class App extends React.Component {
@@ -18,12 +20,46 @@ class App extends React.Component {
       .catch(err => console.log(err));
   }
 
+  addNewFriend = newFriend => {
+    axios
+      .post('http://localhost:5000/friends', newFriend)
+      .then(res => {
+        this.setState({
+          friends: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
+  deleteFriend = id => {
+    axios
+      .delete(`http://localhost:5000/friends/${id}`)
+      .then(res => {
+        this.setState({
+          friends: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
           <h1>React Friends</h1>
-          <FriendsList friends={this.state.friends} />
+          <Link className='friends' exact to='/new-friend'>
+            FRIENDS
+          </Link>
+          <Route
+            path='/new-friend'
+            render={props => (
+              <FriendForm {...props} addNewFriend={this.addNewFriend} />
+            )}
+          />
+          <FriendsList
+            friends={this.state.friends}
+            deleteFriend={this.deleteFriend}
+          />
         </header>
       </div>
     );
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FriendsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`;
+const FriendsCardWrapper = styled.div`
+  border: 1px solid white;
+  margin: 3px;
+  padding: 30px 20px;
+  font-size: 17px;
+`;
+const DeleteBtn = styled.button`
+  width: 50px;
+  margin: 5%;
+  background-color: #9f001e;
+  border-color: #9f001e;
+`;
 
 function FriendsList(props) {
   return (
     <div>
-      <div>
+      <FriendsWrapper>
         {props.friends.map(friend => {
           return (
-            <div key={friend.id}>
+            <FriendsCardWrapper key={friend.id}>
               <h2>{friend.name}</h2>
               <p>{friend.age}</p>
               <code>{friend.email}</code>
-            </div>
+              <br />{' '}
+              <DeleteBtn onClick={() => props.deleteFriend(friend.id)}>
+                X
+              </DeleteBtn>
+            </FriendsCardWrapper>
           );
         })}
-      </div>
+      </FriendsWrapper>
     </div>
   );
 }
