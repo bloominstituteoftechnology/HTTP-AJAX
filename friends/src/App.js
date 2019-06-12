58ad4b2477fab6d2fbe6f236e04d676fa5d2c0e9
Sascha <sascha.majewsky93@gmx.de>
2019-06-12 22:37:09 +0200

    deleted default files and set up the base project

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    console.log('I am a mounted teapot')
+  
+    axios
+    .get("http://localhost:5000/friends")
+    .then(res => {
+      console.log(res)
+      this.setState({ friends: res.date });
+    })
+    .catch(err => alert("Sorry something went wrong: ", err));
+  }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"/>
     </div>
-  );
+    )
+  };
 }
 
 export default App;
