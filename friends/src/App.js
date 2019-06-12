e18af4f83661b02933824beb942a7fb55dbfcbc0
rushi444 <rushi44@gmail.com>
2019-06-12 14:43:01 -0700

    first day work done

---
+++ friends/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FriendList from "./FriendList";
+
+function App() {
+  return (
+    <div className="App">
+      <FriendList />
+    </div>
+  );
+}
+
+export default App;
