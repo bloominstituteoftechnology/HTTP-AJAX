016c768d21222bc284f19ba9712d7cb8e57920e1
quansenB <inaki.breinbauer@gmail.com>
2019-06-13 16:44:20 +0200

    add post functionality

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import axios from "axios";
 import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.refOne = React.createRef();
     this.state = {
       friends: [],
       errorMessage: "",
@@ -15,7 +16,7 @@ class App extends React.Component {
     };
   }
 
-  componentDidMount() {
+  fetchFriends() {
     this.setState({ spinner: true });
     fetch("http://localhost:5000/friends")
       .then(response => {
@@ -32,40 +33,91 @@ class App extends React.Component {
       });
   }
 
-  changeNewFriendName = (event) => {
-    this.setState( {newFriendName: event.target.value} )
+  componentDidMount() {
+    this.fetchFriends();
   }
 
-  changeNewFriendAge = (event) => {
-    this.setState( {newFriendAge: event.target.value} )
-  }
+  changeNewFriendName = event => {
+    this.setState({ newFriendName: event.target.value });
+  };
 
-  changeNewFriendEmail = (event) => {
-    this.setState( {newFriendEmail: event.target.value} )
-  }
+  changeNewFriendAge = event => {
+    this.setState({ newFriendAge: event.target.value });
+  };
+
+  changeNewFriendEmail = event => {
+    this.setState({ newFriendEmail: event.target.value });
+  };
 
   submitFriend = () => {
-    console.log("placeholder");
-  }
+    const newFriend = {
+      name: this.state.newFriendName,
+      age: this.state.newFriendAge,
+      email: this.state.newFriendEmail
+    };
+
+    axios
+      .post("http://localhost:5000/friends", newFriend)
+      .then(() => this.fetchFriends())
+      .catch(() =>
+        this.setState({ errorMessage: "Friend could not be sumbitted" })
+      );
+  };
+
+  deleteFriend = id => {
+    axios
+      .delete(`http://localhost:5000/${id}`)
+      .then(() => this.fetchFriends())
+      .catch(() =>
+        this.setState({ errorMessage: "Friend could not be deleted" })
+      );
+  };
+
+  modifyFriend = id => {};
 
   render() {
     return (
       <div className="App">
         <h1>All your few friends:</h1>
-        {this.state.errorMessage && <div> Data did not fetch! </div>}
+        {this.state.errorMessage && <div> {this.state.errorMessage} </div>}
         {this.state.spinner && <div> Data is fetching... </div>}
         <div>
-        {this.state.friends.map(friend => (
-          <div className="contentDiv">
-            <span>Name: {friend.name}</span> <span>Age: {friend.age}</span> <span>Email: {friend.email}</span>
-          </div>
-        ))}
+          {this.state.friends.map(friend => (
+            <div className="contentDiv">
+              <span>Name: {friend.name}</span>
+              <span>Age: {friend.age}</span>
+              <span>Email: {friend.email}</span>
+              <span>
+                <button onClick={() => this.deleteFriend(friend.id)}>
+                  Delete
+                </button>
+                <button onClick={() => this.modifyFriend(friend.id)}>
+                  Modify
+                </button>
+              </span>
+            </div>
+          ))}
         </div>
-        <br/>
-        <form onSubmit={this.submitFriend} >
-          <input onChange={this.changeNewFriendName} value={this.state.newFriendName} placeholder="Name" type="text" />
-          <input onChange={this.changeNewFriendAge} value={this.state.newFriendAge} placeholder="Age" type="number" />
-          <input onChange={this.changeNewFriendEmail} value={this.state.changeNewFriendEmail} placeholder="Email" type="text" />
+        <br />
+        <form onSubmit={this.submitFriend}>
+          <input
+            onChange={this.changeNewFriendName}
+            value={this.state.newFriendName}
+            placeholder="Name"
+            type="text"
+          />
+          <input
+            onChange={this.changeNewFriendAge}
+            value={this.state.newFriendAge}
+            placeholder="Age"
+            type="number"
+          />
+          <input
+            onChange={this.changeNewFriendEmail}
+            value={this.state.changeNewFriendEmail}
+            placeholder="Email"
+            type="text"
+          />
           <input type="submit" value="Add new friend" />
         </form>
       </div>
