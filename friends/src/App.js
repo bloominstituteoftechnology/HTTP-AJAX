ed31d0c5b3df877a8f8624e93dd3ec94ed2a3585
ckopecky <kopecky12112@gmail.com>
2019-06-14 09:31:29 -0700

    complete CRUD functionality

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import Friend from './Friend';
+import Form from './Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    friends: []
+  }
+
+  componentDidMount(){
+    axios.get("http://localhost:5000/friends")
+      .then(response => {
+        this.setState({friends: response.data});
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  }
+
+  addFriend = (name, age, email) => {
+    let newData = { name, age, email, id: Date.now() }
+    axios.post("http://localhost:5000/friends", newData)
+      .then(response => {
+        console.log(response);
+        this.setState({friends: response.data});
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
+
+  handleDelete = (id) => {
+    axios.delete(`http://localhost:5000/friends/${id}`) 
+      .then(response => {
+        this.setState({friends: response.data});
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+  updateFriend = (name, age, email, id) => {
+    let newData = {name, age, email};
+    axios.put(`http://localhost:5000/friends/${id}`, newData )
+      .then(response => {
+        this.setState({friends: response.data});
+      })
+  }
+
+
+  render() {
+    console.log(this.state);
+    return (
+      <>
+      <h1>Lambda Friends</h1>
+      <Form addFriend={this.addFriend}/>
+      <div className="friend-container">
+        {this.state.friends.map(friend => {
+          return (
+            <Friend friend={friend} key={friend.id} handleDelete={this.handleDelete} updateFriend={this.updateFriend}/>
+          )
+        })}
+      </div>
+      </>
+    );
+  }
 }
 
 export default App;
