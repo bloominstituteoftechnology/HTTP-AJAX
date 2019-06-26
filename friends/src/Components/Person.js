5b8e288138e16ef0f9bd9474b6072530ea7ea48e
JeffJohnson <jeff.jj.9494@gmail.com>
2019-06-25 18:01:41 -0600

    refactoring and trying to use a different way to retrieve and distrubute the data than last time also trying to use bootstrap and reactstrap but per usual im running into seemingly unnecessary errors its why i dont normally use reactstrap or bootstrap

---
+++ friends/src/Components/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import './Person.css';
+
+function Person(props){
+    return(
+        <div className="personCard">
+            <h2>{props.name}</h2>
+            <div className='info'>
+                <h1 className="age">{props.age} years old</h1>
+                <h1 className="email">{props.address}</h1>
+            </div>
+        </div>
+    )
+}
+export default Person;
