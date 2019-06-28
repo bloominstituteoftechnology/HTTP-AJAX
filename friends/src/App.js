23734b7dafee99b4840b1dc926027d15f13af3be
Lucas Larrubia <Lucas@Marcias-iMac.local>
2019-06-27 21:50:11 -0400

    Fixed errors

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Link } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import './App.css';
@@ -41,30 +40,3 @@ class App extends React.Component {
 }
 
 export default App;
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> 68377a60ed6f9fca0ce42ab525503fd2c1cf4490
--- friends/src/index.js
+++ friends/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -11,13 +10,3 @@ ReactDOM.render(
     <App />,
 </Router>,
 document.getElementById('root'));
-=======
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
->>>>>>> 68377a60ed6f9fca0ce42ab525503fd2c1cf4490
